cf46d9f703fea2bdbcf7268c4b377bb21e91fb3e
Brandon Stoll <bstoll@users.noreply.github.com>
2021-12-22 22:26:36 +0000

    Add timezone configuration to base system profile

--- featureprofiles/system/system_base/tests/time_test.go
+++ featureprofiles/system/system_base/tests/time_test.go
@@ -40,3 +40,50 @@ func TestBootTime(t *testing.T) {
 		t.Errorf("Unexpected boot timestamp: got %d; check clock", bt)
 	}
 }
+
+// TestTimeZone verifies the timezone-name config values can be read and set
+//
+// config_path:/system/config/timezone-name
+// telemetry_path:/system/state/timezone-name
+func TestTimeZone(t *testing.T) {
+	t.Skip("Need working implementation to validate against")
+
+	dut := ondatra.DUT(t, "dut1")
+	configTz := dut.Config().System().Clock().TimezoneName()
+	stateTz := dut.Telemetry().System().Clock().TimezoneName()
+
+	var timezones = []string{
+		"Etc/UTC",
+		"Etc/GMT",
+		"UTC",
+		"GMT",
+		"America/Chicago",
+		"PST8PDT",
+		"Europe/London",
+	}
+
+	for _, timezone := range timezones {
+		configTz.Replace(t, timezone)
+
+		configGot := configTz.Get(t)
+		if configGot != timezone {
+			t.Errorf("Config timezone got %s want %s", configGot, timezone)
+		}
+
+		stateGot := stateTz.Await(t, 5*time.Second, timezone)
+		success := false
+		for _, v := range stateGot {
+			if v.Present && v.Val(t) == timezone {
+				success = true
+			}
+		}
+		if !success {
+			t.Errorf("Telemetry timezone got %v want %s", stateGot, timezone)
+		}
+	}
+
+	configTz.Delete(t)
+	if qs := configTz.GetFull(t); qs.IsPresent() == true {
+		t.Errorf("Delete timezone fail; got %v", qs)
+	}
+}
