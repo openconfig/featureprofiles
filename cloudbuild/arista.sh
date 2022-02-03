fd5979eb2259ab3bf0ce9e351a8e0d46eadf1c88
Brandon Stoll <bstoll@users.noreply.github.com>
2022-02-03 20:19:58 +0000

    Use make for CI

--- cloudbuild/arista.sh
+++ cloudbuild/arista.sh
@@ -20,14 +20,7 @@ docker tag gcr.io/disco-idea-817/ceos ceos:latest
 kind load docker-image --name=kne ceos:latest
 
 pushd /tmp/workspace
-# TODO(bstoll): Replace this with the proper test execution process
-kne_cli create topologies/kne/arista_ceos.textproto
-cat >topologies/kne/testbed.kne.yml << EOF
-username: admin
-password: admin
-topology: ${PWD}/topologies/kne/arista_ceos.textproto
-cli: ${HOME}/go/bin/kne_cli
-EOF
-go test -v feature/system/system_base/tests/*.go -config "$PWD"/topologies/kne/testbed.kne.yml -testbed "$PWD"/topologies/dut.testbed
-go test -v feature/system/system_ntp/tests/*.go -config "$PWD"/topologies/kne/testbed.kne.yml -testbed "$PWD"/topologies/dut.testbed
+make kne_arista_setup
+make kne_tests
+make kne_arista_cleanup
 popd
