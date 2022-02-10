51912da4a5d83155969d4e446a460dd2864ff5bc
Suresh Thesayi <sthesayi@google.com>
2022-02-10 11:10:13 -0500

    LLDP feature profile config library

---
+++ feature/lldp/lldp_base/go/lldp.go
@@ -0,0 +1,56 @@
+/*
+ Copyright 2022 Google LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+// Package lldp implements the Config Library for LLDP feature profile.
+package lldp
+
+import (
+	"errors"
+
+	"github.com/openconfig/featureprofiles/yang/oc"
+	"github.com/openconfig/ygot/ygot"
+)
+
+// LLDP struct to store OC attributes.
+type LLDP struct {
+	oc oc.Lldp
+}
+
+// New returns a new LLDP object with the feature enabled.
+func New() *LLDP {
+	return &LLDP{
+		oc: oc.Lldp{
+			Enabled: ygot.Bool(true),
+		},
+	}
+}
+
+// WithInterface enables LLDP on the specified interface.
+func (l *LLDP) WithInterface(name string) *LLDP {
+	l.oc.GetOrCreateInterface(name).Enabled = ygot.Bool(true)
+	return l
+}
+
+// AugmentDevice implements the device.Feature interface.
+// This method augments the device OC with LLDP feature.
+// Use d.WithFeature(l) instead of calling this method directly.
+func (l *LLDP) AugmentDevice(d *oc.Device) error {
+	if d.Lldp != nil {
+		return errors.New("lldp OC is not nil")
+	}
+	d.Lldp = &l.oc
+	return nil
+}
