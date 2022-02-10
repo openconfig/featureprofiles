996f3d79c0df1e53e8fbd329542261a48b7d796a
Likai Liu <liulk@google.com>
2022-02-10 17:51:37 -0500

    binding cli implementation over SSH

---
+++ topologies/binding/cli.go
@@ -0,0 +1,96 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package binding
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/openconfig/ondatra/binding"
+	"golang.org/x/crypto/ssh"
+)
+
+type cli struct {
+	ssh    *ssh.Client
+	sess   *ssh.Session
+	stdin  io.WriteCloser
+	stdout io.Reader
+	stderr io.Reader
+}
+
+func newCLI(sc *ssh.Client) (*cli, error) {
+	sess, err := sc.NewSession()
+	if err != nil {
+		return nil, fmt.Errorf("could not create session: %w", err)
+	}
+	if err := sess.RequestPty("ansi", 80, 24, nil); err != nil {
+		return nil, fmt.Errorf("could not request pty: %w", err)
+	}
+	stdin, err := sess.StdinPipe()
+	if err != nil {
+		return nil, fmt.Errorf("could not get stdin: %w", err)
+	}
+	stdout, err := sess.StdoutPipe()
+	if err != nil {
+		return nil, fmt.Errorf("could not get stdout: %w", err)
+	}
+	stderr, err := sess.StderrPipe()
+	if err != nil {
+		return nil, fmt.Errorf("could not get stderr: %w", err)
+	}
+	if err := sess.Shell(); err != nil {
+		return nil, fmt.Errorf("could not start shell: %w", err)
+	}
+	c := &cli{
+		ssh:    sc,
+		sess:   sess,
+		stdin:  stdin,
+		stdout: stdout,
+		stderr: stderr,
+	}
+	return c, nil
+}
+
+func (c *cli) SendCommand(ctx context.Context, cmd string) (string, error) {
+	sess, err := c.ssh.NewSession()
+	if err != nil {
+		return "", fmt.Errorf("could not create session: %w", err)
+	}
+	defer sess.Close()
+	buf, err := sess.CombinedOutput(cmd)
+	if err != nil {
+		return "", fmt.Errorf("could not execute command: %w", err)
+	}
+	return string(buf), nil
+}
+
+func (c *cli) Stdin() io.WriteCloser {
+	return c.stdin
+}
+
+func (c *cli) Stdout() io.ReadCloser {
+	return io.NopCloser(c.stdout)
+}
+
+func (c *cli) Stderr() io.ReadCloser {
+	return io.NopCloser(c.stderr)
+}
+
+func (c *cli) Close() error {
+	return c.ssh.Close()
+}
+
+var _ = binding.StreamClient(&cli{})
