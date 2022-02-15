8e0a089b4fb6b0c4330e78694deab8910a2111fd
Likai Liu <liulk@google.com>
2022-02-15 19:56:37 -0400

    remove deviations that are no longer needed

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -12,12 +12,6 @@ var (
 	InterfaceEnabled = flag.Bool("deviation_interface_enabled", true,
 		"Arista requires interface enabled leaf booleans to be explicitly set to true (b/197141773)")
 
-	LegacyLinkLayerAddress = flag.Bool("deviation_legacy_link_layer_address", true,
-		"Arista does not support setting Static ARP neighbor link_layer_address over gNMI, so setting the interface using ZTC OC to VC (b/197782586)")
-
-	IgnoreSessionParams = flag.Bool("deviation_ignore_session_params", false,
-		"Arista gRIBI always sets ModifyResponse.SessionParamResults to an empty message which is not compliant (b/196998254)")
-
 	OmitL2MTU = flag.Bool("deviation_omit_l2_mtu", true,
 		"Arista does not support setting L2 MTU, so only set L3 MTU (b/201112079)")
 
