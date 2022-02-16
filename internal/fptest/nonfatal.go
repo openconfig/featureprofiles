c52226824cdfc6c00e944956d4aed0a49dd76457
Likai Liu <liulk@google.com>
2022-02-16 17:17:53 -0400

    topology_test for illustrating the use of static binding (#54)
    * add ports to static binding examples
    * fptest/log output writing
    * fptest/nonfatal converts fatal to an error
    * fptest/ports sort ports
    * internal/deviations workaround for vendor deviations
    * topologies/topology_test for testing the testbed topology
    * README.md for static binding topology_test

---
+++ internal/fptest/nonfatal.go
@@ -0,0 +1,34 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fptest
+
+import (
+	"testing"
+
+	"github.com/openconfig/ondatra/negtest"
+)
+
+// NonFatal converts fatal to an error so the test could continue.
+func NonFatal(t testing.TB, f func(t testing.TB)) (ok bool) {
+	msg := negtest.ExpectFatal(t, func(t testing.TB) {
+		f(t)
+		ok = true
+		t.FailNow()
+	})
+	if !ok {
+		t.Error(msg)
+	}
+	return ok
+}
