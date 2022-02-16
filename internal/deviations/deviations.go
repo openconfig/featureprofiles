a2268721520bd36552e319721f35cab31873dce9
Likai Liu <liulk@google.com>
2022-02-16 15:07:53 -0400

    deviations replace vendor name with "device"

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -24,11 +24,11 @@ import "flag"
 // Vendor deviation flags.
 var (
 	InterfaceEnabled = flag.Bool("deviation_interface_enabled", true,
-		"Arista requires interface enabled leaf booleans to be explicitly set to true (b/197141773)")
+		"Device requires interface enabled leaf booleans to be explicitly set to true (b/197141773)")
 
 	OmitL2MTU = flag.Bool("deviation_omit_l2_mtu", true,
-		"Arista does not support setting L2 MTU, so only set L3 MTU (b/201112079)")
+		"Device does not support setting L2 MTU, so only set L3 MTU (b/201112079)")
 
 	AggregateAtomicUpdate = flag.Bool("deviation_aggregate_atomic_update", true,
-		"Arista requires that aggregate Port-Channel and its members be defined in a single gNMI Update transaction at /interfaces; otherwise lag-type will be dropped, and no member can be added to the aggregate (b/201574574)")
+		"Device requires that aggregate Port-Channel and its members be defined in a single gNMI Update transaction at /interfaces; otherwise lag-type will be dropped, and no member can be added to the aggregate (b/201574574)")
 )
