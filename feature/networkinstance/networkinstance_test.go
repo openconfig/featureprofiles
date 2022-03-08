1508d10e501f8ca156192927ffc9af87751654d4
Hariram Natarajan <hariramnat@google.com>
2022-03-08 10:03:21 -0800

    Update missing paths from go/wbb-oc-paths to ISIS, policy, sflow, component and BGP Feature profiles

--- feature/networkinstance/networkinstance_test.go
+++ feature/networkinstance/networkinstance_test.go
@@ -91,8 +91,8 @@ func TestAugment(t *testing.T) {
 			},
 		},
 	}, {
-		desc:     "Static route",
-		ni:       New("default", oc.NetworkInstanceTypes_NETWORK_INSTANCE_TYPE_DEFAULT_INSTANCE),
+		desc:     "Static route with no next-hops",
+		ni:       New("default", oc.NetworkInstanceTypes_NETWORK_INSTANCE_TYPE_DEFAULT_INSTANCE).WithStaticRoute("1.1.1.1", []string{""}),
 		inDevice: &oc.Device{},
 		wantDevice: &oc.Device{
 			NetworkInstance: map[string]*oc.NetworkInstance{
@@ -115,8 +115,8 @@ func TestAugment(t *testing.T) {
 			},
 		},
 	}, {
-		desc:     "One Next Hop",
-		ni:       New("default", oc.NetworkInstanceTypes_NETWORK_INSTANCE_TYPE_DEFAULT_INSTANCE),
+		desc:     "Static route with one next-hop",
+		ni:       New("default", oc.NetworkInstanceTypes_NETWORK_INSTANCE_TYPE_DEFAULT_INSTANCE).WithStaticRoute("1.1.1.1", []string{"1.2.3.44"}),
 		inDevice: &oc.Device{},
 		wantDevice: &oc.Device{
 			NetworkInstance: map[string]*oc.NetworkInstance{
@@ -130,8 +130,10 @@ func TestAugment(t *testing.T) {
 							Name:       ygot.String("static"),
 							Static: map[string]*oc.NetworkInstance_Protocol_Static{
 								"primary": {
+									Prefix: ygot.String("1.1.1.1"),
 									NextHop: map[string]*oc.NetworkInstance_Protocol_Static_NextHop{
 										"0": {
+											Index:   ygot.String("0"),
 											NextHop: oc.UnionString("1.2.3.44"),
 										},
 									},
@@ -143,8 +145,8 @@ func TestAugment(t *testing.T) {
 			},
 		},
 	}, {
-		desc:     "Zero Next Hop",
-		ni:       New("default", oc.NetworkInstanceTypes_NETWORK_INSTANCE_TYPE_DEFAULT_INSTANCE),
+		desc:     "Static route Multiple Next Hops",
+		ni:       New("default", oc.NetworkInstanceTypes_NETWORK_INSTANCE_TYPE_DEFAULT_INSTANCE).WithStaticRoute("1.1.1.1", []string{"1.2.3.44", "1.2.3.45"}),
 		inDevice: &oc.Device{},
 		wantDevice: &oc.Device{
 			NetworkInstance: map[string]*oc.NetworkInstance{
@@ -158,39 +160,14 @@ func TestAugment(t *testing.T) {
 							Name:       ygot.String("static"),
 							Static: map[string]*oc.NetworkInstance_Protocol_Static{
 								"primary": {
+									Prefix: ygot.String("1.1.1.1"),
 									NextHop: map[string]*oc.NetworkInstance_Protocol_Static_NextHop{
 										"0": {
-											NextHop: oc.UnionString(""),
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}, {
-		desc:     "Multiple Next Hops",
-		ni:       New("default", oc.NetworkInstanceTypes_NETWORK_INSTANCE_TYPE_DEFAULT_INSTANCE),
-		inDevice: &oc.Device{},
-		wantDevice: &oc.Device{
-			NetworkInstance: map[string]*oc.NetworkInstance{
-				"default": {
-					Name:    ygot.String("default"),
-					Enabled: ygot.Bool(true),
-					Type:    oc.NetworkInstanceTypes_NETWORK_INSTANCE_TYPE_DEFAULT_INSTANCE,
-					Protocol: map[oc.NetworkInstance_Protocol_Key]*oc.NetworkInstance_Protocol{
-						protocolKey: {
-							Identifier: oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_STATIC,
-							Name:       ygot.String("static"),
-							Static: map[string]*oc.NetworkInstance_Protocol_Static{
-								"primary": {
-									NextHop: map[string]*oc.NetworkInstance_Protocol_Static_NextHop{
-										"0": {
+											Index:   ygot.String("0"),
 											NextHop: oc.UnionString("1.2.3.44"),
 										},
 										"1": {
+											Index:   ygot.String("1"),
 											NextHop: oc.UnionString("1.2.3.45"),
 										},
 									},
