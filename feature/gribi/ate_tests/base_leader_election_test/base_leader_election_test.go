8b9b5d4255137b67fd70e29af7676ce7c6fcead8
mojiiba <mbagherz@cisco.com>
2022-04-05 14:30:20 -0400

    undo the changes to base_leader_election_test

--- feature/gribi/ate_tests/base_leader_election_test/base_leader_election_test.go
+++ feature/gribi/ate_tests/base_leader_election_test/base_leader_election_test.go
@@ -22,7 +22,9 @@ import (
 	"github.com/openconfig/featureprofiles/internal/attrs"
 	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/featureprofiles/internal/fptest"
-	"github.com/openconfig/featureprofiles/internal/gribi"
+
+	"github.com/openconfig/gribigo/chk"
+	"github.com/openconfig/gribigo/constants"
 	"github.com/openconfig/gribigo/fluent"
 	"github.com/openconfig/ondatra"
 	"github.com/openconfig/ondatra/telemetry"
@@ -180,46 +182,167 @@ func testTraffic(t *testing.T, ate *ondatra.ATEDevice, top *ondatra.ATETopology,
 	}
 }
 
+// awaitTimeout calls a fluent client Await, adding a timeout to the context.
+func awaitTimeout(ctx context.Context, c *fluent.GRIBIClient, t testing.TB, timeout time.Duration) error {
+	subctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+	return c.Await(subctx, t)
+}
+
 // testArgs holds the objects needed by a test case.
 type testArgs struct {
 	ctx     context.Context
-	clientA *gribi.GRIBIHandler
-	clientB *gribi.GRIBIHandler
+	clientA *fluent.GRIBIClient
+	clientB *fluent.GRIBIClient
 	dut     *ondatra.DUTDevice
 	ate     *ondatra.ATEDevice
 	top     *ondatra.ATETopology
 }
 
-// configureIPv4ViaNonLeaderClient configures a IPv4 Entry via a Client
-// that is not leader.
-// Ensure that the entry via the Client is ignored and not installed.
-func configureIPv4ViaNonLeaderClient(t *testing.T, args *testArgs, nonleader *gribi.GRIBIHandler) {
-	t.Logf("Adding an IPv4Entry for %s pointing to ATE port-2 via a client that is not leader.", ateDstNetCIDR)
-	nonleader.AddNH(t, nhIndex, "192.0.2.10", instance, fluent.ProgrammingFailed)
-	nonleader.AddNHG(t, nhgIndex, map[uint64]uint64{nhIndex: 1}, instance, fluent.ProgrammingFailed)
-	nonleader.AddIPV4Entry(t, nhgIndex, "", ateDstNetCIDR, instance, fluent.ProgrammingFailed)
+// helperAddEntry configures a sequence of adding the NH, NHG and IPv4Entry by a client.
+func helperAddEntry(ctx context.Context, t *testing.T, client *fluent.GRIBIClient, nextHop string) {
+	t.Helper()
+	client.Modify().AddEntry(t,
+		fluent.NextHopEntry().
+			WithNetworkInstance(instance).
+			WithIndex(nhIndex).
+			WithIPAddress(nextHop),
+		fluent.NextHopGroupEntry().
+			WithNetworkInstance(instance).
+			WithID(nhgIndex).
+			AddNextHop(nhIndex, 1),
+		fluent.IPv4Entry().
+			WithNetworkInstance(instance).
+			WithPrefix(ateDstNetCIDR).
+			WithNextHopGroup(nhgIndex),
+	)
+
+	if err := awaitTimeout(ctx, client, t, time.Minute); err != nil {
+		t.Fatalf("Could not program entries via %v, got err: %v", client, err)
+	}
+
 }
 
-// configureIPv4ViaLeaderClientInstalled configures a IPv4 Entry via the leader ClientA.
-//  Ensure that the entry via ClientA is installed.
-func configureIPv4ViaLeaderClientInstalled(t *testing.T, args *testArgs, leader *gribi.GRIBIHandler, nh string) {
-	t.Logf("Adding an IPv4Entry for %s pointing to ATE port-2 via a client as leader.", ateDstNetCIDR)
-	leader.AddNH(t, nhIndex, nh, instance, fluent.InstalledInRIB)
-	leader.AddNHG(t, nhgIndex, map[uint64]uint64{nhIndex: 1}, instance, fluent.InstalledInRIB)
-	leader.AddIPV4Entry(t, nhgIndex, "", ateDstNetCIDR, instance, fluent.InstalledInRIB)
+// configureIPv4ViaClientB configures a IPv4 Entry via ClientB with an Election
+// ID of 11 when ClientA is already connected with Election ID of 10. Ensure
+// that the entry via ClientB is active through AFT Telemetry.
+func configureIPv4ViaClientB(t *testing.T, args *testArgs) {
+	t.Logf("Adding an IPv4Entry for %s pointing to ATE port-3 via clientB.", ateDstNetCIDR)
+
+	helperAddEntry(args.ctx, t, args.clientB, "192.0.2.10")
+
+	chk.HasResult(t, args.clientB.Results(t),
+		fluent.OperationResult().
+			WithNextHopOperation(nhIndex).
+			WithOperationType(constants.Add).
+			WithProgrammingResult(fluent.InstalledInRIB).
+			AsResult(),
+		chk.IgnoreOperationID(),
+	)
+
+	chk.HasResult(t, args.clientB.Results(t),
+		fluent.OperationResult().
+			WithNextHopGroupOperation(nhgIndex).
+			WithOperationType(constants.Add).
+			WithProgrammingResult(fluent.InstalledInRIB).
+			AsResult(),
+		chk.IgnoreOperationID(),
+	)
+
+	chk.HasResult(t, args.clientB.Results(t),
+		fluent.OperationResult().
+			WithIPv4Operation(ateDstNetCIDR).
+			WithOperationType(constants.Add).
+			WithProgrammingResult(fluent.InstalledInRIB).
+			AsResult(),
+		chk.IgnoreOperationID(),
+	)
 }
 
-// testIPv4LeaderActiveChange first makes the cleintB leader.
-// Second it configures an IPV4 through clinetB and ensures
-// that the entry is avtive through through AFT Telemetry and traffic.
-// Thrid, it configures a IPv4 entry through clientA without
-// making it the leader and ensures that the installation fails..
-// Forth, it makes the ClientA master, configures an IPV4 through clinetB
-// and ensures that the entry is active through through AFT Telemetry and traffic.
+// configureIPv4ViaClientA configures a IPv4 Entry via ClientA with an Election
+// ID of 10 when ClientB is already primary and connected with Election ID of
+// 11. Ensure that the entry via ClientA is ignored and not installed.
+func configureIPv4ViaClientA(t *testing.T, args *testArgs) {
+	t.Logf("Adding an IPv4Entry for %s pointing to ATE port-2 via clientA.", ateDstNetCIDR)
+
+	helperAddEntry(args.ctx, t, args.clientA, "192.0.2.6")
+
+	// Verify the entry is not installed due to client A having lower election ID.
+	chk.HasResult(t, args.clientA.Results(t),
+		fluent.OperationResult().
+			WithIPv4Operation(ateDstNetCIDR).
+			WithOperationType(constants.Add).
+			WithProgrammingResult(fluent.ProgrammingFailed).
+			AsResult(),
+		chk.IgnoreOperationID(),
+	)
+}
+
+// configureIPv4ViaClientAInstalled configures a IPv4 Entry via ClientA with an
+// Election ID of 12. Ensure that the entry via ClientA is installed.
+func configureIPv4ViaClientAInstalled(t *testing.T, args *testArgs) {
+	t.Logf("Adding an IPv4Entry for %s pointing to ATE port-2 via clientA with election ID of 12.", ateDstNetCIDR)
+
+	// TODO: Remove WithElectionID and reuse helperAddEntry
+	args.clientA.Modify().AddEntry(t,
+		fluent.NextHopEntry().
+			WithNetworkInstance(instance).
+			WithIndex(nhIndex).
+			WithIPAddress("192.0.2.6").
+			WithElectionID(12, 0))
+
+	args.clientA.Modify().AddEntry(t,
+		fluent.NextHopGroupEntry().
+			WithNetworkInstance(instance).
+			WithID(nhgIndex).
+			AddNextHop(nhIndex, 1).
+			WithElectionID(12, 0))
+
+	args.clientA.Modify().AddEntry(t,
+		fluent.IPv4Entry().
+			WithPrefix(ateDstNetCIDR).
+			WithNetworkInstance(instance).
+			WithNextHopGroup(nhgIndex).
+			WithElectionID(12, 0))
+
+	if err := awaitTimeout(args.ctx, args.clientA, t, time.Minute); err != nil {
+		t.Fatalf("Could not program entries via clientA, got err: %v", err)
+	}
+
+	chk.HasResult(t, args.clientA.Results(t),
+		fluent.OperationResult().
+			WithNextHopOperation(nhIndex).
+			WithOperationType(constants.Add).
+			WithProgrammingResult(fluent.InstalledInRIB).
+			AsResult(),
+		chk.IgnoreOperationID(),
+	)
+
+	chk.HasResult(t, args.clientA.Results(t),
+		fluent.OperationResult().
+			WithNextHopGroupOperation(nhgIndex).
+			WithOperationType(constants.Add).
+			WithProgrammingResult(fluent.InstalledInRIB).
+			AsResult(),
+		chk.IgnoreOperationID(),
+	)
+
+	chk.HasResult(t, args.clientA.Results(t),
+		fluent.OperationResult().
+			WithIPv4Operation(ateDstNetCIDR).
+			WithOperationType(constants.Add).
+			WithProgrammingResult(fluent.InstalledInRIB).
+			AsResult(),
+		chk.IgnoreOperationID(),
+	)
+}
+
+// testIPv4LeaderActiveChange modifies election ID of ClientA with an Election ID of 12
+// and configures a IPv4 entry through this client. Ensure that the entry via ClientA
+// is active through AFT Telemetry.
 func testIPv4LeaderActiveChange(ctx context.Context, t *testing.T, args *testArgs) {
-	// Configure IPv4 route for 198.51.100.0/24 pointing to ATE port-3 via clientB as the leader.
-	args.clientB.BecomeLeader(t)
-	configureIPv4ViaLeaderClientInstalled(t, args, args.clientB, atePort3.IPv4)
+	// Configure IPv4 route for 198.51.100.0/24 pointing to ATE port-3 via clientB.
+	configureIPv4ViaClientB(t, args)
 
 	// Verify the entry for 198.51.100.0/24 is active through AFT Telemetry.
 	ipv4Path := args.dut.Telemetry().NetworkInstance(instance).Afts().Ipv4Entry(ateDstNetCIDR)
@@ -227,22 +350,33 @@ func testIPv4LeaderActiveChange(ctx context.Context, t *testing.T, args *testArg
 		t.Errorf("ipv4-entry/state/prefix got %s, want %s", got, want)
 	}
 
-	// Verify the entry for 198.51.100.0/24 is active through Traffic.
 	srcEndPoint := args.top.Interfaces()[atePort1.Name]
 	dstEndPoint := args.top.Interfaces()[atePort3.Name]
+
+	// Verify the entry for 198.51.100.0/24 is active through Traffic.
 	testTraffic(t, args.ate, args.top, srcEndPoint, dstEndPoint)
 
-	// Configure IPv4 route for 198.51.100.0/24 pointing to ATE port-3 via clientA without being the leader.
-	// The entry should not be installed since clientA is not the leader.
-	configureIPv4ViaNonLeaderClient(t, args, args.clientA)
+	// Configure IPv4 route for 198.51.100.0/24 pointing to ATE port-3 via clientB.
+	// The entry should not be installed due to client A having lower election ID.
+	configureIPv4ViaClientA(t, args)
 
-	// Modify client A to become the active leader.
-	args.clientA.BecomeLeader(t)
+	// Modify the election ID of client A to 12 so clientA becomes the active Leader.
+	args.clientA.Modify().UpdateElectionID(t, 12, 0)
 
-	// Configure IPv4 route for 198.51.100.0/24 pointing to ATE port-2 via clientA that is the active leader.
-	configureIPv4ViaLeaderClientInstalled(t, args, args.clientA, atePort2.IPv4)
+	if err := awaitTimeout(ctx, args.clientA, t, time.Minute); err != nil {
+		t.Fatalf("could not update election ID via clientA, got err: %v", err)
+	}
 
-	// Verify the entry for 198.51.100.0/24 is active through AFT Telemetry.
+	chk.HasResult(t, args.clientA.Results(t),
+		fluent.OperationResult().
+			WithCurrentServerElectionID(12, 0).
+			AsResult(),
+		chk.IgnoreOperationID(),
+	)
+
+	// Configure IPv4 route for 198.51.100.0/24 pointing to ATE port-2 via clientA with election ID of 12.
+	configureIPv4ViaClientAInstalled(t, args)
+
 	ipv4Path = args.dut.Telemetry().NetworkInstance(instance).Afts().Ipv4Entry(ateDstNetCIDR)
 	if got, want := ipv4Path.Prefix().Get(t), ateDstNetCIDR; got != want {
 		t.Errorf("ipv4-entry/state/prefix got %s, want %s", got, want)
@@ -251,6 +385,7 @@ func testIPv4LeaderActiveChange(ctx context.Context, t *testing.T, args *testArg
 	// Verify with traffic that the entry is installed through the ATE port-2.
 	srcEndPoint = args.top.Interfaces()[atePort1.Name]
 	dstEndPoint = args.top.Interfaces()[atePort2.Name]
+
 	testTraffic(t, args.ate, args.top, srcEndPoint, dstEndPoint)
 }
 
@@ -259,6 +394,7 @@ func TestElectionIDChange(t *testing.T) {
 
 	// Dial gRIBI
 	ctx := context.Background()
+	gribic := dut.RawAPIs().GRIBI().Default(t)
 
 	// Configure the DUT
 	configureDUT(t, dut)
@@ -268,44 +404,56 @@ func TestElectionIDChange(t *testing.T) {
 	top := configureATE(t, ate)
 	top.Push(t).StartProtocols(t)
 
-	tests := []struct {
-		name        string
-		desc        string
-		fn          func(ctx context.Context, t *testing.T, args *testArgs)
-		persistance bool
+	tt := struct {
+		name string
+		desc string
+		fn   func(ctx context.Context, t *testing.T, args *testArgs)
 	}{
-		{
-			name:        "IPv4EntryWithLeaderChange",
-			desc:        "Connect gRIBI-A and B to DUT specifying SINGLE_PRIMARY client redundancy without persistance and FibACK",
-			fn:          testIPv4LeaderActiveChange,
-			persistance: true,
-		},
+		name: "IPv4EntryWithLeaderChange",
+		desc: "Connect gRIBI-A to DUT specifying SINGLE_PRIMARY client redundancy with election_id 12.",
+		fn:   testIPv4LeaderActiveChange,
 	}
 
 	// Each case will run with its own gRIBI fluent client.
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			t.Logf("Name: %s", tt.name)
-			t.Logf("Description: %s", tt.desc)
+	t.Run(tt.name, func(t *testing.T) {
+		t.Logf("Name: %s", tt.name)
+		t.Logf("Description: %s", tt.desc)
 
-			// Configure the gRIBI client clientA
-			clientA := gribi.NewGRIBIFluent(t, dut, tt.persistance, false)
-			defer clientA.Close(t)
+		// Configure the gRIBI client clientA with election ID of 10.
+		clientA := fluent.NewClient()
 
-			// Configure the gRIBI client clientB
-			clientB := gribi.NewGRIBIFluent(t, dut, tt.persistance, false)
-			defer clientB.Close(t)
+		clientA.Connection().WithStub(gribic).WithInitialElectionID(10, 0).
+			WithRedundancyMode(fluent.ElectedPrimaryClient)
 
-			args := &testArgs{
-				ctx:     ctx,
-				clientA: clientA,
-				clientB: clientB,
-				dut:     dut,
-				ate:     ate,
-				top:     top,
-			}
+		clientA.Start(ctx, t)
+		defer clientA.Stop(t)
+		clientA.StartSending(ctx, t)
+		if err := awaitTimeout(ctx, clientA, t, time.Minute); err != nil {
+			t.Fatalf("Await got error during session negotiation for clientA: %v", err)
+		}
 
-			tt.fn(ctx, t, args)
-		})
-	}
+		// Configure the gRIBI client clientB with election ID of 11.
+		clientB := fluent.NewClient()
+
+		clientB.Connection().WithStub(gribic).WithInitialElectionID(11, 0).
+			WithRedundancyMode(fluent.ElectedPrimaryClient)
+
+		clientB.Start(context.Background(), t)
+		defer clientB.Stop(t)
+		clientB.StartSending(context.Background(), t)
+		if err := awaitTimeout(ctx, clientB, t, time.Minute); err != nil {
+			t.Fatalf("Await got error during session negotiation for clientB: %v", err)
+		}
+
+		args := &testArgs{
+			ctx:     ctx,
+			clientA: clientA,
+			clientB: clientB,
+			dut:     dut,
+			ate:     ate,
+			top:     top,
+		}
+
+		tt.fn(ctx, t, args)
+	})
 }
