15a330a52527b80938d900452e882bc6bd015b61
mojiiba <mbagherz@cisco.com>
2022-05-11 18:14:49 -0400

    add apis for ssh, and commit replace with gnmi

---
+++ feature/experimental/cli/cisco_config_test.go
@@ -0,0 +1,130 @@
+package config
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/openconfig/featureprofiles/internal/fptest"
+	"github.com/openconfig/featureprofiles/topologies/binding/cisco/config"
+	"github.com/openconfig/ondatra"
+)
+
+const fullConfig = ` 
+hostname DUT11
+logging console disable
+username cisco
+ group root-lr
+ group cisco-support
+ secret 10 $6$lToan5htAPC1n...$sDDES6OVdZvfHnZ2iZf7ThFBJDoarCL05d/gR02GcjySEZ/HTeEcQ90ZoF5rY3oq3XbQRfZGzXt55JGnxOB/W1
+!
+grpc
+ port 57777
+!
+line console
+ exec-timeout 0 0
+ absolute-timeout 0
+ session-timeout 0
+!
+line default
+ exec-timeout 0 0
+ absolute-timeout 0
+ session-timeout 0
+!
+call-home
+ service active
+ contact smart-licensing
+ profile CiscoTAC-1
+  active
+  destination transport-method email disable
+  destination transport-method http
+ !
+!
+interface MgmtEth0/RP0/CPU0/0
+ ipv4 address dhcp
+!
+interface FourHundredGigE0/0/0/0
+ shutdown
+!
+interface FourHundredGigE0/0/0/1
+ shutdown
+!
+interface FourHundredGigE0/0/0/2
+ shutdown
+!
+interface FourHundredGigE0/0/0/3
+ shutdown
+!
+interface FourHundredGigE0/0/0/4
+ shutdown
+!
+interface FourHundredGigE0/0/0/5
+ shutdown 
+!         
+interface FourHundredGigE0/0/0/6
+ shutdown 
+!         
+interface FourHundredGigE0/0/0/7
+ shutdown 
+!         
+interface FourHundredGigE0/0/0/8
+ shutdown 
+!         
+interface FourHundredGigE0/0/0/9
+ shutdown 
+!         
+interface FourHundredGigE0/0/0/21
+ shutdown 
+!         
+interface FourHundredGigE0/0/0/22
+ shutdown 
+!         
+interface FourHundredGigE0/0/0/23
+ shutdown 
+!         
+interface FourHundredGigE0/0/0/24
+ shutdown 
+!         
+interface FourHundredGigE0/0/0/25
+ shutdown 
+!         
+interface FourHundredGigE0/0/0/26
+ shutdown 
+!         
+interface FourHundredGigE0/0/0/27
+ shutdown 
+!         
+interface FourHundredGigE0/0/0/28
+ shutdown 
+!         
+interface FourHundredGigE0/0/0/29
+ shutdown 
+!         
+interface FourHundredGigE0/0/0/30
+ shutdown 
+!         
+interface FourHundredGigE0/0/0/31
+ shutdown 
+!         
+ssh server v2
+ssh server netconf vrf default
+end   
+`
+
+func TestMain(m *testing.M) {
+	fptest.RunTests(m)
+}
+
+func TestCLIConfigViaSSH(t *testing.T) {
+	dut := ondatra.DUT(t, "dut")
+	_, err := config.ConfigWithSSH(context.Background(), t, dut, "config \n hostname test \n commit \n", 30*time.Second); if err != nil {
+		t.Fatalf("TestCLIConfigViaSSH is failed: %v", err)
+	}
+}
+
+func TestGNMIFullCommitReplace(t *testing.T) {
+	dut := ondatra.DUT(t, "dut")
+	_, err := config.GNMICommitReplace(context.Background(), t, dut, fullConfig); if err != nil {
+		t.Fatalf("TestGNMIFullCommitReplace is failed: %v", err)
+	}
+}
---
+++ topologies/binding/cisco/config/config.go
@@ -0,0 +1,135 @@
+// This package contains cisco specefic binding APIs to config a router using oc and text and cli.
+
+package config
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"strings"
+	"testing"
+	"time"
+
+	gpb "github.com/openconfig/gnmi/proto/gnmi"
+	"github.com/openconfig/ondatra"
+)
+
+// ConfigWithSSH applies the cli confguration via ssh on the device
+func ConfigWithSSH(ctx context.Context, t *testing.T, dut *ondatra.DUTDevice, cfg string, timeout time.Duration) (string, error) {
+	sshClient := dut.RawAPIs().CLI(t)
+	cliOut := sshClient.Stdout()
+	cliIn := sshClient.Stdin()
+	if _, err := cliIn.Write([]byte(cfg)); err != nil {
+		return "", fmt.Errorf("failed to write using ssh: %w", err)
+	}
+	buf := make([]byte, 32768) // RFC 4253 max payload size for ssh
+	ch := make(chan bool)
+	n := 0
+	var err error
+	response := ""
+	go func() {
+		for {
+			n, err = cliOut.Read(buf)
+			if err != nil {
+				if err == io.EOF {
+					response = fmt.Sprintf("%s%s", response, string(buf[:n]))
+					if checkCLIConfigIsApplied(response) {
+						ch <- true
+					} else {
+						ch <- false
+					}
+
+				} else {
+					ch <- false
+				}
+				break
+			} else {
+				response = fmt.Sprintf("%s%s", response, string(buf[:n]))
+				if checkCLIConfigIsApplied(response) {
+					ch <- true
+					break
+				}
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	select {
+	case resp := <-ch:
+		if resp {
+			// add logging here
+			return response, nil
+		} else {
+			// add logging here
+			return response, fmt.Errorf("response message for ssh command is not as expected")
+		}
+	case <-time.After(timeout):
+		// add logging here
+		return response, fmt.Errorf("did not recieve the expected response (timeout)")
+	}
+}
+
+func checkCLIConfigIsApplied(output string) bool {
+	// Note that we assume the config contains only one commit and ends with that commit
+	/* commit
+	*****
+	****(config)# */
+	if strings.Contains(output, "commit") && strings.HasSuffix(output, "(config)#") {
+		return true
+	}
+	return false
+}
+
+// GNMICommitReplace replace the device configuration with the one as specefied via cfg  (cisco text config).
+func GNMICommitReplace(ctx context.Context, t *testing.T, dut *ondatra.DUTDevice, cfg string) (string, error) {
+	// create a set request
+	// Encoding_ASCII
+	gnmiC := dut.RawAPIs().GNMI().New(t)
+	textReplaceReq := &gpb.Update{
+		Val: &gpb.TypedValue{
+			Value: &gpb.TypedValue_AsciiVal{
+				AsciiVal: cfg,
+			},
+		},
+	}
+	replaceRequest := &gpb.SetRequest{
+		Replace: []*gpb.Update{textReplaceReq},
+	}
+	// fmt.Println(prototext.Format(inGetRequest))
+	resp, err := gnmiC.Set(context.Background(), replaceRequest)
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+	fmt.Printf("%v", resp)
+	return "", nil
+}
+
+// GNMICommitReplaceWithOC apply the oc config and text config on the device. The result expected to be the merge of both configuations
+func GNMICommitReplaceWithOC(ctx context.Context, t *testing.T, dut *ondatra.DUTDevice, cfg string, timeout time.Duration) (string, error) {
+	gnmiC := dut.RawAPIs().GNMI().New(t)
+	textReplaceReq := &gpb.Update{
+		Val: &gpb.TypedValue{
+			Value: &gpb.TypedValue_AsciiVal{
+				AsciiVal: cfg,
+			},
+		},
+	}
+
+	ocReplaceReq := &gpb.Update{
+		Val: &gpb.TypedValue{
+			Value: &gpb.TypedValue_AsciiVal{
+				AsciiVal: cfg, // use ietf json  for oc models
+			},
+		},
+	}
+
+	replaceRequest := &gpb.SetRequest{
+		Replace: []*gpb.Update{ocReplaceReq, textReplaceReq},
+	}
+	// fmt.Println(prototext.Format(inGetRequest))
+	resp, err := gnmiC.Set(context.Background(), replaceRequest)
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+	fmt.Printf("%v", resp)
+	return "", nil
+}
