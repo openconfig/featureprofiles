16c7d2c9de7fd6aaa4d52e8df96d1d16b919a017
Mingyang Sun (mingyasu) <mingyasu@cisco.com>
2022-05-25 14:36:19 -0400

    add 2 testcases for PBR policies and policy replace function

--- feature/gribi/ate_tests/cisco/cd5_test.go
+++ feature/gribi/ate_tests/cisco/cd5_test.go
@@ -1057,3 +1057,96 @@ func testAclAndPBRUnderSameInterface(ctx context.Context, t *testing.T, args *te
 	args.dut.Config().Acl().Delete(t)
 
 }
+
+func testPolicesReplace(ctx context.Context, t *testing.T, args *testArgs) {
+	t.Skip()
+	defer configBasePBR(t, args.dut)
+	defer flushSever(t, args)
+
+	weights := []float64{10 * 15, 20 * 15, 30 * 15, 10 * 85, 20 * 85, 30 * 85, 40 * 85}
+
+	configureBaseDoubleRecusionVip1Entry(ctx, t, args)
+	configureBaseDoubleRecusionVip2Entry(ctx, t, args)
+	configureBaseDoubleRecusionVrfEntry(ctx, t, args.prefix.scale, args.prefix.host, "32", args)
+
+	srcEndPoint := args.top.Interfaces()[atePort1.Name]
+	testTraffic(t, true, args.ate, args.top, srcEndPoint, args.top.Interfaces(), args.prefix.scale, args.prefix.host, args, 0, weights...)
+
+	r2 := telemetry.NetworkInstance_PolicyForwarding_Policy_Rule{}
+	r2.SequenceId = ygot.Uint32(2)
+	r2.Ipv4 = &telemetry.NetworkInstance_PolicyForwarding_Policy_Rule_Ipv4{
+		DscpSet: []uint8{*ygot.Uint8(16)},
+	}
+	r2.Action = &telemetry.NetworkInstance_PolicyForwarding_Policy_Rule_Action{NetworkInstance: ygot.String("TE")}
+
+	r3 := telemetry.NetworkInstance_PolicyForwarding_Policy_Rule{}
+	r3.SequenceId = ygot.Uint32(3)
+	r3.Ipv4 = &telemetry.NetworkInstance_PolicyForwarding_Policy_Rule_Ipv4{
+		DscpSet: []uint8{*ygot.Uint8(18)},
+	}
+	r3.Action = &telemetry.NetworkInstance_PolicyForwarding_Policy_Rule_Action{NetworkInstance: ygot.String("VRF1")}
+
+	r4 := telemetry.NetworkInstance_PolicyForwarding_Policy_Rule{}
+	r4.SequenceId = ygot.Uint32(4)
+	r4.Ipv4 = &telemetry.NetworkInstance_PolicyForwarding_Policy_Rule_Ipv4{
+		DscpSet: []uint8{*ygot.Uint8(48)},
+	}
+	r4.Action = &telemetry.NetworkInstance_PolicyForwarding_Policy_Rule_Action{NetworkInstance: ygot.String("TE")}
+
+	p := telemetry.NetworkInstance_PolicyForwarding_Policy{}
+	p.PolicyId = ygot.String(pbrName)
+	p.Type = telemetry.Policy_Type_VRF_SELECTION_POLICY
+	p.Rule = map[uint32]*telemetry.NetworkInstance_PolicyForwarding_Policy_Rule{2: &r2, 3: &r3, 4: &r4}
+
+	policy := telemetry.NetworkInstance_PolicyForwarding{}
+	policy.Policy = map[string]*telemetry.NetworkInstance_PolicyForwarding_Policy{pbrName: &p}
+
+	args.dut.Config().NetworkInstance("default").PolicyForwarding().Replace(t, &policy)
+
+	testTraffic(t, false, args.ate, args.top, srcEndPoint, args.top.Interfaces(), args.prefix.scale, args.prefix.host, args, 0, weights...)
+}
+
+func testPolicyReplace(ctx context.Context, t *testing.T, args *testArgs) {
+	t.Skip()
+	defer configBasePBR(t, args.dut)
+	defer flushSever(t, args)
+
+	weights := []float64{10 * 15, 20 * 15, 30 * 15, 10 * 85, 20 * 85, 30 * 85, 40 * 85}
+
+	configureBaseDoubleRecusionVip1Entry(ctx, t, args)
+	configureBaseDoubleRecusionVip2Entry(ctx, t, args)
+	configureBaseDoubleRecusionVrfEntry(ctx, t, args.prefix.scale, args.prefix.host, "32", args)
+
+	srcEndPoint := args.top.Interfaces()[atePort1.Name]
+	testTraffic(t, true, args.ate, args.top, srcEndPoint, args.top.Interfaces(), args.prefix.scale, args.prefix.host, args, 0, weights...)
+
+	r2 := telemetry.NetworkInstance_PolicyForwarding_Policy_Rule{}
+	r2.SequenceId = ygot.Uint32(2)
+	r2.Ipv4 = &telemetry.NetworkInstance_PolicyForwarding_Policy_Rule_Ipv4{
+		DscpSet: []uint8{*ygot.Uint8(16)},
+	}
+	r2.Action = &telemetry.NetworkInstance_PolicyForwarding_Policy_Rule_Action{NetworkInstance: ygot.String("TE")}
+
+	r3 := telemetry.NetworkInstance_PolicyForwarding_Policy_Rule{}
+	r3.SequenceId = ygot.Uint32(3)
+	r3.Ipv4 = &telemetry.NetworkInstance_PolicyForwarding_Policy_Rule_Ipv4{
+		DscpSet: []uint8{*ygot.Uint8(18)},
+	}
+	r3.Action = &telemetry.NetworkInstance_PolicyForwarding_Policy_Rule_Action{NetworkInstance: ygot.String("VRF1")}
+
+	r4 := telemetry.NetworkInstance_PolicyForwarding_Policy_Rule{}
+	r4.SequenceId = ygot.Uint32(4)
+	r4.Ipv4 = &telemetry.NetworkInstance_PolicyForwarding_Policy_Rule_Ipv4{
+		DscpSet: []uint8{*ygot.Uint8(48)},
+	}
+	r4.Action = &telemetry.NetworkInstance_PolicyForwarding_Policy_Rule_Action{NetworkInstance: ygot.String("TE")}
+
+	p := telemetry.NetworkInstance_PolicyForwarding_Policy{}
+	p.PolicyId = ygot.String(pbrName)
+	p.Type = telemetry.Policy_Type_VRF_SELECTION_POLICY
+	p.Rule = map[uint32]*telemetry.NetworkInstance_PolicyForwarding_Policy_Rule{2: &r2, 3: &r3, 4: &r4}
+
+	args.dut.Config().NetworkInstance("default").PolicyForwarding().Policy(pbrName).Replace(t, &p)
+
+	testTraffic(t, false, args.ate, args.top, srcEndPoint, args.top.Interfaces(), args.prefix.scale, args.prefix.host, args, 0, weights...)
+}
