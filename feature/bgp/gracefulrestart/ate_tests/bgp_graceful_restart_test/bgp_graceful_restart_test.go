8c3544b42a82766b26898a888dd3becc63433736
Brandon Stoll <bstoll@users.noreply.github.com>
2022-06-07 20:12:21 +0000

    Rename package

--- feature/bgp/gracefulrestart/ate_tests/bgp_graceful_restart_test/bgp_graceful_restart_test.go
+++ feature/bgp/gracefulrestart/ate_tests/bgp_graceful_restart_test/bgp_graceful_restart_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package rt_1_4_bgp_graceful_restart_test
+package bgp_graceful_restart_test
 
 import (
 	"testing"
