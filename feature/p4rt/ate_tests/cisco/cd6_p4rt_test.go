8b3e6c8dcfd64a3598b1132477a962f3ac9361a4
Mingyang Sun (mingyasu) <mingyasu@cisco.com>
2022-06-13 14:53:00 -0400

    Initial Sample testcase for CD6 P4RT

--- feature/p4rt/ate_tests/cisco/cd6_p4rt_test.go
+++ feature/p4rt/ate_tests/cisco/cd6_p4rt_test.go
@@ -3,8 +3,79 @@ package cisco_p4rt_test
 import (
 	"context"
 	"testing"
+	"time"
+
+	"github.com/openconfig/ondatra"
+	p4_v1 "github.com/p4lang/p4runtime/go/p4/v1"
+	p4rt_client "wwwin-github.cisco.com/rehaddad/go-p4/p4rt_client"
+	"wwwin-github.cisco.com/rehaddad/go-wbb/p4info/wbb"
 )
 
-func testGDPEntryProgramming(ctx context.Context, t *testing.T, args *testArgs) {
+func testTraffic(t *testing.T, ate *ondatra.ATEDevice, srcEndPoint *ondatra.Interface, duration int, args *testArgs) {
+	ethHeader := ondatra.NewEthernetHeader()
+	ethHeader.WithSrcAddress("00:11:01:00:00:01")
+	ethHeader.WithDstAddress("00:22:01:00:00:01")
+	ethHeader.WithEthernetType("0x6007")
 
+	flow := ate.Traffic().NewFlow("GDP").
+		WithSrcEndpoints(srcEndPoint).
+		WithDstEndpoints(srcEndPoint)
+
+	flow.WithFrameSize(300).WithFrameRateFPS(2).WithHeaders(ethHeader)
+
+	ate.Traffic().Start(t, flow)
+	time.Sleep(time.Duration(duration) * time.Second)
+
+	ate.Traffic().Stop(t)
+}
+
+// programmGDPMatchEntry programms or deletes GDP entry
+func programmGDPMatchEntry(ctx context.Context, t *testing.T, client *p4rt_client.P4RTClient, delete bool) error {
+	actionType := p4_v1.Update_INSERT
+	if delete {
+		actionType = p4_v1.Update_DELETE
+	}
+	err := client.Write(&p4_v1.WriteRequest{
+		DeviceId:   uint64(1),
+		ElectionId: &p4_v1.Uint128{High: uint64(0), Low: uint64(100)},
+		Updates: wbb.AclWbbIngressTableEntryGet([]*wbb.AclWbbIngressTableEntryInfo{
+			&wbb.AclWbbIngressTableEntryInfo{
+				Type:          actionType,
+				EtherType:     0x6007,
+				EtherTypeMask: 0xFFFF,
+			},
+		}),
+		Atomicity: p4_v1.WriteRequest_CONTINUE_ON_ERROR,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func testGDPEntryProgramming(ctx context.Context, t *testing.T, args *testArgs) {
+	client := args.p4rtClientA
+	// Program the GDP entry
+	if err := programmGDPMatchEntry(ctx, t, client, false); err != nil {
+		t.Errorf("There is error when inserting the GDP entry")
+	}
+	defer programmGDPMatchEntry(ctx, t, client, true)
+
+	// Send GDP Packet
+	srcEndPoint := args.top.Interfaces()[atePort1.Name]
+	testTraffic(t, args.ate, srcEndPoint, 10, args)
+
+	// Check PacketIn on P4Client
+	_, packets, err := client.StreamChannelGetPacket(&streamName, 0)
+	if err != nil {
+		t.Errorf("There is error when checking packets in PacketIn ")
+	}
+
+	if packets != nil {
+		t.Logf("PacketIn: %s", packets.Pkt.String())
+	} else {
+		t.Logf("There is no packets received")
+	}
+
+	//TODO check detail packets
 }
--- feature/p4rt/ate_tests/cisco/p4rt_base_test.go
+++ feature/p4rt/ate_tests/cisco/p4rt_base_test.go
@@ -2,13 +2,23 @@ package cisco_p4rt_test
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"testing"
 
+	"github.com/openconfig/featureprofiles/internal/attrs"
 	"github.com/openconfig/featureprofiles/internal/fptest"
 	"github.com/openconfig/ondatra"
 	p4_v1 "github.com/p4lang/p4runtime/go/p4/v1"
 	p4rt_client "wwwin-github.cisco.com/rehaddad/go-p4/p4rt_client"
+	"wwwin-github.cisco.com/rehaddad/go-p4/utils"
+)
+
+var (
+	p4InfoFile = flag.String("p4info_file_location", "./wbb.p4info.pb.txt",
+		"Path to the p4info file.")
+	electionID = uint64(100)
+	streamName = "Primary"
 )
 
 // Testcase defines testcase structure
@@ -67,8 +77,8 @@ func TestMain(m *testing.M) {
 var (
 	P4RTTestcases = []Testcase{
 		{
-			name: "Move physical interface to bundle with same policy",
-			desc: "Remove the policy under physical interface and add the related physical interface under bundle interface which use the same PBR policy",
+			name: "Program GDP Match Entry",
+			desc: "programm GDP match entry and verify send traffic from tgen to verify PacketIn",
 			fn:   testGDPEntryProgramming,
 		},
 	}
@@ -83,66 +93,78 @@ func TestP4RT(t *testing.T) {
 	// Configure the ATE
 	ate := ondatra.ATE(t, "ate")
 	top := configureATE(t, ate)
-	// top.Push(t).StartProtocols(t)
+	top.Push(t).StartProtocols(t)
+
+	p4rtClientA := p4rt_client.P4RTClient{}
+	if err := p4rtClientA.P4rtClientSet(dut.RawAPIs().P4RT(t)); err != nil {
+		t.Fatalf("Could not initialize p4rt client: %v", err)
+	}
+
+	p4rtClientB := p4rt_client.P4RTClient{}
+	if err := p4rtClientB.P4rtClientSet(dut.RawAPIs().P4RT(t)); err != nil {
+		t.Fatalf("Could not initialize p4rt client: %v", err)
+	}
+
+	p4rtClientC := p4rt_client.P4RTClient{}
+	if err := p4rtClientC.P4rtClientSet(dut.RawAPIs().P4RT(t)); err != nil {
+		t.Fatalf("Could not initialize p4rt client: %v", err)
+	}
+
+	p4rtClientD := p4rt_client.P4RTClient{}
+	if err := p4rtClientD.P4rtClientSet(dut.RawAPIs().P4RT(t)); err != nil {
+		t.Fatalf("Could not initialize p4rt client: %v", err)
+	}
+
+	interfaceList := []string{}
+	for i := 121; i < 128; i++ {
+		interfaceList = append(interfaceList, fmt.Sprintf("Bundle-Ether%d", i))
+	}
+
+	interfaces := interfaces{
+		in:  []string{"Bundle-Ether120"},
+		out: interfaceList,
+	}
+
+	args := &testArgs{
+		ctx:         ctx,
+		p4rtClientA: &p4rtClientA,
+		p4rtClientB: &p4rtClientB,
+		p4rtClientC: &p4rtClientC,
+		p4rtClientD: &p4rtClientD,
+		dut:         dut,
+		ate:         ate,
+		top:         top,
+		usecase:     0,
+		interfaces:  &interfaces,
+		prefix: &gribiPrefix{
+			scale:           1,
+			host:            "11.11.11.0",
+			vrfName:         "TE",
+			vipPrefixLength: "32",
+
+			vip1Ip: "192.0.2.40",
+			vip2Ip: "192.0.2.42",
+
+			vip1NhIndex:  uint64(100),
+			vip1NhgIndex: uint64(100),
+
+			vip2NhIndex:  uint64(200),
+			vip2NhgIndex: uint64(200),
+
+			vrfNhIndex:  uint64(1000),
+			vrfNhgIndex: uint64(1000),
+		},
+	}
+
+	if err := setupP4RTClient(ctx, t, args); err != nil {
+		t.Fatalf("Could not setup p4rt client: %v", err)
+	}
 
 	for _, tt := range P4RTTestcases {
 		// Each case will run with its own gRIBI fluent client.
 		t.Run(tt.name, func(t *testing.T) {
 			t.Logf("Name: %s", tt.name)
 			t.Logf("Description: %s", tt.desc)
-
-			p4rtClientA := p4rt_client.P4RTClient{}
-			if err := p4rtClientA.P4rtClientSet(dut.RawAPIs().P4RT(t)); err != nil {
-				t.Fatalf("Could not initialize p4rt client: %v", err)
-			}
-
-			p4rtClientB := p4rt_client.P4RTClient{}
-			if err := p4rtClientB.P4rtClientSet(dut.RawAPIs().P4RT(t)); err != nil {
-				t.Fatalf("Could not initialize p4rt client: %v", err)
-			}
-
-			interfaceList := []string{}
-			for i := 121; i < 128; i++ {
-				interfaceList = append(interfaceList, fmt.Sprintf("Bundle-Ether%d", i))
-			}
-
-			interfaces := interfaces{
-				in:  []string{"Bundle-Ether120"},
-				out: interfaceList,
-			}
-
-			args := &testArgs{
-				ctx:         ctx,
-				p4rtClientA: &p4rtClientA,
-				p4rtClientB: &p4rtClientB,
-				dut:         dut,
-				ate:         ate,
-				top:         top,
-				usecase:     0,
-				interfaces:  &interfaces,
-				prefix: &gribiPrefix{
-					scale:           1,
-					host:            "11.11.11.0",
-					vrfName:         "TE",
-					vipPrefixLength: "32",
-
-					vip1Ip: "192.0.2.40",
-					vip2Ip: "192.0.2.42",
-
-					vip1NhIndex:  uint64(100),
-					vip1NhgIndex: uint64(100),
-
-					vip2NhIndex:  uint64(200),
-					vip2NhgIndex: uint64(200),
-
-					vrfNhIndex:  uint64(1000),
-					vrfNhgIndex: uint64(1000),
-				},
-			}
-
-			if err := setupP4RTClient(ctx, t, args); err != nil {
-				t.Fatalf("Could not setup p4rt client: %v", err)
-			}
 
 			tt.fn(ctx, t, args)
 		})
@@ -154,61 +176,279 @@ func setupP4RTClient(ctx context.Context, t *testing.T, args *testArgs) error {
 	deviceID := uint64(1)
 
 	// Setup P4RT ClientA
-	streamName := "Primary"
-	electionID := uint64(100)
 	streamParameter := p4rt_client.P4RTStreamParameters{
 		Name:        streamName,
 		DeviceId:    deviceID,
 		ElectionIdH: uint64(0),
 		ElectionIdL: electionID,
 	}
-	if client := args.p4rtClientA; client != nil {
-		client.StreamChannelCreate(&streamParameter)
-		if err := client.StreamChannelSendMsg(&streamName, &p4_v1.StreamMessageRequest{
-			Update: &p4_v1.StreamMessageRequest_Arbitration{
-				Arbitration: &p4_v1.MasterArbitrationUpdate{
-					DeviceId: streamParameter.DeviceId,
-					ElectionId: &p4_v1.Uint128{
-						High: streamParameter.ElectionIdH,
-						Low:  streamParameter.ElectionIdL,
+
+	// Send client arbitration message
+	clients := []*p4rt_client.P4RTClient{args.p4rtClientA, args.p4rtClientB, args.p4rtClientC, args.p4rtClientD}
+	for index, client := range clients {
+		if client != nil {
+			client.StreamChannelCreate(&streamParameter)
+			if err := client.StreamChannelSendMsg(&streamName, &p4_v1.StreamMessageRequest{
+				Update: &p4_v1.StreamMessageRequest_Arbitration{
+					Arbitration: &p4_v1.MasterArbitrationUpdate{
+						DeviceId: streamParameter.DeviceId,
+						ElectionId: &p4_v1.Uint128{
+							High: streamParameter.ElectionIdH,
+							Low:  streamParameter.ElectionIdL - uint64(index),
+						},
 					},
 				},
-			},
-		}); err != nil {
-			t.Logf("There is error when setting up p4rtClientA")
-			return err
-		}
-		_, _, arbErr := client.StreamChannelGetArbitrationResp(&streamName, 1)
+			}); err != nil {
+				t.Logf("There is error when setting up p4rtClientA")
+				return err
+			}
+			_, _, arbErr := client.StreamChannelGetArbitrationResp(&streamName, 1)
 
-		if arbErr != nil {
-			t.Logf("There is error at Arbitration time: %v", arbErr)
+			if arbErr != nil {
+				t.Logf("There is error at Arbitration time: %v", arbErr)
+				return arbErr
+			}
 		}
-		streamParameter.ElectionIdL -= 1
 	}
 
-	if client := args.p4rtClientB; client != nil {
-		client.StreamChannelCreate(&streamParameter)
-		if err := client.StreamChannelSendMsg(&streamName, &p4_v1.StreamMessageRequest{
-			Update: &p4_v1.StreamMessageRequest_Arbitration{
-				Arbitration: &p4_v1.MasterArbitrationUpdate{
-					DeviceId: streamParameter.DeviceId,
-					ElectionId: &p4_v1.Uint128{
-						High: streamParameter.ElectionIdH,
-						Low:  streamParameter.ElectionIdL,
-					},
-				},
-			},
-		}); err != nil {
-			t.Logf("There is error when setting up p4rtClientB")
-			return err
-		}
-		streamParameter.ElectionIdL -= 1
+	p4Info, err := utils.P4InfoLoad(p4InfoFile)
+	if err != nil {
+		t.Logf("There is error when loading p4info file")
+		return err
 	}
 
+	// SetForwardingPipeline for p4rtClientA which is Primary Client
+	err = args.p4rtClientA.SetForwardingPipelineConfig(&p4_v1.SetForwardingPipelineConfigRequest{
+		DeviceId:   deviceID,
+		ElectionId: &p4_v1.Uint128{High: uint64(0), Low: electionID},
+		Action:     p4_v1.SetForwardingPipelineConfigRequest_VERIFY_AND_COMMIT,
+		Config: &p4_v1.ForwardingPipelineConfig{
+			P4Info: &p4Info,
+			Cookie: &p4_v1.ForwardingPipelineConfig_Cookie{
+				Cookie: 159,
+			},
+		},
+	})
+	if err != nil {
+		t.Logf("There is error seen when setting SetForwardingPipelineConfig")
+		return err
+	}
 	return nil
 }
 
+const (
+	ipv4PrefixLen = 24
+	ipv6PrefixLen = 126
+	instance      = "default"
+	vlanMTU       = 1518
+)
+
+var (
+	dutPort1 = attrs.Attributes{
+		Desc:    "dutPort1",
+		IPv4:    "100.120.1.1",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:120:1:1",
+		IPv6Len: ipv6PrefixLen,
+	}
+
+	atePort1 = attrs.Attributes{
+		Name:    "atePort1",
+		IPv4:    "100.120.1.2",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:120:1:2",
+		IPv6Len: ipv6PrefixLen,
+	}
+
+	dutPort2 = attrs.Attributes{
+		Desc:    "dutPort2",
+		IPv4:    "100.121.1.1",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:121:1:1",
+		IPv6Len: ipv6PrefixLen,
+	}
+
+	atePort2 = attrs.Attributes{
+		Name:    "atePort2",
+		IPv4:    "100.121.1.2",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:121:1:2",
+		IPv6Len: ipv6PrefixLen,
+	}
+
+	dutPort3 = attrs.Attributes{
+		Desc:    "dutPort3",
+		IPv4:    "100.122.1.1",
+		IPv4Len: ipv4PrefixLen,
+	}
+
+	atePort3 = attrs.Attributes{
+		Name:    "atePort3",
+		IPv4:    "100.122.1.2",
+		IPv4Len: ipv4PrefixLen,
+	}
+
+	dutPort4 = attrs.Attributes{
+		Desc:    "dutPort4",
+		IPv4:    "100.123.1.1",
+		IPv4Len: ipv4PrefixLen,
+	}
+
+	atePort4 = attrs.Attributes{
+		Name:    "atePort4",
+		IPv4:    "100.123.1.2",
+		IPv4Len: ipv4PrefixLen,
+	}
+	dutPort5 = attrs.Attributes{
+		Desc:    "dutPort5",
+		IPv4:    "100.124.1.1",
+		IPv4Len: ipv4PrefixLen,
+	}
+
+	atePort5 = attrs.Attributes{
+		Name:    "atePort5",
+		IPv4:    "100.124.1.2",
+		IPv4Len: ipv4PrefixLen,
+	}
+	dutPort6 = attrs.Attributes{
+		Desc:    "dutPort6",
+		IPv4:    "100.125.1.1",
+		IPv4Len: ipv4PrefixLen,
+	}
+
+	atePort6 = attrs.Attributes{
+		Name:    "atePort6",
+		IPv4:    "100.125.1.2",
+		IPv4Len: ipv4PrefixLen,
+	}
+	dutPort7 = attrs.Attributes{
+		Desc:    "dutPort7",
+		IPv4:    "100.126.1.1",
+		IPv4Len: ipv4PrefixLen,
+	}
+
+	atePort7 = attrs.Attributes{
+		Name:    "atePort7",
+		IPv4:    "100.126.1.2",
+		IPv4Len: ipv4PrefixLen,
+	}
+	dutPort8 = attrs.Attributes{
+		Desc:    "dutPort8",
+		IPv4:    "100.127.1.1",
+		IPv4Len: ipv4PrefixLen,
+	}
+	atePort8 = attrs.Attributes{
+		Name:    "atePort8",
+		IPv4:    "100.127.1.2",
+		IPv4Len: ipv4PrefixLen,
+	}
+
+	dutPort2Vlan10 = attrs.Attributes{
+		Desc:    "dutPort2Vlan10",
+		IPv4:    "100.121.10.1",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:121:10:1",
+		IPv6Len: ipv6PrefixLen,
+		MTU:     vlanMTU,
+	}
+
+	atePort2Vlan10 = attrs.Attributes{
+		Name:    "atePort2Vlan10",
+		IPv4:    "100.121.10.2",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:121:10:2",
+		IPv6Len: ipv6PrefixLen,
+		MTU:     vlanMTU,
+	}
+
+	dutPort2Vlan20 = attrs.Attributes{
+		Desc:    "dutPort2Vlan20",
+		IPv4:    "100.121.20.1",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:121:20:1",
+		IPv6Len: ipv6PrefixLen,
+		MTU:     vlanMTU,
+	}
+
+	atePort2Vlan20 = attrs.Attributes{
+		Name:    "atePort2Vlan20",
+		IPv4:    "100.121.20.2",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:121:20:2",
+		IPv6Len: ipv6PrefixLen,
+		MTU:     vlanMTU,
+	}
+
+	dutPort2Vlan30 = attrs.Attributes{
+		Desc:    "dutPort2Vlan30",
+		IPv4:    "100.121.30.1",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:121:30:1",
+		IPv6Len: ipv6PrefixLen,
+		MTU:     vlanMTU,
+	}
+
+	atePort2Vlan30 = attrs.Attributes{
+		Name:    "atePort2Vlan20",
+		IPv4:    "100.121.30.2",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:121:30:2",
+		IPv6Len: ipv6PrefixLen,
+		MTU:     vlanMTU,
+	}
+)
+
+// configureATE configures port1, port2 and port3 on the ATE.
 func configureATE(t *testing.T, ate *ondatra.ATEDevice) *ondatra.ATETopology {
 	top := ate.Topology().New()
+
+	p1 := ate.Port(t, "port1")
+	i1 := top.AddInterface(atePort1.Name).WithPort(p1)
+	i1.IPv4().
+		WithAddress(atePort1.IPv4CIDR()).
+		WithDefaultGateway(dutPort1.IPv4)
+
+	p2 := ate.Port(t, "port2")
+	i2 := top.AddInterface(atePort2.Name).WithPort(p2)
+	i2.IPv4().
+		WithAddress(atePort2.IPv4CIDR()).
+		WithDefaultGateway(dutPort2.IPv4)
+
+	p3 := ate.Port(t, "port3")
+	i3 := top.AddInterface(atePort3.Name).WithPort(p3)
+	i3.IPv4().
+		WithAddress(atePort3.IPv4CIDR()).
+		WithDefaultGateway(dutPort3.IPv4)
+
+	p4 := ate.Port(t, "port4")
+	i4 := top.AddInterface(atePort4.Name).WithPort(p4)
+	i4.IPv4().
+		WithAddress(atePort4.IPv4CIDR()).
+		WithDefaultGateway(dutPort4.IPv4)
+
+	p5 := ate.Port(t, "port5")
+	i5 := top.AddInterface(atePort5.Name).WithPort(p5)
+	i5.IPv4().
+		WithAddress(atePort5.IPv4CIDR()).
+		WithDefaultGateway(dutPort5.IPv4)
+
+	p6 := ate.Port(t, "port6")
+	i6 := top.AddInterface(atePort6.Name).WithPort(p6)
+	i6.IPv4().
+		WithAddress(atePort6.IPv4CIDR()).
+		WithDefaultGateway(dutPort6.IPv4)
+
+	p7 := ate.Port(t, "port7")
+	i7 := top.AddInterface(atePort7.Name).WithPort(p7)
+	i7.IPv4().
+		WithAddress(atePort7.IPv4CIDR()).
+		WithDefaultGateway(dutPort7.IPv4)
+
+	p8 := ate.Port(t, "port8")
+	i8 := top.AddInterface(atePort8.Name).WithPort(p8)
+	i8.IPv4().
+		WithAddress(atePort8.IPv4CIDR()).
+		WithDefaultGateway(dutPort8.IPv4)
+
 	return top
 }
