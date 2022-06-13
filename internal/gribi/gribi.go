f318ea3bb741e5cfdd21f64c28a0dc19bf7a13b2
mananpat <mananpat@cisco.com>
2022-06-13 13:17:14 -0400

    local

--- internal/gribi/gribi.go
+++ internal/gribi/gribi.go
@@ -35,114 +35,117 @@ const (
 	timeout = time.Minute
 )
 
-// GRIBIHandler provides access to GRIBI APIs of the DUT.
+// Client provides access to GRIBI APIs of the DUT.
+//
 // Usage:
 //
-//   g := &GRIBIHandler{
+//   c := &Client{
 //     DUT: ondatra.DUT(t, "dut"),
 //     FibACK: true,
 //     Persistence: true,
 //   }
-//   defer g.Close(t)
-//   if err := g.Start(t); err != nil {
+//   defer c.Close(t)
+//   if err := c.Start(t); err != nil {
 //     t.Fatalf("Could not initialize gRIBI: %v", err)
 //   }
-type GRIBIHandler struct {
-	DUT         *ondatra.DUTDevice
-	FibACK      bool
-	Persistence bool
+type Client struct {
+	DUT                   *ondatra.DUTDevice
+	FibACK                bool
+	Persistence           bool
+	InitialElectionIDLow  uint64
+	InitialElectionIDHigh uint64
 
 	// Unexport fields below.
 	fluentC *fluent.GRIBIClient
 }
 
 // Fluent resturns the fluent client that can be used to directly call the gribi fluent APIs
-func (g *GRIBIHandler) Fluent(t testing.TB) *fluent.GRIBIClient {
-	return g.fluentC
+func (c *Client) Fluent(t testing.TB) *fluent.GRIBIClient {
+	return c.fluentC
 }
 
 // Start function start establish a client connection with the gribi server.
 // By default the client is not the leader and for that function BecomeLeader
 // needs to be called.
-func (g *GRIBIHandler) Start(t testing.TB) error {
+func (c *Client) Start(t testing.TB) error {
 	t.Helper()
-	t.Logf("Starting GRIBI connection for dut: %s", g.DUT.Name())
-	gribiC := g.DUT.RawAPIs().GRIBI().Default(t)
-	g.fluentC = fluent.NewClient()
-	g.fluentC.Connection().WithStub(gribiC)
-	if g.Persistence {
-		g.fluentC.Connection().WithInitialElectionID(1, 0).
+	t.Logf("Starting GRIBI connection for dut: %s", c.DUT.Name())
+	gribiC := c.DUT.RawAPIs().GRIBI().Default(t)
+	c.fluentC = fluent.NewClient()
+	c.fluentC.Connection().WithStub(gribiC)
+	if c.Persistence {
+		c.fluentC.Connection().WithInitialElectionID(c.InitialElectionIDLow, c.InitialElectionIDHigh).
 			WithRedundancyMode(fluent.ElectedPrimaryClient).WithPersistence()
 	} else {
-		g.fluentC.Connection().WithInitialElectionID(1, 0).
+		c.fluentC.Connection().WithInitialElectionID(c.InitialElectionIDLow, c.InitialElectionIDHigh).
 			WithRedundancyMode(fluent.ElectedPrimaryClient)
 	}
-	if g.FibACK {
-		g.fluentC.Connection().WithFIBACK()
+	if c.FibACK {
+		c.fluentC.Connection().WithFIBACK()
 	}
 	ctx := context.Background()
-	g.fluentC.Start(ctx, t)
-	g.fluentC.StartSending(ctx, t)
-	err := g.AwaitTimeout(ctx, t, timeout)
+	c.fluentC.Start(ctx, t)
+	c.fluentC.StartSending(ctx, t)
+	err := c.AwaitTimeout(ctx, t, timeout)
 	return err
 }
 
 // Close function closes the gribi session with the dut by stopping the fluent client.
-func (g *GRIBIHandler) Close(t testing.TB) {
+func (c *Client) Close(t testing.TB) {
 	t.Helper()
-	t.Logf("Closing GRIBI connection for dut: %s", g.DUT.Name())
-	if g.fluentC != nil {
-		g.fluentC.Stop(t)
-		g.fluentC = nil
+	t.Logf("Closing GRIBI connection for dut: %s", c.DUT.Name())
+	if c.fluentC != nil {
+		c.fluentC.Stop(t)
+		c.fluentC = nil
 	}
 }
 
 // AwaitTimeout calls a fluent client Await by adding a timeout to the context.
-func (g *GRIBIHandler) AwaitTimeout(ctx context.Context, t testing.TB, timeout time.Duration) error {
+func (c *Client) AwaitTimeout(ctx context.Context, t testing.TB, timeout time.Duration) error {
 	subctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
-	return g.fluentC.Await(subctx, t)
+	return c.fluentC.Await(subctx, t)
 }
 
 // learnElectionID learns the current server election id by sending
 // a dummy modify request with election id 1.
-func (g *GRIBIHandler) learnElectionID(t testing.TB) (low, high uint64) {
+func (c *Client) learnElectionID(t testing.TB) (low, high uint64) {
 	t.Helper()
-	t.Logf("Learn GRIBI Election ID from dut: %s", g.DUT.Name())
-	g.fluentC.Modify().UpdateElectionID(t, 1, 0)
-	if err := g.AwaitTimeout(context.Background(), t, timeout); err != nil {
+	t.Logf("Learn GRIBI Election ID from dut: %s", c.DUT.Name())
+	c.fluentC.Modify().UpdateElectionID(t, 1, 0)
+	if err := c.AwaitTimeout(context.Background(), t, timeout); err != nil {
 		t.Fatalf("Error waiting to update Election ID: %v", err)
 	}
-	results := g.fluentC.Results(t)
+	results := c.fluentC.Results(t)
 	electionID := results[len(results)-1].CurrentServerElectionID
 	return electionID.Low, electionID.High
 }
 
 // UpdateElectionID updates the election id of the dut.
 // The function fails if the requsted election id is less than the server election id.
-func (g *GRIBIHandler) UpdateElectionID(t testing.TB, lowElecId, highElecId uint64) {
+func (c *Client) UpdateElectionID(t testing.TB, lowElecID, highElecID uint64) {
 	t.Helper()
-	t.Logf("Setting GRIBI Election ID for dut: %s to low=%d,high=%d", g.DUT.Name(), lowElecId, highElecId)
-	g.fluentC.Modify().UpdateElectionID(t, lowElecId, highElecId)
-	if err := g.AwaitTimeout(context.Background(), t, timeout); err != nil {
+	t.Logf("Setting GRIBI Election ID for dut: %s to low=%d, high=%d", c.DUT.Name(), lowElecID, highElecID)
+	c.fluentC.Modify().UpdateElectionID(t, lowElecID, highElecID)
+	if err := c.AwaitTimeout(context.Background(), t, timeout); err != nil {
 		t.Fatalf("Error waiting to update Election ID: %v", err)
 	}
-	chk.HasResult(t, g.fluentC.Results(t),
+	chk.HasResult(t, c.fluentC.Results(t),
 		fluent.OperationResult().
-			WithCurrentServerElectionID(lowElecId, highElecId).
+			WithCurrentServerElectionID(lowElecID, highElecID).
 			AsResult(),
 	)
 }
 
 // BecomeLeader learns the latest election id and the make the client leader by increasing the election id by one.
-func (g *GRIBIHandler) BecomeLeader(t testing.TB) {
-	t.Logf("Trying to be a master with increasing the election id by one on dut: %s", g.DUT.Name())
-	low, high := g.learnElectionID(t)
+func (c *Client) BecomeLeader(t testing.TB) {
+	t.Logf("Trying to be a master with increasing the election id by one on dut: %s", c.DUT.Name())
+	low, high := c.learnElectionID(t)
 	newLow := low + 1
 	if newLow < low {
-		high += 1 // Carry to high.
+		high++ // Carry to high.
 	}
-	g.UpdateElectionID(t, newLow, high)
+	c.UpdateElectionID(t, newLow, high)
 }
 
 // AddNHG adds a NextHopGroupEntry with a given index, and a map of next hop entry indices to the weights,
@@ -179,31 +182,6 @@ func (g *Client) NHG(t testing.TB, nhgIndex uint64, bkhgIndex uint64, nhWeights 
 	)
 }
 
-// func (g *Client) RemoveNHG(t testing.TB, nhgIndex uint64, bkhgIndex uint64, nhWeights map[uint64]uint64, instance string, expectedResult fluent.ProgrammingResult) {
-// 	nhg := fluent.NextHopGroupEntry().WithNetworkInstance(instance).WithID(nhgIndex)
-
-// 	// checking if backup provided
-// 	if bkhgIndex != 0 {
-// 		nhg.WithBackupNHG(bkhgIndex)
-// 	}
-
-// 	if len(nhWeights) != 0 {
-// 		for nhIndex, weight := range nhWeights {
-// 			nhg.AddNextHop(nhIndex, weight)
-// 			g.fluentC.Modify().DeleteEntry(t, nhg)
-// 		}
-// 	}
-
-// 	chk.HasResult(t, g.fluentC.Results(t),
-// 		fluent.OperationResult().
-// 			WithNextHopGroupOperation(nhgIndex).
-// 			WithOperationType(constants.Add).
-// 			WithProgrammingResult(expectedResult).
-// 			AsResult(),
-// 		chk.IgnoreOperationID(),
-// 	)
-// }
-
 // AddNH adds a NextHopEntry with a given index to an address within a given network instance.
 func (g *Client) NH(t testing.TB, nhIndex uint64, nh_entry, instance string, nh_Instance string, action string, expectedResult fluent.ProgrammingResult) {
 	addr := net.ParseIP(nh_entry)
@@ -235,31 +213,6 @@ func (g *Client) NH(t testing.TB, nhIndex uint64, nh_entry, instance string, nh_
 	)
 }
 
-// func (g *Client) RemoveNH(t testing.TB, nhIndex uint64, nh_entry, instance string, expectedResult fluent.ProgrammingResult) {
-// 	g.fluentC.Modify().DeleteEntry(t,
-// 		fluent.NextHopEntry().
-// 			WithNetworkInstance(instance).
-// 			WithIndex(nhIndex).
-// 			WithIPAddress(nh_entry))
-
-// 	// nhg := fluent.NextHopGroupEntry().WithNetworkInstance(instance).WithID(200)
-
-// 	// nhg.AddNextHop(2000, 100)
-// 	// g.fluentC.Modify().DeleteEntry(t, nhg)
-
-// 	// if err := g.AwaitTimeout(context.Background(), t, timeout); err != nil {
-// 	// 	t.Fatalf("Error waiting to add NH: %v", err)
-// 	// }
-// 	chk.HasResult(t, g.fluentC.Results(t),
-// 		fluent.OperationResult().
-// 			WithNextHopOperation(nhIndex).
-// 			WithOperationType(constants.Add).
-// 			WithProgrammingResult(expectedResult).
-// 			AsResult(),
-// 		chk.IgnoreOperationID(),
-// 	)
-// }
-
 // AddIPv4 adds an IPv4Entry mapping a prefix to a given next hop group index within a given network instance.
 func (g *Client) IPv4(t testing.TB, prefix string, nhgIndex uint64, instance, nhgInstance string, action string, expectedResult fluent.ProgrammingResult) {
 	ipv4Entry := fluent.IPv4Entry().WithPrefix(prefix).
@@ -288,21 +241,3 @@ func (g *Client) IPv4(t testing.TB, prefix string, nhgIndex uint64, instance, nh
 		chk.IgnoreOperationID(),
 	)
 }
-
-// func (g *Client) RemoveIPv4(t testing.TB, prefix string, nhgIndex uint64, instance, nhgInstance string, expectedResult fluent.ProgrammingResult) {
-// 	ipv4Entry := fluent.IPv4Entry().WithPrefix(prefix).
-// 		WithNetworkInstance(instance).
-// 		WithNextHopGroup(nhgIndex)
-// 	if nhgInstance != "" && nhgInstance != instance {
-// 		ipv4Entry.WithNextHopGroupNetworkInstance(nhgInstance)
-// 	}
-// 	g.fluentC.Modify().DeleteEntry(t, ipv4Entry)
-// 	chk.HasResult(t, g.fluentC.Results(t),
-// 		fluent.OperationResult().
-// 			WithIPv4Operation(prefix).
-// 			WithOperationType(constants.Add).
-// 			WithProgrammingResult(expectedResult).
-// 			AsResult(),
-// 		chk.IgnoreOperationID(),
-// 	)
-// }
