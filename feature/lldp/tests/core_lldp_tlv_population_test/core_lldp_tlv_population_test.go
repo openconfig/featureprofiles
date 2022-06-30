d6e26e236d5254669ecb2936a5b61067f63c376b
Ankur19 <ankursaikia@google.com>
2022-06-30 15:50:58 -0400

    change name of package

--- feature/lldp/tests/core_lldp_tlv_population_test/core_lldp_tlv_population_test.go
+++ feature/lldp/tests/core_lldp_tlv_population_test/core_lldp_tlv_population_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package lldp_test
+package core_lldp_tlv_population_test
 
 import (
 	"testing"
