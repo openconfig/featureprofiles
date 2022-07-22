938fd7f3564c33bdcb5c5fdb9cbd41d192de83b3
Octav <octavian.petre@keysight.com>
2022-07-22 15:11:12 +0000

    - updated the nhIndexes according to ate test - defaulted otg configs for size, rate and duration - minor changes requested in the PR comments

--- feature/gribi/otg_tests/base_leader_election_test/base_leader_election_test.go
+++ feature/gribi/otg_tests/base_leader_election_test/base_leader_election_test.go
@@ -53,7 +53,6 @@ const (
 	instance        = "default"
 	ateDstNetCIDR   = "198.51.100.0/24"
 	ateDstNetStart  = "198.51.100.0"
-	ateDstNetPrefix = 24
 	nhIndex         = 1
 	nhgIndex        = 42
 	trafficDuration = 10 * time.Second
@@ -100,7 +99,7 @@ var (
 	}
 )
 
-var inputMap = map[attrs.Attributes]attrs.Attributes{
+var gatewayMap = map[attrs.Attributes]attrs.Attributes{
 	atePort1: dutPort1,
 	atePort2: dutPort2,
 	atePort3: dutPort3,
@@ -175,7 +174,7 @@ func configureATE(t *testing.T, ate *ondatra.ATEDevice) gosnappi.Config {
 	return top
 }
 
-func waitOtgArpEntry(t *testing.T) {
+func waitOTGARPEntry(t *testing.T) {
 	ate := ondatra.ATE(t, "ate")
 	ate.OTG().Telemetry().InterfaceAny().Ipv4NeighborAny().LinkLayerAddress().Watch(
 		t, time.Minute, func(val *otgtelemetry.QualifiedString) bool {
@@ -190,16 +189,13 @@ func testTraffic(t *testing.T, ate *ondatra.ATEDevice, config gosnappi.Config, s
 
 	// srcEndPoint is atePort1
 	otg := ate.OTG()
-	gwIp := inputMap[srcEndPoint].IPv4
-	waitOtgArpEntry(t)
+	gwIp := gatewayMap[srcEndPoint].IPv4
+	waitOTGARPEntry(t)
 	dstMac := otg.Telemetry().Interface(srcEndPoint.Name + ".Eth").Ipv4Neighbor(gwIp).LinkLayerAddress().Get(t)
 	config.Flows().Clear().Items()
 	flowipv4 := config.Flows().Add().SetName("Flow")
 	flowipv4.Metrics().SetEnable(true)
 	flowipv4.TxRx().Port().SetTxName(srcEndPoint.Name).SetRxName(dstEndPoint.Name)
-	flowipv4.Size().SetFixed(512)
-	flowipv4.Rate().SetPps(10)
-	flowipv4.Duration().SetChoice("continuous")
 	e1 := flowipv4.Packet().Add().Ethernet()
 	e1.Src().SetValue(srcEndPoint.MAC)
 	e1.Dst().SetChoice("value").SetValue(dstMac)
@@ -265,18 +261,18 @@ func testIPv4LeaderActiveChange(ctx context.Context, t *testing.T, args *testArg
 	// Add an IPv4Entry for 198.51.100.0/24 pointing to ATE port-2 via gRIBI-A,
 	// ensure that the entry is ignored by the DUT.
 	t.Logf("Adding an IPv4Entry for %s pointing to ATE port-2 via gRIBI-A", ateDstNetCIDR)
-	args.clientA.AddNH(t, nhIndex, atePort2.IPv4, instance, fluent.ProgrammingFailed)
-	args.clientA.AddNHG(t, nhgIndex, map[uint64]uint64{nhIndex: 1}, instance, fluent.ProgrammingFailed)
-	args.clientA.AddIPv4(t, ateDstNetCIDR, nhgIndex, instance, "", fluent.ProgrammingFailed)
+	args.clientA.AddNH(t, nhIndex+1, atePort2.IPv4, instance, fluent.ProgrammingFailed)
+	args.clientA.AddNHG(t, nhgIndex+1, map[uint64]uint64{nhIndex + 1: 1}, instance, fluent.ProgrammingFailed)
+	args.clientA.AddIPv4(t, ateDstNetCIDR, nhgIndex+1, instance, "", fluent.ProgrammingFailed)
 
 	// Send a ModifyRequest from gRIBI-A specifying election_id 12,
 	// followed by a ModifyRequest updating 198.51.100.0/24 pointing to ATE port-2,
 	// ensure that routing is updated to receive packets for 198.51.100.0/24 at ATE port-2.
 	args.clientA.UpdateElectionID(t, 12, 0)
 	t.Logf("Adding an IPv4Entry for %s pointing to ATE port-2 via client gRIBI-A", ateDstNetCIDR)
-	args.clientA.AddNH(t, nhIndex, atePort2.IPv4, instance, fluent.InstalledInRIB)
-	args.clientA.AddNHG(t, nhgIndex, map[uint64]uint64{nhIndex: 1}, instance, fluent.InstalledInRIB)
-	args.clientA.AddIPv4(t, ateDstNetCIDR, nhgIndex, instance, "", fluent.InstalledInRIB)
+	args.clientA.AddNH(t, nhIndex+2, atePort2.IPv4, instance, fluent.InstalledInRIB)
+	args.clientA.AddNHG(t, nhgIndex+2, map[uint64]uint64{nhIndex + 2: 1}, instance, fluent.InstalledInRIB)
+	args.clientA.AddIPv4(t, ateDstNetCIDR, nhgIndex+2, instance, "", fluent.InstalledInRIB)
 
 	// Verify the entry for 198.51.100.0/24 is active through AFT Telemetry.
 	ipv4Path = args.dut.Telemetry().NetworkInstance(instance).Afts().Ipv4Entry(ateDstNetCIDR)
