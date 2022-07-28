fafac6e3edbd9e1830f14572295831edee0659e2
amrindrr <hackter1882@gmail.com>
2022-07-28 12:39:37 -0700

    Fixing go fmt errors

--- feature/gnmi/ate_tests/telemetry_interface_packet_counters_test/telemetry_interface_packet_counters_test.go
+++ feature/gnmi/ate_tests/telemetry_interface_packet_counters_test/telemetry_interface_packet_counters_test.go
@@ -134,12 +134,12 @@ func TestInterfaceCounters(t *testing.T) {
 		counter: ipv6Counters.OutPkts().Lookup(t),
 	}, {
 		// desc: "IPv6InDiscardedPkts",
-		path:    ipv6CounterPath + "in-discarded-pkts",
+		path: ipv6CounterPath + "in-discarded-pkts",
 		// TODO: Uncomment counter in-discarded-pkts after the issue fixed.
 		// counter: ipv6Counters.InDiscardedPkts().Lookup(t),
 	}, {
 		// desc: "IPv6OutDiscardedPkts",
-		path:    ipv6CounterPath + "out-discarded-pkts",
+		path: ipv6CounterPath + "out-discarded-pkts",
 		// TODO: Uncomment counter out-discarded-pkts after the issue fixed.
 		// counter: ipv6Counters.OutDiscardedPkts().Lookup(t),
 	}}
