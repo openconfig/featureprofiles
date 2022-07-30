5d76ec8500bcc1a620ea74f9b23288d1837f3eea
Darren Loher <dloher@google.com>
2022-07-29 19:40:37 -0700

    Update deviations.go

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+//
 // Package deviations documents defines arguments to enable
 // workarounds for a featureprofiles test suite.
 //
@@ -21,8 +21,6 @@
 //
 // Passing with a deviation enabled is considered non-compliant to the
 // openconfig featureprofiles test.
-
-
 package deviations
 
 import "flag"
