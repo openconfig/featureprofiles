2401d6ae29532657564f25b7a3cd127d5ba39840
amrindrr <amrinderr@google.com>
2022-08-01 18:56:45 -0700

    Add Sub interface packet discard counter deviation

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -30,4 +30,7 @@ var (
 		"Device requires that aggregate Port-Channel and its members be defined in a single gNMI Update transaction at /interfaces; otherwise lag-type will be dropped, and no member can be added to the aggregate.")
 
 	DefaultNetworkInstance = flag.String("deviation_default_network_instance", "DEFAULT", "The name used for the default network instance for VRF.")
+
+	SubInterfacePacketCountersSupported = flag.Bool("deviation_subinterface_counter_supported", true,
+		"Subinterface discard packet counters for ipv4/ipv6 are not supported by default. Manually set it to False to skip lookup of discard counters in the test")
 )
