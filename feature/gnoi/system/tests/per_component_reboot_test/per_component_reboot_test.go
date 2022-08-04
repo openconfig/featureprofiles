77ca0eff91012b6bd4fe812218980b545fb4364f
sezhang2 <sezhang@google.com>
2022-08-03 18:07:56 -0700

    Get component list by type instead of matching the component name
      - Added func findComponentsByType()

--- feature/gnoi/system/tests/per_component_reboot_test/per_component_reboot_test.go
+++ feature/gnoi/system/tests/per_component_reboot_test/per_component_reboot_test.go
@@ -16,7 +16,6 @@ package per_component_reboot_test
 
 import (
 	"context"
-	"regexp"
 	"sort"
 	"strings"
 	"testing"
@@ -31,6 +30,11 @@ import (
 
 	spb "github.com/openconfig/gnoi/system"
 	tpb "github.com/openconfig/gnoi/types"
+)
+
+const (
+	controlcardType = telemetry.PlatformTypes_OPENCONFIG_HARDWARE_COMPONENT_CONTROLLER_CARD
+	linecardType    = telemetry.PlatformTypes_OPENCONFIG_HARDWARE_COMPONENT_LINECARD
 )
 
 func TestMain(m *testing.M) {
@@ -74,8 +78,7 @@ func TestMain(m *testing.M) {
 func TestStandbySupervisorReboot(t *testing.T) {
 	dut := ondatra.DUT(t, "dut")
 
-	r := regexp.MustCompile("^Supervisor[0-9]$")
-	supervisors := findMatchedComponents(t, dut, r)
+	supervisors := findComponentsByType(t, dut, controlcardType)
 	t.Logf("Found supervisor list: %v", supervisors)
 	// Only perform the standby RP rebooting for the chassis with dual RPs/Supervisors.
 	if len(supervisors) < 2 {
@@ -109,8 +112,7 @@ func TestLinecardReboot(t *testing.T) {
 	const linecardBoottime = 10 * time.Minute
 	dut := ondatra.DUT(t, "dut")
 
-	r := regexp.MustCompile("^Linecard[0-9]$")
-	lcs := findMatchedComponents(t, dut, r)
+	lcs := findComponentsByType(t, dut, linecardType)
 	t.Logf("Found linecard list: %v", lcs)
 	if got := len(lcs); got == 0 {
 		t.Errorf("Get number of Linecards on %v: got %v, want > 0", dut.Model(), got)
@@ -200,12 +202,28 @@ func TestLinecardReboot(t *testing.T) {
 	// TODO: Check the line card uptime has been reset.
 }
 
-func findMatchedComponents(t *testing.T, dut *ondatra.DUTDevice, r *regexp.Regexp) []string {
+func findComponentsByType(t *testing.T, dut *ondatra.DUTDevice, cType telemetry.E_PlatformTypes_OPENCONFIG_HARDWARE_COMPONENT) []string {
 	components := dut.Telemetry().ComponentAny().Name().Get(t)
 	var s []string
 	for _, c := range components {
-		if len(r.FindString(c)) > 0 {
-			s = append(s, c)
+		lookupType := dut.Telemetry().Component(c).Type().Lookup(t)
+		if !lookupType.IsPresent() {
+			t.Logf("Component %s type is not found", c)
+		} else {
+			componentType := lookupType.Val(t)
+			t.Logf("Component %s has type: %v", c, componentType)
+
+			switch v := componentType.(type) {
+			case telemetry.E_PlatformTypes_OPENCONFIG_HARDWARE_COMPONENT:
+				switch v {
+				case cType:
+					s = append(s, c)
+				default:
+					continue
+				}
+			default:
+				t.Fatalf("Error extracting component state, could not type assert union. union: %v", componentType)
+			}
 		}
 	}
 	return s
