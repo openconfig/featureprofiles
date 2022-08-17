bd098f9c843f636fec7604135ca00201cc0ed639
manan-patel <mananpat@cisco.com>
2022-08-17 13:34:34 -0400

    updated info

--- feature/gribi/ate_tests/cisco/pbr/policy_test.go
+++ feature/gribi/ate_tests/cisco/pbr/policy_test.go
@@ -5,9 +5,172 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/openconfig/featureprofiles/internal/attrs"
 	"github.com/openconfig/featureprofiles/internal/fptest"
 	"github.com/openconfig/featureprofiles/internal/gribi"
 	"github.com/openconfig/ondatra"
+)
+
+const (
+	ipv4PrefixLen = 24
+	ipv6PrefixLen = 126
+	instance      = "default"
+	vlanMTU       = 1518
+)
+
+var (
+	dutPort1 = attrs.Attributes{
+		Desc:    "dutPort1",
+		IPv4:    "100.120.1.1",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:120:1:1",
+		IPv6Len: ipv6PrefixLen,
+	}
+
+	atePort1 = attrs.Attributes{
+		Name:    "atePort1",
+		IPv4:    "100.120.1.2",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:120:1:2",
+		IPv6Len: ipv6PrefixLen,
+	}
+
+	dutPort2 = attrs.Attributes{
+		Desc:    "dutPort2",
+		IPv4:    "100.121.1.1",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:121:1:1",
+		IPv6Len: ipv6PrefixLen,
+	}
+
+	atePort2 = attrs.Attributes{
+		Name:    "atePort2",
+		IPv4:    "100.121.1.2",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:121:1:2",
+		IPv6Len: ipv6PrefixLen,
+	}
+
+	dutPort3 = attrs.Attributes{
+		Desc:    "dutPort3",
+		IPv4:    "100.122.1.1",
+		IPv4Len: ipv4PrefixLen,
+	}
+
+	atePort3 = attrs.Attributes{
+		Name:    "atePort3",
+		IPv4:    "100.122.1.2",
+		IPv4Len: ipv4PrefixLen,
+	}
+
+	dutPort4 = attrs.Attributes{
+		Desc:    "dutPort4",
+		IPv4:    "100.123.1.1",
+		IPv4Len: ipv4PrefixLen,
+	}
+
+	atePort4 = attrs.Attributes{
+		Name:    "atePort4",
+		IPv4:    "100.123.1.2",
+		IPv4Len: ipv4PrefixLen,
+	}
+	dutPort5 = attrs.Attributes{
+		Desc:    "dutPort5",
+		IPv4:    "100.124.1.1",
+		IPv4Len: ipv4PrefixLen,
+	}
+
+	atePort5 = attrs.Attributes{
+		Name:    "atePort5",
+		IPv4:    "100.124.1.2",
+		IPv4Len: ipv4PrefixLen,
+	}
+	dutPort6 = attrs.Attributes{
+		Desc:    "dutPort6",
+		IPv4:    "100.125.1.1",
+		IPv4Len: ipv4PrefixLen,
+	}
+
+	atePort6 = attrs.Attributes{
+		Name:    "atePort6",
+		IPv4:    "100.125.1.2",
+		IPv4Len: ipv4PrefixLen,
+	}
+	dutPort7 = attrs.Attributes{
+		Desc:    "dutPort7",
+		IPv4:    "100.126.1.1",
+		IPv4Len: ipv4PrefixLen,
+	}
+
+	atePort7 = attrs.Attributes{
+		Name:    "atePort7",
+		IPv4:    "100.126.1.2",
+		IPv4Len: ipv4PrefixLen,
+	}
+	dutPort8 = attrs.Attributes{
+		Desc:    "dutPort8",
+		IPv4:    "100.127.1.1",
+		IPv4Len: ipv4PrefixLen,
+	}
+	atePort8 = attrs.Attributes{
+		Name:    "atePort8",
+		IPv4:    "100.127.1.2",
+		IPv4Len: ipv4PrefixLen,
+	}
+
+	dutPort2Vlan10 = attrs.Attributes{
+		Desc:    "dutPort2Vlan10",
+		IPv4:    "100.121.10.1",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:121:10:1",
+		IPv6Len: ipv6PrefixLen,
+		MTU:     vlanMTU,
+	}
+
+	atePort2Vlan10 = attrs.Attributes{
+		Name:    "atePort2Vlan10",
+		IPv4:    "100.121.10.2",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:121:10:2",
+		IPv6Len: ipv6PrefixLen,
+		MTU:     vlanMTU,
+	}
+
+	dutPort2Vlan20 = attrs.Attributes{
+		Desc:    "dutPort2Vlan20",
+		IPv4:    "100.121.20.1",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:121:20:1",
+		IPv6Len: ipv6PrefixLen,
+		MTU:     vlanMTU,
+	}
+
+	atePort2Vlan20 = attrs.Attributes{
+		Name:    "atePort2Vlan20",
+		IPv4:    "100.121.20.2",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:121:20:2",
+		IPv6Len: ipv6PrefixLen,
+		MTU:     vlanMTU,
+	}
+
+	dutPort2Vlan30 = attrs.Attributes{
+		Desc:    "dutPort2Vlan30",
+		IPv4:    "100.121.30.1",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:121:30:1",
+		IPv6Len: ipv6PrefixLen,
+		MTU:     vlanMTU,
+	}
+
+	atePort2Vlan30 = attrs.Attributes{
+		Name:    "atePort2Vlan20",
+		IPv4:    "100.121.30.2",
+		IPv4Len: ipv4PrefixLen,
+		IPv6:    "2000::100:121:30:2",
+		IPv6Len: ipv6PrefixLen,
+		MTU:     vlanMTU,
+	}
 )
 
 // Testcase defines testcase structure
