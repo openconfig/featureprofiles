7d2a1dc6dee3f57434f369eed0660048d429c1c6
Likai Liu <liulk@google.com>
2022-08-17 15:37:39 -0400

    Rectified deviation so "true" always indicates deviated behavior. (#412)
    * Rectified deviation so "true" always indicates deviated behavior.
    
    * Clarified --deviation_default_network_instance compliant behavior
    
    * Clarified the case when OC ambiguity allows device to reject deviation
    
    * Update deviations.go
    
    * Update deviations.go
    
    * Clarified that deviations should always use OC compliant behavior
    
    * Clarify the compliance tiers.

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -12,30 +12,51 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package deviations defines the arguments to enable temporary workarounds
-// for the featureprofiles test suite.
+// Package deviations defines the arguments to enable temporary workarounds for the
+// featureprofiles test suite using command line flags.
 //
-// Deviations may be introduced to temporarily work around non-compliant issues
-// so further sub-tests can be implemented.  Deviations should be
-// small in scope, typically affecting one sub-test, one OpenConfig
-// path or small OpenConfig sub-tree.  Deviations are enabled using
-// command line parameters.
+// If we consider device compliance level in tiers:
 //
-// Passing with a deviation enabled is considered non-compliant to the
-// OpenConfig featureprofiles test.
+//   - Tier 0: Full OpenConfig compliance.  The device can do everything specified by
+//     OpenConfig.
+//   - Tier 1: Test plan compliance.  The device can pass a test without deviation, which
+//     means it satisfies the test requirements.  This is the target compliance tier for
+//     featureprofiles tests.
+//   - Tier 2: Deviated test plan compliance.  The device can pass a test with deviation.
+//
+// Deviations typically work by reducing testing requirements or by changing the way the
+// configuration is done.  However, the targeted compliance tier is always without
+// deviation.
+//
+// Requirements for deviations:
+//
+//   - Deviations may only use OpenConfig compliant behavior.
+//   - Deviations should be small in scope, typically affecting one sub-test, one
+//     OpenConfig path or small OpenConfig sub-tree.
+//
+// If a device could not pass without deviation, that is considered non-compliant
+// behavior.  Ideally, a device should pass both with and without a deviation which means
+// the deviation could be safely removed.  However, when the OpenConfig model allows the
+// device to reject the deviated case even if it is compliant, then this should be
+// explained on a case-by-case basis.
 //
 // To add a deviation:
+//
 //   - Submit a github issue explaining the need for the deviation.
 //   - Submit a pull request referencing the above issue to add a flag to
 //     this file and updates to the tests where it is intended to be used.
+//   - Make sure the deviation defaults to false.  False (not deviated) means strictly
+//     compliant behavior.  True (deviated) activates the workaround.
 //
 // To remove a deviation:
+//
 //   - Submit a pull request which proposes to resolve the relevant
 //     github issue by removing the deviation and it's usage within tests.
 //   - Typically the author or an affiliate of the author's organization
 //     is expected to remove a deviation they introduced.
 //
 // To enable the deviations for a test run:
+//
 //   - By default, deviations are not enabled and instead require the
 //     test invocation to set an argument to enable the deviation.
 //   - For example:
@@ -47,13 +68,14 @@ import "flag"
 // Vendor deviation flags.
 var (
 	InterfaceEnabled = flag.Bool("deviation_interface_enabled", false,
-		"Device requires interface enabled leaf booleans to be explicitly set to true.")
+		"Device requires interface enabled leaf booleans to be explicitly set to true.  Full OpenConfig compliant devices should pass both with and without this deviation.")
 
-	AggregateAtomicUpdate = flag.Bool("deviation_aggregate_atomic_update", true,
-		"Device requires that aggregate Port-Channel and its members be defined in a single gNMI Update transaction at /interfaces; otherwise lag-type will be dropped, and no member can be added to the aggregate.")
+	AggregateAtomicUpdate = flag.Bool("deviation_aggregate_atomic_update", false,
+		"Device requires that aggregate Port-Channel and its members be defined in a single gNMI Update transaction at /interfaces; otherwise lag-type will be dropped, and no member can be added to the aggregate.  Full OpenConfig compliant devices should pass both with and without this deviation.")
 
-	DefaultNetworkInstance = flag.String("deviation_default_network_instance", "DEFAULT", "The name used for the default network instance for VRF.  This has been standardized in OpenConfig as \"DEFAULT\" but some legacy devices are using \"default\"; tests should use this deviation as a temporary workaround.")
+	DefaultNetworkInstance = flag.String("deviation_default_network_instance", "DEFAULT",
+		"The name used for the default network instance for VRF.  The default name in OpenConfig is \"DEFAULT\" but some legacy devices still use \"default\".  Full OpenConfig compliant devices should be able to use any operator-assigned value.")
 
-	SubInterfacePacketCountersSupported = flag.Bool("deviation_subinterface_packet_counters_supported", true,
-		"Subinterface discard packet counters for ipv4/ipv6 are not always supported. Manually set it to False to skip lookup of discard counters in the test")
+	SubinterfacePacketCountersMissing = flag.Bool("deviation_subinterface_packet_counters_missing", false,
+		"Device is missing subinterface packet counters for IPv4/IPv6, so the test will skip checking them.  Full OpenConfig compliant devices should pass both with and without this deviation.")
 )
