2355342120af837974b9a8694e971b0e122b515c
manan-patel <mananpat@cisco.com>
2022-08-24 17:05:53 -0400

    setting traffic check to true by default and aft false

--- internal/cisco/flags/flags.go
+++ internal/cisco/flags/flags.go
@@ -4,13 +4,13 @@ import "flag"
 
 // cisco tests flags.
 var (
-	GRIBITrafficCheck = flag.Bool("gribi_traffic_check", false,
+	GRIBITrafficCheck = flag.Bool("gribi_traffic_check", true,
 		"This enable/disable traffic check for gribi tests.")
 
 	GRIBIAFTCheck = flag.Bool("gribi_aft_check", false,
 		"This enable/disable AFT check for gribi entries in gribi tests.")
 
-	GRIBIAFTChainCheck = flag.Bool("gribi_aft_chain_check", true,
+	GRIBIAFTChainCheck = flag.Bool("gribi_aft_chain_check", false,
 		"This enable/disable AFT chain check for gribi prefix in gribi tests.")
 
 	GRIBIFIBCheck = flag.Bool("gribi_fib_check", false,
