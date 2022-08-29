f5b0e661b19851ba7b18ff7ea4bdc7b6a7ed1e7b
Arvind Sankaran <saarvind@cisco.com>
2022-08-29 16:03:42 +0530

    resolving-merge-conflict

---
+++ feature/cisco/qos/setup/setup_ate.go
@@ -0,0 +1,28 @@
+package setup
+
+import (
+	"fmt"
+
+	"os"
+
+	"strings"
+)
+
+var (
+	jsonConfig  []uint8
+	oCPackagest = []string{"system", "acl",
+		"networkinstance", "lacp", "local-routes", "lldp", "network-instance", "components", "qos", "interface"} // order is important
+)
+
+func FindTestDataPath() string {
+	path, err := os.Getwd()
+	if err != nil {
+		panic(fmt.Sprintf("Error: %v", err))
+	}
+	for _, ocPkg := range oCPackagest {
+		if strings.Contains(path, ocPkg) {
+			return strings.Split(path, ocPkg)[0] + "/" + ocPkg + "/testdata/base_config.json"
+		}
+	}
+	return "testdata/base_config.json"
+}
