d0e63842eff63101f50024712a50a55c6ff50e9f
Arvind Sankaran <saarvind@blr-b4-lnx1.cisco.com>
2022-08-30 15:42:37 +0530

    removing-workaround-for-CSCwc68712

--- feature/cisco/qos/interface/setup_test.go
+++ feature/cisco/qos/interface/setup_test.go
@@ -68,21 +68,7 @@ func setupQosEgress(t *testing.T, dut *ondatra.DUTDevice, baseConfigFile string)
 	}
 	return bc
 }
-
 func teardownQos(t *testing.T, dut *ondatra.DUTDevice, baseConfig *oc.Qos) {
-	var err *string
-	for attempt := 1; attempt <= 2; attempt++ {
-		err = testt.CaptureFatal(t, func(t testing.TB) {
-			dut.Config().Qos().Delete(t)
-			for queueName := range baseConfig.Queue {
-				dut.Config().Qos().Queue(queueName).Delete(t)
-			}
-		})
-		if err == nil {
-			break
-		}
-	}
-	if err != nil {
-		t.Errorf(*err)
-	}
+        dut.Config().Qos().Delete(t)
 }
+
--- feature/cisco/qos/scheduler-policies/scheduler-policy/setup_test.go
+++ feature/cisco/qos/scheduler-policies/scheduler-policy/setup_test.go
@@ -5,23 +5,9 @@ import (
 
 	"github.com/openconfig/ondatra"
 	oc "github.com/openconfig/ondatra/telemetry"
-	"github.com/openconfig/testt"
+//	"github.com/openconfig/testt"
 )
-
 func teardownQos(t *testing.T, dut *ondatra.DUTDevice, baseConfig *oc.Qos) {
-	var err *string
-	for attempt := 1; attempt <= 2; attempt++ {
-		err = testt.CaptureFatal(t, func(t testing.TB) {
-			dut.Config().Qos().Delete(t)
-			for queueName := range baseConfig.Queue {
-				dut.Config().Qos().Queue(queueName).Delete(t)
-			}
-		})
-		if err == nil {
-			break
-		}
-	}
-	if err != nil {
-		t.Errorf(*err)
-	}
+        dut.Config().Qos().Delete(t)
 }
+
