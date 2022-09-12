e4be6d06e2a7e485ed3da4f71624c5ed7e501dcc
moji <mbagherz@cisco.com>
2022-09-12 18:21:18 -0400

    fix args

--- exec/firex/testsuite_generator.go
+++ exec/firex/testsuite_generator.go
@@ -54,48 +54,48 @@ var (
 	}).Parse(`
 {{- range $i, $ft := $.TestSuite }}
 {{- .Name }}:
-framework: b4_fp
-owners:
-	- {{ $ft.Owner }}
-{{- if $ft.Pyvxr.Topology }}
-plugins:
-	- vxsim.py
-topo_file: {{ $.Workspace }}/{{ $ft.Pyvxr.Topology }}
-{{- end }}
-ondatra_testbed_path: {{ $ft.Testbed }}
-{{- if $ft.Binding }}
-ondatra_binding_path: {{ $ft.Binding }}
-{{- end }}
-supported_platforms:
-	- "8000"
-fp_pre_tests:
-	{{- range $j, $gt := $ft.Pretests}}
-	- {{ $gt.Name }}:
-		test_path: {{ $gt.Path }}
-		{{- if $gt.Args }}
-		test_args: {{ join $gt.Args " " }}
-		{{- end }}
-	{{- end }}
-script_paths:
-	{{- range $j, $gt := $ft.Tests}}
-	- {{ $gt.Name }}:
-		test_path: {{ $gt.Path }}
-		{{- if $gt.Args }}
-		test_args: {{ join $gt.Args " " }}
-		{{- end }}
-		{{- if $gt.Patch }}
-		test_patch: {{ $gt.Patch }}
-		{{- end }}
-	{{- end }}
-fp_post_tests:
-	{{- range $j, $gt := $ft.Posttests}}
-	- {{ $gt.Name }}:
-		test_path: {{ $gt.Path }}
-		{{- if $gt.Args }}
-		test_args: {{ join $gt.Args " " }}
-		{{- end }}
-	{{- end }}
-smart_sanity_exclude: True
+    framework: b4_fp
+    owners:
+        - {{ $ft.Owner }}
+    {{- if $ft.Pyvxr.Topology }}
+    plugins:
+        - vxsim.py
+    topo_file: {{ $.Workspace }}/{{ $ft.Pyvxr.Topology }}
+    {{- end }}
+    ondatra_testbed_path: {{ $ft.Testbed }}
+    {{- if $ft.Binding }}
+    ondatra_binding_path: {{ $ft.Binding }}
+    {{- end }}
+    supported_platforms:
+        - "8000"
+    fp_pre_tests:
+        {{- range $j, $gt := $ft.Pretests}}
+        - {{ $gt.Name }}:
+            test_path: {{ $gt.Path }}
+            {{- if $gt.Args }}
+            test_args: {{ join $gt.Args " " }}
+            {{- end }}
+        {{- end }}
+    script_paths:
+        {{- range $j, $gt := $ft.Tests}}
+        - {{ $gt.Name }}:
+            test_path: {{ $gt.Path }}
+            {{- if $gt.Args }}
+            test_args: {{ join $gt.Args " " }}
+            {{- end }}
+			{{- if $gt.Patch }}
+            test_patch: {{ $gt.Patch }}
+			{{- end }}
+        {{- end }}
+    fp_post_tests:
+        {{- range $j, $gt := $ft.Posttests}}
+        - {{ $gt.Name }}:
+            test_path: {{ $gt.Path }}
+            {{- if $gt.Args }}
+            test_args: {{ join $gt.Args " " }}
+            {{- end }}
+        {{- end }}
+    smart_sanity_exclude: True
 {{ end }}
 `))
 )
