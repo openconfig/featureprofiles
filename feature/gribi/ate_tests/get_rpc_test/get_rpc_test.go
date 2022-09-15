1062d5cc620a980f34890c6c0a629f13e3fbdca3
moji <mbagherz@cisco.com>
2022-09-15 01:50:38 -0400

    roll back

--- feature/gribi/ate_tests/get_rpc_test/get_rpc_test.go
+++ feature/gribi/ate_tests/get_rpc_test/get_rpc_test.go
@@ -16,7 +16,6 @@ package get_rpc_test
 
 import (
 	"context"
-	"flag"
 	"testing"
 	"time"
 
@@ -83,7 +82,6 @@ var (
 
 var (
 	ateDstNetCIDR = []string{"198.51.100.0/26", "198.51.100.64/26", "198.51.100.128/26"}
-	protocolName  = flag.String("protocol_name", "STATIC", "Name of the protocol to configure static route")
 )
 
 const (
@@ -320,7 +318,7 @@ func testIPv4LeaderActive(ctx context.Context, t *testing.T, args *testArgs) {
 	// are returned, with no entry returned for 198.51.100.192/64.
 	dc := args.dut.Config()
 	ni := dc.NetworkInstance(*deviations.DefaultNetworkInstance).
-		Protocol(telemetry.PolicyTypes_INSTALL_PROTOCOL_TYPE_STATIC, *protocolName)
+		Protocol(telemetry.PolicyTypes_INSTALL_PROTOCOL_TYPE_STATIC, "STATIC")
 	static := &telemetry.NetworkInstance_Protocol_Static{
 		Prefix: ygot.String(staticCIDR),
 	}
