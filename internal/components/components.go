490e4026c590cce265f2e340192e4f29840f64f4
amrindrr <amrinderr@google.com>
2022-09-15 10:18:14 -0700

    Edit package name

--- internal/components/components.go
+++ internal/components/components.go
@@ -11,7 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package wbbtest
+
+// Package components provides functions to enumerate components from the device.
+package components
 
 import (
 	"regexp"
