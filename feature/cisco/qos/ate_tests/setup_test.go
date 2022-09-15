18cf4161d5a3de1f750da6646b85e19e3757dadb
Arvind Sankaran <saarvind@cisco.com>
2022-09-15 11:55:34 +0530

    changed-setup-qos

--- feature/cisco/qos/ate_tests/setup_test.go
+++ feature/cisco/qos/ate_tests/setup_test.go
@@ -10,7 +10,6 @@ import (
 	"github.com/openconfig/featureprofiles/feature/cisco/qos/setup"
 	"github.com/openconfig/ondatra"
 	oc "github.com/openconfig/ondatra/telemetry"
-	"github.com/openconfig/testt"
 )
 
 var (
