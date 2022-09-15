5b9bf8ab2571a42896b4a038756b240b9ff06de7
Octav <octavian.petre@keysight.com>
2022-09-15 10:33:05 +0000

    Added otg lag and lacp stats

--- internal/otgutils/print.go
+++ internal/otgutils/print.go
@@ -78,3 +78,71 @@ func LogPortMetrics(t testing.TB, otg *otg.OTG, c gosnappi.Config) {
 	out.WriteString("\n\n")
 	t.Log(out.String())
 }
+
+// LogLagMetrics is displaying otg lag stats.
+func LogLagMetrics(t testing.TB, otg *otg.OTG, c gosnappi.Config) {
+	t.Helper()
+	var out strings.Builder
+	out.WriteString("\nOTG LAG Metrics\n")
+	for i := 1; i <= 120; i++ {
+		out.WriteString("-")
+	}
+	out.WriteString("\n")
+	fmt.Fprintf(&out,
+		"%-25s%-15s%-20s\n",
+		"Name", "Oper Status", "Member Ports UP")
+	for _, lag := range c.Lags().Items() {
+		lagMetrics := otg.Telemetry().Lag(lag.Name()).Get(t)
+		operStatus := lagMetrics.GetOperStatus().String()
+		memberPortsUP := lagMetrics.GetCounters().GetMemberPortsUp()
+		// framesTx := lagMetrics.GetCounters().GetOutFrames()
+		// framesRx := lagMetrics.GetCounters().GetInFrames()
+		out.WriteString(fmt.Sprintf(
+			"%-25v%-15v%-20v\n",
+			lag.Name(), operStatus, memberPortsUP,
+		))
+	}
+	fmt.Fprintln(&out, strings.Repeat("-", 120))
+	out.WriteString("\n\n")
+	t.Log(out.String())
+}
+
+// LogLacpMetrics is displaying otg lacp stats.
+func LogLacpMetrics(t testing.TB, otg *otg.OTG, c gosnappi.Config) {
+	t.Helper()
+	var out strings.Builder
+	out.WriteString("\nOTG LACP Metrics\n")
+	for i := 1; i <= 120; i++ {
+		out.WriteString("-")
+	}
+	out.WriteString("\n")
+	fmt.Fprintf(&out,
+		"%-10s%-15s%-18s%-15s%-15s%-20s%-20s\n",
+		"LAG",
+		"Member Port",
+		"Synchronization",
+		"Collecting",
+		"Distributing",
+		"System Id",
+		"Partner Id")
+
+	for _, lag := range c.Lags().Items() {
+		lagPorts := lag.Ports().Items()
+		for _, lagPort := range lagPorts {
+			lacpMetric := otg.Telemetry().Lacp().LagMember(lagPort.PortName()).Get(t)
+			synchronization := lacpMetric.GetSynchronization().String()
+			collecting := lacpMetric.GetCollecting()
+			distributing := lacpMetric.GetDistributing()
+			systemId := lacpMetric.GetSystemId()
+			partnerId := lacpMetric.GetPartnerId()
+			out.WriteString(fmt.Sprintf(
+				"%-10v%-15v%-18v%-15v%-15v%-20v%-20v\n",
+				lag.Name(), lagPort.PortName(), synchronization, collecting, distributing, systemId, partnerId,
+			))
+
+		}
+	}
+	fmt.Fprintln(&out, strings.Repeat("-", 120))
+	out.WriteString("\n\n")
+	t.Log(out.String())
+}
