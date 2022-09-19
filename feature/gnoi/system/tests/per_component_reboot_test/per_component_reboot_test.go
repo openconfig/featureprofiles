dbb7ad6bd9410d596b112f1ff393f930bca3f327
Amrinder Randhawa <hackter1882@gmail.com>
2022-09-19 13:56:21 -0700

    Replace component related tests with helper function

--- feature/gnoi/system/tests/per_component_reboot_test/per_component_reboot_test.go
+++ feature/gnoi/system/tests/per_component_reboot_test/per_component_reboot_test.go
@@ -26,6 +26,7 @@ import (
 	"github.com/openconfig/ondatra/telemetry"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"github.com/openconfig/featureprofiles/internal/components"
 
 	spb "github.com/openconfig/gnoi/system"
 	tpb "github.com/openconfig/gnoi/types"
@@ -79,7 +80,7 @@ func TestMain(m *testing.M) {
 func TestStandbyControllerCardReboot(t *testing.T) {
 	dut := ondatra.DUT(t, "dut")
 
-	supervisors := findComponentsByType(t, dut, controlcardType)
+	supervisors := FindComponentsByType(t, dut, controlcardType)
 	t.Logf("Found supervisor list: %v", supervisors)
 	// Only perform the standby RP rebooting for the chassis with dual RPs/Supervisors.
 	if len(supervisors) != 2 {
@@ -123,7 +124,7 @@ func TestLinecardReboot(t *testing.T) {
 	const linecardBoottime = 10 * time.Minute
 	dut := ondatra.DUT(t, "dut")
 
-	lcs := findComponentsByType(t, dut, linecardType)
+	lcs := FindComponentsByType(t, dut, linecardType)
 	t.Logf("Found linecard list: %v", lcs)
 	if got := len(lcs); got == 0 {
 		t.Errorf("Get number of Linecards on %v: got %v, want > 0", dut.Model(), got)
@@ -211,30 +212,6 @@ func TestLinecardReboot(t *testing.T) {
 	}
 
 	// TODO: Check the line card uptime has been reset.
-}
-
-func findComponentsByType(t *testing.T, dut *ondatra.DUTDevice, cType telemetry.E_PlatformTypes_OPENCONFIG_HARDWARE_COMPONENT) []string {
-	components := dut.Telemetry().ComponentAny().Name().Get(t)
-	var s []string
-	for _, c := range components {
-		lookupType := dut.Telemetry().Component(c).Type().Lookup(t)
-		if !lookupType.IsPresent() {
-			t.Logf("Component %s type is not found", c)
-		} else {
-			componentType := lookupType.Val(t)
-			t.Logf("Component %s has type: %v", c, componentType)
-
-			switch v := componentType.(type) {
-			case telemetry.E_PlatformTypes_OPENCONFIG_HARDWARE_COMPONENT:
-				if v == cType {
-					s = append(s, c)
-				}
-			default:
-				t.Fatalf("Expected component type to be a hardware component, got (%T, %v)", componentType, componentType)
-			}
-		}
-	}
-	return s
 }
 
 func findStandbyRP(t *testing.T, dut *ondatra.DUTDevice, supervisors []string) (string, string) {
--- feature/gnoi/system/tests/supervisor_switchover_test/supervisor_switchover_test.go
+++ feature/gnoi/system/tests/supervisor_switchover_test/supervisor_switchover_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/openconfig/ondatra"
 	"github.com/openconfig/ondatra/telemetry"
 	"github.com/openconfig/testt"
+	"github.com/openconfig/featureprofiles/internal/components"
 
 	spb "github.com/openconfig/gnoi/system"
 	tpb "github.com/openconfig/gnoi/types"
@@ -70,7 +71,7 @@ func TestMain(m *testing.M) {
 func TestSupervisorSwitchover(t *testing.T) {
 	dut := ondatra.DUT(t, "dut")
 
-	supervisors := findComponentsByType(t, dut, controlcardType)
+	supervisors := FindComponentsByType(t, dut, controlcardType)
 	t.Logf("Found supervisor list: %v", supervisors)
 	// Only perform the switchover for the chassis with dual RPs/Supervisors.
 	if len(supervisors) != 2 {
@@ -179,29 +180,6 @@ func TestSupervisorSwitchover(t *testing.T) {
 	}
 }
 
-func findComponentsByType(t *testing.T, dut *ondatra.DUTDevice, cType telemetry.E_PlatformTypes_OPENCONFIG_HARDWARE_COMPONENT) []string {
-	components := dut.Telemetry().ComponentAny().Name().Get(t)
-	var s []string
-	for _, c := range components {
-		lookupType := dut.Telemetry().Component(c).Type().Lookup(t)
-		if !lookupType.IsPresent() {
-			t.Logf("Component %s type is not found", c)
-		} else {
-			componentType := lookupType.Val(t)
-			t.Logf("Component %s has type: %v", c, componentType)
-
-			switch v := componentType.(type) {
-			case telemetry.E_PlatformTypes_OPENCONFIG_HARDWARE_COMPONENT:
-				if v == cType {
-					s = append(s, c)
-				}
-			default:
-				t.Fatalf("Expected component type to be a hardware component, got (%T, %v)", componentType, componentType)
-			}
-		}
-	}
-	return s
-}
 
 func findStandbyRP(t *testing.T, dut *ondatra.DUTDevice, supervisors []string) (string, string) {
 	var activeRP, standbyRP string
