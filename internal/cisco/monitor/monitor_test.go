01c9b8b9070e3df735594d07a32d91009e78f707
moji <mbagherz@cisco.com>
2022-09-22 09:47:39 -0400

    enable seperate grpc connection for each sub request

--- internal/cisco/monitor/monitor_test.go
+++ internal/cisco/monitor/monitor_test.go
@@ -26,7 +26,7 @@ func (consumer *simpleConsumer) Process(datapoints []*gnmiutil.DataPoint) {
 	//fmt.Println("Processing events")
 	for _, data := range datapoints {
 		pathStr, _ := ygot.PathToString(data.Path)
-		fmt.Printf("Processing events at %v for path %s :  %v ",data.RecvTimestamp, pathStr, prototext.Format(data.Value) )
+		fmt.Printf("Processing events at %v for path %s :  %v ", data.RecvTimestamp, pathStr, prototext.Format(data.Value))
 		//fmt.Println(data.String())
 	}
 }
@@ -35,22 +35,23 @@ func TestStart(t *testing.T) {
 	dut := ondatra.DUT(t, "dut")
 	simpleConsumer := &simpleConsumer{}
 	monitor := GNMIMonior{
-		Paths:    []ygot.PathStruct{dut.Telemetry().Interface(dut.Port(t, "port1").Name()), 
-									dut.Telemetry().Interface(dut.Port(t, "port2").Name()),
-									dut.Telemetry().Interface(dut.Port(t, "port3").Name()),
-									//dut.Telemetry().ComponentAny(),
-					},
+		Paths: []ygot.PathStruct{dut.Telemetry().Interface(dut.Port(t, "port1").Name()),
+			dut.Telemetry().Interface(dut.Port(t, "port2").Name()),
+			dut.Telemetry().Interface(dut.Port(t, "port3").Name()),
+			dut.Telemetry().System(),
+			dut.Telemetry().ComponentAny(),
+		},
 		Consumer: simpleConsumer,
+		DUT:      dut,
 	}
 	ctx, cancelMonitors := context.WithCancel(context.Background())
 	//go monitor.Start(ctx, t, true, gpb.SubscriptionList_ONCE)
 	monitor.Start(ctx, t, true, gpb.SubscriptionList_STREAM)
 	// write other tests here rather. The monitor will recive and process all telemtry streams while the test is running
-	time.Sleep(120*time.Second)
+	time.Sleep(60 * time.Second)
 	cancelMonitors()
-}
+	time.Sleep(60 * time.Second)
 
-func TestStop(t *testing.T) {
 }
 
 /*writePath func write(path *gnmi.Path) {
