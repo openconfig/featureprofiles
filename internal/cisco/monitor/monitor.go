e687633c9443cce2617d33a3cd18e5e3725d20ee
moji <mbagherz@cisco.com>
2022-09-22 01:23:21 -0400

    first commit of infra for stress and load testing

---
+++ internal/cisco/monitor/monitor.go
@@ -0,0 +1,62 @@
+// Package config contains cisco specefic binding APIs to config a router using oc and text and cli.
+package config
+
+import (
+	"context"
+	"testing"
+
+	"github.com/openconfig/featureprofiles/internal/cisco/gnmiutil"
+	"github.com/openconfig/gnmi/proto/gnmi"
+	"github.com/openconfig/ygot/ygot"	
+	gpb "github.com/openconfig/gnmi/proto/gnmi"
+
+)
+
+type EventType int
+
+const (
+	Update EventType = iota + 1
+	Replace
+	Delete
+)
+
+type GNMIMonior struct {
+	Paths  []ygot.PathStruct
+	Consumer  gnmiutil.Consumer
+	//events map[string][]gnmiutil.DataPoint
+}
+
+func (monitor *GNMIMonior) Start(context context.Context, t testing.TB, shareStub bool, mode gpb.SubscriptionList_Mode) {
+	t.Helper()
+	for _, ygotPath := range monitor.Paths {
+		{
+			path, _, err := gnmiutil.ResolvePath(ygotPath)
+			if err != nil {
+				t.Fatalf("Could not start the monitor for path %v", ygotPath)
+			}
+			watcher, path, err := gnmiutil.Watch(context, ygotPath, []*gnmi.Path{path}, false, monitor.Consumer, mode)
+			if err != nil {
+				t.Fatalf("Could not start the watcher for path %v", path)
+			}
+			// this need to be run as part of monitor group
+			go watcher.Await(t)
+		} 
+	}
+}
+
+/*writePath func write(path *gnmi.Path) {
+	pathStr, err := ygot.PathToString(path)
+	if err != nil {
+		pathStr = prototext.Format(path)
+	}
+	fmt.Fprintf(&buf, "%s\n", pathStr)
+}
+
+writeVal := func(val *gnmi.TypedValue) {
+	switch v := val.Value.(type) {
+	case *gnmi.TypedValue_JsonIetfVal:
+		fmt.Fprintf(&buf, "%s\n", v.JsonIetfVal)
+	default:
+		fmt.Fprintf(&buf, "%s\n", prototext.Format(val))
+	}
+}*/
---
+++ internal/cisco/monitor/monitor_test.go
@@ -0,0 +1,71 @@
+// Package config contains cisco specefic binding APIs to config a router using oc and text and cli.
+package config
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/openconfig/featureprofiles/internal/cisco/gnmiutil"
+	"github.com/openconfig/featureprofiles/internal/fptest"
+	gpb "github.com/openconfig/gnmi/proto/gnmi"
+	"github.com/openconfig/ondatra"
+	"github.com/openconfig/ygot/ygot"
+	"google.golang.org/protobuf/encoding/prototext"
+)
+
+type simpleConsumer struct {
+}
+
+func TestMain(m *testing.M) {
+	fptest.RunTests(m)
+}
+
+func (consumer *simpleConsumer) Process(datapoints []*gnmiutil.DataPoint) {
+	//fmt.Println("Processing events")
+	for _, data := range datapoints {
+		pathStr, _ := ygot.PathToString(data.Path)
+		fmt.Printf("Processing events at %v for path %s :  %v ",data.RecvTimestamp, pathStr, prototext.Format(data.Value) )
+		//fmt.Println(data.String())
+	}
+}
+
+func TestStart(t *testing.T) {
+	dut := ondatra.DUT(t, "dut")
+	simpleConsumer := &simpleConsumer{}
+	monitor := GNMIMonior{
+		Paths:    []ygot.PathStruct{dut.Telemetry().Interface(dut.Port(t, "port1").Name()), 
+									dut.Telemetry().Interface(dut.Port(t, "port2").Name()),
+									dut.Telemetry().Interface(dut.Port(t, "port3").Name()),
+									//dut.Telemetry().ComponentAny(),
+					},
+		Consumer: simpleConsumer,
+	}
+	ctx, cancelMonitors := context.WithCancel(context.Background())
+	//go monitor.Start(ctx, t, true, gpb.SubscriptionList_ONCE)
+	monitor.Start(ctx, t, true, gpb.SubscriptionList_STREAM)
+	// write other tests here rather. The monitor will recive and process all telemtry streams while the test is running
+	time.Sleep(120*time.Second)
+	cancelMonitors()
+}
+
+func TestStop(t *testing.T) {
+}
+
+/*writePath func write(path *gnmi.Path) {
+	pathStr, err := ygot.PathToString(path)
+	if err != nil {
+		pathStr = prototext.Format(path)
+	}
+	fmt.Fprintf(&buf, "%s\n", pathStr)
+}
+
+writeVal := func(val *gnmi.TypedValue) {
+	switch v := val.Value.(type) {
+	case *gnmi.TypedValue_JsonIetfVal:
+		fmt.Fprintf(&buf, "%s\n", v.JsonIetfVal)
+	default:
+		fmt.Fprintf(&buf, "%s\n", prototext.Format(val))
+	}
+}*/
