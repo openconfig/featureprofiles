9fc976e28d437a5666af3b2f9bd14d947bbe750c
Karim Jahed <kjahed@cisco.com>
2022-09-27 18:19:57 -0400

    Combined with bgp and isis

--- internal/cisco/ha/confgen/confgen.go
+++ internal/cisco/ha/confgen/confgen.go
@@ -1,14 +1,28 @@
 package confgen
 
 import (
+	"encoding/json"
 	"log"
 
 	"github.com/google/go-jsonnet"
 )
 
-func GenerateConfig(file string) string {
+type bundle struct {
+	Id                int
+	Interfaces        []string
+	SubInterfaceRange []int
+}
+
+func GenerateConfig(bundles []bundle) string {
 	vm := jsonnet.MakeVM()
-	genConfig, err := vm.EvaluateFile(file)
+
+	if bundleJson, err := json.Marshal(bundles); err == nil {
+		vm.ExtCode("bundles", string(bundleJson))
+	} else {
+		log.Fatal(err)
+	}
+
+	genConfig, err := vm.EvaluateFile("templates/gnmi.jsonnet")
 	if err != nil {
 		log.Fatal(err)
 	}
--- internal/cisco/ha/confgen/confgen_test.go
+++ internal/cisco/ha/confgen/confgen_test.go
@@ -4,33 +4,57 @@ import (
 	"os"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
 	oc "github.com/openconfig/ondatra/telemetry"
 )
 
+var bundles = []bundle{
+	{
+		Id:                121,
+		Interfaces:        []string{"HundredGigE0/0/0/1"},
+		SubInterfaceRange: []int{100, 196},
+	},
+	{
+		Id:                122,
+		Interfaces:        []string{"HundredGigE0/0/0/3"},
+		SubInterfaceRange: []int{100, 196},
+	},
+	{
+		Id:                123,
+		Interfaces:        []string{"HundredGigE0/0/0/5"},
+		SubInterfaceRange: []int{100, 196},
+	},
+	{
+		Id:                124,
+		Interfaces:        []string{"HundredGigE0/3/0/0"},
+		SubInterfaceRange: []int{100, 196},
+	},
+	{
+		Id:                125,
+		Interfaces:        []string{"HundredGigE0/3/0/2"},
+		SubInterfaceRange: []int{100, 196},
+	},
+	{
+		Id:                126,
+		Interfaces:        []string{"HundredGigE0/3/0/3"},
+		SubInterfaceRange: []int{100, 196},
+	},
+	{
+		Id:                127,
+		Interfaces:        []string{"HundredGigE0/3/0/3"},
+		SubInterfaceRange: []int{100, 196},
+	},
+	{
+		Id: 128,
+	},
+}
+
 func TestGenerateConfig(t *testing.T) {
-	exepectedConf, err := os.ReadFile("testdata/gnmi_interfaces_want.json")
-	if err != nil {
-		t.Fatalf("Cannot load base config: %v", err)
-	}
-
-	want := oc.Device{}
-	if err := oc.Unmarshal([]byte(exepectedConf), &want); err != nil {
-		t.Fatalf(err.Error())
-	}
-
-	generatedConf := GenerateConfig("templates/gnmi_interfaces.jsonnet")
-	got := oc.Device{}
-	if err := oc.Unmarshal([]byte(generatedConf), &got); err != nil {
-		t.Fatalf(err.Error())
-	}
-
+	generatedConf := GenerateConfig(bundles)
 	if err := os.WriteFile("output.json", []byte(generatedConf), 0644); err != nil {
 		t.Fatalf(err.Error())
 	}
 
-	diff := cmp.Diff(want, got)
-	if len(diff) > 0 {
-		t.Logf("%s", diff)
+	if err := oc.Unmarshal([]byte(generatedConf), &oc.Device{}); err != nil {
+		t.Fatalf(err.Error())
 	}
 }
