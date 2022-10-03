6e3ec6b17a8bacc25a62ddef355edac0e8c738cf
nikiitp18 <nikhilkmr@google.com>
2022-10-03 10:23:54 +0000

    static

--- feature/staticroute/otg_tests/static_route_test.go
+++ feature/staticroute/otg_tests/static_route_test.go
@@ -30,7 +30,7 @@ func TestGNMI(t *testing.T) {
 
 	ifCfg1.GetOrCreateSubinterface(0).
 		GetOrCreateIpv4().
-		GetOrCreateAddress("192.0.2.11").
+		GetOrCreateAddress("192.0.2.10").
 		PrefixLength = ygot.Uint8(31)
 
 	ifCfg2 := &telemetry.Interface{
@@ -40,7 +40,7 @@ func TestGNMI(t *testing.T) {
 
 	ifCfg2.GetOrCreateSubinterface(0).
 		GetOrCreateIpv4().
-		GetOrCreateAddress("192.0.2.21").
+		GetOrCreateAddress("192.0.2.20").
 		PrefixLength = ygot.Uint8(31)
 
 	ifCfg3 := &telemetry.Interface{
@@ -50,7 +50,7 @@ func TestGNMI(t *testing.T) {
 
 	ifCfg3.GetOrCreateSubinterface(0).
 		GetOrCreateIpv4().
-		GetOrCreateAddress("192.0.2.31").
+		GetOrCreateAddress("192.0.2.30").
 		PrefixLength = ygot.Uint8(31)
 
 	ifCfg4 := &telemetry.Interface{
@@ -60,7 +60,7 @@ func TestGNMI(t *testing.T) {
 
 	ifCfg4.GetOrCreateSubinterface(0).
 		GetOrCreateIpv4().
-		GetOrCreateAddress("192.0.2.41").
+		GetOrCreateAddress("192.0.2.40").
 		PrefixLength = ygot.Uint8(31)
 
 	dut.Config().Interface(dp1.Name()).Replace(t, ifCfg1)
@@ -78,32 +78,35 @@ func TestGNMI(t *testing.T) {
 
 	//  Configure an ATE
 
-	// ate := ondatra.ATE(t, "ate")
-	// ap1 := ate.Port(t, "port1")
-	// ap2 := ate.Port(t, "port2")
+	ate := ondatra.ATE(t, "ate")
+	ap1 := ate.Port(t, "port1")
+	ap2 := ate.Port(t, "port2")
 	// ap3 := ate.Port(t, "port3")
 	// ap4 := ate.Port(t, "port4")
 
-	// top := ate.Topology().New()
+	top := ate.OTG().NewConfig(t)
 
-	// intf1 := top.AddInterface("intf1").WithPort(ap1)
-	// intf1.IPv4().
-	// 	WithAddress("192.0.2.12/31").
-	// 	WithDefaultGateway("192.0.2.11")
+	top.Ports().Add().SetName(ap1.ID())
+	i1 := top.Devices().Add().SetName(ap1.ID())
+	eth1 := i1.Ethernets().Add().SetName("src.Eth").SetPortName(i1.Name()).SetMac("02:1a:c0:00:02:01")
+	eth1.Ipv4Addresses().Add().SetName("src.IPv4").SetAddress("192.0.2.11").SetGateway("192.0.2.10").SetPrefix(int32(31))
 
-	// intf2 := top.AddInterface("intf2").WithPort(ap2)
-	// intf2.IPv4().
-	// 	WithAddress("192.0.2.22/31").
-	// 	WithDefaultGateway("192.0.2.21")
+	top.Ports().Add().SetName(ap2.ID())
+	i2 := top.Devices().Add().SetName(ap2.ID())
+	eth2 := i2.Ethernets().Add().SetName("dst1.Eth").SetPortName(i2.Name()).SetMac("02:1a:c0:00:02:02")
+	eth2.Ipv4Addresses().Add().SetName("dst1.IPv4").SetAddress("192.0.2.21").SetGateway("192.0.2.20").SetPrefix(int32(31))
 
-	// intf3 := top.AddInterface("intf3").WithPort(ap3)
-	// intf3.IPv4().
-	// 	WithAddress("192.0.2.32/31").
-	// 	WithDefaultGateway("192.0.2.31")
+	// top.Ports().Add().SetName(ap3.ID())
+	// i3 := top.Devices().Add().SetName(ap3.ID())
+	// eth3 := i3.Ethernets().Add().SetName("dst2.Eth").SetPortName(i3.Name()).SetMac("02:1a:c0:00:02:05")
+	// eth3.Ipv4Addresses().Add().SetName("dst2.IPv4").SetAddress("192.0.2.31").SetGateway("192.0.2.30").SetPrefix(int32(31))
 
-	// intf4 := top.AddInterface("intf4").WithPort(ap4)
-	// intf4.IPv4().
-	// 	WithAddress("192.0.2.42/31").
-	// 	WithDefaultGateway("192.0.2.41")
+	// top.Ports().Add().SetName(ap4.ID())
+	// i4 := top.Devices().Add().SetName(ap4.ID())
+	// eth4 := i4.Ethernets().Add().SetName("dst3.Eth").SetPortName(i4.Name()).SetMac("02:1a:c0:00:02:06")
+	// eth4.Ipv4Addresses().Add().SetName("dst3.IPv4").SetAddress("192.0.2.41").SetGateway("192.0.2.40").SetPrefix(int32(31))
+
+	ate.OTG().PushConfig(t, top)
+	ate.OTG().StartProtocols(t)
 
 }
