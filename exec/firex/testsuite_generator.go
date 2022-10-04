baf9795eada558f8316ee6a050e746d044ecdd99
Karim Jahed <kjahed@cisco.com>
2022-10-04 20:53:42 +0000

    Revert "added missing packages"
    This reverts commit 3f9d60ff01e4e1d93a54ec632a469878b4ebbea9.

--- exec/firex/testsuite_generator.go
+++ exec/firex/testsuite_generator.go
@@ -69,9 +69,6 @@ var (
     framework: b4_fp
     owners:
         - {{ $ft.Owner }}
-    pip_pkgs:
-        - ixnetwork-restpy
-        - protobuf
     {{- if eq $ft.Priority "low" }}
     priority: BCT
     {{- else if eq $ft.Priority "high" }}
