cc267d3849496b4f142a535f22c698fffabda1cb
Agnel Varghese <agvarghe@cisco.com>
2022-10-06 17:19:12 +0530

    Skip unsupported acl testcases

--- feature/cisco/acl/acl-set/acl-entries/acl-entry/input-interface/interface-ref/config_test.go
+++ feature/cisco/acl/acl-set/acl-entries/acl-entry/input-interface/interface-ref/config_test.go
@@ -15,6 +15,7 @@ func TestMain(m *testing.M) {
 }
 
 func setupAcl(t *testing.T, dut *ondatra.DUTDevice) *oc.Acl {
+        t.Skip()
 	bc := new(oc.Acl)
 	*bc = setup.BaseConfig()
 	setup.ResetStruct(bc, []string{"AclSet"})
@@ -31,6 +32,7 @@ func setupAcl(t *testing.T, dut *ondatra.DUTDevice) *oc.Acl {
 }
 
 func teardownAcl(t *testing.T, dut *ondatra.DUTDevice, baseConfig *oc.Acl) {
+        t.Skip()
 	dut.Config().Acl().Delete(t)
 }
 func TestInterface(t *testing.T) {
