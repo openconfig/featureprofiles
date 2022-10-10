deb129605129d3cb654ca9eab6d99321a4cdd341
nikiitp18 <nikhilkmr@google.com>
2022-10-10 11:38:55 +0000

    moved config attributes outside the main function

--- feature/staticroute/otg_tests/static_route_test.go
+++ feature/staticroute/otg_tests/static_route_test.go
@@ -13,43 +13,67 @@ import (
 
 )
 
+var configDUT[]attrs.Attributes = []attrs.Attributes{
+	{
+		Name: "port1",
+		IPv4: "192.0.2.12",
+		IPv4Len: 31,
+		Desc: "From Ixia",
+	},
+	{
+		Name: "port2",
+		IPv4: "192.0.2.22",
+		IPv4Len: 31,
+		Desc: "To Ixia",
+	},
+	{
+		Name: "port3",
+		IPv4: "192.0.2.32",
+		IPv4Len: 31,
+		Desc: "To Ixia",
+	},
+	{
+		Name: "port4",
+		IPv4: "192.0.2.42",
+		IPv4Len: 31,
+		Desc: "To Ixia",
+	},
+}
+
+var configATE[]attrs.Attributes = []attrs.Attributes {
+	{
+		Name: "src",
+		IPv4: "192.0.2.13",
+		MAC: "02:1a:c0:00:02:01",
+		IPv4Len: 31,
+	},
+	{
+		Name: "dst1",
+		IPv4: "192.0.2.23",
+		MAC: "02:1a:c0:00:02:02",
+		IPv4Len: 31,
+	},
+	{
+		Name: "dst2",
+		IPv4: "192.0.2.33",
+		MAC: "02:1a:c0:00:02:03",
+		IPv4Len: 31,
+	},
+	{
+		Name: "dst3",
+		IPv4: "192.0.2.43",
+		MAC: "02:1a:c0:00:02:04",
+		IPv4Len: 31,
+	},
+}
+
 func TestMain(m *testing.M) {
 	fptest.RunTests(m)
 }
 
 func TestGNMI(t *testing.T) {
 
-	// Configuration to be set in DUT Ports
-	configDUT := []attrs.Attributes{
-		{
-			Name: "port1",
-			IPv4: "192.0.2.12",
-			IPv4Len: 31,
-			Desc: "From Ixia",
-		},
-		{
-			Name: "port2",
-			IPv4: "192.0.2.22",
-			IPv4Len: 31,
-			Desc: "To Ixia",
-		},
-		{
-			Name: "port3",
-			IPv4: "192.0.2.32",
-			IPv4Len: 31,
-			Desc: "To Ixia",
-		},
-		{
-			Name: "port4",
-			IPv4: "192.0.2.42",
-			IPv4Len: 31,
-			Desc: "To Ixia",
-		},
-	}
-
-
 	// Configure a DUT
-
 	dut := ondatra.DUT(t, "dut")
 
 	for _, attributes := range configDUT { 
@@ -66,40 +90,11 @@ func TestGNMI(t *testing.T) {
 	}
 
 	Ni1 := &telemetry.NetworkInstance{}
-
 	Ni1.GetOrCreateProtocol(telemetry.PolicyTypes_INSTALL_PROTOCOL_TYPE_STATIC, "static").
 		GetOrCreateStatic("10.0.0.0/24").
 		GetOrCreateNextHop("h1").NextHop = telemetry.UnionString("192.0.2.23")
 
 	dut.Config().NetworkInstance("default").Update(t, Ni1)
-
-	// ATE Ports Config
-	configATE := []attrs.Attributes {
-		{
-			Name: "src",
-			IPv4: "192.0.2.13",
-			MAC: "02:1a:c0:00:02:01",
-			IPv4Len: 31,
-		},
-		{
-			Name: "dst1",
-			IPv4: "192.0.2.23",
-			MAC: "02:1a:c0:00:02:02",
-			IPv4Len: 31,
-		},
-		{
-			Name: "dst2",
-			IPv4: "192.0.2.33",
-			MAC: "02:1a:c0:00:02:03",
-			IPv4Len: 31,
-		},
-		{
-			Name: "dst3",
-			IPv4: "192.0.2.43",
-			MAC: "02:1a:c0:00:02:04",
-			IPv4Len: 31,
-		},
-	}
 
 	//  Configure an ATE
 
@@ -116,17 +111,16 @@ func TestGNMI(t *testing.T) {
 	ate.OTG().PushConfig(t,top)
 	ate.OTG().StartProtocols(t)	
 
-
 	flow := top.Flows().Add().SetName("Flow")
 	flow.TxRx().Device().SetTxNames([]string{"src.IPv4"}).SetRxNames([]string{"dst1.IPv4"})
 	flow.Metrics().Msg().Enable = ygot.Bool(true)
 	endpoint := flow.Packet().Add().Ipv4()
-	endpoint.Src().SetValue("192.0.2.13")
+	endpoint.Src().SetValue("192.0.2.23")
 	endpoint.Dst().SetValue("10.0.0.0")
 	ate.OTG().PushConfig(t,top)
 
 	ate.OTG().StartTraffic(t)
-	time.Sleep(5 * time.Second)
+	time.Sleep(2 * time.Second)
 	ate.OTG().StopTraffic(t)
 
 	fp := ate.OTG().Telemetry().Flow(flow.Name()).Get(t)
@@ -139,10 +133,8 @@ func TestGNMI(t *testing.T) {
 	t.Logf("outoctets are %d",outoctets)
 	t.Logf("inpkts are %d",inpkts)
 	t.Logf("outpkts are %d",outpkts)
-
 	lossPct := float32((outpkts - inpkts) * 100 / outpkts)
 	t.Logf("flow loss-pct %f", lossPct)
-
 	if lossPct>0 {
 		t.Errorf("Packets are not received. Got %f loss percentage and wanted 0",lossPct)
 	}
