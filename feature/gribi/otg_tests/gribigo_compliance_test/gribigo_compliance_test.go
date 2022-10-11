e47f4176fd6f12fbac32edfec2cc43cb8f6663d2
mojiiba <mbagherz@cisco.com>
2022-10-11 16:22:24 -0400

    add static protocol name for otg tests

--- feature/gribi/otg_tests/gribigo_compliance_test/gribigo_compliance_test.go
+++ feature/gribi/otg_tests/gribigo_compliance_test/gribigo_compliance_test.go
@@ -164,7 +164,7 @@ func configureDUT(t *testing.T, dut *ondatra.DUTDevice) {
 	d := &telemetry.Device{}
 	ni := d.GetOrCreateNetworkInstance(*nonDefaultNI)
 	ni.Type = telemetry.NetworkInstanceTypes_NETWORK_INSTANCE_TYPE_L3VRF
-	ni.GetOrCreateProtocol(telemetry.PolicyTypes_INSTALL_PROTOCOL_TYPE_STATIC, "static")
+	ni.GetOrCreateProtocol(telemetry.PolicyTypes_INSTALL_PROTOCOL_TYPE_STATIC, *deviations.DefaultProtocolStaticName)
 	dut.Config().NetworkInstance(*nonDefaultNI).Replace(t, ni)
 
 	nip := dut.Config().NetworkInstance(*nonDefaultNI)
