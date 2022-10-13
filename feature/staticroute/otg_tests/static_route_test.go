dea348d7326d402b663e79f3cd9dcd2a10f7234d
Rob Shakir <robjs@google.com>
2022-10-13 07:53:37 -0700

    Make some bugfixes to the test case code.
     * (M) feature/staticroute/otg_tests/static_route_test.go
       - Make sure to use the port name that is defined by the DUT rather
         than the one that we specified. Annoyingly "port1" maps to
         "Port-Channel1" in some vendors!
       - Add a loop of destinations with subtests to check that traffic that
         should NOT be forwarded is actually dropped.
       - Add an Ethernet header to resolve the OTG warning about the
         configured flow.

--- feature/staticroute/otg_tests/static_route_test.go
+++ feature/staticroute/otg_tests/static_route_test.go
@@ -1,6 +1,7 @@
 package gnmi_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -85,23 +86,23 @@ func TestStaticRouteSingleDestinationPort(t *testing.T) {
 	dut := ondatra.DUT(t, "dut")
 
 	for name, attributes := range dutPorts {
+		pn := dut.Port(t, name).Name()
 		ifCfg := &telemetry.Interface{
-			Name:        ygot.String(name),
+			Name:        ygot.String(pn),
 			Description: ygot.String(attributes.Desc),
 		}
-		// ifCfg.GetOrCreateSubinterface(0).GetOrCreateIpv4().Enabled = ygot.Bool(true)
+		ifCfg.GetOrCreateSubinterface(0).GetOrCreateIpv4().Enabled = ygot.Bool(true)
 		ifCfg.GetOrCreateSubinterface(0).
 			GetOrCreateIpv4().
 			GetOrCreateAddress(attributes.IPv4).PrefixLength = ygot.Uint8(attributes.IPv4Len)
 
-		dut.Config().Interface(name).Update(t, ifCfg)
+		dut.Config().Interface(pn).Update(t, ifCfg)
 	}
 
 	ni := &telemetry.NetworkInstance{}
 	ni.GetOrCreateProtocol(telemetry.PolicyTypes_INSTALL_PROTOCOL_TYPE_STATIC, "static").
 		GetOrCreateStatic("10.0.0.0/24").
 		GetOrCreateNextHop("h").NextHop = telemetry.UnionString(atePorts["port2"].IPv4)
-
 	dut.Config().NetworkInstance("default").Update(t, ni)
 
 	//  Configure an ATE
@@ -119,34 +120,54 @@ func TestStaticRouteSingleDestinationPort(t *testing.T) {
 	ate.OTG().PushConfig(t, top)
 	ate.OTG().StartProtocols(t)
 
-	// Flow
-	flow := top.Flows().Add().SetName("Flow")
-	flow.TxRx().Device().SetTxNames([]string{"port1.IPv4"}).SetRxNames([]string{"port2.IPv4"})
-	flow.Metrics().SetEnable(true)
-	endpoint := flow.Packet().Add().Ipv4()
-	endpoint.Src().SetValue(atePorts["port1"].IPv4)
-	endpoint.Dst().SetValue("1.0.0.25")
-	ate.OTG().PushConfig(t, top)
+	// destinations specifies an IP destination and whether the traffic should be
+	// lost.
+	destinations := map[string]bool{
+		"10.0.0.1":      false,
+		"1.2.3.4":       true,
+		"10.0.0.42":     false,
+		"100.100.64.24": true,
+	}
 
-	ate.OTG().StartTraffic(t)
-	time.Sleep(180 * time.Second)
-	ate.OTG().StopTraffic(t)
+	for dst, want := range destinations {
+		t.Run(fmt.Sprintf("dst_%s", dst), func(t *testing.T) {
+			// Reset the flows to remove any previous ones.
+			top.Flows().Clear().Items()
+			// Configure the flow.
+			flow := top.Flows().Add().SetName("Flow")
+			flow.TxRx().Device().SetTxNames([]string{"port1.IPv4"}).SetRxNames([]string{"port2.IPv4"})
+			flow.Metrics().SetEnable(true)
 
-	fp := ate.OTG().Telemetry().Flow(flow.Name()).Get(t)
-	fpc := fp.GetCounters()
+			// Add an Ethernet header with the source address of the ATE.
+			e1 := flow.Packet().Add().Ethernet()
+			e1.Src().SetValue(atePorts["port1"].MAC)
 
-	outoctets := fpc.GetOutOctets()
-	outpkts := fpc.GetOutPkts()
-	inpkts := fpc.GetInPkts()
+			endpoint := flow.Packet().Add().Ipv4()
+			endpoint.Src().SetValue(atePorts["port1"].IPv4)
+			endpoint.Dst().SetValue(dst)
+			ate.OTG().PushConfig(t, top)
 
-	t.Logf("IPv4 Flow Details")
-	t.Logf("outoctets are %d", outoctets)
-	t.Logf("inpkts are %d", inpkts)
-	t.Logf("outpkts are %d", outpkts)
-	lossPct := float32((outpkts - inpkts) * 100 / outpkts)
-	t.Logf("flow loss-pct %f", lossPct)
-	if lossPct > 0 {
-		t.Errorf("Packets are not received. Got %f loss percentage and wanted 0", lossPct)
+			ate.OTG().StartTraffic(t)
+			time.Sleep(10 * time.Second)
+			ate.OTG().StopTraffic(t)
+
+			fp := ate.OTG().Telemetry().Flow(flow.Name()).Get(t)
+			fpc := fp.GetCounters()
+
+			outoctets := fpc.GetOutOctets()
+			outpkts := fpc.GetOutPkts()
+			inpkts := fpc.GetInPkts()
+
+			t.Logf("Destination: %s, IPv4 Flow Details", dst)
+			t.Logf("outoctets are %d", outoctets)
+			t.Logf("inpkts are %d", inpkts)
+			t.Logf("outpkts are %d", outpkts)
+			lossPct := float32((outpkts - inpkts) * 100 / outpkts)
+			t.Logf("flow loss-pct %f", lossPct)
+			if (lossPct > 0) != want {
+				t.Fatalf("Destination: %s, got loss percentage: %2f, want loss? %v", dst, lossPct, want)
+			}
+		})
 	}
 
 }
