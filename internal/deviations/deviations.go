04347e12238a25562d6073cf1211fd178b88d9f9
mojiiba <mbagherz@cisco.com>
2022-10-13 02:24:02 -0400

    add deviation for counters leaves that are not updated

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -70,6 +70,8 @@ var (
 	InterfaceEnabled = flag.Bool("deviation_interface_enabled", false,
 		"Device requires interface enabled leaf booleans to be explicitly set to true.  Full OpenConfig compliant devices should pass both with and without this deviation.")
 
+	CounterLeavesNotUpdated = flag.Bool("deviation_counters_leaves_notupdated", false, "Device updates counters when they are queried at the container level.")
+
 	AggregateAtomicUpdate = flag.Bool("deviation_aggregate_atomic_update", false,
 		"Device requires that aggregate Port-Channel and its members be defined in a single gNMI Update transaction at /interfaces; otherwise lag-type will be dropped, and no member can be added to the aggregate.  Full OpenConfig compliant devices should pass both with and without this deviation.")
 
