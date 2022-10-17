92c8c8767e70218cb5cf81f20d9a26e363b192f1
Sahana B Hubballi <sahubbal@cisco.com>
2022-10-17 16:29:38 +0530

    trail

--- feature/gribi/ate_tests/cisco/backup_nh/backup_nh_test.go
+++ feature/gribi/ate_tests/cisco/backup_nh/backup_nh_test.go
@@ -112,9 +112,22 @@ func testBackupToDrop(ctx context.Context, t *testing.T, args *testArgs) {
 	args.client.AddNH(t, 1100, atePort3.IPv4, *ciscoFlags.DefaultNetworkInstance, "", "Bundle-Ether122", false, ciscoFlags.GRIBIChecks)
 	args.client.AddNH(t, 1200, atePort4.IPv4, *ciscoFlags.DefaultNetworkInstance, "", "Bundle-Ether123", false, ciscoFlags.GRIBIChecks)
 	args.client.AddNH(t, 1300, atePort5.IPv4, *ciscoFlags.DefaultNetworkInstance, "", "Bundle-Ether124", false, ciscoFlags.GRIBIChecks)
+	dut := ondatra.DUT(t, "dut")
+
+	t.Log("#################################################################################################")
+	t.Log("TRIED INJECTING FAULT ")
+	t.Log("#################################################################################################")
+	//dut.RawAPIs().CLI(t).SendCommand(ctx, "run ssh 172.0.0.1 /pkg/bin/fim_cli -c ofa_la_srv -a 37:-1")
 	args.client.AddNHG(t, 1000, 0, map[uint64]uint64{1000: 50, 1100: 30, 1200: 15, 1300: 5}, *ciscoFlags.DefaultNetworkInstance, false, ciscoFlags.GRIBIChecks)
+	t.Log("#################################################################################################")
+	t.Log("EXECUTION AFTER INJECTING FAULT ")
+	dut.RawAPIs().CLI(t).SendCommand(ctx, "run ssh 172.0.0.1 /pkg/bin/fim_cli -c ofa_la_srv -a 3:3482356236")
+
 	args.client.AddIPv4(t, "192.0.2.40/32", 1000, *ciscoFlags.DefaultNetworkInstance, "", false, ciscoFlags.GRIBIChecks)
 
+	//t.Log("#################################################################################################")
+	//t.Log("EXECUTION AFTER INJECTING FAULT ")
+	//t.Log("#################################################################################################")
 	args.client.AddNH(t, 2000, atePort6.IPv4, *ciscoFlags.DefaultNetworkInstance, "", "Bundle-Ether125", false, ciscoFlags.GRIBIChecks)
 	args.client.AddNH(t, 2100, atePort7.IPv4, *ciscoFlags.DefaultNetworkInstance, "", "Bundle-Ether126", false, ciscoFlags.GRIBIChecks)
 	args.client.AddNHG(t, 2000, 0, map[uint64]uint64{2000: 60, 2100: 40}, *ciscoFlags.DefaultNetworkInstance, false, ciscoFlags.GRIBIChecks)
