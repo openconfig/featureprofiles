2dbc837b1ff787b57c67891104266e7166af6975
Likai Liu <liulk@likai.org>
2022-10-21 18:54:13 -0400

    rundata collects runtime data about the test environment

---
+++ internal/rundata/rundata_test.go
@@ -0,0 +1,100 @@
+package rundata
+
+import (
+	"testing"
+
+	"github.com/openconfig/ondatra/binding"
+)
+
+func TestTopology(t *testing.T) {
+	cases := []struct {
+		name string
+		resv *binding.Reservation
+		want string
+	}{{
+		name: "empty",
+		resv: &binding.Reservation{},
+		want: "",
+	}, {
+		name: "atedut_12",
+		resv: &binding.Reservation{
+			ATEs: map[string]binding.ATE{
+				"ate": &binding.AbstractATE{
+					Dims: &binding.Dims{
+						Ports: map[string]*binding.Port{
+							"port1":  nil,
+							"port2":  nil,
+							"port3":  nil,
+							"port4":  nil,
+							"port5":  nil,
+							"port6":  nil,
+							"port7":  nil,
+							"port8":  nil,
+							"port9":  nil,
+							"port10": nil,
+							"port11": nil,
+							"port12": nil,
+						},
+					},
+				},
+			},
+			DUTs: map[string]binding.DUT{
+				"dut": &binding.AbstractDUT{
+					Dims: &binding.Dims{
+						Ports: map[string]*binding.Port{
+							"port1":  nil,
+							"port2":  nil,
+							"port3":  nil,
+							"port4":  nil,
+							"port5":  nil,
+							"port6":  nil,
+							"port7":  nil,
+							"port8":  nil,
+							"port9":  nil,
+							"port10": nil,
+							"port11": nil,
+							"port12": nil,
+						},
+					},
+				},
+			},
+		},
+		want: "ate:12,dut:12",
+	}, {
+		name: "dutdut",
+		resv: &binding.Reservation{
+			DUTs: map[string]binding.DUT{
+				"dut1": &binding.AbstractDUT{
+					Dims: &binding.Dims{
+						Ports: map[string]*binding.Port{
+							"port1": nil,
+							"port2": nil,
+							"port3": nil,
+							"port4": nil,
+						},
+					},
+				},
+				"dut2": &binding.AbstractDUT{
+					Dims: &binding.Dims{
+						Ports: map[string]*binding.Port{
+							"port1": nil,
+							"port2": nil,
+							"port3": nil,
+							"port4": nil,
+						},
+					},
+				},
+			},
+		},
+		want: "dut1:4,dut2:4",
+	}}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := topology(c.resv)
+			if got != c.want {
+				t.Errorf("topology got %q, want %q", got, c.want)
+			}
+		})
+	}
+}
