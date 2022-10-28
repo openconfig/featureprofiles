d855bf53a495253f8f18f892118065ae44a01f40
Raghavendran (Rocky) Nagarajan <nraghavendran@google.com>
2022-10-28 21:20:53 +0000

    gnoi 5.3 test case commit - adding review comments from PR

--- feature/experimental/gnoi/copying_debug_files_test/copying_debug_files_test.go
+++ feature/experimental/gnoi/copying_debug_files_test/copying_debug_files_test.go
@@ -44,8 +44,8 @@ func TestMain(m *testing.M) {
 //   DUT
 //
 // Test notes:
-//. Note: Initiating checkin to experimental
-//  - KillProcess system call is used to kill bgp process. 
+// Note: Initiating checkin to experimental
+//  - KillProcess system call is used to kill bgp process.
 //  - The healthz call needs to be modified to reflect the right component and its path.
 //
 //  - gnoi operation commands can be sent and tested using CLI command grpcurl.
@@ -76,11 +76,20 @@ func TestCopyingDebugFiles(t *testing.T) {
 	time.Sleep(60 * time.Second)
 
 	// construct struct with values for GetRequest for healthz call
-	req := &hpb.GetRequest{
+	req = &hpb.GetRequest{
 		Path: &tpb.Path{
 			Elem: []*tpb.PathElem{
 				&tpb.PathElem{
-					Name: "components/component/state/software-version",
+					Name: "openconfig-platform",
+				},
+				&tpb.PathElem{
+					Name: "components",
+				},
+				&tpb.PathElem{
+					Name: "component",
+				},
+				&tpb.PathElem{
+					Name: "chassis",
 				},
 			},
 		},
