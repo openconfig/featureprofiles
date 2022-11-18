63a442381a22cd9c52224d5924f0ddaac7ffff05
Likai Liu <liulk@google.com>
2022-11-18 17:36:57 -0500

    collect rundata from DUT (#798)
    * collect rundata from DUT
    
    * add a device test for rundata collection
    
    * hook up static binding to collect rundata
    
    * fix typo
    
    * suppress staticcheck warning

--- internal/rundata/rundata_test.go
+++ internal/rundata/rundata_test.go
@@ -15,6 +15,7 @@
 package rundata
 
 import (
+	"context"
 	"testing"
 
 	"github.com/openconfig/ondatra/binding"
@@ -117,7 +118,7 @@ func TestProperties(t *testing.T) {
 	TestPlanID = "UnitTest-1.1"
 	*knownIssueURL = "https://example.com"
 
-	got := Properties(&binding.Reservation{})
+	got := Properties(context.Background(), &binding.Reservation{})
 	t.Log(got)
 
 	if got, want := got["test.plan_id"], TestPlanID; got != want {
