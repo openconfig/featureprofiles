3bedcc4295c73872e2f44fd25ead8c917a35c4ef
Mihir Pitale <116768161+mihirpitale-googler@users.noreply.github.com>
2022-11-21 18:32:23 -0800

    Update singleton_test.go

--- feature/interface/singleton/ate_tests/singleton_test/singleton_test.go
+++ feature/interface/singleton/ate_tests/singleton_test/singleton_test.go
@@ -25,10 +25,10 @@ import (
 	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/featureprofiles/internal/fptest"
 	"github.com/openconfig/ondatra"
-	"github.com/openconfig/ygot/ygot"
-	"github.com/openconfig/ondatra/telemetry"
 	"github.com/openconfig/ondatra/gnmi"
 	"github.com/openconfig/ondatra/gnmi/oc"
+	"github.com/openconfig/ondatra/telemetry"
+	"github.com/openconfig/ygot/ygot"
 )
 
 func TestMain(m *testing.M) {
