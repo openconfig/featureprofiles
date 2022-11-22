af8301b14d293bb6a6ccf984962725ebddae00be
sezhang2 <sezhang@google.com>
2022-11-21 17:24:04 -0800

    Update qos_policy_config_test.go
    pkg name is changed to qos_policy_config_test

--- feature/experimental/qos/ate_tests/qos_policy_config_test/qos_policy_config_test.go
+++ feature/experimental/qos/ate_tests/qos_policy_config_test/qos_policy_config_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package qos_output_queue_counters_test
+package qos_policy_config_test
 
 import (
 	"testing"
