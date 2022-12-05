eae04d4b3aca58b85d5b1f224777c3a549247001
sezhang2 <sezhang@google.com>
2022-12-05 10:23:17 -0800

    Update qos_policy_config_test.go
    Added experimental qos scheduler-policies config

--- feature/experimental/qos/ate_tests/qos_policy_config_test/qos_policy_config_test.go
+++ feature/experimental/qos/ate_tests/qos_policy_config_test/qos_policy_config_test.go
@@ -45,6 +45,14 @@ func TestMain(m *testing.M) {
 //    - /qos/forwarding-groups/forwarding-group/config/name
 //    - /qos/forwarding-groups/forwarding-group/config/output-queue
 //    - /qos/queues/queue/config/name
+//  - scheduler-policies:
+//    - /qos/scheduler-policies/scheduler-policy/config/name
+//    - /qos/scheduler-policies/scheduler-policy/schedulers/scheduler/config/priority
+//    - /qos/scheduler-policies/scheduler-policy/schedulers/scheduler/config/sequence
+//    - /qos/scheduler-policies/scheduler-policy/schedulers/scheduler/inputs/input/config/id
+//    - /qos/scheduler-policies/scheduler-policy/schedulers/scheduler/inputs/input/config/input-type
+//    - /qos/scheduler-policies/scheduler-policy/schedulers/scheduler/inputs/input/config/queue
+//    - /qos/scheduler-policies/scheduler-policy/schedulers/scheduler/inputs/input/config/weight
 //
 // Topology:
 //   ate:port1 <--> port1:dut:port2 <--> ate:port2
@@ -58,7 +66,7 @@ func TestMain(m *testing.M) {
 //     - https://github.com/karimra/gnmic/blob/main/README.md
 //
 
-func TestQoSPolicyConfig(t *testing.T) {
+func TestQoSClassifierConfig(t *testing.T) {
 	dut := ondatra.DUT(t, "dut")
 	d := &oc.Root{}
 	q := d.GetOrCreateQos()
@@ -284,4 +292,104 @@ func TestQoSForwadingGroupsConfig(t *testing.T) {
 	gnmi.Replace(t, dut, gnmi.OC().Qos().Config(), q)
 	// qosfwdGroups := gnmi.GetAll(t, dut, gnmi.OC().Qos().ForwardingGroupAny().Name().Config())
 	// t.Logf("qosfwdGroups from telmetry: %v", qosfwdGroups)
+}
+
+func TestSchedulerPoliciesConfig(t *testing.T) {
+	dut := ondatra.DUT(t, "dut")
+	d := &oc.Root{}
+	q := d.GetOrCreateQos()
+
+	cases := []struct {
+		desc         string
+		sequence     uint32
+		priority     oc.E_Scheduler_Priority
+		inputID      string
+		inputType    oc.E_Input_InputType
+		weight       uint64
+		queueName    string
+		targetGrpoup string
+	}{{
+		desc:         "scheduler-policy-BE1",
+		sequence:     uint32(1),
+		priority:     oc.Scheduler_Priority_UNSET,
+		inputID:      "BE1",
+		inputType:    oc.Input_InputType_QUEUE,
+		weight:       uint64(1),
+		queueName:    "BE1",
+		targetGrpoup: "target-group-BE1",
+	}, {
+		desc:         "scheduler-policy-BE0",
+		sequence:     uint32(1),
+		priority:     oc.Scheduler_Priority_UNSET,
+		inputID:      "BE0",
+		inputType:    oc.Input_InputType_QUEUE,
+		weight:       uint64(2),
+		queueName:    "BE0",
+		targetGrpoup: "target-group-BE0",
+	}, {
+		desc:         "scheduler-policy-AF1",
+		sequence:     uint32(1),
+		priority:     oc.Scheduler_Priority_UNSET,
+		inputID:      "AF1",
+		inputType:    oc.Input_InputType_QUEUE,
+		weight:       uint64(4),
+		queueName:    "AF1",
+		targetGrpoup: "target-group-AF1",
+	}, {
+		desc:         "scheduler-policy-AF2",
+		sequence:     uint32(1),
+		priority:     oc.Scheduler_Priority_UNSET,
+		inputID:      "AF2",
+		inputType:    oc.Input_InputType_QUEUE,
+		weight:       uint64(8),
+		queueName:    "AF2",
+		targetGrpoup: "target-group-AF2",
+	}, {
+		desc:         "scheduler-policy-AF3",
+		sequence:     uint32(1),
+		priority:     oc.Scheduler_Priority_UNSET,
+		inputID:      "AF3",
+		inputType:    oc.Input_InputType_QUEUE,
+		weight:       uint64(16),
+		queueName:    "AF3",
+		targetGrpoup: "target-group-AF3",
+	}, {
+		desc:         "scheduler-policy-AF4",
+		sequence:     uint32(0),
+		priority:     oc.Scheduler_Priority_STRICT,
+		inputID:      "AF4",
+		inputType:    oc.Input_InputType_QUEUE,
+		weight:       uint64(100),
+		queueName:    "AF4",
+		targetGrpoup: "target-group-AF4",
+	}, {
+		desc:         "scheduler-policy-NC1",
+		sequence:     uint32(0),
+		priority:     oc.Scheduler_Priority_STRICT,
+		inputID:      "NC1",
+		inputType:    oc.Input_InputType_QUEUE,
+		weight:       uint64(200),
+		queueName:    "NC1",
+		targetGrpoup: "target-group-NC1",
+	}}
+
+	schedulerPolicy := q.GetOrCreateSchedulerPolicy("scheduler")
+	schedulerPolicy.SetName("scheduler")
+	t.Logf("qos scheduler policies config cases: %v", cases)
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+			s := schedulerPolicy.GetOrCreateScheduler(tc.sequence)
+			s.SetSequence(tc.sequence)
+			s.SetPriority(tc.priority)
+			input := s.GetOrCreateInput(tc.inputID)
+			input.SetId(tc.inputID)
+			input.SetInputType(tc.inputType)
+			input.SetQueue(tc.queueName)
+			input.SetWeight(tc.weight)
+		})
+	}
+
+	gnmi.Replace(t, dut, gnmi.OC().Qos().Config(), q)
+	// qosSchedulerPolicies := gnmi.GetAll(t, dut, gnmi.OC().Qos().SchedulerPolicyAny().Name().Config())
+	// t.Logf("qosSchedulerPolicies from telmetry: %v", qosSchedulerPolicies)
 }
