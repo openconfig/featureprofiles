b5d7dfcd3b37a7a4f3fc47b74e40a4892e32837f
Karim Jahed (kjahed) <kjahed@cisco.com>
2022-12-07 15:34:49 -0500

    Update runner.py

--- exec/firex/plugins/runner.py
+++ exec/firex/plugins/runner.py
@@ -128,11 +128,15 @@ def BringupTestbed(self, ws, images = None,
             file=ondatra_testbed_path)
     else:
         if ondatra_binding_path[0] != '/':
-            ondatra_binding_path = os.path.join(exec_repo_dir, ondatra_binding_path)
+            shutil.copyfile(os.path.join(exec_repo_dir, ondatra_binding_path), 
+                os.path.join(ws, 'ondatra.binding'))
+            ondatra_binding_path = os.path.join(ws, 'ondatra.binding')
 
         if ondatra_testbed_path[0] != '/':
-            ondatra_testbed_path = os.path.join(exec_repo_dir, ondatra_testbed_path)
-
+            shutil.copyfile(os.path.join(exec_repo_dir, ondatra_testbed_path), 
+                os.path.join(ws, 'ondatra.testbed'))
+            ondatra_testbed_path = os.path.join(ws, 'ondatra.testbed')
+        
         if base_conf_path and len(base_conf_path) > 0:
             if base_conf_path[0] != '/':
                 base_conf_path = os.path.join(exec_repo_dir, base_conf_path)
