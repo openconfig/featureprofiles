6319b9cf4c4842bc8c7db9582bc0e080e7701123
Hobbydos <char0772@gmail.com>
2022-12-07 15:42:34 -0800

    added deviations to gNOI-5.1

--- feature/gnoi/system/tests/ping_test/ping_test.go
+++ feature/gnoi/system/tests/ping_test/ping_test.go
@@ -19,6 +19,7 @@ import (
 	"io"
 	"testing"
 
+	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/featureprofiles/internal/fptest"
 	spb "github.com/openconfig/gnoi/system"
 	tpb "github.com/openconfig/gnoi/types"
@@ -110,7 +111,11 @@ func TestGNOIPing(t *testing.T) {
 	if len(ipv6Addrs) == 0 {
 		t.Fatalf("Failed to get a valid IPv6 loopback address: %+v", ipv6Addrs)
 	}
-
+	if *deviations.ExplicitInterfaceInDefaultVRF {
+		if dut.Vendor() == ondatra.NOKIA {
+			fptest.AssignToNetworkInstance(t, dut, lbIntf, "mgmt", 0)
+		}
+	}
 	commonExpectedIPv4Reply := &spb.PingResponse{
 		Source:   ipv4Addrs[0].GetIp(),
 		Time:     minimumPingTime,
