933fbe63d1d27cc74b87393d75810187ef91fc4e
avaneesh90 <103159151+avaneesh90@users.noreply.github.com>
2022-12-23 14:10:20 +0530

    Update optics_power_and_bias_current_test.go

--- feature/platform/tests/optics_power_and_bias_current_test/optics_power_and_bias_current_test.go
+++ feature/platform/tests/optics_power_and_bias_current_test/optics_power_and_bias_current_test.go
@@ -29,7 +29,7 @@ import (
 )
 
 const (
-	ethernetCsmacd         = telemetry.IETFInterfaces_InterfaceType_ethernetCsmacd
+	ethernetCsmacd         = oc.IETFInterfaces_InterfaceType_ethernetCsmacd
 	transceiverType        = oc.PlatformTypes_OPENCONFIG_HARDWARE_COMPONENT_TRANSCEIVER
 	sleepDuration          = time.Minute
 	minOpticsPower         = -30.0
