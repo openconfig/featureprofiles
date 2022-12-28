b4effa0932a6bd7453e990fd6e8a17a90da86edd
Yan Chen <vcheny@outlook.com>
2022-12-28 18:14:40 -0500

    Fix another two import package names.

--- feature/networkinstance/ate_tests/defaults_test/defaults_test.go
+++ feature/networkinstance/ate_tests/defaults_test/defaults_test.go
@@ -24,8 +24,8 @@ import (
 	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/featureprofiles/internal/fptest"
 	"github.com/openconfig/ondatra"
-	"github.com/openconfig/ondatra/gnmi/gnmi"
-	"github.com/openconfig/ondatra/gnmi/oc/oc"
+	"github.com/openconfig/ondatra/gnmi"
+	"github.com/openconfig/ondatra/gnmi/oc"
 	"github.com/openconfig/ygot/ygot"
 )
 
