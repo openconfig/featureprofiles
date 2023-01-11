86803c78c5214aba671ded4c7fa695814fd870a2
averasko <averasko@users.noreply.github.com>
2023-01-10 17:06:39 -0800

    Adding 2 tests to cover a more elaborated (QoS case) for the simultaneous CLI + OC in SetRequest().

--- feature/system/gnmi/cliorigin/ate_tests/mixed_oc_cli_origin_support_test/mixed_oc_cli_origin_support_test.go
+++ feature/system/gnmi/cliorigin/ate_tests/mixed_oc_cli_origin_support_test/mixed_oc_cli_origin_support_test.go
@@ -24,6 +24,7 @@ import (
 	gpb "github.com/openconfig/gnmi/proto/gnmi"
 	"github.com/openconfig/ondatra"
 	"github.com/openconfig/ondatra/gnmi"
+	"github.com/openconfig/ondatra/gnmi/oc"
 	"github.com/openconfig/ygnmi/ygnmi"
 	"github.com/openconfig/ygot/ygot"
 )
@@ -58,6 +59,21 @@ func buildOCUpdate(path *gpb.Path, value string) *gpb.Update {
 		path.Origin = "openconfig"
 	}
 	jsonVal, _ := ygot.Marshal7951(ygot.String(value))
+	update := &gpb.Update{
+		Path: path,
+		Val:  &gpb.TypedValue{Value: &gpb.TypedValue_JsonIetfVal{JsonIetfVal: jsonVal}},
+	}
+	return update
+}
+
+func buildOCUpdateFromGo(t *testing.T, path *gpb.Path, goStruct ygot.ValidatedGoStruct) *gpb.Update {
+	if len(path.GetElem()) == 0 || path.GetElem()[0].GetName() != "meta" {
+		path.Origin = "openconfig"
+	}
+	jsonVal, err := ygot.Marshal7951(goStruct, ygot.JSONIndent("  "), &ygot.RFC7951JSONConfig{AppendModuleName: true, PreferShadowPath: true})
+	if err != nil {
+		t.Fatalf("Error while marshalling a goStruct: %v", err)
+	}
 	update := &gpb.Update{
 		Path: path,
 		Val:  &gpb.TypedValue{Value: &gpb.TypedValue_JsonIetfVal{JsonIetfVal: jsonVal}},
@@ -117,9 +133,9 @@ func TestCLIBeforeOpenConfig(t *testing.T) {
 	t.Log("gnmiClient Set Response for CLI and OpenConfig modelled config")
 	t.Log(response)
 
-	// Validate that DUT port-1 description is `"from oc"`
+	// Validate that DUT port-1 description is `"from cli"`
 	got := gnmi.Get(t, dut, gnmi.OC().Interface(dp.Name()).Description().State())
-	want := "from oc"
+	want := "from cli"
 	if got != want {
 		t.Errorf("Get(DUT port description): got %v, want %v", got, want)
 	}
@@ -215,5 +231,110 @@ func TestMixedOriginOCCLIConfig(t *testing.T) {
 	if got := gnmi.Get(t, dut, gnmi.OC().Interface(dp2.Name()).Description().State()); got != "foo2" {
 		t.Errorf("Get(DUT port description): got %v, want %v", got, "foo2")
 	}
+}
 
+type testCase struct {
+	cmdCfg           string
+	queueName        string
+	forwardGroupName string
+	reorderFunc      func(input []*gpb.Update) []*gpb.Update
+	okToFail         bool
+}
+
+func testQoSWithCLIAndOCUpdates(t *testing.T, tCase testCase) {
+	dut := ondatra.DUT(t, "dut")
+
+	// We use ARISTA-specific command-line commands here. Other vendor devices won't accept it.
+	if dut.Vendor() != ondatra.ARISTA {
+		t.Skipf("Unsupported vendor device: got %v, expected %v", dut.Vendor(), ondatra.ARISTA)
+	}
+
+	t.Logf("Generated an update for the CLI config:\n%s", tCase.cmdCfg)
+
+	qosPath := gnmi.OC().Qos().Config().PathStruct()
+	resolvedQosPath, _, err := ygnmi.ResolvePath(qosPath)
+	if err != nil {
+		t.Fatalf("Could not resolve QoS path: %v", err)
+	}
+
+	// Make sure the current config does not contain new data already.
+	if existingQueue := gnmi.LookupConfig(t, dut, gnmi.OC().Qos().Queue(tCase.queueName).Config()); existingQueue.IsPresent() {
+		t.Errorf("Detected an existing %v queue. This is unexpected.", tCase.queueName)
+	}
+
+	// Creating OC addition to the config.
+	r := &oc.Root{}
+	qos := r.GetOrCreateQos()
+	qos.GetOrCreateQueue(tCase.queueName)
+	fg := qos.GetOrCreateForwardingGroup(tCase.forwardGroupName)
+	fg.SetOutputQueue(tCase.queueName)
+
+	fptest.LogQuery(t, "QoS update for the OC config:", gnmi.OC().Qos().Config(), qos)
+
+	update1 := buildCLIUpdate(tCase.cmdCfg)
+	update2 := buildOCUpdateFromGo(t, resolvedQosPath, qos)
+	gpbSetRequest := &gpb.SetRequest{Update: tCase.reorderFunc([]*gpb.Update{update1, update2})}
+
+	t.Log("Calling gnmiClient.Set() CLI + OpenConfig config:")
+	t.Log(gpbSetRequest)
+
+	gnmiClient := dut.RawAPIs().GNMI().Default(t)
+	response, err := gnmiClient.Set(context.Background(), gpbSetRequest)
+	if err != nil {
+		if tCase.okToFail {
+			t.Skipf("gnmiClient.Set() with unexpected error: %v\nSkipping as this test has okToFail: %v", err, tCase.okToFail)
+		} else {
+			t.Fatalf("gnmiClient.Set() with unexpected error: %v", err)
+		}
+	}
+	t.Log("gnmiClient.Set() response:\n", response)
+
+	// Validate
+	gotQueue := gnmi.GetConfig(t, dut, gnmi.OC().Qos().Queue(tCase.queueName).Config())
+	if got := gotQueue.GetName(); got != tCase.queueName {
+		t.Errorf("Get(DUT queue name): got %v, want %v", got, tCase.queueName)
+	}
+	gotFG := gnmi.GetConfig(t, dut, gnmi.OC().Qos().ForwardingGroup(tCase.forwardGroupName).Config())
+	if got := gotFG.GetName(); got != tCase.forwardGroupName {
+		t.Errorf("Get(DUT forwarding group name): got %v, want %v", got, tCase.forwardGroupName)
+	}
+	if got := gotFG.GetOutputQueue(); got != tCase.queueName {
+		t.Errorf("Get(DUT forwarding group output queue): got %v, want %v", got, tCase.queueName)
+	}
+}
+
+var (
+	passthru = func(input []*gpb.Update) []*gpb.Update {
+		return []*gpb.Update{input[0], input[1]}
+	}
+	swap = func(input []*gpb.Update) []*gpb.Update {
+		return []*gpb.Update{input[1], input[0]}
+	}
+)
+
+func TestQoSDependentCLIThenOC(t *testing.T) {
+	testQoSWithCLIAndOCUpdates(t, testCase{
+		cmdCfg: `
+qos traffic-class 0 name target-group-BE0
+qos tx-queue 0 name BE0
+	`,
+		queueName:        "BE0",
+		forwardGroupName: "target-group-BE0",
+		reorderFunc:      passthru,
+		okToFail:         false,
+	})
+}
+
+// This test (dependent OC preceding CLI) is not required to succeed.
+func TestQoSDependentOCThenCLI(t *testing.T) {
+	testQoSWithCLIAndOCUpdates(t, testCase{
+		cmdCfg: `
+qos traffic-class 1 name target-group-BE1
+qos tx-queue 1 name BE1
+	`,
+		queueName:        "BE1",
+		forwardGroupName: "target-group-BE1",
+		reorderFunc:      swap,
+		okToFail:         true,
+	})
 }
