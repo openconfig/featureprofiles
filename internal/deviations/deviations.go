669f249bb0d7c2cb02ea0e672d18c78d126164da
mojiiba <91647529+mojiiba@users.noreply.github.com>
2023-01-18 18:19:27 -0800

    Update deviations.go

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -83,7 +83,7 @@ var (
 
 	IPv4MissingEnabled = flag.Bool("deviation_ipv4_missing_enabled", false, "Device does not support interface/ipv4/enabled, so suppress configuring this leaf.")
 
-	IPNeighborMissing = flag.Bool("deviation_ip_neighbor_missing", true, "Device does not support interface/ipv4(6)/neighbor, so suppress the related check for interface/ipv4(6)/neighbor.")
+	IPNeighborMissing = flag.Bool("deviation_ip_neighbor_missing", false, "Device does not support interface/ipv4(6)/neighbor, so suppress the related check for interface/ipv4(6)/neighbor.")
 
 	InterfaceCountersFromContainer = flag.Bool("deviation_interface_counters_from_container", false, "Device only supports querying counters from the state container, not from individual counter leaves.")
 
