c530b2d269e32b0e73b7cdd47d6e7eace0a047c8
avaneesh90 <avaneeshs@juniper.net>
2023-01-25 16:02:05 +0000

    gNMI-1.8_Configuration_Metadata-only_Retrieve_and_Replace : Adding hostname configuration

--- feature/system/gnmi/metadata/tests/annotation_test/annotation_test.go
+++ feature/system/gnmi/metadata/tests/annotation_test/annotation_test.go
@@ -97,6 +97,8 @@ func TestGNMIMetadataAnnotation(t *testing.T) {
 	for _, tc := range cases {
 		t.Log(tc.desc)
 		gnmiClient := dut.RawAPIs().GNMI().Default(t)
+		hostnameConfigPath := gnmi.OC().System().Hostname()
+		gnmi.Replace(t, dut, hostnameConfigPath.Config(), string("ondatraHost"))
 
 		t.Log("Build an annotated gNMI SetRequest from proto message")
 		gpbSetRequest, err := buildMetadataAnnotation(t, tc.protoMsg)
