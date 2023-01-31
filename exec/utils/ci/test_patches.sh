b9341614fdb9bdff3a3ec26917642eb03ca1c8f0
Karim Jahed (kjahed) <kjahed@cisco.com>
2023-01-31 15:39:05 -0500

    Update test_patches.sh

--- exec/utils/ci/test_patches.sh
+++ exec/utils/ci/test_patches.sh
@@ -2,13 +2,16 @@
 git reset --hard HEAD
 git checkout main
 
+failed=0
 for f in $1/*
 do
  echo "Processing $f"
  git reset --hard HEAD
- git apply $f --ignore-space-change --ignore-whitespace --verbose || exit 1
+ git apply $f --ignore-space-change --ignore-whitespace --verbose || failed=1
  echo "************************************************"
 done
 
 git reset --hard HEAD
-exit 0
+if [ "$failed" -ne 0 ] ; then
+    exit 1
+fi
