ddfb8646e96a4c0d9df5b76c91a7648a3660d3c0
Karim Jahed <kjahed@cisco.com>
2023-01-31 07:42:30 -0600

    bug fix

--- exec/utils/ci/test_patches.sh
+++ exec/utils/ci/test_patches.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+cp -r ../firex/plugins/fp_patch _fp_patch
 git reset --hard HEAD
 git checkout origin/main
-
-for f in ../firex/plugins/fp_patch/*.patch
+for f in _fp_patch/*
  echo "Processing $f"
  git reset --hard HEAD
  git apply $f --ignore-space-change --ignore-whitespace --verbose
