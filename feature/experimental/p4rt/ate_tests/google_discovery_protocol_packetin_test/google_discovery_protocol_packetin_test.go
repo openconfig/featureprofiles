07b050c8cabc364d63d01250fd00adb3fa05cdf4
Sally Jin <sallyjin@google.com>
2023-02-10 10:31:26 -0800

    fix indent level

--- feature/experimental/p4rt/ate_tests/google_discovery_protocol_packetin_test/google_discovery_protocol_packetin_test.go
+++ feature/experimental/p4rt/ate_tests/google_discovery_protocol_packetin_test/google_discovery_protocol_packetin_test.go
@@ -165,7 +165,7 @@ func fetchPackets(ctx context.Context, t *testing.T, client *p4rt_client.P4RTCli
 			t.Fatalf("There is error seen when receving packets. %v, %s", err, err)
 			break
 		}
-	time.Sleep(1 * time.Second)
+		time.Sleep(1 * time.Second)
 	}
 	return packets
 }
