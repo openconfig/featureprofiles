28a164bc9a3a912f03ad1a09dc1b92fe125bd9c1
ANISH-GOTTAPU <anish.gottapu@keysight.com>
2023-02-14 20:44:57 +0530

    OTG converted test

--- feature/experimental/qos/otg_tests/two_sp_queue_traffic_test/two_sp_queue_traffic_test.go
+++ feature/experimental/qos/otg_tests/two_sp_queue_traffic_test/two_sp_queue_traffic_test.go
@@ -18,6 +18,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/openconfig/featureprofiles/internal/attrs"
 	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/featureprofiles/internal/fptest"
 	"github.com/openconfig/ondatra"
@@ -26,13 +27,40 @@ import (
 	"github.com/openconfig/ygot/ygot"
 )
 
+var (
+	intf1 = attrs.Attributes{
+		Name:    "ate1",
+		MAC:     "02:00:01:01:01:01",
+		IPv4:    "198.51.100.1",
+		IPv4Len: 31,
+	}
+
+	intf2 = attrs.Attributes{
+		Name:    "ate2",
+		MAC:     "02:00:01:01:01:02",
+		IPv4:    "198.51.100.3",
+		IPv4Len: 31,
+	}
+
+	intf3 = attrs.Attributes{
+		Name:    "ate3",
+		MAC:     "02:00:01:01:01:03",
+		IPv4:    "198.51.100.5",
+		IPv4Len: 31,
+	}
+
+	intf1Gateway = "198.51.100.0"
+	intf2Gateway = "198.51.100.2"
+	intf3Gateway = "198.51.100.4"
+)
+
 type trafficData struct {
 	trafficRate           float64
 	expectedThroughputPct float32
 	frameSize             uint32
 	dscp                  uint8
 	queue                 string
-	inputIntf             *ondatra.Interface
+	inputIntf             attrs.Attributes
 }
 
 func TestMain(m *testing.M) {
@@ -114,20 +142,26 @@ func TestTwoSPQueueTraffic(t *testing.T) {
 	ap1 := ate.Port(t, "port1")
 	ap2 := ate.Port(t, "port2")
 	ap3 := ate.Port(t, "port3")
-	top := ate.Topology().New()
-	intf1 := top.AddInterface("intf1").WithPort(ap1)
-	intf1.IPv4().
-		WithAddress("198.51.100.1/31").
-		WithDefaultGateway("198.51.100.0")
-	intf2 := top.AddInterface("intf2").WithPort(ap2)
-	intf2.IPv4().
-		WithAddress("198.51.100.3/31").
-		WithDefaultGateway("198.51.100.2")
-	intf3 := top.AddInterface("intf3").WithPort(ap3)
-	intf3.IPv4().
-		WithAddress("198.51.100.5/31").
-		WithDefaultGateway("198.51.100.4")
-	top.Push(t).StartProtocols(t)
+	top := ate.OTG().NewConfig(t)
+
+	top.Ports().Add().SetName(ap1.ID())
+	top.Ports().Add().SetName(ap2.ID())
+	top.Ports().Add().SetName(ap3.ID())
+
+	dev1 := top.Devices().Add().SetName(intf1.Name)
+	eth1 := dev1.Ethernets().Add().SetName(dev1.Name() + ".eth")
+	eth1.SetPortName(ap1.ID()).SetMac(intf1.MAC)
+	eth1.Ipv4Addresses().Add().SetName(dev1.Name() + ".ipv4").SetAddress(intf1.IPv4).SetGateway(intf1Gateway).SetPrefix(int32(intf1.IPv4Len))
+
+	dev2 := top.Devices().Add().SetName(intf2.Name)
+	eth2 := dev2.Ethernets().Add().SetName(dev2.Name() + ".eth")
+	eth2.SetPortName(ap2.ID()).SetMac(intf2.MAC)
+	eth2.Ipv4Addresses().Add().SetName(dev2.Name() + ".ipv4").SetAddress(intf2.IPv4).SetGateway(intf2Gateway).SetPrefix(int32(intf2.IPv4Len))
+
+	dev3 := top.Devices().Add().SetName(intf3.Name)
+	eth3 := dev3.Ethernets().Add().SetName(dev3.Name() + ".eth")
+	eth3.SetPortName(ap3.ID()).SetMac(intf3.MAC)
+	eth3.Ipv4Addresses().Add().SetName(dev3.Name() + ".ipv4").SetAddress(intf3.IPv4).SetGateway(intf3Gateway).SetPrefix(int32(intf3.IPv4Len))
 
 	var tolerance float32 = 2.0
 
@@ -1058,18 +1092,28 @@ func TestTwoSPQueueTraffic(t *testing.T) {
 	for _, tc := range cases {
 		t.Run(tc.desc, func(t *testing.T) {
 			trafficFlows := tc.trafficFlows
+			top.Flows().Clear()
 
-			var flows []*ondatra.Flow
 			for trafficID, data := range trafficFlows {
 				t.Logf("Configuring flow %s", trafficID)
-				flow := ate.Traffic().NewFlow(trafficID).
-					WithSrcEndpoints(data.inputIntf).
-					WithDstEndpoints(intf3).
-					WithHeaders(ondatra.NewEthernetHeader(), ondatra.NewIPv4Header().WithDSCP(data.dscp)).
-					WithFrameRatePct(data.trafficRate).
-					WithFrameSize(data.frameSize)
-				flows = append(flows, flow)
+				flow := top.Flows().Add().SetName(trafficID)
+				flow.Metrics().SetEnable(true)
+				flow.TxRx().Device().SetTxNames([]string{data.inputIntf.Name + ".ipv4"}).SetRxNames([]string{dev3.Name() + ".ipv4"})
+				ethHeader := flow.Packet().Add().Ethernet()
+				ethHeader.Src().SetValue(data.inputIntf.MAC)
+
+				ipHeader := flow.Packet().Add().Ipv4()
+				ipHeader.Src().SetValue(data.inputIntf.IPv4)
+				ipHeader.Dst().SetValue(intf3.IPv4)
+				ipHeader.Priority().Dscp().Phb().SetValue(int32(data.dscp))
+
+				flow.Size().SetFixed(int32(data.frameSize))
+				flow.Rate().SetPercentage(float32(data.trafficRate))
 			}
+
+			ate.OTG().PushConfig(t, top)
+			ate.OTG().StartProtocols(t)
+			time.Sleep(30 * time.Second)
 
 			ateOutPkts := make(map[string]uint64)
 			ateInPkts := make(map[string]uint64)
@@ -1097,19 +1141,24 @@ func TestTwoSPQueueTraffic(t *testing.T) {
 			t.Logf("Running traffic 1 on DUT interfaces: %s => %s ", dp1.Name(), dp3.Name())
 			t.Logf("Running traffic 2 on DUT interfaces: %s => %s ", dp2.Name(), dp3.Name())
 			t.Logf("Sending traffic flows: \n%v\n\n", trafficFlows)
-			ate.Traffic().Start(t, flows...)
+			ate.OTG().StartTraffic(t)
 			time.Sleep(20 * time.Second)
-			ate.Traffic().Stop(t)
+			ate.OTG().StopTraffic(t)
 			time.Sleep(10 * time.Second)
 
 			for trafficID, data := range trafficFlows {
-				ateOutPkts[data.queue] += gnmi.Get(t, ate, gnmi.OC().Flow(trafficID).Counters().OutPkts().State())
-				ateInPkts[data.queue] += gnmi.Get(t, ate, gnmi.OC().Flow(trafficID).Counters().InPkts().State())
+				ateOutPkts[data.queue] += gnmi.Get(t, ate.OTG(), gnmi.OTG().Flow(trafficID).Counters().OutPkts().State())
+				ateInPkts[data.queue] += gnmi.Get(t, ate.OTG(), gnmi.OTG().Flow(trafficID).Counters().InPkts().State())
 				dutQosPktsAfterTraffic[data.queue] += gnmi.Get(t, dut, gnmi.OC().Qos().Interface(dp3.Name()).Output().Queue(data.queue).TransmitPkts().State())
 				dutQosDroppedPktsAfterTraffic[data.queue] += gnmi.Get(t, dut, gnmi.OC().Qos().Interface(dp3.Name()).Output().Queue(data.queue).DroppedPkts().State())
 				t.Logf("ateInPkts: %v, txPkts %v, Queue: %v", ateInPkts[data.queue], dutQosPktsAfterTraffic[data.queue], data.queue)
 
-				lossPct := gnmi.Get(t, ate, gnmi.OC().Flow(trafficID).LossPct().State())
+				ateTxPkts := gnmi.Get(t, ate.OTG(), gnmi.OTG().Flow(trafficID).Counters().OutPkts().State())
+				ateRxPkts := gnmi.Get(t, ate.OTG(), gnmi.OTG().Flow(trafficID).Counters().InPkts().State())
+				if ateTxPkts == 0 {
+					t.Fatalf("TxPkts == 0, want >0.")
+				}
+				lossPct := float32(((ateTxPkts - ateRxPkts) * 100) / ateTxPkts)
 				t.Logf("Get flow %q: lossPct: %.2f%% or rxPct: %.2f%%, want: %.2f%%\n\n", data.queue, lossPct, 100.0-lossPct, data.expectedThroughputPct)
 				if got, want := 100.0-lossPct, data.expectedThroughputPct; got < want-tolerance || got > want+tolerance {
 					t.Errorf("Get(throughput for queue %q): got %.2f%%, want within [%.2f%%, %.2f%%]", data.queue, got, want-tolerance, want+tolerance)
