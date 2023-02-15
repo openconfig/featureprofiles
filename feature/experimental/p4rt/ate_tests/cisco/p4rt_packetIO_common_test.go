c14049aabe371676c827b5865f76f993172c337d
Prem Singh <sprem@cisco.com>
2023-02-15 17:34:23 +0530

    added ipv6 packed handling

--- feature/experimental/p4rt/ate_tests/cisco/p4rt_packetIO_common_test.go
+++ feature/experimental/p4rt/ate_tests/cisco/p4rt_packetIO_common_test.go
@@ -99,21 +99,20 @@ func decodePacket(t *testing.T, packetData []byte) (string, layers.EthernetType)
 	return "", layers.EthernetType(0)
 }
 
-func decodePacket62(t *testing.T, packetData []byte) (string, string) {
+func decodePacket6(t *testing.T, packetData []byte) (string, string) {
 	t.Helper()
 	ethpacket := gopacket.NewPacket(packetData[:14], layers.LayerTypeEthernet, gopacket.Default)
 	if Ethernet := ethpacket.Layer(layers.LayerTypeEthernet); Ethernet != nil {
 		header, decoded := Ethernet.(*layers.Ethernet)
-		fmt.Println("***Ethernet Information: ")
-		fmt.Println(header, " ", decoded)
-	}
-	packet := gopacket.NewPacket(packetData[14:], layers.LayerTypeIPv6, gopacket.Default)
-	if IPv6 := packet.Layer(layers.LayerTypeIPv6); IPv6 != nil {
-		ipv6, _ := IPv6.(*layers.IPv6)
-		//IPv6 := ipv6.HopLimit
-		//return IPv6
-		fmt.Println("IPv6 hoplimit length payload: ", ipv6.HopLimit, " ", ipv6.Length, " ", ipv6.Payload)
-		return ipv6.SrcIP.String(), ipv6.DstIP.String()
+		//t.Logf("Ethernet Information: header, decoded: %v, %v", header, decoded)
+		if header.EthernetType.String() == "IPv6" && decoded {
+			packet := gopacket.NewPacket(packetData[14:], layers.LayerTypeIPv6, gopacket.Default)
+			if IPv6 := packet.Layer(layers.LayerTypeIPv6); IPv6 != nil {
+				ipv6, _ := IPv6.(*layers.IPv6)
+				//t.Log("IPv6 hoplimit length payload: ", ipv6.HopLimit, " ", ipv6.Length, " ", ipv6.Payload)
+				return ipv6.SrcIP.String(), ipv6.DstIP.String()
+			}
+		}
 	}
 	return "", ""
 }
@@ -121,24 +120,26 @@ func decodePacket62(t *testing.T, packetData []byte) (string, string) {
 func decodeIPPacket(t *testing.T, packetData []byte) (string, string) {
 	t.Helper()
 
+	//handle ipv6 packets
+	a, b := decodePacket6(t, packetData)
+	if a != "" && b != "" {
+		return a, b
+	}
+
 	var eth layers.Ethernet
 	var ip4 layers.IPv4
 	var ip6 layers.IPv6
-	//parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip4, &ip6)
-	parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip6, &ip4)
+	parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip4, &ip6)
 	decoded := []gopacket.LayerType{}
 	if err := parser.DecodeLayers(packetData, &decoded); err != nil {
 		t.Log("Problem in parsing the packet: ", err)
-		//return "", ""
+		return "", ""
 	}
 	for _, layerType := range decoded {
 		switch layerType {
 		case layers.LayerTypeIPv6:
-			//return ip6.SrcIP.String(), ip6.DstIP.String()
-			fmt.Println(decodePacket62(t, packetData))
-			return decodePacket62(t, packetData)
+			return ip6.SrcIP.String(), ip6.DstIP.String()
 		case layers.LayerTypeIPv4:
-			fmt.Println(decodePacket62(t, packetData))
 			return ip4.SrcIP.String(), ip4.DstIP.String()
 		}
 	}
@@ -164,32 +165,17 @@ func getPackets(t *testing.T, client *p4rt_client.P4RTClient, packetCount int) [
 	return packets
 }
 
-func decodePacket6(t *testing.T, packetData []byte) uint8 {
-	t.Helper()
-	packet := gopacket.NewPacket(packetData[14:], layers.LayerTypeIPv6, gopacket.Default)
-	if IPv6 := packet.Layer(layers.LayerTypeIPv6); IPv6 != nil {
-		ipv6, _ := IPv6.(*layers.IPv6)
-		IPv6 := ipv6.HopLimit
-		return IPv6
-	}
-	return 7
-}
-
 func validatePackets(t *testing.T, args *testArgs, packets []*p4rt_client.P4RTPacketInfo) {
 	t.Logf("Start to decode packet.")
 	wantPacket := args.packetIO.GetPacketTemplate(t)
 	for _, packet := range packets {
-		//packet = *p4rt_client.P4RTPacketInfo{9 payload:"\000\001\000\002\000\003\000\001\000\001\000\001\206\335`\000\000\000\000\362;\001\001\000\001 \000\001\000\000\000\000\000\000\000\000\000\001\001\000\001!\000\001\000\000\000\000\000\000\000\000\000\0029\236\004\373yH\361`Ixia\000\000\000\000\020\021\022\023\016\347+-\030\031\032\033\034\035\036\037 !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\177\200\201\202\203\204\205\206\207\210\211\212\213\214\215\216\217\220\221\222\223\224\225\226\227\230\231\232\233\234\235\236\237\240\241\242\243\244\245\246\247\250\251\252\253\254\255\256\257\260\261\262\263\264\265\266\267\270\271\272\273\274\275\276\277\300\301\302\303\304\305\306\307\310\311\312\313\314\315\316\317\320\321\322\323\324\325\326\327\330\331\332\333\334\335\336\337\340\341\342\343\344\345\346\347\350\351\352\353\354\355\356\357\360\361" metadata:<metadata_id:1 value:"10" > metadata:<metadata_id:2 value:"11" > }
 		//t.Logf("Packet: %v", packet)
 		//t.Logf("Packet: %v", binary.BigEndian.Uint16(packet.Pkt.GetPayload()))
 		if packet != nil {
 			// t.Logf("Packet Payload: %v", packet.Pkt.GetPayload())
-			//if wantPacket.DstMAC != nil && wantPacket.EthernetType != nil {
-			dstMac, etherType := decodePacket(t, packet.Pkt.GetPayload())
-			t.Logf("Ethernet dstMac, etherType %s, %s:", dstMac, etherType)
-			if wantPacket.DstMAC == nil && wantPacket.EthernetType == nil {
+			if wantPacket.DstMAC != nil && wantPacket.EthernetType != nil {
 				dstMac, etherType := decodePacket(t, packet.Pkt.GetPayload())
-				t.Logf("Ethernet dstMac, etherType %s, %s:", dstMac, etherType)
+				//t.Logf("Ethernet dstMac, etherType %s, %s:", dstMac, etherType)
 				// t.Logf("Decoded Ether Type: %v; Decoded DST MAC: %v", etherType, dstMac)
 				if dstMac != *wantPacket.DstMAC || etherType != layers.EthernetType(*wantPacket.EthernetType) {
 					t.Errorf("Packet is not matching wanted packet.")
@@ -198,11 +184,11 @@ func validatePackets(t *testing.T, args *testArgs, packets []*p4rt_client.P4RTPa
 
 			if wantPacket.DstIPv4 != nil || wantPacket.DstIPv6 != nil {
 				srcIP, dstIP := decodeIPPacket(t, packet.Pkt.GetPayload())
-				t.Logf("srcIP, dstIP %s, %s:", srcIP, dstIP)
+				//t.Logf("srcIP, dstIP %s, %s:", srcIP, dstIP)
 				// t.Logf("Decoded SRC IP: %v; Decoded DST IP: %v", srcIP, dstIP)
 				if *wantPacket.SrcIPv4 != srcIP && *wantPacket.SrcIPv6 != srcIP && *wantPacket.DstIPv4 != dstIP && *wantPacket.DstIPv6 != dstIP {
-					t.Logf("SourceIP: wanted %s, or %s, got %s", *wantPacket.SrcIPv4, *wantPacket.SrcIPv6, srcIP)
-					t.Logf("DestinationIP: wanted %s, or %s, got %s", *wantPacket.DstIPv4, *wantPacket.DstIPv6, dstIP)
+					// t.Logf("SourceIP: wanted %s, or %s, got %s", *wantPacket.SrcIPv4, *wantPacket.SrcIPv6, srcIP)
+					// t.Logf("DestinationIP: wanted %s, or %s, got %s", *wantPacket.DstIPv4, *wantPacket.DstIPv6, dstIP)
 					t.Errorf("IP header in Packet is not matching wanted packet.")
 				}
 			}
@@ -210,9 +196,9 @@ func validatePackets(t *testing.T, args *testArgs, packets []*p4rt_client.P4RTPa
 			// TODO: Check Port-id in MetaData
 			metaData := packet.Pkt.GetMetadata()
 			for _, data := range metaData {
-				t.Logf("Metadata: %d, %s", data.GetMetadataId(), data.GetValue())
+				//t.Logf("Metadata: %d, %s", data.GetMetadataId(), data.GetValue())
 				if data.GetMetadataId() == METADATA_INGRESS_PORT {
-					t.Logf("Expected Ingress Port Id: %v", args.packetIO.GetIngressPort(t))
+					//t.Logf("Expected Ingress Port Id: %v", args.packetIO.GetIngressPort(t))
 					if string(data.GetValue()) != args.packetIO.GetIngressPort(t) {
 						t.Errorf("Ingress Port Id is not matching expectation...")
 					}
