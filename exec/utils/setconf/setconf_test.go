edb5f38a61fc1baa02bedf88ef2043e14c1c6953
Karim Jahed <kjahed@cisco.com>
2023-02-16 06:55:18 -0600

    added virtual ip for sim

---
+++ exec/utils/setconf/setconf_test.go
@@ -0,0 +1,56 @@
+package setconf_test
+
+import (
+	"context"
+	"flag"
+	"os"
+	"testing"
+
+	"github.com/openconfig/featureprofiles/internal/fptest"
+	"github.com/openconfig/gnmi/proto/gnmi"
+	"github.com/openconfig/ondatra"
+)
+
+var (
+	confFlag   = flag.String("conf", "", "CLI configuration file")
+	updateFlag = flag.Bool("update", false, "Perform Update instead of Replace")
+)
+
+func TestMain(m *testing.M) {
+	fptest.RunTests(m)
+}
+
+func TestSetConf(t *testing.T) {
+	if *confFlag == "" {
+		t.Fatal("Missing conf arg")
+	}
+
+	ctx := context.Background()
+	dut := ondatra.DUT(t, "dut")
+
+	conf, err := os.ReadFile(*confFlag)
+	if err != nil {
+		t.Fatalf("Error reading cli config file: %v", err)
+	}
+
+	updateRequest := &gnmi.Update{
+		Path: &gnmi.Path{
+			Origin: "cli",
+		},
+		Val: &gnmi.TypedValue{
+			Value: &gnmi.TypedValue_AsciiVal{
+				AsciiVal: string(conf),
+			},
+		},
+	}
+
+	setRequest := &gnmi.SetRequest{}
+	if *updateFlag {
+		setRequest.Update = []*gnmi.Update{updateRequest}
+	} else {
+		setRequest.Replace = []*gnmi.Update{updateRequest}
+	}
+
+	gnmiClient := dut.RawAPIs().GNMI().New(t)
+	gnmiClient.Set(ctx, setRequest)
+}
