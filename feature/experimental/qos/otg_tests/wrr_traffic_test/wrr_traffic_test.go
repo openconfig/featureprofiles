ddfd7bb9744bc600e87d873a708e7cc37be7d658
Octavian Petre <octavian.petre@keysight.com>
2023-02-27 22:58:05 +0000

    otg changes to the test

--- feature/experimental/qos/otg_tests/wrr_traffic_test/wrr_traffic_test.go
+++ feature/experimental/qos/otg_tests/wrr_traffic_test/wrr_traffic_test.go
@@ -18,12 +18,47 @@ import (
 	"testing"
 	"time"
 
+	"github.com/openconfig/featureprofiles/internal/attrs"
 	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/featureprofiles/internal/fptest"
+	"github.com/openconfig/featureprofiles/internal/otgutils"
 	"github.com/openconfig/ondatra"
 	"github.com/openconfig/ondatra/gnmi"
 	"github.com/openconfig/ondatra/gnmi/oc"
 	"github.com/openconfig/ygot/ygot"
+)
+
+var (
+	intf1 = attrs.Attributes{
+		Name:    "ate1",
+		MAC:     "02:00:01:01:01:01",
+		IPv4:    "198.51.100.1",
+		IPv4Len: 31,
+	}
+
+	intf2 = attrs.Attributes{
+		Name:    "ate2",
+		MAC:     "02:00:01:02:01:01",
+		IPv4:    "198.51.100.3",
+		IPv4Len: 31,
+	}
+
+	intf3 = attrs.Attributes{
+		Name:    "ate3",
+		MAC:     "02:00:01:03:01:01",
+		IPv4:    "198.51.100.5",
+		IPv4Len: 31,
+	}
+
+	dutPort1 = attrs.Attributes{
+		IPv4: "198.51.100.0",
+	}
+	dutPort2 = attrs.Attributes{
+		IPv4: "198.51.100.2",
+	}
+	dutPort3 = attrs.Attributes{
+		IPv4: "198.51.100.4",
+	}
 )
 
 type trafficData struct {
@@ -32,7 +67,7 @@ type trafficData struct {
 	frameSize             uint32
 	dscp                  uint8
 	queue                 string
-	inputIntf             *ondatra.Interface
+	inputIntf             attrs.Attributes
 }
 
 func TestMain(m *testing.M) {
@@ -102,20 +137,12 @@ func TestWrrTraffic(t *testing.T) {
 	ap1 := ate.Port(t, "port1")
 	ap2 := ate.Port(t, "port2")
 	ap3 := ate.Port(t, "port3")
-	top := ate.Topology().New()
-	intf1 := top.AddInterface("intf1").WithPort(ap1)
-	intf1.IPv4().
-		WithAddress("198.51.100.1/31").
-		WithDefaultGateway("198.51.100.0")
-	intf2 := top.AddInterface("intf2").WithPort(ap2)
-	intf2.IPv4().
-		WithAddress("198.51.100.3/31").
-		WithDefaultGateway("198.51.100.2")
-	intf3 := top.AddInterface("intf3").WithPort(ap3)
-	intf3.IPv4().
-		WithAddress("198.51.100.5/31").
-		WithDefaultGateway("198.51.100.4")
-	top.Push(t).StartProtocols(t)
+	top := ate.OTG().NewConfig(t)
+
+	intf1.AddToOTG(top, ap1, &dutPort1)
+	intf2.AddToOTG(top, ap2, &dutPort2)
+	intf3.AddToOTG(top, ap3, &dutPort3)
+	ate.OTG().PushConfig(t, top)
 
 	var tolerance float32 = 3.0
 
@@ -822,18 +849,28 @@ func TestWrrTraffic(t *testing.T) {
 	for _, tc := range cases {
 		t.Run(tc.desc, func(t *testing.T) {
 			trafficFlows := tc.trafficFlows
+			top.Flows().Clear()
 
-			var flows []*ondatra.Flow
 			for trafficID, data := range trafficFlows {
 				t.Logf("Configuring flow %s", trafficID)
-				flow := ate.Traffic().NewFlow(trafficID).
-					WithSrcEndpoints(data.inputIntf).
-					WithDstEndpoints(intf3).
-					WithHeaders(ondatra.NewEthernetHeader(), ondatra.NewIPv4Header().WithDSCP(data.dscp)).
-					WithFrameRatePct(data.trafficRate).
-					WithFrameSize(data.frameSize)
-				flows = append(flows, flow)
+				flow := top.Flows().Add().SetName(trafficID)
+				flow.Metrics().SetEnable(true)
+				flow.TxRx().Device().SetTxNames([]string{data.inputIntf.Name + ".IPv4"}).SetRxNames([]string{intf3.Name + ".IPv4"})
+				ethHeader := flow.Packet().Add().Ethernet()
+				ethHeader.Src().SetValue(data.inputIntf.MAC)
+
+				ipHeader := flow.Packet().Add().Ipv4()
+				ipHeader.Src().SetValue(data.inputIntf.IPv4)
+				ipHeader.Dst().SetValue(intf3.IPv4)
+				ipHeader.Priority().Dscp().Phb().SetValue(int32(data.dscp))
+
+				flow.Size().SetFixed(int32(data.frameSize))
+				flow.Rate().SetPercentage(float32(data.trafficRate))
 			}
+
+			ate.OTG().PushConfig(t, top)
+			ate.OTG().StartProtocols(t)
+			time.Sleep(30 * time.Second)
 
 			ateOutPkts := make(map[string]uint64)
 			ateInPkts := make(map[string]uint64)
@@ -861,19 +898,25 @@ func TestWrrTraffic(t *testing.T) {
 			t.Logf("Running traffic 1 on DUT interfaces: %s => %s ", dp1.Name(), dp3.Name())
 			t.Logf("Running traffic 2 on DUT interfaces: %s => %s ", dp2.Name(), dp3.Name())
 			t.Logf("Sending traffic flows: \n%v\n\n", trafficFlows)
-			ate.Traffic().Start(t, flows...)
+			ate.OTG().StartTraffic(t)
 			time.Sleep(tc.trafficDuration)
-			ate.Traffic().Stop(t)
+			ate.OTG().StopTraffic(t)
 			time.Sleep(10 * time.Second)
 
+			otgutils.LogFlowMetrics(t, ate.OTG(), top)
 			for trafficID, data := range trafficFlows {
-				ateOutPkts[data.queue] += gnmi.Get(t, ate, gnmi.OC().Flow(trafficID).Counters().OutPkts().State())
-				ateInPkts[data.queue] += gnmi.Get(t, ate, gnmi.OC().Flow(trafficID).Counters().InPkts().State())
+				ateOutPkts[data.queue] += gnmi.Get(t, ate.OTG(), gnmi.OTG().Flow(trafficID).Counters().OutPkts().State())
+				ateInPkts[data.queue] += gnmi.Get(t, ate.OTG(), gnmi.OTG().Flow(trafficID).Counters().InPkts().State())
 				dutQosPktsAfterTraffic[data.queue] += gnmi.Get(t, dut, gnmi.OC().Qos().Interface(dp3.Name()).Output().Queue(data.queue).TransmitPkts().State())
 				dutQosDroppedPktsAfterTraffic[data.queue] += gnmi.Get(t, dut, gnmi.OC().Qos().Interface(dp3.Name()).Output().Queue(data.queue).DroppedPkts().State())
 				t.Logf("ateInPkts: %v, txPkts %v, Queue: %v", ateInPkts[data.queue], dutQosPktsAfterTraffic[data.queue], data.queue)
 
-				lossPct := gnmi.Get(t, ate, gnmi.OC().Flow(trafficID).LossPct().State())
+				ateTxPkts := gnmi.Get(t, ate.OTG(), gnmi.OTG().Flow(trafficID).Counters().OutPkts().State())
+				ateRxPkts := gnmi.Get(t, ate.OTG(), gnmi.OTG().Flow(trafficID).Counters().InPkts().State())
+				if ateTxPkts == 0 {
+					t.Fatalf("TxPkts == 0, want >0.")
+				}
+				lossPct := float32(((ateTxPkts - ateRxPkts) * 100) / ateTxPkts)
 				t.Logf("Get flow %q: lossPct: %.2f%% or rxPct: %.2f%%, want: %.2f%%\n\n", data.queue, lossPct, 100.0-lossPct, data.expectedThroughputPct)
 				if got, want := 100.0-lossPct, data.expectedThroughputPct; got < want-tolerance || got > want+tolerance {
 					t.Errorf("Get(throughput for queue %q): got %.2f%%, want within [%.2f%%, %.2f%%]", data.queue, got, want-tolerance, want+tolerance)
@@ -915,17 +958,17 @@ func ConfigureDUTIntf(t *testing.T, dut *ondatra.DUTDevice) {
 	}{{
 		desc:      "Input interface port1",
 		intfName:  dp1.Name(),
-		ipAddr:    "198.51.100.0",
+		ipAddr:    dutPort1.IPv4,
 		prefixLen: 31,
 	}, {
 		desc:      "Input interface port2",
 		intfName:  dp2.Name(),
-		ipAddr:    "198.51.100.2",
+		ipAddr:    dutPort2.IPv4,
 		prefixLen: 31,
 	}, {
 		desc:      "Output interface port3",
 		intfName:  dp3.Name(),
-		ipAddr:    "198.51.100.4",
+		ipAddr:    dutPort3.IPv4,
 		prefixLen: 31,
 	}}
 
