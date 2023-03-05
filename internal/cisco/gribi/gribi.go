448b440403671f1df30da116735fe8d2bd4344f1
manan-patel <mananpat@cisco.com>
2023-03-05 12:48:37 -0500

    aft_backup_decap_fix

--- internal/cisco/gribi/gribi.go
+++ internal/cisco/gribi/gribi.go
@@ -251,6 +251,8 @@ func (c *Client) AddNH(t testing.TB, nhIndex uint64, address, instance string, n
 	if address == DECAP {
 		NH = NH.WithDecapsulateHeader(fluent.IPinIP)
 		aftNh.DecapsulateHeader = oc.Aft_EncapsulationHeaderType_IPV4
+		temp_address := "0.0.0.0"
+		aftNh.IpAddress = &temp_address
 	} else if address == ENCAP {
 		NH = NH.WithEncapsulateHeader(fluent.IPinIP)
 	} else if address == DecapEncap {
