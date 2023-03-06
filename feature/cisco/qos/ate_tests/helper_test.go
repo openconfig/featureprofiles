917be306a5a827f6f1ab6a725d2339394ff7ba87
Arvind Sankaran <saarvind@cisco.com>
2023-03-06 20:05:08 +0530

    added-type-for-bundle

--- feature/cisco/qos/ate_tests/helper_test.go
+++ feature/cisco/qos/ate_tests/helper_test.go
@@ -43,19 +43,24 @@ func getSubInterface(ipv4 string, prefixlen4 uint8, ipv6 string, prefixlen6 uint
 	return s
 }
 
-func addIpv6Address(ipv6 string, prefixlen uint8, index uint32) *telemetry.Interface_Subinterface {
-	s := &telemetry.Interface_Subinterface{}
+func addIpv6Address(i *telemetry.Interface, ipv6 string, prefixlen uint8, index uint32) *telemetry.Interface {
+	i.Type = telemetry.IETFInterfaces_InterfaceType_ieee8023adLag
+	//Type = oc.IETFInterfaces_InterfaceType_ieee8023adLag
+
+	s := i.GetOrCreateSubinterface(index)
 	s.Index = ygot.Uint32(index)
 	s4 := s.GetOrCreateIpv6()
 	a := s4.GetOrCreateAddress(ipv6)
 	a.PrefixLength = ygot.Uint8(prefixlen)
-	return s
+	return i
 }
 
 func configureIpv6AndVlans(t *testing.T, dut *ondatra.DUTDevice) {
 	//Configure IPv6 address on Bundle-Ether120, Bundle-Ether121
-	gnmi.Update(t, dut, gnmi.OC().Interface("Bundle-Ether120").Subinterface(0).Config(), addIpv6Address(dutPort1.IPv6, dutPort1.IPv6Len, 0))
-	gnmi.Update(t, dut, gnmi.OC().Interface("Bundle-Ether121").Subinterface(0).Config(), addIpv6Address(dutPort2.IPv6, dutPort2.IPv6Len, 0))
+	i1 := &telemetry.Interface{Name: ygot.String("Bundle-Ether120")}
+	i2 := &telemetry.Interface{Name: ygot.String("Bundle-Ether121")}
+	gnmi.Update(t, dut, gnmi.OC().Interface("Bundle-Ether120").Config(), addIpv6Address(i1, dutPort1.IPv6, dutPort1.IPv6Len, 0))
+	gnmi.Update(t, dut, gnmi.OC().Interface("Bundle-Ether121").Config(), addIpv6Address(i2, dutPort2.IPv6, dutPort2.IPv6Len, 0))
 
 	//Configure VLANs on Bundle-Ether121
 	for i := 1; i <= 3; i++ {
