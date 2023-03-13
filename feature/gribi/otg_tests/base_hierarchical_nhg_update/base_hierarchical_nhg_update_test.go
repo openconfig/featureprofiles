c54e6e1c040cb7bf4899d225f0f6f8af9dfd0446
Sergey Fomin <sergey.fomin@nokia.com>
2023-03-12 19:25:01 -0700

    te-3.7: add implicit replaces to OTG
    This code is a Contribution to the OpenConfig Feature Profiles project ("Work") made under the Google Software Grant and Corporate Contributor License Agreement ("CLA") and governed by the Apache License 2.0. No other rights or licenses in or to any of Nokia’s intellectual property are granted for any other purpose. This code is provided on an "as is" basis without any warranties of any kind.

--- feature/gribi/otg_tests/base_hierarchical_nhg_update/base_hierarchical_nhg_update_test.go
+++ feature/gribi/otg_tests/base_hierarchical_nhg_update/base_hierarchical_nhg_update_test.go
@@ -135,16 +135,29 @@ func TestBaseHierarchicalNHGUpdate(t *testing.T) {
 	}()
 
 	gribi.BecomeLeader(t, gribic)
+	dutP2 := dut.Port(t, "port2").Name()
+	dutP3 := dut.Port(t, "port3").Name()
 
-	addInterfaceRoute(ctx, t, gribic, p2NHID, dut.Port(t, "port2").Name(), atePort2.IPv4)
+	t.Logf("Adding gribi routes and validating traffic forwarding via port %v and NH ID %v", dutP2, p2NHID)
+	addInterfaceRoute(ctx, t, gribic, p2NHID, dutP2, atePort2.IPv4)
 	addDestinationRoute(ctx, t, gribic)
 
 	waitOTGARPEntry(t)
 	validateTrafficFlows(t, p2flow, p3flow)
 
-	addInterfaceRoute(ctx, t, gribic, p3NHID, dut.Port(t, "port3").Name(), atePort3.IPv4)
+	t.Logf("Adding a new NH via port %v with ID %v", dutP3, p3NHID)
+	addNH(ctx, t, gribic, p3NHID, dutP3, pMAC)
+	t.Logf("Performing implicit in-place replace with two next-hops (NH IDs: %v and %v)", p2NHID, p3NHID)
+	addNHG(ctx, t, gribic, interfaceNHGID, []uint64{p2NHID, p3NHID})
+	// TODO: implement traffic validation for two next hops case
 
+	t.Logf("Performing implicit in-place replace using the next-hop with ID %v", p3NHID)
+	addNHG(ctx, t, gribic, interfaceNHGID, []uint64{p3NHID})
 	validateTrafficFlows(t, p3flow, p2flow)
+
+	t.Logf("Performing implicit in-place replace using the next-hop with ID %v", p2NHID)
+	addNHG(ctx, t, gribic, interfaceNHGID, []uint64{p2NHID})
+	validateTrafficFlows(t, p2flow, p3flow)
 }
 
 // addNH adds a GRIBI NH with a FIB ACK confirmation via Modify RPC
