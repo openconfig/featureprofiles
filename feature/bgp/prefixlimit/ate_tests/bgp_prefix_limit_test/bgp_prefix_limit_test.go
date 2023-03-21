bc26836890d6d5ddb8edbd2530dccf2f79190929
arvbaska1 <arvbaska@cisco.com>
2023-03-21 16:11:57 -0400

    RT-1.5 changes

--- feature/bgp/prefixlimit/ate_tests/bgp_prefix_limit_test/bgp_prefix_limit_test.go
+++ feature/bgp/prefixlimit/ate_tests/bgp_prefix_limit_test/bgp_prefix_limit_test.go
@@ -389,7 +389,7 @@ func (tc *testCase) verifyBGPTelemetry(t *testing.T, dut *ondatra.DUTDevice) {
 	}
 	if got, ok := gnmi.Watch(t, dut, prefixes.Installed().State(), time.Minute, compare).Await(t); !ok {
 		t.Errorf("Installed prefixes v4 mismatch: got %v, want %v", got, installedRoutes)
-	}	
+	}
 	nv4 := gnmi.Get(t, dut, statePath.Neighbor(ateDst.IPv4).State())
 	verifyPrefixLimitTelemetry(t, nv4, tc.wantEstablished)
 
