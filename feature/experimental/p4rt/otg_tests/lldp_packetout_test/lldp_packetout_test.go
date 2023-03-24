eb69df3b0e8280df78fb15b6652f69bf498cc822
ANISH-GOTTAPU <anish.gottapu@keysight.com>
2023-03-24 15:36:38 +0530

    address review comment

--- feature/experimental/p4rt/otg_tests/lldp_packetout_test/lldp_packetout_test.go
+++ feature/experimental/p4rt/otg_tests/lldp_packetout_test/lldp_packetout_test.go
@@ -225,12 +225,22 @@ func configureDUT(t *testing.T, dut *ondatra.DUTDevice) {
 	d := gnmi.OC()
 
 	p1 := dut.Port(t, "port1")
-	i1 := &oc.Interface{Name: ygot.String(p1.Name())}
+	i1 := &oc.Interface{Name: ygot.String(p1.Name()), Id: ygot.Uint32(portID)}
 	gnmi.Replace(t, dut, d.Interface(p1.Name()).Config(), configInterfaceDUT(i1, &dutPort1))
 
 	p2 := dut.Port(t, "port2")
-	i2 := &oc.Interface{Name: ygot.String(p2.Name())}
+	i2 := &oc.Interface{Name: ygot.String(p2.Name()), Id: ygot.Uint32(portID + 1)}
 	gnmi.Replace(t, dut, d.Interface(p2.Name()).Config(), configInterfaceDUT(i2, &dutPort2))
+
+	if *deviations.ExplicitPortSpeed {
+		fptest.SetPortSpeed(t, p1)
+		fptest.SetPortSpeed(t, p2)
+	}
+
+	if *deviations.ExplicitInterfaceInDefaultVRF {
+		fptest.AssignToNetworkInstance(t, dut, p1.Name(), *deviations.DefaultNetworkInstance, 0)
+		fptest.AssignToNetworkInstance(t, dut, p2.Name(), *deviations.DefaultNetworkInstance, 0)
+	}
 }
 
 // configureATE configures port1 and port2 on the ATE.
@@ -260,14 +270,6 @@ func configureDeviceID(ctx context.Context, t *testing.T, dut *ondatra.DUTDevice
 	component.Name = ygot.String(p4rtNode)
 	component.IntegratedCircuit.NodeId = ygot.Uint64(deviceID)
 	gnmi.Replace(t, dut, gnmi.OC().Component(p4rtNode).Config(), &component)
-}
-
-// configurePortID configures p4rt port-id on the DUT.
-func configurePortID(ctx context.Context, t *testing.T, dut *ondatra.DUTDevice) {
-	ports := sortPorts(dut.Ports())
-	for i, port := range ports {
-		gnmi.Replace(t, dut, gnmi.OC().Interface(port.Name()).Id().Config(), uint32(i)+portID)
-	}
 }
 
 // setupP4RTClient sends client arbitration message for both leader and follower clients,
@@ -350,9 +352,8 @@ func TestPacketOut(t *testing.T) {
 	ate.OTG().PushConfig(t, top)
 	ate.OTG().StartProtocols(t)
 
-	// Configure P4RT device-id and port-id on the DUT
+	// Configure P4RT device-id
 	configureDeviceID(ctx, t, dut)
-	configurePortID(ctx, t, dut)
 
 	t.Logf("Disable LLDP config")
 	gnmi.Replace(t, dut, gnmi.OC().Lldp().Enabled().Config(), false)
