19415c9c448487ea6fa472ff3e39ad7a78577d22
Agnel Varghese <agvarghecisco@gmail.com>
2023-03-31 14:08:05 +0530

    Fix conflict

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -65,9 +65,18 @@ package deviations
 
 import (
 	"flag"
+
+	"github.com/openconfig/ondatra"
 )
 
+// P4RTMissingDelete returns whether the device does not support delete mode in P4RT write requests.
+func P4RTMissingDelete(_ *ondatra.DUTDevice) bool {
+	return *p4rtMissingDelete
+}
+
 // Vendor deviation flags.
+// All new flags should not be exported (define them in lowercase) and accessed
+// from tests through a public accessors like those above.
 var (
 	BannerDelimiter = flag.String("deviation_banner_delimiter", "",
 		"Device requires the banner to have a delimiter character. Full OpenConfig compliant devices should work without delimiter.")
@@ -203,7 +212,7 @@ var (
 
 	SchedulerInputParamsUnsupported = flag.Bool("deviation_scheduler_input_params_unsupported", false, "Device does not support scheduler input parameters")
 
-	P4RTMissingDelete = flag.Bool("deviation_p4rt_missing_delete", false, "Device does not support delete mode in P4RT write requests")
+	p4rtMissingDelete = flag.Bool("deviation_p4rt_missing_delete", false, "Device does not support delete mode in P4RT write requests")
 
 	NetworkInstanceTableDeletionRequired = flag.Bool("deviation_network_instance_table_deletion_required", false,
 		"Set to true for device requiring explicit deletion of network-instance table, default is false")
@@ -213,4 +222,6 @@ var (
 
 	ISISRestartSuppressUnsupported = flag.Bool("deviation_isis_restart_suppress_unsupported", false,
 		"Device skip isis restart-suppress check if value is true, Default value is false")
+
+	MacAddressMissing = flag.Bool("deviation_mac_address_missing", false, "Device does not support /system/mac-address/state.")
 )
