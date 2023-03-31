2328bf1e43f7f1b72d188925a2e753f46f857338
trathod <tushar.rathod@nokia.com>
2023-03-31 16:06:43 -0400

    RT-5.4: Enabling deviation and updating config order
    Following changes are made -
    1. Enabling deviation ExplicitInterfaceInDefaultVRF and ExplicitPortSpeed
    2. Moving ATE configuration before verifyDUT is called to make sure interfaces are up
    3. Adding batch-delete under clearAggregate to delete dependent configs together
    
    "This code is a Contribution to the OpenConfig Feature Profiles project ("Work") made under the Google Software Grant and Corporate Contributor License Agreement ("CLA") and governed by the Apache License 2.0. No other rights or licenses in or to any of Nokia's intellectual property are granted for any other purpose. This code is provided on an "as is" basis without any warranties of any kind."

--- feature/interface/aggregate/ate_tests/aggregate_forwarding_viable_test/aggregate_forwarding_viable_test.go
+++ feature/interface/aggregate/ate_tests/aggregate_forwarding_viable_test/aggregate_forwarding_viable_test.go
@@ -232,7 +232,10 @@ func (tc *testArgs) clearAggregate(t *testing.T) {
 
 	// Clear the members of the aggregate.
 	for _, port := range tc.dutPorts[1:] {
-		gnmi.Delete(t, tc.dut, gnmi.OC().Interface(port.Name()).Ethernet().AggregateId().Config())
+		resetBatch := &gnmi.SetBatch{}
+		gnmi.BatchDelete(resetBatch, gnmi.OC().Interface(port.Name()).Ethernet().AggregateId().Config())
+		gnmi.BatchDelete(resetBatch, gnmi.OC().Interface(port.Name()).ForwardingViable().Config())
+		resetBatch.Set(t, tc.dut)
 	}
 }
 
@@ -274,6 +277,9 @@ func (tc *testArgs) configureDUT(t *testing.T) {
 	aggPath := d.Interface(tc.aggID)
 	fptest.LogQuery(t, tc.aggID, aggPath.Config(), agg)
 	gnmi.Replace(t, tc.dut, aggPath.Config(), agg)
+	if *deviations.ExplicitInterfaceInDefaultVRF {
+		fptest.AssignToNetworkInstance(t, tc.dut, tc.aggID, *deviations.DefaultNetworkInstance, 0)
+	}
 
 	srcp := tc.dutPorts[0]
 	srci := &oc.Interface{Name: ygot.String(srcp.Name())}
@@ -282,6 +288,9 @@ func (tc *testArgs) configureDUT(t *testing.T) {
 	srciPath := d.Interface(srcp.Name())
 	fptest.LogQuery(t, srcp.String(), srciPath.Config(), srci)
 	gnmi.Replace(t, tc.dut, srciPath.Config(), srci)
+	if *deviations.ExplicitInterfaceInDefaultVRF {
+		fptest.AssignToNetworkInstance(t, tc.dut, srcp.Name(), *deviations.DefaultNetworkInstance, 0)
+	}
 
 	for _, port := range tc.dutPorts[1:] {
 		i := &oc.Interface{Name: ygot.String(port.Name())}
@@ -296,6 +305,9 @@ func (tc *testArgs) configureDUT(t *testing.T) {
 
 		fptest.LogQuery(t, port.String(), iPath.Config(), i)
 		gnmi.Replace(t, tc.dut, iPath.Config(), i)
+		if *deviations.ExplicitPortSpeed {
+			fptest.SetPortSpeed(t, port)
+		}
 	}
 }
 
@@ -546,8 +558,8 @@ func TestAggregateForwardingViable(t *testing.T) {
 		}
 		t.Run(fmt.Sprintf("LagType=%s", lagType), func(t *testing.T) {
 			args.configureDUT(t)
-			args.verifyDUT(t)
 			args.configureATE(t)
+			args.verifyDUT(t)
 
 			for _, forwardingViable := range []bool{true, false} {
 				t.Run(fmt.Sprintf("ForwardingViable=%t", forwardingViable), func(t *testing.T) {
