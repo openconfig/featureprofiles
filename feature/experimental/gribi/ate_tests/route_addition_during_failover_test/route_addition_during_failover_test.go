14a363030aaf58d6c785bca5dff26a59f7beac7e
sambhavjain <sambhavjain@arista.com>
2023-04-17 10:37:51 -0400

    removed changes to vlanID and Index based on review comments

--- feature/experimental/gribi/ate_tests/route_addition_during_failover_test/route_addition_during_failover_test.go
+++ feature/experimental/gribi/ate_tests/route_addition_during_failover_test/route_addition_during_failover_test.go
@@ -281,12 +281,9 @@ func generateSubIntfPair(t *testing.T, dut *ondatra.DUTDevice, dutPort *ondatra.
 	nextHops := []string{}
 	nextHopCount := 63 // nextHopCount specifies number of nextHop IPs needed.
 	for i := 0; i <= nextHopCount; i++ {
-		vlanID := uint16(i)
-		if *deviations.NoMixOfTaggedAndUntaggedSubinterfaces {
-			vlanID = uint16(i) + 1
-		}
+		vlanID := uint16(i) + 1
 		name := fmt.Sprintf(`dst%d`, i)
-		Index := uint32(i)
+		Index := uint32(i) + 1
 		ateIPv4 := fmt.Sprintf(`198.51.100.%d`, ((4 * i) + 1))
 		dutIPv4 := fmt.Sprintf(`198.51.100.%d`, ((4 * i) + 2))
 		configureSubinterfaceDUT(t, d, dutPort, Index, vlanID, dutIPv4)
