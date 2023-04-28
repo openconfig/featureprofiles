fea7e3c0e76442f826ccb48715f5856f969bae4a
arvbaska1 <123760606+arvbaska1@users.noreply.github.com>
2023-04-28 17:01:04 -0400

    Changes to RT-1.4 & remove deviation (#1463)

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -284,9 +284,6 @@ var (
 
 	GRIBIDelayedAckResponse = flag.Bool("deviation_gribi_delayed_ack_response", false, "Device requires delay in sending ack response")
 
-	BGPStateActiveACLDeny = flag.Bool("deviation_bgp_state_active_acl_deny", false,
-		"Device requires bgp state to be active after ACL deny policy")
-
 	LLDPInterfaceConfigOverrideGlobal = flag.Bool("deviation_lldp_interface_config_override_global", false,
 		"Set this flag for LLDP interface config to override the global config,expect neighbours are seen when lldp is disabled globally but enabled on interface")
 
