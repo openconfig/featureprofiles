7b136278f61701ae670acd54008ade10bd1351da
romeyod <78448014+romeyod@users.noreply.github.com>
2023-05-05 12:05:38 -0400

    Add a deviation to wait after delete link qualification for post call cleanup. (#1446)
    * 1. Added LinkQualWaitAfterDeleteRequired deviation
    
    2. In TestListDelete, used LinkQualWaitAfterDeleteRequired deviation at the end
    
    * Update deviations.go
    
    * Update deviations.go
    
    * Update deviations.go
    
    * Update deviations.go
    
    ---------
    
    Co-authored-by: rszarecki <46606165+rszarecki@users.noreply.github.com>

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -187,6 +187,11 @@ func FanOperStatusUnsupported(_ *ondatra.DUTDevice) bool {
 	return *fanOperStatusUnsupported
 }
 
+// LinkQualWaitAfterDeleteRequired returns whether the device requires additional time to complete post delete link qualification cleanup.
+func LinkQualWaitAfterDeleteRequired(_ *ondatra.DUTDevice) bool {
+	return *linkQualWaitAfterDeleteRequired
+}
+
 // StatePathsUnsupported returns whether the device supports following state paths
 func StatePathsUnsupported(_ *ondatra.DUTDevice) bool {
 	return *statePathsUnsupported
@@ -466,6 +471,8 @@ var (
 	ipv6DiscardedPktsUnsupported = flag.Bool("deviation_ipv6_discarded_pkts_unsupported", false, "Set true for device that does not support interface ipv6 discarded packet statistics, default is false")
 
 	fanOperStatusUnsupported = flag.Bool("deviation_fan_oper_status_unsupported", false, "Device does not support oper-status leaves for some of the fan components. Set this flag to skip checking the leaf.")
+
+	linkQualWaitAfterDeleteRequired = flag.Bool("deviation_link_qual_wait_after_delete_required", false, "Device requires additional time to complete post delete link qualification cleanup.")
 
 	statePathsUnsupported = flag.Bool("deviation_state_path_unsupported", false, "Device does not support these state paths, Set this flag to skip checking the leaves")
 
