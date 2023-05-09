730b793522efa409cd5f13876e2375c35f888202
prinikasn <117314826+prinikasn@users.noreply.github.com>
2023-05-09 08:43:37 -0700

    Project import generated by Copybara. (#1528)
    PiperOrigin-RevId: 529570104

--- feature/qos/ate_tests/qos_output_queue_counters_test/qos_output_queue_counters_test.go
+++ feature/qos/ate_tests/qos_output_queue_counters_test/qos_output_queue_counters_test.go
@@ -138,7 +138,7 @@ func TestQoSCounters(t *testing.T) {
 
 	var counterNames []string
 	counters := make(map[string]map[string]uint64)
-	if !*deviations.QOSDroppedOctets {
+	if !deviations.QOSDroppedOctets(dut) {
 		counterNames = []string{
 
 			"ateOutPkts", "ateInPkts", "dutQosPktsBeforeTraffic", "dutQosOctetsBeforeTraffic",
@@ -169,7 +169,7 @@ func TestQoSCounters(t *testing.T) {
 		counters["dutQosPktsBeforeTraffic"][data.queue] = gnmi.Get(t, dut, gnmi.OC().Qos().Interface(dp2.Name()).Output().Queue(data.queue).TransmitPkts().State())
 		counters["dutQosOctetsBeforeTraffic"][data.queue] = gnmi.Get(t, dut, gnmi.OC().Qos().Interface(dp2.Name()).Output().Queue(data.queue).TransmitOctets().State())
 		counters["dutQosDroppedPktsBeforeTraffic"][data.queue] = gnmi.Get(t, dut, gnmi.OC().Qos().Interface(dp2.Name()).Output().Queue(data.queue).DroppedPkts().State())
-		if !*deviations.QOSDroppedOctets {
+		if !deviations.QOSDroppedOctets(dut) {
 			counters["dutQosDroppedOctetsBeforeTraffic"][data.queue] = gnmi.Get(t, dut, gnmi.OC().Qos().Interface(dp2.Name()).Output().Queue(data.queue).DroppedOctets().State())
 		}
 	}
@@ -188,7 +188,7 @@ func TestQoSCounters(t *testing.T) {
 		counters["dutQosPktsAfterTraffic"][data.queue] = gnmi.Get(t, dut, gnmi.OC().Qos().Interface(dp2.Name()).Output().Queue(data.queue).TransmitPkts().State())
 		counters["dutQosOctetsAfterTraffic"][data.queue] = gnmi.Get(t, dut, gnmi.OC().Qos().Interface(dp2.Name()).Output().Queue(data.queue).TransmitOctets().State())
 		counters["dutQosDroppedPktsAfterTraffic"][data.queue] = gnmi.Get(t, dut, gnmi.OC().Qos().Interface(dp2.Name()).Output().Queue(data.queue).DroppedPkts().State())
-		if !*deviations.QOSDroppedOctets {
+		if !deviations.QOSDroppedOctets(dut) {
 			counters["dutQosDroppedOctetsAfterTraffic"][data.queue] = gnmi.Get(t, dut, gnmi.OC().Qos().Interface(dp2.Name()).Output().Queue(data.queue).DroppedOctets().State())
 		}
 		t.Logf("ateInPkts: %v, txPkts %v, Queue: %v", counters["ateInPkts"][data.queue], counters["dutQosPktsAfterTraffic"][data.queue], data.queue)
@@ -225,7 +225,7 @@ func TestQoSCounters(t *testing.T) {
 		if dutOctetCounterDiff < ateOctetCounterDiff {
 			t.Errorf("Get dutOctetCounterDiff for queue %q: got %v, want >= %v", data.queue, dutOctetCounterDiff, ateOctetCounterDiff)
 		}
-		if !*deviations.QOSDroppedOctets {
+		if !deviations.QOSDroppedOctets(dut) {
 			dutDropOctetCounterDiff := counters["dutQosDroppedOctetsAfterTraffic"][data.queue] - counters["dutQosDroppedOctetsBeforeTraffic"][data.queue]
 			t.Logf("Queue %q: dutDropOctetCounterDiff: %v", data.queue, dutDropOctetCounterDiff)
 			if dutDropOctetCounterDiff != 0 {
