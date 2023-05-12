2f132985b7e026bec1677e63f51bcd862e216312
yashikakn <yashika@juniper.net>
2023-05-12 14:08:23 -0700

    added MTU config, ASIC loopback mode and a new deviation

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -247,11 +247,6 @@ func MissingInterfacePhysicalChannel(_ *ondatra.DUTDevice) bool {
 	return *missingInterfacePhysicalChannel
 }
 
-// MissingInterfaceHardwarePort returns if device does not support interface/hardwareport leaf.
-func MissingInterfaceHardwarePort(_ *ondatra.DUTDevice) bool {
-	return *missingInterfaceHardwarePort
-}
-
 // TraceRouteL4ProtocolUDP returns if device only support UDP as l4 protocol for traceroute.
 func TraceRouteL4ProtocolUDP(_ *ondatra.DUTDevice) bool {
 	return *traceRouteL4ProtocolUDP
@@ -301,6 +296,16 @@ func GNOIStatusWithEmptySubcomponent(_ *ondatra.DUTDevice) bool {
 	return *gNOIStatusWithEmptySubcomponent
 }
 
+// NetworkInstanceTableDeletionRequired returns if device requires explicit deletion of network-instance table.
+func NetworkInstanceTableDeletionRequired(_ *ondatra.DUTDevice) bool {
+	return *networkInstanceTableDeletionRequired
+}
+
+// InterfaceConfigVRFBeforeAddress returns if vrf should be configured before IP address when configuring interface.
+func InterfaceConfigVRFBeforeAddress(_ *ondatra.DUTDevice) bool {
+	return *interfaceConfigVRFBeforeAddress
+}
+
 // ExplicitInterfaceRefDefinition returns if device requires explicit interface ref configuration when applying features to interface.
 func ExplicitInterfaceRefDefinition(_ *ondatra.DUTDevice) bool {
 	return *explicitInterfaceRefDefinition
@@ -344,6 +349,11 @@ func ISISInstanceEnabledNotRequired(_ *ondatra.DUTDevice) bool {
 // NoMixOfTaggedAndUntaggedSubinterfaces returns if device does not support a mix of tagged and untagged subinterfaces
 func NoMixOfTaggedAndUntaggedSubinterfaces(_ *ondatra.DUTDevice) bool {
 	return *noMixOfTaggedAndUntaggedSubinterfaces
+}
+
+// SkipPlqQualificationRateCheck returns if PLQ QualificationRateBytesPerSecond and ExpectedRateBytesPerSecond comparison should be skipped
+func SkipPlqQualificationRateCheck(_ *ondatra.DUTDevice) bool {
+	return *skipPlqQualificationRateCheck
 }
 
 // Vendor deviation flags.
@@ -440,10 +450,7 @@ var (
 	missingInterfacePhysicalChannel = flag.Bool("deviation_missing_interface_physical_channel", false,
 		"Device does not support interface/physicalchannel leaf. Set this flag to skip checking the leaf.")
 
-	missingInterfaceHardwarePort = flag.Bool("deviation_missing_interface_hardware_port", false,
-		"Device does not support interface/hardwareport leaf. Set this flag to skip checking the leaf.")
-
-	InterfaceConfigVrfBeforeAddress = flag.Bool("deviation_interface_config_vrf_before_address", false, "When configuring interface, config Vrf prior config IP address")
+	interfaceConfigVRFBeforeAddress = flag.Bool("deviation_interface_config_vrf_before_address", false, "When configuring interface, config Vrf prior config IP address")
 
 	bgpTrafficTolerance = flag.Int("deviation_bgp_tolerance_value", 0,
 		"Allowed tolerance for BGP traffic flow while comparing for pass or fail condition.")
@@ -462,7 +469,7 @@ var (
 
 	p4rtUnsetElectionIDUnsupported = flag.Bool("deviation_p4rt_unsetelectionid_unsupported", false, "Device does not support unset Election ID")
 
-	NetworkInstanceTableDeletionRequired = flag.Bool("deviation_network_instance_table_deletion_required", false,
+	networkInstanceTableDeletionRequired = flag.Bool("deviation_network_instance_table_deletion_required", false,
 		"Set to true for device requiring explicit deletion of network-instance table, default is false")
 
 	isisMultiTopologyUnsupported = flag.Bool("deviation_isis_multi_topology_unsupported", false,
@@ -512,4 +519,7 @@ var (
 	swVersionUnsupported = flag.Bool("deviation_sw_version_unsupported", false, "Device does not support reporting software version according to the requirements in gNMI-1.10.")
 
 	hierarchicalWeightResolutionTolerance = flag.Float64("deviation_hierarchical_weight_resolution_tolerance", 0.2, "Set it to expected ucmp traffic tolerance, default is 0.2")
+
+	skipPlqQualificationRateCheck = flag.Bool("deviation_skip_plq_qualification_rate_check", false,
+		"Skip PLQ QualificationRateBytesPerSecond and ExpectedRateBytesPerSecond comparison if value is true, Default value is false.")
 )
