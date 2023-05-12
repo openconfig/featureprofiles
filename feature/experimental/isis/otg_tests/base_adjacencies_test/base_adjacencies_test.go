d54c1bfe5ab40e499f742bb396a012f480c11887
Prinika Nair <prinikasn@google.com>
2023-05-12 14:08:28 -0700

    remove ondatra import

--- feature/experimental/isis/otg_tests/base_adjacencies_test/base_adjacencies_test.go
+++ feature/experimental/isis/otg_tests/base_adjacencies_test/base_adjacencies_test.go
@@ -28,7 +28,6 @@ import (
 	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/featureprofiles/internal/fptest"
 	"github.com/openconfig/featureprofiles/internal/otgutils"
-	"github.com/openconfig/ondatra"
 	"github.com/openconfig/ondatra/gnmi"
 	"github.com/openconfig/ondatra/gnmi/oc"
 
