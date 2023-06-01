996514298d7a9b7248f3a2038c6aec1c596fd755
saarvind6481 <123205208+saarvind6481@users.noreply.github.com>
2023-06-01 16:55:27 +0530

    Update qos_output_queue_counters_test.go

--- feature/qos/ate_tests/qos_output_queue_counters_test/qos_output_queue_counters_test.go
+++ feature/qos/ate_tests/qos_output_queue_counters_test/qos_output_queue_counters_test.go
@@ -112,13 +112,13 @@ func TestQoSCounters(t *testing.T) {
 		}
 	case ondatra.CISCO:
 		trafficFlows = map[string]*trafficData{
-			"flow-nc1": {frameSize: 700, trafficRate: 7, dscp: 56, queue: "a_NC1"},
-			"flow-af4": {frameSize: 400, trafficRate: 4, dscp: 32, queue: "b_AF4"},
-			"flow-af3": {frameSize: 1300, trafficRate: 3, dscp: 24, queue: "c_AF3"},
-			"flow-af2": {frameSize: 1200, trafficRate: 2, dscp: 16, queue: "d_AF2"},
-			"flow-af1": {frameSize: 1000, trafficRate: 10, dscp: 8, queue: "e_AF1"},
-			"flow-be0": {frameSize: 1110, trafficRate: 1, dscp: 4, queue: "f_BE0"},
-			"flow-be1": {frameSize: 1111, trafficRate: 1, dscp: 0, queue: "g_BE1"},
+			"flow-nc1": {frameSize: 700, trafficRate: 7, dscp: 56, queue: "NC1"},
+			"flow-af4": {frameSize: 400, trafficRate: 4, dscp: 32, queue: "AF4"},
+			"flow-af3": {frameSize: 1300, trafficRate: 3, dscp: 24, queue: "AF3"},
+			"flow-af2": {frameSize: 1200, trafficRate: 2, dscp: 16, queue: "AF2"},
+			"flow-af1": {frameSize: 1000, trafficRate: 10, dscp: 8, queue: "AF1"},
+			"flow-be0": {frameSize: 1110, trafficRate: 1, dscp: 4, queue: "BE0"},
+			"flow-be1": {frameSize: 1111, trafficRate: 1, dscp: 0, queue: "BE1"},
 		}
 	default:
 		t.Fatalf("Output queue mapping is missing for %v", dut.Vendor().String())
