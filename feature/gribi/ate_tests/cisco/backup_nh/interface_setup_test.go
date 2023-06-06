e8061988e039a9a184c92d9b44a0879eb2f17978
manan-patel <mananpat@cisco.com>
2023-06-06 16:44:29 -0400

    fixes

--- feature/gribi/ate_tests/cisco/backup_nh/interface_setup_test.go
+++ feature/gribi/ate_tests/cisco/backup_nh/interface_setup_test.go
@@ -20,7 +20,6 @@ import (
 
 	"github.com/openconfig/featureprofiles/internal/attrs"
 	ciscoFlags "github.com/openconfig/featureprofiles/internal/cisco/flags"
-	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/ondatra"
 	"github.com/openconfig/ondatra/gnmi"
 	"github.com/openconfig/ondatra/gnmi/oc"
@@ -158,22 +157,13 @@ var (
 func configInterfaceDUT(i *oc.Interface, a *attrs.Attributes) *oc.Interface {
 	i.Description = ygot.String(a.Desc)
 	i.Type = oc.IETFInterfaces_InterfaceType_ieee8023adLag
-	if *deviations.InterfaceEnabled {
-		i.Enabled = ygot.Bool(true)
-	}
 
 	s := i.GetOrCreateSubinterface(0)
 	s4 := s.GetOrCreateIpv4()
-	if *deviations.InterfaceEnabled {
-		s4.Enabled = ygot.Bool(true)
-	}
 	s4a := s4.GetOrCreateAddress(a.IPv4)
 	s4a.PrefixLength = ygot.Uint8(ipv4PrefixLen)
 
 	s6 := s.GetOrCreateIpv6()
-	if *deviations.InterfaceEnabled {
-		s6.Enabled = ygot.Bool(true)
-	}
 	s6a := s6.GetOrCreateAddress(a.IPv6)
 	s6a.PrefixLength = ygot.Uint8(ipv6PrefixLen)
 
