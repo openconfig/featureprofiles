3b4e8d95ff00a3c92e91245c0bf470daa42d7011
Prinika Nair <prinikasn@google.com>
2023-06-13 15:31:31 -0700

    compare with vendor String

--- topologies/binding/binding.go
+++ topologies/binding/binding.go
@@ -322,8 +322,8 @@ func dims(td *opb.Device, bd *bindpb.Device) (*binding.Dims, error) {
 	if err != nil {
 		return nil, err
 	}
-  if bd.GetVendor() != "" && bd.GetHardwareModel() != "" {
-		if bd.GetVendor() != td.Vendor || bd.GetHardwareModel() != td.GetHardwareModel() {
+  if bd.GetVendor().String() != "" && bd.GetHardwareModel() != "" {
+		if bd.GetVendor().String() != td.Vendor.String() || bd.GetHardwareModel() != td.GetHardwareModel() {
 			return nil, fmt.Errorf("values for device vendor %v and device hardwaremodel %v do not match binding vendor %v and binding hardwaremodel %v", td.GetVendor(), td.GetHardwareModel(), bd.GetVendor(), bd.GetHardwareModel())
 		}
 		return &binding.Dims{
