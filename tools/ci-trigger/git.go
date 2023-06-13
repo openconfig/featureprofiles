d0d479ce4a7090439ac68b8ffa4397b9449e61ef
Brandon Stoll <bstoll@users.noreply.github.com>
2023-06-13 19:02:55 +0000

    Identify the common ancestor for diff using git merge-base instead of GitHub API base hash.

--- tools/ci-trigger/git.go
+++ tools/ci-trigger/git.go
@@ -15,9 +15,12 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
 // setupGitClone clones the GitHub repository into tmpDir and fetches refs from remoteURL.
@@ -53,12 +56,18 @@ func setupGitClone(tmpDir, remoteURL, head string) (*git.Repository, error) {
 	return repo, nil
 }
 
-// modifiedFiles returns a list of files affected for any reason between the head and base SHA hashes.
-func modifiedFiles(repo *git.Repository, head, base string) ([]string, error) {
+// modifiedFiles returns a list of files affected for any reason between the head and main branch.
+func modifiedFiles(repo *git.Repository, head string) ([]string, error) {
 	headCommit, err := repo.CommitObject(plumbing.NewHash(head))
 	if err != nil {
 		return nil, err
 	}
+
+	base, err := mergeBase(repo, head)
+	if err != nil {
+		return nil, err
+	}
+
 	baseCommit, err := repo.CommitObject(plumbing.NewHash(base))
 	if err != nil {
 		return nil, err
@@ -80,4 +89,38 @@ func modifiedFiles(repo *git.Repository, head, base string) ([]string, error) {
 		}
 	}
 	return result, nil
+}
+
+// mergeBase returns the common ancestor hash from the head commit and
+// origin/main of the repo.
+func mergeBase(repo *git.Repository, head string) (string, error) {
+	commitRevs := []string{"origin/main", head}
+	var hashes []*plumbing.Hash
+	for _, rev := range commitRevs {
+		hash, err := repo.ResolveRevision(plumbing.Revision(rev))
+		if err != nil {
+			return "", fmt.Errorf("could not parse revision '%s': %w", rev, err)
+		}
+		hashes = append(hashes, hash)
+	}
+
+	var commits []*object.Commit
+	for _, hash := range hashes {
+		commit, err := repo.CommitObject(*hash)
+		if err != nil {
+			return "", fmt.Errorf("could not find commit '%s': %w", hash.String(), err)
+		}
+		commits = append(commits, commit)
+	}
+
+	res, err := commits[0].MergeBase(commits[1])
+	if err != nil {
+		return "", fmt.Errorf("could not traverse the repository history: %w", err)
+	}
+
+	if len(res) < 1 {
+		return "", fmt.Errorf("no common git ancestor found for hash '%s'", head)
+	}
+
+	return res[0].Hash.String(), nil
 }
