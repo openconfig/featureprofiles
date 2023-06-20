ed9826195ae8d6dbb3a69680944f5eef0c8d769b
Chris Morrow <morrowc@ops-netman.net>
2023-06-20 05:16:16 +0000

    Add to the creation script the ability to create client/server certs.
    Add client and server certificate/key/req creation to the mk_cas shell
    script.

--- feature/security/certificate-authorities/mk_cas.sh
+++ feature/security/certificate-authorities/mk_cas.sh
@@ -3,7 +3,8 @@
 # Create test certificate authority content for feature
 # profile test cases.
 
-# The list of directories of CA contents, also the count of CAs built in each directory.
+# The list of directories of CA contents, also the count of CAs built
+# in each directory.
 DIRS=(01 02 10 1000)
 
 # The types of signatures to support for the CA Certs.
@@ -11,6 +12,12 @@ TYPES=(rsa ecdsa)
 
 # The ECDSA curve to use in creating ECDSA keys/certificates.
 CURVE=prime256v1
+
+# The length of an RSA key to generate/use in openssl comamnds.
+RSAKEYLEN=2048
+
+# Lifetime of certificates.
+LIFETIME=3650
 
 # Create RSA and ECDSA CA keys, and associated certificates.
 for d in ${DIRS[@]} ; do 
@@ -22,14 +29,15 @@ for d in ${DIRS[@]} ; do
     for t in ${TYPES[@]}; do
       case ${t} in
         rsa)
-          openssl genrsa -out ca-${d}/ca-${OFFSET}-${t}-key.pem 2048
+          openssl genrsa -out ca-${d}/ca-${OFFSET}-${t}-key.pem ${RSAKEYLEN}
           ;;
         ecdsa)
-          openssl ecparam -name ${CURVE} -out ca-${d}/ca-${OFFSET}-${t}-key.pem -genkey
+          openssl ecparam -name ${CURVE} \
+            -out ca-${d}/ca-${OFFSET}-${t}-key.pem -genkey
           ;;
       esac
       # Create a cert with the fresh key.
-      openssl req -new -x509 -nodes -days 3650 \
+      openssl req -new -x509 -nodes -days ${LIFETIME} \
         -key ca-${d}/ca-${OFFSET}-${t}-key.pem \
         -out ca-${d}/ca-${OFFSET}-${t}-cert.pem \
         -subj "/CN=CA ${OFFSET}/C=AQ/ST=NZ/L=NZ/O=OpenConfigFeatureProfiles"
@@ -41,5 +49,60 @@ done
 for d in ${DIRS[@]}; do
   for t in ${TYPES[@]}; do
     cat ca-${d}/ca-*-${t}-cert.pem > ca-${d}/trust_bundle_${d}_${t}.pem
+  done
+done
+
+# Create client / server certificates for each CA set.
+# One client and one server certificates are all that are required per type.
+#   * Create a key per type for each client/server certificate to create.
+#   * Create a CSR per type for each client/server certificate to create.
+#   * Use the CA + extensions config to create the client certificates.
+#
+# Repeat for the server certificate creation.
+for  d in ${DIRS[@]}; do
+  if [ ! -d ca-${d} ] ; then
+    mkdir ca-${d}
+  fi
+  for t in ${TYPES[@]}; do
+    OFFSET=$(printf "%04i" ${d})
+    # Create both client and server cert keys for each type.
+    case ${t} in
+      rsa)
+        openssl genrsa -out ca-${d}/client-${t}-key.pem ${RSAKEYLEN}
+        openssl genrsa -out ca-${d}/server-${t}-key.pem ${RSAKEYLEN}
+        ;;
+      ecdsa)
+        openssl ecparam -name ${CURVE} \
+          -out ca-${d}/client-${t}-key.pem -genkey
+        openssl ecparam -name ${CURVE} \
+          -out ca-${d}/server-${t}-key.pem -genkey
+        ;;
+    esac
+
+    # Create the client and server requests.
+    openssl req -new -key ca-${d}/client-${t}-key.pem \
+      -out ca-${d}/client-${t}-req.pem \
+      -config client_cert.cnf
+    openssl req -new -key ca-${d}/server-${t}-key.pem \
+      -out ca-${d}/server-${t}-req.pem \
+      -config server_cert.cnf
+
+    # Create the client and server complete certificates.
+    openssl x509 -req -in ca-${d}/client-${t}-req.pem \
+      -CA ca-${d}/ca-${OFFSET}-${t}-cert.pem \
+      -CAkey ca-${d}/ca-${OFFSET}-${t}-key.pem \
+      -out ca-${d}/client-${t}-cert.pem \
+      -CAcreateserial \
+      -days ${LIFETIME} \
+      -sha256 \
+      -extfile client_cert_ext.cnf
+    openssl x509 -req -in ca-${d}/server-${t}-req.pem \
+      -CA ca-${d}/ca-${OFFSET}-${t}-cert.pem \
+      -CAkey ca-${d}/ca-${OFFSET}-${t}-key.pem \
+      -out ca-${d}/server-${t}-cert.pem \
+      -CAcreateserial \
+      -days ${LIFETIME} \
+      -sha256 \
+      -extfile server_cert_ext.cnf
   done
 done
