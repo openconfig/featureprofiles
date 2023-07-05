89265256cbdd30c5b2e3532ceb59dfa2e5481a5e
Rob Shakir <robjs@google.com>
2023-07-05 21:17:38 +0000

    Reduce size of change for MPLS testing.

--- feature/gribi/otg_tests/mpls_compliance/compliance_test.go
+++ feature/gribi/otg_tests/mpls_compliance/compliance_test.go
@@ -1,6 +1,7 @@
 package gribi_mpls_compliance_test
 
 import (
+	"flag"
 	"fmt"
 	"testing"
 	"time"
@@ -17,15 +18,13 @@ const (
 	baseLabel = 42
 )
 
+var (
+	sleep = flag.Int("sleep", 0, "seconds to sleep within test before exiting")
+)
+
 func TestMain(m *testing.M) {
 	fptest.RunTests(m)
 }
-
-// Test cases to write:
-//	* push(N) labels, N = 1-20.
-//	* pop(1) - terminating action
-//	* pop(1) + push(N)
-//	* pop(all) + push(N)
 
 // TestMPLSLabelPushDepth validates the gRIBI actions that are used to push N labels onto
 // a packet as part of routing towards a next-hop. Note that this test does not
@@ -37,75 +36,12 @@ func TestMPLSLabelPushDepth(t *testing.T) {
 	c.Connection().WithStub(gribic)
 
 	_ = PushBaseConfigs(t, ondatra.DUT(t, "dut"), ondatra.ATE(t, "ate"))
-	sleepFn := func(_ *testing.T, _ []uint32) { time.Sleep(30 * time.Second) }
+	sleepFn := func(_ *testing.T, _ []uint32) { time.Sleep(*sleep * time.Second) }
 
 	baseLabel := 42
 	for i := 1; i <= 20; i++ {
 		t.Run(fmt.Sprintf("push %d labels", i), func(t *testing.T) {
 			EgressLabelStack(t, c, defNIName, baseLabel, i, sleepFn)
-		})
-	}
-}
-
-// TestMPLSPushToIP validates the gRIBI actions that are used to push N labels onto
-// an IP packet. Note that this test does not validate against the dataplane.
-func TestMPLSPushToIP(t *testing.T) {
-	dut := ondatra.DUT(t, "dut")
-	gribic := dut.RawAPIs().GRIBI().Default(t)
-	c := fluent.NewClient()
-	c.Connection().WithStub(gribic)
-
-	baseLabel := 42
-	numLabels := 20
-	for i := 1; i <= numLabels; i++ {
-		t.Run(fmt.Sprintf("push %d labels to IP", i), func(t *testing.T) {
-			PushToIPPacket(t, c, defNIName, baseLabel, i, nil)
-		})
-	}
-}
-
-// TestPopTopLabel validates the gRIBI actions that are used to pop the top label
-// when specified in a next-hop.
-func TestPopTopLabel(t *testing.T) {
-	dut := ondatra.DUT(t, "dut")
-	gribic := dut.RawAPIs().GRIBI().Default(t)
-	c := fluent.NewClient()
-	c.Connection().WithStub(gribic)
-
-	PopTopLabel(t, c, defNIName, nil)
-}
-
-// TestPopNLabels validates the gRIBI actions that are used to pop N labels from a
-// label stack when specified in a next-hop.
-func TestPopNLabels(t *testing.T) {
-	dut := ondatra.DUT(t, "dut")
-	gribic := dut.RawAPIs().GRIBI().Default(t)
-	c := fluent.NewClient()
-	c.Connection().WithStub(gribic)
-
-	for _, stack := range [][]uint32{{100}, {100, 42}, {100, 42, 43, 44, 45}} {
-		t.Run(fmt.Sprintf("pop N labels, stack %v", stack), func(t *testing.T) {
-			PopNLabels(t, c, defNIName, stack, nil)
-		})
-	}
-}
-
-// TestPopOnePushN validates the gRIBI actions that are used to pop 1 label and then
-// push N when specified in a next-hop.
-func TestPopOnePushN(t *testing.T) {
-	dut := ondatra.DUT(t, "dut")
-	gribic := dut.RawAPIs().GRIBI().Default(t)
-	c := fluent.NewClient()
-	c.Connection().WithStub(gribic)
-
-	stacks := [][]uint32{
-		{100}, // swap for label 100, pop+push for label 200
-		{100, 200, 300, 400},
-		{100, 200, 300, 400, 500, 600},
-	}
-	for _, stack := range stacks {
-		t.Run(fmt.Sprintf("pop one, push N, stack: %v", stack), func(t *testing.T) {
-			PopOnePushN(t, c, defNIName, stack, nil)
 		})
 	}
 }
