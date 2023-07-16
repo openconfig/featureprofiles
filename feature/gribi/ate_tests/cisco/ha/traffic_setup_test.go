c7d10118a335d753eca4690551fa9c41bbcd233f
manan-patel <mananpat@cisco.com>
2023-07-15 21:16:53 -0400

    july15

--- feature/gribi/ate_tests/cisco/ha/traffic_setup_test.go
+++ feature/gribi/ate_tests/cisco/ha/traffic_setup_test.go
@@ -117,20 +117,20 @@ func configureATE(t *testing.T, ate *ondatra.ATEDevice) *ondatra.ATETopology {
 }
 
 // addAteISISL2 configures ISIS L2 ATE config
-func addAteISISL2(t *testing.T, topo *ondatra.ATETopology, atePort, areaId, network_name string, metric uint32, v4prefix string, count uint32) {
+// func addAteISISL2(t *testing.T, topo *ondatra.ATETopology, atePort, areaId, network_name string, metric uint32, v4prefix string, count uint32) {
 
-	intfs := topo.Interfaces()
-	if len(intfs) == 0 {
-		t.Fatal("There are no interfaces in the Topology")
-	}
-	network := intfs[atePort].AddNetwork(network_name)
-	network.ISIS().WithIPReachabilityMetric(metric + 1)
-	network.IPv4().WithAddress(v4prefix).WithCount(count)
-	rnetwork := intfs[atePort].AddNetwork("recursive")
-	rnetwork.ISIS().WithIPReachabilityMetric(metric + 1)
-	rnetwork.IPv4().WithAddress("100.100.100.100/32")
-	intfs[atePort].ISIS().WithAreaID(areaId).WithLevelL2().WithNetworkTypePointToPoint().WithMetric(metric).WithWideMetricEnabled(true)
-}
+// 	intfs := topo.Interfaces()
+// 	if len(intfs) == 0 {
+// 		t.Fatal("There are no interfaces in the Topology")
+// 	}
+// 	network := intfs[atePort].AddNetwork(network_name)
+// 	network.ISIS().WithIPReachabilityMetric(metric + 1)
+// 	network.IPv4().WithAddress(v4prefix).WithCount(count)
+// 	rnetwork := intfs[atePort].AddNetwork("recursive")
+// 	rnetwork.ISIS().WithIPReachabilityMetric(metric + 1)
+// 	rnetwork.IPv4().WithAddress("100.100.100.100/32")
+// 	intfs[atePort].ISIS().WithAreaID(areaId).WithLevelL2().WithNetworkTypePointToPoint().WithMetric(metric).WithWideMetricEnabled(true)
+// }
 
 // addAteEBGPPeer configures EBGP ATE config
 func addAteEBGPPeer(t *testing.T, topo *ondatra.ATETopology, atePort, peerAddress string, localAsn uint32, network_name, nexthop, prefix string, count uint32, useLoopback bool) {
