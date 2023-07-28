3c204a352c7fdb1101eac9060875d4b46d1dd331
mojiiba <mbagherz@cisco.com>
2023-07-28 02:47:19 -0400

    push generated code

---
+++ internal/cisco/security/gnxi/gnxi.go
@@ -0,0 +1,18 @@
+package gnxi
+import (
+	//"testing"
+	"crypto/tls"
+	"crypto/x509"
+
+	//"github.com/openconfig/ondatra"
+	//
+
+)
+
+type ExecRPCFunction func(deviceAddress string, certificate tls.Certificate, trustBundle *x509.CertPool, params ...any) error
+
+type RPC struct {
+	Service, Name, QFN, Path string
+	ExecFunc                 ExecRPCFunction
+}
+
