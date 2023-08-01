fb1acdef599b1d083b3e686cfbac146cb8f14ae6
Harish Kumar Krishnan <harikri4@cisco.com>
2023-07-31 22:22:02 -0700

    Addressed Karim Comments + Added Fail Logic

---
+++ feature/cisco/performance/gnmiscale_test.go
@@ -0,0 +1,26 @@
+package gnmi_scale_test
+
+import (
+	"strconv"
+	"testing"
+	"time"
+	"github.com/openconfig/featureprofiles/internal/fptest"
+	"github.com/openconfig/ondatra/gnmi"
+	"github.com/openconfig/ondatra"
+)
+
+func TestMain(m *testing.M) {
+	fptest.RunTests(m)
+}
+
+func TestGNMIScale(t *testing.T) {
+	dut := ondatra.DUT(t, "dut")
+	beforeTime := time.Now()
+	for i := 0; i <= 100; i++ {
+		gnmi.Update(t, dut, gnmi.OC().System().Hostname().Config(), "test"+strconv.Itoa(i))
+	}
+	t.Logf("Time to do 100 gnmi uodate is %s", time.Since(beforeTime).String())
+	if int(time.Since(beforeTime).Seconds()) >= 180 {
+		t.Fatalf("GNMI Scale Took too long")
+	}
+}
