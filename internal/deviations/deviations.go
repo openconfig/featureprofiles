5c59e1b4b39a1f12dd09bc7c79aeef549e6ec005
anksaiki <ankursaikia@google.com>
2023-08-03 11:52:12 -0400

    deviations for drained_configuration (#1946)
    * deviations for drained_configuration
    
    * update packages
    
    * update ondatra
    
    * Update med checks
    
    * go vet changes

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -506,3 +506,15 @@ func SkipFabricCardPowerAdmin(dut *ondatra.DUTDevice) bool {
 func ComponentPowerDownReturnsInactiveState(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetComponentPowerDownReturnsInactiveState()
 }
+
+// ISISRequireSameL1MetricWithL2Metric returns true for devices that require configuring
+// the same ISIS Metrics for Level 1 when configuring Level 2 Metrics.
+func ISISRequireSameL1MetricWithL2Metric(dut *ondatra.DUTDevice) bool {
+	return lookupDUTDeviations(dut).GetIsisRequireSameL1MetricWithL2Metric()
+}
+
+// BGPSetMedRequiresEqualOspfSetMetric returns true for devices that require configuring
+// the same OSPF setMetric when BGP SetMED is configured.
+func BGPSetMedRequiresEqualOspfSetMetric(dut *ondatra.DUTDevice) bool {
+	return lookupDUTDeviations(dut).GetBgpSetMedRequiresEqualOspfSetMetric()
+}
