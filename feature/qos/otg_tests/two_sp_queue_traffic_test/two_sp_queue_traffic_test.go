1c985bdae1d1f8d002f9dc8692a099179a3f23db
trathod <tushar.rathod@nokia.com>
2023-08-10 19:02:09 -0400

    DP-1.8: Adding test support for Nokia    Following changes are made in this PR -       1. Updating ConfigureQoS for QoS OC configuration for Nokia       2. Adding function (SetForwardingGroupWithFabricPriority) to configure ForwardingGroup with Fabric-priority in qoscfg.go
    "This code is a Contribution to the OpenConfig Feature Profiles project ("Work") made under the Google Software Grant and Corporate Contributor License Agreement ("CLA") and governed by the Apache License 2.0. No other rights or licenses in or to any of Nokia's intellectual property are granted for any other purpose. This code is provided on an "as is" basis without any warranties of any kind."
    
    Co-authored-by: Krishna Bovilla <krishna.bovilla@nokia.com>

--- feature/qos/otg_tests/two_sp_queue_traffic_test/two_sp_queue_traffic_test.go
+++ feature/qos/otg_tests/two_sp_queue_traffic_test/two_sp_queue_traffic_test.go
@@ -18,6 +18,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/openconfig/entity-naming/entname"
 	"github.com/openconfig/featureprofiles/internal/attrs"
 	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/featureprofiles/internal/fptest"
@@ -164,6 +165,17 @@ func TestTwoSPQueueTraffic(t *testing.T) {
 	var tolerance float32 = 2.0
 	queues := netutil.CommonTrafficQueues(t, dut)
 
+	if dut.Vendor() == ondatra.NOKIA {
+		queues = &entname.CommonTrafficQueueNames{
+			NC1: "NC1",
+			AF4: "AF4",
+			AF3: "AF3",
+			AF2: "AF2",
+			AF1: "AF1",
+			BE1: "BE1",
+			BE0: "BE0",
+		}
+	}
 	// Test case 1: Non-oversubscription NC1 and AF4 traffic.
 	//   - There should be no packet drop for all traffic classes.
 	nonOversubscribedTrafficFlows1 := map[string]*trafficData{
@@ -1203,6 +1215,14 @@ func ConfigureDUTIntf(t *testing.T, dut *ondatra.DUTDevice) {
 		a := s.GetOrCreateAddress(intf.ipAddr)
 		a.PrefixLength = ygot.Uint8(intf.prefixLen)
 		gnmi.Replace(t, dut, gnmi.OC().Interface(intf.intfName).Config(), i)
+		if deviations.ExplicitInterfaceInDefaultVRF(dut) {
+			fptest.AssignToNetworkInstance(t, dut, intf.intfName, deviations.DefaultNetworkInstance(dut), 0)
+		}
+	}
+	if deviations.ExplicitPortSpeed(dut) {
+		fptest.SetPortSpeed(t, dp1)
+		fptest.SetPortSpeed(t, dp2)
+		fptest.SetPortSpeed(t, dp3)
 	}
 }
 
@@ -1215,44 +1235,67 @@ func ConfigureQoS(t *testing.T, dut *ondatra.DUTDevice) {
 	q := d.GetOrCreateQos()
 	queues := netutil.CommonTrafficQueues(t, dut)
 
+	if dut.Vendor() == ondatra.NOKIA {
+		queues = &entname.CommonTrafficQueueNames{
+			NC1: "NC1",
+			AF4: "AF4",
+			AF3: "AF3",
+			AF2: "AF2",
+			AF1: "AF1",
+			BE1: "BE1",
+			BE0: "BE0",
+		}
+	}
 	t.Logf("Create qos forwarding groups config")
 	forwardingGroups := []struct {
-		desc        string
-		queueName   string
-		targetGroup string
+		desc           string
+		queueName      string
+		targetGroup    string
+		fabricPriority uint8
 	}{{
-		desc:        "forwarding-group-BE1",
-		queueName:   queues.BE1,
-		targetGroup: "target-group-BE1",
+		desc:           "forwarding-group-BE1",
+		queueName:      queues.BE1,
+		targetGroup:    "target-group-BE1",
+		fabricPriority: 1,
 	}, {
-		desc:        "forwarding-group-BE0",
-		queueName:   queues.BE0,
-		targetGroup: "target-group-BE0",
+		desc:           "forwarding-group-BE0",
+		queueName:      queues.BE0,
+		targetGroup:    "target-group-BE0",
+		fabricPriority: 2,
 	}, {
-		desc:        "forwarding-group-AF1",
-		queueName:   queues.AF1,
-		targetGroup: "target-group-AF1",
+		desc:           "forwarding-group-AF1",
+		queueName:      queues.AF1,
+		targetGroup:    "target-group-AF1",
+		fabricPriority: 3,
 	}, {
-		desc:        "forwarding-group-AF2",
-		queueName:   queues.AF2,
-		targetGroup: "target-group-AF2",
+		desc:           "forwarding-group-AF2",
+		queueName:      queues.AF2,
+		targetGroup:    "target-group-AF2",
+		fabricPriority: 4,
 	}, {
-		desc:        "forwarding-group-AF3",
-		queueName:   queues.AF3,
-		targetGroup: "target-group-AF3",
+		desc:           "forwarding-group-AF3",
+		queueName:      queues.AF3,
+		targetGroup:    "target-group-AF3",
+		fabricPriority: 5,
 	}, {
-		desc:        "forwarding-group-AF4",
-		queueName:   queues.AF4,
-		targetGroup: "target-group-AF4",
+		desc:           "forwarding-group-AF4",
+		queueName:      queues.AF4,
+		targetGroup:    "target-group-AF4",
+		fabricPriority: 6,
 	}, {
-		desc:        "forwarding-group-NC1",
-		queueName:   queues.NC1,
-		targetGroup: "target-group-NC1",
+		desc:           "forwarding-group-NC1",
+		queueName:      queues.NC1,
+		targetGroup:    "target-group-NC1",
+		fabricPriority: 7,
 	}}
 
 	t.Logf("qos forwarding groups config: %v", forwardingGroups)
 	for _, tc := range forwardingGroups {
-		qoscfg.SetForwardingGroup(t, dut, q, tc.targetGroup, tc.queueName)
+		if dut.Vendor() == ondatra.NOKIA {
+			qoscfg.SetForwardingGroupWithFabricPriority(t, dut, q, tc.targetGroup, tc.queueName, tc.fabricPriority)
+		} else {
+			qoscfg.SetForwardingGroup(t, dut, q, tc.targetGroup, tc.queueName)
+		}
 	}
 
 	t.Logf("Create qos Classifiers config")
@@ -1511,7 +1554,13 @@ func ConfigureQoS(t *testing.T, dut *ondatra.DUTDevice) {
 		input.SetId(tc.inputID)
 		input.SetInputType(tc.inputType)
 		input.SetQueue(tc.queueName)
-		input.SetWeight(tc.weight)
+		if dut.Vendor() == ondatra.NOKIA {
+			if tc.priority != oc.Scheduler_Priority_STRICT {
+				input.SetWeight(tc.weight)
+			}
+		} else {
+			input.SetWeight(tc.weight)
+		}
 		gnmi.Replace(t, dut, gnmi.OC().Qos().Config(), q)
 	}
 
