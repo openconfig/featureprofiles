064d2a37632e13d5ac213ac3ca6276e569fab5c2
Karim Jahed <kjahed@cisco.com>
2023-08-21 17:20:30 -0400

    added binding utils

---
+++ exec/utils/binding/tojson/tojson.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	bindpb "github.com/openconfig/featureprofiles/topologies/proto/binding"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/encoding/prototext"
+)
+
+var (
+	bindingFileFlag = flag.String("binding", "", "Path to binding file")
+	outputFileFlag  = flag.String("out", "", "Path to output json file")
+)
+
+func main() {
+	flag.Parse()
+
+	if *bindingFileFlag == "" {
+		die("Missing binding arg")
+	}
+	if *outputFileFlag == "" {
+		die("Missing out arg")
+	}
+
+	in, err := os.ReadFile(*bindingFileFlag)
+	if err != nil {
+		die("Unable to read binding file: %v", err)
+	}
+
+	b := &bindpb.Binding{}
+	if err := prototext.Unmarshal(in, b); err != nil {
+		die("Unable to parse binding file: %v", err)
+	}
+
+	opts := protojson.MarshalOptions{
+		Multiline: true,
+		Indent:    "  ",
+	}
+
+	j, err := opts.Marshal(b)
+	if err != nil {
+		die("Unable to marshal binding: %v", err)
+	}
+
+	os.WriteFile(*outputFileFlag, j, 0644)
+}
+
+func die(format string, a ...any) {
+	fmt.Printf(format+"\n", a...)
+	os.Exit(1)
+}
