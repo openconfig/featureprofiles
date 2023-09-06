3c9d0c3a3364956d7d835c3492f021891624f514
mojiiba <mbagherz@cisco.com>
2023-09-06 16:28:18 -0400

    gofmt

--- internal/security/svid/svid_test.go
+++ internal/security/svid/svid_test.go
@@ -3,61 +3,60 @@ package svid
 
 import (
 	"crypto/x509"
-	
+
 	"net/url"
 	"testing"
 
-	"github.com/h-fam/errdiff"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/h-fam/errdiff"
 )
 
-
-
-
 func TestGenRSASVID(t *testing.T) {
-	tests:=[]struct{
-		name string
-		caCert string
-		caKey  string
-		username string
-		err string
+	tests := []struct {
+		name         string
+		caCert       string
+		caKey        string
+		username     string
+		err          string
 		keyAlgorithm x509.PublicKeyAlgorithm
-		uris []*url.URL
+		uris         []*url.URL
 	}{
 		{
-			name: "Successful SVID with RSA certificate",
-			username: "test",
-			err: "",
-			caCert: "../testdata/rsa/ca-rsa-cert.pem",
-			caKey: "../testdata/rsa/ca-rsa-key.pem",
+			name:         "Successful SVID with RSA certificate",
+			username:     "test",
+			err:          "",
+			caCert:       "../testdata/rsa/ca-rsa-cert.pem",
+			caKey:        "../testdata/rsa/ca-rsa-key.pem",
 			keyAlgorithm: x509.RSA,
-			uris : []*url.URL{
+			uris: []*url.URL{
 				{
-					Scheme:"" ,
-					Host: "", 
-					Path: "test",
+					Scheme: "",
+					Host:   "",
+					Path:   "test",
 				},
 			},
 		},
 	}
 
-	for _,test:= range tests {
+	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			caPrivateKey, CACert, err := loadKeyPair(test.caKey, test.caCert) ; if err!=nil {
-				t.Fatalf("Unexpected Error, %v", err)	
+			caPrivateKey, CACert, err := loadKeyPair(test.caKey, test.caCert)
+			if err != nil {
+				t.Fatalf("Unexpected Error, %v", err)
 			}
-			key, cert, err:=GenRSASVID(test.username,300,CACert,caPrivateKey); if errdiff.Substring(err,test.err)!=""{
-				t.Fatalf("Unexpected Error, want: %s, got %v", test.err, err)	
+			key, cert, err := GenRSASVID(test.username, 300, CACert, caPrivateKey)
+			if errdiff.Substring(err, test.err) != "" {
+				t.Fatalf("Unexpected Error, want: %s, got %v", test.err, err)
 			}
-			if key==nil || cert==nil {
-				t.Fatalf("Key and CERT must not be nil")	
+			if key == nil || cert == nil {
+				t.Fatalf("Key and CERT must not be nil")
 			}
-			if cert.PublicKeyAlgorithm!=test.keyAlgorithm {
+			if cert.PublicKeyAlgorithm != test.keyAlgorithm {
 				t.Fatalf("KeyAlgorithm mismatch, got %s, wanted %s", cert.PublicKeyAlgorithm.String(), test.keyAlgorithm.String())
 			}
-			if cert.Subject.CommonName!=test.username {
-				t.Errorf("Common name is not as expected, want: %s, got:%s",test.username, cert.Subject.CommonName)
+			if cert.Subject.CommonName != test.username {
+				t.Errorf("Common name is not as expected, want: %s, got:%s", test.username, cert.Subject.CommonName)
 			}
 			opts := []cmp.Option{cmpopts.IgnoreUnexported(*test.uris[0])}
 			if !cmp.Equal(test.uris, cert.URIs, opts...) {
@@ -69,48 +68,50 @@ func TestGenRSASVID(t *testing.T) {
 }
 
 func TestGenECDSASVID(t *testing.T) {
-	tests:=[]struct{
-		name string
-		caCert string
-		caKey  string
-		username string
-		err string
+	tests := []struct {
+		name         string
+		caCert       string
+		caKey        string
+		username     string
+		err          string
 		keyAlgorithm x509.PublicKeyAlgorithm
-		uris []*url.URL
+		uris         []*url.URL
 	}{
 		{
-			name: "Successful SVID with ECDSA certificate",
-			username: "spiffe://test-abc.foo.bar/xyz/admin",
-			err: "",
-			caCert: "../testdata/ecdsa/ca-ecdsa-cert.pem",
-			caKey:  "../testdata/ecdsa/ca-ecdsa-key.pem",
+			name:         "Successful SVID with ECDSA certificate",
+			username:     "spiffe://test-abc.foo.bar/xyz/admin",
+			err:          "",
+			caCert:       "../testdata/ecdsa/ca-ecdsa-cert.pem",
+			caKey:        "../testdata/ecdsa/ca-ecdsa-key.pem",
 			keyAlgorithm: x509.ECDSA,
-			uris : []*url.URL{
+			uris: []*url.URL{
 				{
-					Scheme:"spiffe" ,
-					Host: "test-abc.foo.bar", 
-					Path: "/xyz/admin",
+					Scheme: "spiffe",
+					Host:   "test-abc.foo.bar",
+					Path:   "/xyz/admin",
 				},
 			},
 		},
 	}
 
-	for _,test:= range tests {
+	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			caPrivateKey, CACert, err := loadKeyPair(test.caKey, test.caCert) ; if err!=nil {
-				t.Fatalf("Unexpected Error, %v", err)	
+			caPrivateKey, CACert, err := loadKeyPair(test.caKey, test.caCert)
+			if err != nil {
+				t.Fatalf("Unexpected Error, %v", err)
 			}
-			key, cert, err:=GenECDSASVID(test.username,300,CACert,caPrivateKey); if errdiff.Substring(err,test.err)!=""{
-				t.Fatalf("Unexpected Error, want: %s, got %v", test.err, err)	
+			key, cert, err := GenECDSASVID(test.username, 300, CACert, caPrivateKey)
+			if errdiff.Substring(err, test.err) != "" {
+				t.Fatalf("Unexpected Error, want: %s, got %v", test.err, err)
 			}
-			if key==nil || cert==nil {
-				t.Fatalf("Key and CERT must not be nil")	
+			if key == nil || cert == nil {
+				t.Fatalf("Key and CERT must not be nil")
 			}
-			if cert.PublicKeyAlgorithm!=test.keyAlgorithm {
+			if cert.PublicKeyAlgorithm != test.keyAlgorithm {
 				t.Fatalf("KeyAlgorithm mismatch, got %s, wanted %s", cert.PublicKeyAlgorithm.String(), test.keyAlgorithm.String())
 			}
-			if cert.Subject.CommonName!=test.username {
-				t.Errorf("Common name is not as expected, want: %s, got:%s",test.username, cert.Subject.CommonName)
+			if cert.Subject.CommonName != test.username {
+				t.Errorf("Common name is not as expected, want: %s, got:%s", test.username, cert.Subject.CommonName)
 			}
 			opts := []cmp.Option{cmpopts.IgnoreUnexported(*test.uris[0])}
 			if !cmp.Equal(test.uris, cert.URIs, opts...) {
