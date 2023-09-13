3f0f327b90e672ca4202a47ac3a14e6b7944769c
SenthilkumarSelvaganapathy <kusenthil@juniper.net>
2023-09-13 00:11:42 -0700

    comment

--- feature/experimental/tunnel/otg_tests/tunnel_interface_based_disable_enable_test/README.md
+++ feature/experimental/tunnel/otg_tests/tunnel_interface_based_disable_enable_test/README.md
@@ -1,51 +1,71 @@
 
  ##  TUN-1.7: Tunnel Interfaces disable and enable - Interface Based GRE Tunnel
  ## Summary
-   Tunnel Interfaces disable and enable - Interface Based GRE Tunnel
+ *   Tunnel Interfaces disable and enable - Interface Based GRE Tunnel
  ## Procedure
-
- *   Configure DUT with 8 GRE encapsulation tunnels and configure another router with 8 GRE Decapsulation tunnel interfaces.(Tunnel-1.1)
- *   Configure 4 tunnel as IPv4 tunnel source and destination address (Tunnel-1.3), 4 as IPv6 tunnel source and destination address (Tunnel-1.4)
- *   Configure static router to point original destination to 8 tunnel interface to do overlay loadbalance
- *   Keep topology tunnel destination will be reachable via 2 underlay interface on both routers
- *   Disable and enable  the numbers of Tunnel interface being used
- *   Disabling Tunnel interfaces:
-     Disable 5 tunnel interfaces
-      *  One by one
-      *  All 5 at once
- *   Static route using the disable interface should become invalid and should not be used in forwarding traffic
- *   Incoming traffic on DUT-PORT1 should be load balanced to available Tunnel interfaces for encapsulation
- *   Incoming traffic flow should be equally distributed for Encapsulation(ECMP)
- *   No traffic loss expected
- *   Enable the tunnel interfaces:
- *   Enable or bring up the same interfaces which were disabled in previous step
-      *  One by one
-      *  All 5 at once
- *   Incoming traffic on DUT-PORT1 should start using additional Tunnel Interfaces  for encapsulation
- *   Incoming traffic flow should be equally distributed for Encapsulation(ECMP)
- *   No traffic loss expected
- *   Disabling and enabling the number of tunnel interfaces and related static route shouldn’t case traffic drops
- *   Verify the Next hop counters for packet being diverted or sent for encapsulation
- *   Verify the tunnel interfaces counters to confirm the traffic encapsulation
- *   Verify the tunnel interfaces traffic/flow for equal distribution for optimal load balancing
- *   After decapsulation, traffic should be load balanced/hash to all available L3 ECMP or LAG or combination of both features
- *   Verify the tunnel interfaces counters to confirm the traffic decapsulation
+ 
+ *   Apply the config mentioned in Tunnel-1.1 
+ *   Send the traffic as mentioned in Tunnel-1.3 and Tunnel-1.4 with TP-1.1 and TP-1.2 
+ *   Disable and enable  the numbers of Tunnel interface being used  
+ *   Disabling Tunnel interfaces: 
+     *  Disable 5 tunnel interfaces 
+        *  One by one  
+        *  All 5 at once 
+ *   Static route using the disable interface should become invalid and should not be used in forwarding traffic 
+ *   Incoming traffic on DUT-PORT1 should be load balanced to available Tunnel interfaces for encapsulation 
+ *   Incoming traffic flow should be equally distributed for Encapsulation(ECMP) 
+ *   No traffic loss expected 
+ *   Enable the tunnel interfaces:  
+     *  Enable or bring up the same interfaces which were disabled in previous step 
+        *  One by one 
+        *  All 5 at once 
+ *   Incoming traffic on DUT-PORT1 should start using additional Tunnel Interfaces  for encapsulation 
+ *   Incoming traffic flow should be equally distributed for Encapsulation(ECMP) 
+ *   No traffic loss expected 
+ *   Disabling and enabling the number of tunnel interfaces and related static route shouldn’t case traffic drops 
+ *   Verify the Next hop counters for packet being diverted or sent for encapsulation 
+ *   Verify the tunnel interfaces counters to confirm the traffic encapsulation  
+ *   Verify the tunnel interfaces traffic/flow for equal distribution for optimal load balancing  
+ *   After decapsulation, traffic should be load balanced/hash to all available L3 ECMP or LAG or combination of both features 
+ *   Verify the tunnel interfaces counters to confirm the traffic decapsulation 
+ 
+ *   Validate system for: 
+ *   Health-1.1 
+ *   No feature related error or drop counters incrementing, 
+ *   discussion with vendors required to highlight additional fields to monitor based on implementation and architecture 
 
  ## Config Parameter coverage
 
- *   openconfig-interfaces:interfaces/interface[name='fti0']
- *   openconfig-interfaces:interfaces/interface[name='fti0']/tunnel/
- *   openconfig-interfaces:interfaces/interface[name='fti0']/tunnel/gre
+ *   openconfig-interfaces:interfaces/interface 
+ *   gre/ 
+ *   gre/decap-group/ 
+ *   gre/dest/ 
+ *   gre/dest/address/ 
+ *   gre/dest/address/ipv4/ 
+ *   gre/dest/address/ipv6/ 
+ *   gre/dest/nexthop-group/ 
+ *   gre/source/ 
+ *   gre/source/address/ 
+ *   gre/source/address/ipv4/ 
+ *   gre/source/address/ipv6/ 
+ *   gre/source/interface/ 
 
  ## Telemetry Parameter coverage
 
+ *   state/counters/in-pkts 
+ *   state/counters/in-octets 
+ *   state/counters/out-pkts 
+ *   state/counters/out-octets 
+ *   state/counters/in-error-pkts 
+ *   state/counters/in-forwarded-pkts 
+ *   state/counters/in-forwarded-octets 
+ *   state/counters/in-discarded-pkts 
+ *   state/counters/out-error-pkts 
+ *   state/counters/out-forwarded-pkts 
+ *   state/counters/out-forwarded-octets 
+ *   state/counters/out-discarded-pkts 
 
- *   /interfaces/interface[name='fti0']/state/counters/in-pkts
- *   /interfaces/interface[name='fti0']/state/counters/in-octets
- *   /interfaces/interface[name='fti0']/state/counters/out-pkts
- *   /interfaces/interface[name='fti0']/state/counters/out-octets
-
-	## Topology:
-
+ ## Topology:
  *   otg:port1 <--> port1:dut1:port3 <--> port3:dut2:port5<--->otg:port5
- *   otg:port2 <--> port2:dut1:port4 <--> port4:dut2:port6<--->otg:port6 
+ *   otg:port2 <--> port2:dut1:port4 <--> port4:dut2:port6<--->otg:port6
+
--- feature/experimental/tunnel/otg_tests/tunnel_interface_based_disable_enable_test/tunnel_interface_disable_enable_test.go
+++ feature/experimental/tunnel/otg_tests/tunnel_interface_based_disable_enable_test/tunnel_interface_disable_enable_test.go
@@ -43,55 +43,77 @@ func TestMain(m *testing.M) {
 	fptest.RunTests(m)
 }
 
-// ## Summary
-// TUN-1.7: Tunnel Interfaces disable and enable - Interface Based GRE Tunnel
-// ## Procedure
-//
-// *   Configure DUT with 8 GRE encapsulation tunnels and configure another router with 8 GRE Decapsulation tunnel interfaces.(Tunnel-1.1)
-// *   Configure 4 tunnel as IPv4 tunnel source and destination address (Tunnel-1.3), 4 as IPv6 tunnel source and destination address (Tunnel-1.4)
-// *   Configure static router to point original destination to 8 tunnel interface to do overlay loadbalance
-// *   Keep topology tunnel destination will be reachable via 2 underlay interface on both routers
-// *   Disable and enable  the numbers of Tunnel interface being used
-// *   Disabling Tunnel interfaces:
-//        Disable 5 tunnel interfaces
-//              *  One by one
-//              *  All 5 at once
-// *   Static route using the disable interface should become invalid and should not be used in forwarding traffic
-// *   Incoming traffic on DUT-PORT1 should be load balanced to available Tunnel interfaces for encapsulation
-// *   Incoming traffic flow should be equally distributed for Encapsulation(ECMP)
-// *   No traffic loss expected
-// *   Enable the tunnel interfaces:
-// *   Enable or bring up the same interfaces which were disabled in previous step
-//      *  One by one
-//      *  All 5 at once
-// *   Incoming traffic on DUT-PORT1 should start using additional Tunnel Interfaces  for encapsulation
-// *   Incoming traffic flow should be equally distributed for Encapsulation(ECMP)
-// *   No traffic loss expected
-// *   Disabling and enabling the number of tunnel interfaces and related static route shouldn’t case traffic drops
-// *   Verify the Next hop counters for packet being diverted or sent for encapsulation
-// *   Verify the tunnel interfaces counters to confirm the traffic encapsulation
-// *   Verify the tunnel interfaces traffic/flow for equal distribution for optimal load balancing
-// *   After decapsulation, traffic should be load balanced/hash to all available L3 ECMP or LAG or combination of both features
-// *   Verify the tunnel interfaces counters to confirm the traffic decapsulation
 
-// ## Config Parameter coverage
-//
-// *   openconfig-interfaces:interfaces/interface[name='fti0']
-// *   openconfig-interfaces:interfaces/interface[name='fti0']/tunnel/
-// *   openconfig-interfaces:interfaces/interface[name='fti0']/tunnel/gre
-//
-// ## Telemetry Parameter coverage
-//
-// *   /interfaces/interface[name='fti0']/state/counters/operstatus
-// *   /interfaces/interface[name='fti0']/state/counters/in-pkts
-// *   /interfaces/interface[name='fti0']/state/counters/in-octets
-// *   /interfaces/interface[name='fti0']/state/counters/out-pkts
-// *   /interfaces/interface[name='fti0']/state/counters/out-octets
-//
-//	## Topology:
-//
-// *   otg:port1 <--> port1:dut1:port3 <--> port3:dut2:port5<--->otg:port5
-// *   otg:port2 <--> port2:dut1:port4 <--> port4:dut2:port6<--->otg:port6
+//  ##  TUN-1.7: Tunnel Interfaces disable and enable - Interface Based GRE Tunnel
+//  ## Summary
+//  *   Tunnel Interfaces disable and enable - Interface Based GRE Tunnel
+//  ## Procedure
+//  
+//  *   Apply the config mentioned in Tunnel-1.1 
+//  *   Send the traffic as mentioned in Tunnel-1.3 and Tunnel-1.4 with TP-1.1 and TP-1.2 
+//  *   Disable and enable  the numbers of Tunnel interface being used  
+//  *   Disabling Tunnel interfaces: 
+//      *  Disable 5 tunnel interfaces 
+//         *  One by one  
+//         *  All 5 at once 
+//  *   Static route using the disable interface should become invalid and should not be used in forwarding traffic 
+//  *   Incoming traffic on DUT-PORT1 should be load balanced to available Tunnel interfaces for encapsulation 
+//  *   Incoming traffic flow should be equally distributed for Encapsulation(ECMP) 
+//  *   No traffic loss expected 
+//  *   Enable the tunnel interfaces:  
+//      *  Enable or bring up the same interfaces which were disabled in previous step 
+//         *  One by one 
+//         *  All 5 at once 
+//  *   Incoming traffic on DUT-PORT1 should start using additional Tunnel Interfaces  for encapsulation 
+//  *   Incoming traffic flow should be equally distributed for Encapsulation(ECMP) 
+//  *   No traffic loss expected 
+//  *   Disabling and enabling the number of tunnel interfaces and related static route shouldn’t case traffic drops 
+//  *   Verify the Next hop counters for packet being diverted or sent for encapsulation 
+//  *   Verify the tunnel interfaces counters to confirm the traffic encapsulation  
+//  *   Verify the tunnel interfaces traffic/flow for equal distribution for optimal load balancing  
+//  *   After decapsulation, traffic should be load balanced/hash to all available L3 ECMP or LAG or combination of both features 
+//  *   Verify the tunnel interfaces counters to confirm the traffic decapsulation 
+// 
+//  *   Validate system for: 
+//  *   Health-1.1 
+//  *   No feature related error or drop counters incrementing, 
+//  *   discussion with vendors required to highlight additional fields to monitor based on implementation and architecture 
+// 
+//  ## Config Parameter coverage
+// 
+//  *   openconfig-interfaces:interfaces/interface 
+//  *   gre/ 
+//  *   gre/decap-group/ 
+//  *   gre/dest/ 
+//  *   gre/dest/address/ 
+//  *   gre/dest/address/ipv4/ 
+//  *   gre/dest/address/ipv6/ 
+//  *   gre/dest/nexthop-group/ 
+//  *   gre/source/ 
+//  *   gre/source/address/ 
+//  *   gre/source/address/ipv4/ 
+//  *   gre/source/address/ipv6/ 
+//  *   gre/source/interface/ 
+// 
+//  ## Telemetry Parameter coverage
+// 
+//  *   state/counters/in-pkts 
+//  *   state/counters/in-octets 
+//  *   state/counters/out-pkts 
+//  *   state/counters/out-octets 
+//  *   state/counters/in-error-pkts 
+//  *   state/counters/in-forwarded-pkts 
+//  *   state/counters/in-forwarded-octets 
+//  *   state/counters/in-discarded-pkts 
+//  *   state/counters/out-error-pkts 
+//  *   state/counters/out-forwarded-pkts 
+//  *   state/counters/out-forwarded-octets 
+//  *   state/counters/out-discarded-pkts 
+// 
+//  ## Topology:
+//  *   otg:port1 <--> port1:dut1:port3 <--> port3:dut2:port5<--->otg:port5
+//  *   otg:port2 <--> port2:dut1:port4 <--> port4:dut2:port6<--->otg:port6
+
 type parameters struct {
 	rtIntf1Ipv4Add  string
 	rtIntf2Ipv4Add  string
@@ -178,7 +200,7 @@ type parameters struct {
 	flow3           string
 	flow4           string
 	trafficDuration int64
-	trafficRate     int64
+	trafficRate     uint64
 }
 
 func GetNetworkAddress(t *testing.T, address string, mask int) string {
@@ -717,85 +739,85 @@ func ConfigureTunnelTermination(t *testing.T, intf *ondatra.Port, dut *ondatra.D
 func TestFtiTunnels(t *testing.T) {
 
 	p := &parameters{
-		rtIntf1Ipv4Add:  "10.1.1.2",
-		rtIntf2Ipv4Add:  "11.1.1.2",
-		rtIntf5Ipv4Add:  "30.1.1.1",
-		rtIntf6Ipv4Add:  "31.1.1.1",
+		rtIntf1Ipv4Add:  "198.18.10.2",
+		rtIntf2Ipv4Add:  "198.18.11.2",
+		rtIntf5Ipv4Add:  "198.18.30.1", 
+		rtIntf6Ipv4Add:  "198.18.31.1",
 		rtIntf1MacAdd:   "00:00:aa:aa:aa:aa",
 		rtIntf2MacAdd:   "00:00:bb:bb:bb:bb",
 		rtIntf5MacAdd:   "00:00:cc:cc:cc:cc",
 		rtIntf6MacAdd:   "00:00:dd:dd:dd:dd",
-		r0Intf1Ipv4Add:  "10.1.1.1",
-		r0Intf2Ipv4Add:  "11.1.1.1",
-		r0Intf3Ipv4Add:  "20.1.1.1",
-		r0Intf4Ipv4Add:  "21.1.1.1",
-		r0Fti0Ipv4Add:   "90.1.1.1",
-		r0Fti1Ipv4Add:   "91.1.1.1",
-		r0Fti2Ipv4Add:   "92.1.1.1",
-		r0Fti3Ipv4Add:   "93.1.1.1",
-		r0Fti4Ipv4Add:   "94.1.1.1",
-		r0Fti5Ipv4Add:   "95.1.1.1",
-		r0Fti6Ipv4Add:   "96.1.1.1",
-		r0Fti7Ipv4Add:   "97.1.1.1",
-		r0Lo0Ut0Ipv4Add: "70.1.1.1",
-		r0Lo0Ut1Ipv4Add: "71.1.1.1",
-		r0Lo0Ut2Ipv4Add: "72.1.1.1",
-		r0Lo0Ut3Ipv4Add: "73.1.1.1",
+		r0Intf1Ipv4Add:  "198.18.10.1",
+		r0Intf2Ipv4Add:  "198.18.11.1",
+		r0Intf3Ipv4Add:  "198.18.20.1",
+		r0Intf4Ipv4Add:  "198.18.21.1",
+		r0Fti0Ipv4Add:   "198.18.90.1",
+		r0Fti1Ipv4Add:   "198.18.91.1",
+		r0Fti2Ipv4Add:   "198.18.92.1",
+		r0Fti3Ipv4Add:   "198.18.93.1",
+		r0Fti4Ipv4Add:   "198.18.94.1",
+		r0Fti5Ipv4Add:   "198.18.95.1",
+		r0Fti6Ipv4Add:   "198.18.96.1",
+		r0Fti7Ipv4Add:   "198.18.97.1",
+		r0Lo0Ut0Ipv4Add: "198.18.70.1",
+		r0Lo0Ut1Ipv4Add: "198.18.71.1",
+		r0Lo0Ut2Ipv4Add: "198.18.72.1",
+		r0Lo0Ut3Ipv4Add: "198.18.73.1",
 		ipv4Mask:        24,
 		ipv4FullMask:    32,
-		r1Intf5Ipv4Add:  "30.1.1.2",
-		r1Intf6Ipv4Add:  "31.1.1.2",
-		r1Intf3Ipv4Add:  "20.1.1.2",
-		r1Intf4Ipv4Add:  "21.1.1.2",
-		r1Fti0Ipv4Add:   "90.1.1.2",
-		r1Fti1Ipv4Add:   "91.1.1.2",
-		r1Fti2Ipv4Add:   "92.1.1.2",
-		r1Fti3Ipv4Add:   "93.1.1.2",
-		r1Fti4Ipv4Add:   "94.1.1.2",
-		r1Fti5Ipv4Add:   "95.1.1.2",
-		r1Fti6Ipv4Add:   "96.1.1.2",
-		r1Fti7Ipv4Add:   "97.1.1.2",
-		r1Lo0Ut0Ipv4Add: "80.1.1.1",
-		r1Lo0Ut1Ipv4Add: "81.1.1.1",
-		r1Lo0Ut2Ipv4Add: "82.1.1.1",
-		r1Lo0Ut3Ipv4Add: "83.1.1.1",
-		rtIntf1Ipv6Add:  "2000:10:1:1::2",
-		rtIntf2Ipv6Add:  "2000:11:1:1::2",
-		rtIntf5Ipv6Add:  "2000:30:1:1::1",
-		rtIntf6Ipv6Add:  "2000:31:1:1::1",
-		r0Intf1Ipv6Add:  "2000:10:1:1::1",
-		r0Intf2Ipv6Add:  "2000:11:1:1::1",
-		r0Intf3Ipv6Add:  "2000:20:1:1::1",
-		r0Intf4Ipv6Add:  "2000:21:1:1::1",
-		r0Fti0Ipv6Add:   "2000:90:1:1::1",
-		r0Fti1Ipv6Add:   "2000:91:1:1::1",
-		r0Fti2Ipv6Add:   "2000:92:1:1::1",
-		r0Fti3Ipv6Add:   "2000:93:1:1::1",
-		r0Fti4Ipv6Add:   "2000:94:1:1::1",
-		r0Fti5Ipv6Add:   "2000:95:1:1::1",
-		r0Fti6Ipv6Add:   "2000:96:1:1::1",
-		r0Fti7Ipv6Add:   "2000:97:1:1::1",
-		r0Lo0Ut0Ipv6Add: "3000:70:1:1::1",
-		r0Lo0Ut1Ipv6Add: "3000:71:1:1::1",
-		r0Lo0Ut2Ipv6Add: "3000:72:1:1::1",
-		r0Lo0Ut3Ipv6Add: "3000:73:1:1::1",
+		r1Intf5Ipv4Add:  "198.18.30.2",
+		r1Intf6Ipv4Add:  "198.18.31.2",
+		r1Intf3Ipv4Add:  "198.18.20.2",
+		r1Intf4Ipv4Add:  "198.18.21.2",
+		r1Fti0Ipv4Add:   "198.18.90.2",
+		r1Fti1Ipv4Add:   "198.18.91.2",
+		r1Fti2Ipv4Add:   "198.18.92.2",
+		r1Fti3Ipv4Add:   "198.18.93.2",
+		r1Fti4Ipv4Add:   "198.18.94.2",
+		r1Fti5Ipv4Add:   "198.18.95.2",
+		r1Fti6Ipv4Add:   "198.18.96.2",
+		r1Fti7Ipv4Add:   "198.18.97.2",
+		r1Lo0Ut0Ipv4Add: "198.18.80.1",
+		r1Lo0Ut1Ipv4Add: "198.18.81.1",
+		r1Lo0Ut2Ipv4Add: "198.18.82.1",
+		r1Lo0Ut3Ipv4Add: "198.18.83.1",
+		rtIntf1Ipv6Add:  "2001:DB8:1:198:18:10::2",
+		rtIntf2Ipv6Add:  "2001:DB8:1:198:18:11::2",
+		rtIntf5Ipv6Add:  "2001:DB8:1:198:18:30::1",
+		rtIntf6Ipv6Add:  "2001:DB8:1:198:18:31::1",
+		r0Intf1Ipv6Add:  "2001:DB8:1:198:18:10::1",
+		r0Intf2Ipv6Add:  "2001:DB8:1:198:18:11::1",
+		r0Intf3Ipv6Add:  "2001:DB8:1:198:18:20::1",
+		r0Intf4Ipv6Add:  "2001:DB8:1:198:18:21::1",
+		r0Fti0Ipv6Add:   "2001:DB8:1:198:18:90::1",
+		r0Fti1Ipv6Add:   "2001:DB8:1:198:18:91::1",
+		r0Fti2Ipv6Add:   "2001:DB8:1:198:18:92::1",
+		r0Fti3Ipv6Add:   "2001:DB8:1:198:18:93::1",
+		r0Fti4Ipv6Add:   "2001:DB8:1:198:18:94::1",
+		r0Fti5Ipv6Add:   "2001:DB8:1:198:18:95::1",
+		r0Fti6Ipv6Add:   "2001:DB8:1:198:18:96::1",
+		r0Fti7Ipv6Add:   "2001:DB8:1:198:18:97::1",
+		r0Lo0Ut0Ipv6Add: "2001:DB8:1:198:18:70::1",
+		r0Lo0Ut1Ipv6Add: "2001:DB8:1:198:18:71::1",
+		r0Lo0Ut2Ipv6Add: "2001:DB8:1:198:18:72::1",
+		r0Lo0Ut3Ipv6Add: "2001:DB8:1:198:18:73::1",
 		ipv6Mask:        120,
-		r1Intf5Ipv6Add:  "2000:30:1:1::2",
-		r1Intf6Ipv6Add:  "2000:31:1:1::2",
-		r1Intf3Ipv6Add:  "2000:20:1:1::2",
-		r1Intf4Ipv6Add:  "2000:21:1:1::2",
-		r1Fti0Ipv6Add:   "2000:90:1:1::2",
-		r1Fti1Ipv6Add:   "2000:91:1:1::2",
-		r1Fti2Ipv6Add:   "2000:92:1:1::2",
-		r1Fti3Ipv6Add:   "2000:93:1:1::2",
-		r1Fti4Ipv6Add:   "2000:94:1:1::2",
-		r1Fti5Ipv6Add:   "2000:95:1:1::2",
-		r1Fti6Ipv6Add:   "2000:96:1:1::2",
-		r1Fti7Ipv6Add:   "2000:97:1:1::2",
-		r1Lo0Ut0Ipv6Add: "3000:80:1:1::1",
-		r1Lo0Ut1Ipv6Add: "3000:81:1:1::1",
-		r1Lo0Ut2Ipv6Add: "3000:82:1:1::1",
-		r1Lo0Ut3Ipv6Add: "3000:83:1:1::1",
+		r1Intf5Ipv6Add:  "2001:DB8:1:198:18:30::2",
+		r1Intf6Ipv6Add:  "2001:DB8:1:198:18:31::2",
+		r1Intf3Ipv6Add:  "2001:DB8:1:198:18:20::2",
+		r1Intf4Ipv6Add:  "2001:DB8:1:198:18:21::2",
+		r1Fti0Ipv6Add:   "2001:DB8:1:198:18:90::2",
+		r1Fti1Ipv6Add:   "2001:DB8:1:198:18:91::2",
+		r1Fti2Ipv6Add:   "2001:DB8:1:198:18:92::2",
+		r1Fti3Ipv6Add:   "2001:DB8:1:198:18:93::2",
+		r1Fti4Ipv6Add:   "2001:DB8:1:198:18:94::2",
+		r1Fti5Ipv6Add:   "2001:DB8:1:198:18:95::2",
+		r1Fti6Ipv6Add:   "2001:DB8:1:198:18:96::2",
+		r1Fti7Ipv6Add:   "2001:DB8:1:198:18:97::2",
+		r1Lo0Ut0Ipv6Add: "2001:DB8:1:198:18:80::1",
+		r1Lo0Ut1Ipv6Add: "2001:DB8:1:198:18:81::1",
+		r1Lo0Ut2Ipv6Add: "2001:DB8:1:198:18:82::1",
+		r1Lo0Ut3Ipv6Add: "2001:DB8:1:198:18:83::1",
 		ipv6FullMask:    128,
 		flow1:           "IPv4-flow1",
 		flow2:           "IPv4-flow2",
@@ -803,7 +825,7 @@ func TestFtiTunnels(t *testing.T) {
 		flow4:           "IPv6-flow4",
 		trafficDuration: 60,
 		trafficRate:     1000,
-	}
+}
 	t.Logf("the input variable %+v", p)
 	t.Helper()
 
@@ -1125,7 +1147,7 @@ func configureOtg(t *testing.T, otg *otg.OTG, p *parameters) gosnappi.Config {
 	iDut1Ipv4 := iDut1Eth.Ipv4Addresses().Add().SetName("port1" + ".IPv4")
 	iDut1Ipv4.SetAddress(p.rtIntf1Ipv4Add).SetGateway(p.r0Intf1Ipv4Add).SetPrefix(uint32(p.ipv4Mask))
 	iDut1Ipv6 := iDut1Eth.Ipv6Addresses().Add().SetName("port1" + ".IPv6")
-	iDut1Ipv6.SetAddress(p.rtIntf1Ipv6Add).SetGateway(p.r0Intf1Ipv6Add).SetPrefix(uint32(p.ipv6Mask))
+	iDut1Ipv6.SetAddress(p.rtIntf1Ipv6Add).SetGateway(p.r0Intf1Ipv6Add).SetPrefix(uint32int32(p.ipv6Mask))
 
 	//port2
 	iDut2Dev := config.Devices().Add().SetName("port2")
@@ -1141,18 +1163,18 @@ func configureOtg(t *testing.T, otg *otg.OTG, p *parameters) gosnappi.Config {
 	iDut3Eth := iDut3Dev.Ethernets().Add().SetName("port5" + ".Eth").SetMac(p.rtIntf5MacAdd)
 	iDut3Eth.Connection().SetChoice(gosnappi.EthernetConnectionChoice.PORT_NAME).SetPortName(port3.Name())
 	iDut3Ipv4 := iDut3Eth.Ipv4Addresses().Add().SetName("port5" + ".IPv4")
-	iDut3Ipv4.SetAddress(p.rtIntf5Ipv4Add).SetGateway(p.r1Intf5Ipv4Add).SetPrefix(uint32(p.ipv4Mask))
+	iDut3Ipv4.SetAddress(p.rtIntf5Ipv4Add).SetGateway(p.r1Intf5Ipv4Add).SetPrefix(int32(p.ipv4Mask))
 	iDut3Ipv6 := iDut3Eth.Ipv6Addresses().Add().SetName("port5" + ".IPv6")
-	iDut3Ipv6.SetAddress(p.rtIntf5Ipv6Add).SetGateway(p.r1Intf5Ipv6Add).SetPrefix(uint32(p.ipv6Mask))
+	iDut3Ipv6.SetAddress(p.rtIntf5Ipv6Add).SetGateway(p.r1Intf5Ipv6Add).SetPrefix(int32(p.ipv6Mask))
 
 	//port6
 	iDut4Dev := config.Devices().Add().SetName("port6")
 	iDut4Eth := iDut4Dev.Ethernets().Add().SetName("port6" + ".Eth").SetMac(p.rtIntf6MacAdd)
 	iDut4Eth.Connection().SetChoice(gosnappi.EthernetConnectionChoice.PORT_NAME).SetPortName(port4.Name())
 	iDut4Ipv4 := iDut4Eth.Ipv4Addresses().Add().SetName("port6" + ".IPv4")
-	iDut4Ipv4.SetAddress(p.rtIntf6Ipv4Add).SetGateway(p.r1Intf6Ipv4Add).SetPrefix(uint32(p.ipv4Mask))
+	iDut4Ipv4.SetAddress(p.rtIntf6Ipv4Add).SetGateway(p.r1Intf6Ipv4Add).SetPrefix(int32(p.ipv4Mask))
 	iDut4Ipv6 := iDut4Eth.Ipv6Addresses().Add().SetName("port6" + ".IPv6")
-	iDut4Ipv6.SetAddress(p.rtIntf6Ipv6Add).SetGateway(p.r1Intf6Ipv6Add).SetPrefix(uint32(p.ipv6Mask))
+	iDut4Ipv6.SetAddress(p.rtIntf6Ipv6Add).SetGateway(p.r1Intf6Ipv6Add).SetPrefix(int32(p.ipv6Mask))
 
 	t.Logf("Start Ote Traffic config")
 	t.Logf("configure IPv4 flow from %s to %s ", port1.Name(), port3.Name())
@@ -1294,13 +1316,13 @@ func SendTraffic(t *testing.T, ate *ondatra.ATEDevice, p *parameters) {
 	otg.StopTraffic(t)
 }
 
-func VerifyLoadbalance(t *testing.T, flowCount int64, rate int64, duration int64, sharingIntfCont int64, initialStats int64, finalStats int64, wantLoss bool) {
+func VerifyLoadbalance(t *testing.T, flowCount int64, rate uint64, duration int64, sharingIntfCont int64, initialStats int64, finalStats int64, wantLoss bool) {
 
 	tolerance := 20
 	// colculate correct stats on interface
 	stats := finalStats - initialStats
 	t.Logf("Initial packets %d Final Packets %d ", finalStats, initialStats)
-	expectedTotalPkts := (flowCount * rate * duration)
+	expectedTotalPkts := (flowCount * int64(rate) * duration)
 	expectedPerLinkPkts := expectedTotalPkts / sharingIntfCont
 	t.Logf("Total packets %d flow through the %d links", expectedTotalPkts, sharingIntfCont)
 	t.Logf("Expected per link packets %d ", expectedPerLinkPkts)
