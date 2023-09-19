f2308790fbdd4d0fe98429da9876b18921cf4804
Karim Jahed <kjahed@users.noreply.github.com>
2023-09-19 17:03:39 -0400

    Configure NI name whenever configuring type (#2066)
    * Use update for ni
    
    * fixed missing otg tests
    
    * added missing pbf config
    
    * sync otg
    
    * Added function for default NI configuration
    
    * removed unrelated changes
    
    ---------
    
    Co-authored-by: anksaiki <ankursaikia@google.com>
    Co-authored-by: guoshiuan <guoshiuan@gmail.com>

--- feature/bgp/policybase/otg_tests/route_installation_test/route_installation_test.go
+++ feature/bgp/policybase/otg_tests/route_installation_test/route_installation_test.go
@@ -596,9 +596,7 @@ func TestEstablish(t *testing.T) {
 
 	// Configure Network instance type on DUT.
 	t.Log("Configure Network Instance type")
-	dutConfNIPath := gnmi.OC().NetworkInstance(deviations.DefaultNetworkInstance(dut))
-	gnmi.Replace(t, dut, dutConfNIPath.Type().Config(), oc.NetworkInstanceTypes_NETWORK_INSTANCE_TYPE_DEFAULT_INSTANCE)
-
+	fptest.ConfigureDefaultNetworkInstance(t, dut)
 	// Configure BGP+Neighbors on the DUT.
 	t.Logf("Start DUT BGP Config")
 	dutConfPath := gnmi.OC().NetworkInstance(deviations.DefaultNetworkInstance(dut)).Protocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_BGP, "BGP")
