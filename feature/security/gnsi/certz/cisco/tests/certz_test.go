c0b453de6e1a51f078af9e0c459584eb8ae2bcee
Moji Ba (mbagherz) <mbagherz@cisco.com>
2023-10-05 19:22:50 -0400

    Update certz_test.go

--- feature/security/gnsi/certz/cisco/tests/certz_test.go
+++ feature/security/gnsi/certz/cisco/tests/certz_test.go
@@ -64,7 +64,10 @@ func TestAddProfile(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			_, err := gnsiC.Certz().AddProfile(context.Background(), tt.args.req)
-			if (err != nil) != tt.wantErr {
+			if (err != nil) && !tt.wantErr {
+				t.Errorf("Server.AddProfile() error = %v, wantErr %v, %s", err, tt.wantErr, tt.args.req)
+			}
+			else if (err == nil) && tt.wantErr {
 				t.Errorf("Server.AddProfile() error = %v, wantErr %v, %s", err, tt.wantErr, tt.args.req)
 			}
 		})
