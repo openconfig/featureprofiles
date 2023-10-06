8cd21fcddcb12f6a075f248b22ba54daeaf103f4
Karim Jahed <kjahed@cisco.com>
2023-10-06 05:51:06 -0400

    bug fix

--- exec/utils/reporting/google_reporter.py
+++ exec/utils/reporting/google_reporter.py
@@ -39,12 +39,12 @@ def _did_fail(log_file):
     try:
         with open(log_file, 'r') as file:
             data = file.read()
-            if data.find('failures="0"') != -1: 
-                return False
+            if data.find('failures="0"') == -1: 
+                return True
     except:
         print("Error processing file " + log_file)
-    finally:
         return True
+    return False
 
 def _did_pass(log_file):
     return not _did_fail(log_file)
