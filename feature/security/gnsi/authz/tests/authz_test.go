a014cbf83101347d6825c5c627b9f25d6ce8aeb1
mojiiba <mbagherz@cisco.com>
2023-10-07 10:34:26 -0400

    add sample ca keys

--- feature/security/gnsi/authz/tests/authz_test.go
+++ feature/security/gnsi/authz/tests/authz_test.go
@@ -49,8 +49,8 @@ type spiffe struct {
 
 var (
 	testInfraID = flag.String("test_infra_id", "cafyauto", "test Infra ID user for authz operation")
-	caCertPem   = flag.String("ca_cert_pem", "", "a pem file for ca cert that will be used to generate svid")
-	caKeyPem    = flag.String("ca_key_pem", "", "a pem file for ca key that will be used to generate svid")
+	caCertPem   = flag.String("ca_cert_pem", "testdata/ca.cert.pem", "a pem file for ca cert that will be used to generate svid")
+	caKeyPem    = flag.String("ca_key_pem", "testdata/ca.key.pem", "a pem file for ca key that will be used to generate svid")
 	usersMap    = map[string]spiffe{
 		"cert_user_admin": {
 			spiffeID: "spiffe://test-abc.foo.bar/xyz/admin",
