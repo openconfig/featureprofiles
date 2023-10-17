c889231f760b02db518bfece69ddf83c9c461a95
greg-dennis <gdennis@google.com>
2023-10-17 10:58:38 -0400

    Use ParseURL

--- feature/experimental/replay/tests/presession_test/presession_test.go
+++ feature/experimental/replay/tests/presession_test/presession_test.go
@@ -31,7 +31,7 @@ func TestMain(m *testing.M) {
 func TestReplay(t *testing.T) {
 	const logFile = "https://github.com/openconfig/featureprofiles/raw/main/feature/experimental/replay/tests/presession_test/grpclog.pb"
 	t.Logf("Parsing log file: %v", logFile)
-	rec, err := replayer.Parse(logFile)
+	rec, err := replayer.ParseURL(logFile)
 	if err != nil {
 		t.Fatalf("Parse(): cannot parse log file: %v", err)
 	}
