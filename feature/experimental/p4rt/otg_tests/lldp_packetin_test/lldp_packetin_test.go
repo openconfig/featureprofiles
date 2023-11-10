9eea5bb56a67a9467650001656adb6504d2765e6
Sergey Fomin <sergey.fomin@nokia.com>
2023-11-09 17:37:03 -0800

    p4rt-7.1: upd. check on metadata length with a strict check
    This code is a Contribution to the OpenConfig Feature Profiles project ("Work") made under the Google Software Grant and Corporate Contributor License Agreement ("CLA") and governed by the Apache License 2.0. No other rights or licenses in or to any of Nokia’s intellectual property are granted for any other purpose. This code is provided on an "as is" basis without any warranties of any kind.

--- feature/experimental/p4rt/otg_tests/lldp_packetin_test/lldp_packetin_test.go
+++ feature/experimental/p4rt/otg_tests/lldp_packetin_test/lldp_packetin_test.go
@@ -202,8 +202,8 @@ func testPacketIn(ctx context.Context, t *testing.T, args *testArgs) {
 			}
 
 			metaData := packet.Pkt.GetMetadata()
-			if len(metaData) < 2 {
-				t.Fatalf("Metadata is missing")
+			if len(metaData) != 2 {
+				t.Fatalf("Incorrect number of Metadata headers, want: %v, got: %v", 2, len(metaData))
 			}
 			for _, data := range metaData {
 				switch data.GetMetadataId() {
