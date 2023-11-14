a20a1e8941de8abd83716cf18d7034803c4038e9
cprabha <cprabha@juniper.net>
2023-11-14 13:23:07 -0800

    Replaced getconfig with get

--- feature/experimental/bgp/otg_tests/bgp_afi_safi_defaults/bgp_afi_safi_defaults_test.go
+++ feature/experimental/bgp/otg_tests/bgp_afi_safi_defaults/bgp_afi_safi_defaults_test.go
@@ -526,7 +526,7 @@ func TestAfiSafiSetToFalse(t *testing.T) {
 				bgpClearConfig(t, dut)
 				dutConf := bgpCreateNbr(t, dutAS, ateAS, dut, tc.afiSafiLevel, tc.nbrs, tc.isV4Only)
 				gnmi.Replace(t, dut, dutConfPath.Config(), dutConf)
-				fptest.LogQuery(t, "DUT BGP Config", dutConfPath.Config(), gnmi.GetConfig(t, dut, dutConfPath.Config()))
+				fptest.LogQuery(t, "DUT BGP Config", dutConfPath.Config(), gnmi.Get(t, dut, dutConfPath.Config()))
 			})
 
 			otg := ate.OTG()
