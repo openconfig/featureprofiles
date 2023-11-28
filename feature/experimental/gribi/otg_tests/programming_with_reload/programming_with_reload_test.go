4e4e4f02d8fd87c54bc6a84ac6442d94c972a8f8
manan-patel <mananpat@cisco.com>
2023-11-28 14:57:46 -0500

    incorporated comments

--- feature/experimental/gribi/otg_tests/programming_with_reload/programming_with_reload_test.go
+++ feature/experimental/gribi/otg_tests/programming_with_reload/programming_with_reload_test.go
@@ -37,30 +37,34 @@ import (
 )
 
 const (
-	ipv4PrefixLen  = 30
-	ipv6PrefixLen  = 126
-	mask           = "32"
-	outerDstIP1    = "198.51.100.1"
-	outerSrcIP1    = "198.51.100.2"
-	outerDstIP2    = "203.0.113.1"
-	outerSrcIP2    = "203.0.113.2"
-	innerDstIP1    = "198.18.0.1"
-	innerSrcIP1    = "198.18.0.255"
-	vip1           = "198.18.1.1"
-	vip2           = "198.18.1.2"
-	vrfA           = "VRF-A"
-	vrfB           = "VRF-B"
-	nh1ID          = 1
-	nhg1ID         = 1
-	nh2ID          = 2
-	nhg2ID         = 2
-	nh100ID        = 100
-	nhg100ID       = 100
-	nh101ID        = 101
-	nhg101ID       = 101
-	nh102ID        = 102
-	nhg102ID       = 102
-	ethernetCsmacd = oc.IETFInterfaces_InterfaceType_ethernetCsmacd
+	ipv4PrefixLen         = 30
+	ipv6PrefixLen         = 126
+	mask                  = "32"
+	outerDstIP1           = "198.51.100.1"
+	outerSrcIP1           = "198.51.100.2"
+	outerDstIP2           = "203.0.113.1"
+	outerSrcIP2           = "203.0.113.2"
+	innerDstIP1           = "198.18.0.1"
+	innerSrcIP1           = "198.18.0.255"
+	vip1                  = "198.18.1.1"
+	vip2                  = "198.18.1.2"
+	vrfA                  = "VRF-A"
+	vrfB                  = "VRF-B"
+	nh1ID                 = 1
+	nhg1ID                = 1
+	nh2ID                 = 2
+	nhg2ID                = 2
+	nh100ID               = 100
+	nhg100ID              = 100
+	nh101ID               = 101
+	nhg101ID              = 101
+	nh102ID               = 102
+	nhg102ID              = 102
+	oneSecondInNanoSecond = 1e9
+	rebootDelay           = 120
+	ethernetCsmacd        = oc.IETFInterfaces_InterfaceType_ethernetCsmacd
+	// Maximum reboot time is 900 seconds (15 minutes).
+	maxRebootTime = 900
 	// Maximum gribi connection time is 180 seconds following reload
 	maxGribiConnectTime = 180
 )
@@ -435,17 +439,41 @@ func (args *testArgs) reloadChassis(t *testing.T) {
 
 	rebootRequest := &spb.RebootRequest{
 		Method:  spb.RebootMethod_COLD,
-		Delay:   0,
+		Delay:   rebootDelay * oneSecondInNanoSecond,
 		Message: "Reboot chassis with delay",
 		Force:   true,
 	}
 
 	t.Logf("Send reboot request: %v", rebootRequest)
 	rebootResponse, err := gnoiClient.System().Reboot(context.Background(), rebootRequest)
-	defer gnoiClient.System().CancelReboot(context.Background(), &spb.CancelRebootRequest{})
 	t.Logf("Got reboot response: %v, err: %v", rebootResponse, err)
 	if err != nil {
 		t.Fatalf("Failed to reboot chassis with unexpected err: %v", err)
+	}
+
+	if rebootRequest.GetDelay() > 1 {
+		t.Logf("Validating DUT remains reachable for at least %d seconds", rebootDelay)
+		prevTime, err := time.Parse(time.RFC3339, gnmi.Get(t, args.dut, gnmi.OC().System().CurrentDatetime().State()))
+		if err != nil {
+			t.Fatalf("Failed parsing current-datetime: %s", err)
+		}
+		start := time.Now()
+		for {
+			time.Sleep(10 * time.Second)
+			t.Logf("Time elapsed %.2f seconds since reboot was requested.", time.Since(start).Seconds())
+			if uint64(time.Since(start).Seconds()) > rebootDelay {
+				t.Logf("Time elapsed %.2f seconds > %d reboot delay", time.Since(start).Seconds(), rebootDelay)
+				break
+			}
+			latestTime, err := time.Parse(time.RFC3339, gnmi.Get(t, args.dut, gnmi.OC().System().CurrentDatetime().State()))
+			if err != nil {
+				t.Fatalf("Failed parsing current-datetime: %s", err)
+			}
+			if latestTime.Before(prevTime) || latestTime.Equal(prevTime) {
+				t.Errorf("Get latest system time: got %v, want newer time than %v", latestTime, prevTime)
+			}
+			prevTime = latestTime
+		}
 	}
 
 	startReboot := time.Now()
@@ -462,6 +490,9 @@ func (args *testArgs) reloadChassis(t *testing.T) {
 			t.Logf("Device rebooted successfully with received time: %v", currentTime)
 			break
 		}
+		if uint64(time.Since(startReboot).Seconds()) > maxRebootTime {
+			t.Errorf("Check boot time: got %v, want < %v", time.Since(startReboot), maxRebootTime)
+		}
 	}
 	t.Logf("Device boot time: %.2f seconds", time.Since(startReboot).Seconds())
 
@@ -471,42 +502,36 @@ func (args *testArgs) reloadChassis(t *testing.T) {
 		t.Errorf("Get boot time: got %v, want > %v", bootTimeAfterReboot, bootTimeBeforeReboot)
 	}
 
-	// starting a goroutine to attempt for gribi connection while system finishes bringup process
-	t.Logf("Starting a goroutine to attempt for gribi connection within max timeout value")
-	err_msg := make(chan error)
-	args.gribi_reconnect(t, err_msg)
-
-	t.Logf("inspect the error msg returned from goroutine for gribi connection establishment")
-	gribi_err_msg := <-err_msg
-	if gribi_err_msg != nil {
+	// establish new gribi programming following reload
+	err_msg := args.gribi_reconnect(t)
+	if err_msg != nil {
 		t.Fatalf("gribi connection fails post reboot with in max timeout value of %d", maxGribiConnectTime)
 	}
 }
 
 // gribi reconnect following reload
-func (args *testArgs) gribi_reconnect(t *testing.T, err_msg chan<- error) {
-	go func() {
-		start := time.Now()
-		client := gribi.Client{
-			DUT:         args.dut,
-			FIBACK:      true,
-			Persistence: true,
-		}
+func (args *testArgs) gribi_reconnect(t *testing.T) error {
 
-		for {
-			if time.Since(start).Seconds() > maxGribiConnectTime {
-				err_msg <- fmt.Errorf("gribi connection fails post reboot with in max timeout value of %d", maxGribiConnectTime)
+	start := time.Now()
+	client := gribi.Client{
+		DUT:         args.dut,
+		FIBACK:      true,
+		Persistence: true,
+	}
+
+	for {
+		if time.Since(start).Seconds() > maxGribiConnectTime {
+			return fmt.Errorf("gribi connection fails post reboot")
+		} else {
+			if err := client.Start(t); err != nil {
+				t.Logf("gRIBI Connection could not be established: %v\nRetrying...", err)
+				time.Sleep(30 * time.Second)
 			} else {
-				if err := client.Start(t); err != nil {
-					t.Logf("gRIBI Connection could not be established: %v\nRetrying...", err)
-					time.Sleep(30 * time.Second)
-				} else {
-					t.Logf("New gRIBI Connection established after reload in %d seconds", uint64(time.Since(start).Seconds()))
-					args.client = &client
-					err_msg <- nil
-					return
-				}
+				t.Logf("New gRIBI Connection established after reload in %d seconds", uint64(time.Since(start).Seconds()))
+				args.client = &client
+				args.client.BecomeLeader(t)
+				return nil
 			}
 		}
-	}()
+	}
 }
