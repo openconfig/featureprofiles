21b839d1fae909a72450c7cdd5f743e193fbe800
Marc Charlebois <marc.charlebois@nokia.com>
2023-12-11 15:51:32 -0800

    actually adding the fixes to move ocPortName var inside if statement

--- feature/gnoi/os/tests/osinstall/osinstall_test.go
+++ feature/gnoi/os/tests/osinstall/osinstall_test.go
@@ -425,9 +425,9 @@ func watchStatus(t *testing.T, ic ospb.OS_InstallClient, standby bool) error {
 func TestPushAndVerifyInterfaceConfig(t *testing.T) {
 
 	dut := ondatra.DUT(t, "dut")
-	portNameOC := dut.Port(t, "port1").Name()
 	if deviations.ExplicitInterfaceInDefaultVRF(dut) {
-		fptest.AssignToNetworkInstance(t, dut, portNameOC, deviations.DefaultNetworkInstance(dut), 0)
+		ocPortName := dut.Port(t, "port1").Name()
+		fptest.AssignToNetworkInstance(t, dut, ocPortName, deviations.DefaultNetworkInstance(dut), 0)
 	}
 
 	t.Logf("Create and push interface config to the DUT")
