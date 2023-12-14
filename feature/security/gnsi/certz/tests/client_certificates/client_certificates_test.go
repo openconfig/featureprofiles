9a24e4d2bec1fcc26e1437dc226fb41eff42564f
priyacj <priyacj@juniper.net>
2023-12-14 00:00:27 -0800

    Initial Commit of CERTZ-1.1

---
+++ feature/security/gnsi/certz/tests/client_certificates/client_certificates_test.go
@@ -0,0 +1,234 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package client_certificates_test
+
+import (
+	context "context"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"os"
+	"testing"
+
+	"github.com/openconfig/featureprofiles/feature/security/gnsi/certz/internal/setup_service"
+	"github.com/openconfig/featureprofiles/internal/fptest"
+	"github.com/openconfig/ondatra"
+	"github.com/openconfig/ondatra/gnmi"
+	"github.com/openconfig/ondatra/gnmi/oc"
+	"github.com/openconfig/ygot/ygot"
+)
+
+var (
+	testProfile = "gNxI"
+	serverAddr  string
+	username    = "certzuser"
+	password    = "certzpswd"
+)
+
+type EntityType int8
+
+/*type rpcCredentials struct {
+	*creds.UserPass
+}
+
+func (r *rpcCredentials) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string{
+		"username": username,
+		"password": password,
+	}, nil
+}
+
+func (r *rpcCredentials) RequireTransportSecurity() bool {
+	return true
+}*/
+
+// function to add an user in admin role.
+func createUser(t *testing.T, dut *ondatra.DUTDevice, user, pswd string) bool {
+	ocUser := &oc.System_Aaa_Authentication_User{
+		Username: ygot.String(user),
+		Role:     oc.AaaTypes_SYSTEM_DEFINED_ROLES_SYSTEM_ROLE_ADMIN,
+		Password: ygot.String(pswd),
+	}
+	gnmi.Update(t, dut, gnmi.OC().System().Aaa().Authentication().User(user).Config(), ocUser)
+	return true
+}
+
+func TestMain(m *testing.M) {
+	fptest.RunTests(m)
+}
+
+// Certz1.1
+// Test that client certificates from a set of one CA are able to be validated and
+// used for authentication to a device when used by a client connecting to each
+// gRPC service.
+
+func TestClientCertTcOne(t *testing.T) {
+
+	dut := ondatra.DUT(t, "dut")
+	serverAddr = dut.Name()
+
+	checkUser := createUser(t, dut, username, password)
+	if !checkUser {
+		t.Fatalf("Failed to create certz user.")
+	}
+
+	t.Logf("Validation of all services that are using gRPC before certz rotation.")
+	initCheck := setup_service.PreInitCheck(t, dut, context.Background())
+	if !initCheck {
+		t.Fatalf("Failed in the preInit checks.")
+	}
+
+	ctx := context.Background()
+	gnsiC, err := dut.RawAPIs().BindingDUT().DialGNSI(ctx)
+	if err != nil {
+		t.Fatalf("Could not create gNSI Connection %v", err)
+	}
+	t.Logf("Precheck:gNSI connection is successful%v", gnsiC)
+
+	cases := []struct {
+		desc             string
+		serverCertzFile  string
+		serverKeyzFile   string
+		trustBundlezFile string
+		clientCertzFile  string
+		clientKeyzFile   string
+	}{
+		{
+			desc:             "Load the correct key-type rsa trustbundle with 1 CA configuration",
+			serverCertzFile:  "test_data/ca-01/server-rsa-a-cert.pem",
+			serverKeyzFile:   "test_data/ca-01/server-rsa-a-key.pem",
+			trustBundlezFile: "test_data/ca-01/trust_bundle_01_rsa_a.pem",
+			clientCertzFile:  "test_data/ca-01/client-rsa-a-cert.pem",
+			clientKeyzFile:   "test_data/ca-01/client-rsa-a-key.pem",
+		},
+		{
+			desc:             "Load the correct key-type ecdsa trustbundle with 1 CA configuration",
+			serverCertzFile:  "test_data/ca-01/server-ecdsa-a-cert.pem",
+			serverKeyzFile:   "test_data/ca-01/server-ecdsa-a-key.pem",
+			trustBundlezFile: "test_data/ca-01/trust_bundle_01_ecdsa_a.pem",
+			clientCertzFile:  "test_data/ca-01/client-ecdsa-a-cert.pem",
+			clientKeyzFile:   "test_data/ca-01/client-ecdsa-a-key.pem",
+		},
+		{
+			desc:             "Load the correct key-type rsa trustbundle with 2 CA configuration",
+			serverCertzFile:  "test_data/ca-02/server-rsa-a-cert.pem",
+			serverKeyzFile:   "test_data/ca-02/server-rsa-a-key.pem",
+			trustBundlezFile: "test_data/ca-02/trust_bundle_02_rsa_a.pem",
+			clientCertzFile:  "test_data/ca-02/client-rsa-a-cert.pem",
+			clientKeyzFile:   "test_data/ca-02/client-rsa-a-key.pem",
+		},
+		{
+			desc:             "Load the correct key-type ecdsa trustbundle with 2 CA configuration",
+			serverCertzFile:  "test_data/ca-02/server-ecdsa-a-cert.pem",
+			serverKeyzFile:   "test_data/ca-02/server-ecdsa-a-key.pem",
+			trustBundlezFile: "test_data/ca-02/trust_bundle_02_ecdsa_a.pem",
+			clientCertzFile:  "test_data/ca-02/client-ecdsa-a-cert.pem",
+			clientKeyzFile:   "test_data/ca-02/client-ecdsa-a-key.pem",
+		},
+		{
+			desc:             "Load the correct key-type rsa trustbundle with 10CA configuration",
+			serverCertzFile:  "test_data/ca-10/server-rsa-a-cert.pem",
+			serverKeyzFile:   "test_data/ca-10/server-rsa-a-key.pem",
+			trustBundlezFile: "test_data/ca-10/trust_bundle_10_rsa_a.pem",
+			clientCertzFile:  "test_data/ca-10/client-rsa-a-cert.pem",
+			clientKeyzFile:   "test_data/ca-10/client-rsa-a-key.pem",
+		},
+		{
+			desc:             "Load the correct key-type ecdsa trustbundle with 10CA configuration",
+			serverCertzFile:  "test_data/ca-10/server-ecdsa-a-cert.pem",
+			serverKeyzFile:   "test_data/ca-10/server-ecdsa-a-key.pem",
+			trustBundlezFile: "test_data/ca-10/trust_bundle_10_ecdsa_a.pem",
+			clientCertzFile:  "test_data/ca-10/client-ecdsa-a-cert.pem",
+			clientKeyzFile:   "test_data/ca-10/client-ecdsa-a-key.pem",
+		},
+		{
+			desc:             "Load the correct key-type rsa trustbundle with 1000CA configuration",
+			serverCertzFile:  "test_data/ca-1000/server-rsa-a-cert.pem",
+			serverKeyzFile:   "test_data/ca-1000/server-rsa-a-key.pem",
+			trustBundlezFile: "test_data/ca-1000/trust_bundle_1000_rsa_a.pem",
+			clientCertzFile:  "test_data/ca-1000/client-rsa-a-cert.pem",
+			clientKeyzFile:   "test_data/ca-1000/client-rsa-a-key.pem",
+		},
+		{
+			desc:             "Load the correct key-type ecdsa trustbundle with 1000CA configuration",
+			serverCertzFile:  "test_data/ca-1000/server-ecdsa-a-cert.pem",
+			serverKeyzFile:   "test_data/ca-1000/server-ecdsa-a-key.pem",
+			trustBundlezFile: "test_data/ca-1000/trust_bundle_1000_ecdsa_a.pem",
+			clientCertzFile:  "test_data/ca-1000/client-ecdsa-a-cert.pem",
+			clientKeyzFile:   "test_data/ca-1000/client-ecdsa-a-key.pem",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+
+			sc1, err := os.ReadFile(tc.serverCertzFile)
+			if err != nil {
+				t.Fatalf("Failed to read certificate: %v", err)
+			}
+			block, _ := pem.Decode(sc1)
+			if block == nil {
+				t.Fatalf("Failed to parse PEM block containing the public key.")
+			}
+			sCert, err := x509.ParseCertificate(block.Bytes)
+			if err != nil {
+				t.Fatalf("Failed to parse certificate: %v", err)
+			}
+
+			san := sCert.DNSNames[0]
+
+			serverCert := setup_service.CreateCertzChain(t, setup_service.CertificateChainRequest{
+				RequestType:    setup_service.EntityTypeCertificateChain,
+				ServerCertFile: tc.serverCertzFile,
+				ServerKeyFile:  tc.serverKeyzFile})
+
+			serverCertEntity := setup_service.CreateCertzEntity(t, setup_service.EntityTypeCertificateChain, &serverCert)
+
+			trustCertChain := setup_service.CreateCertChainFromTrustBundle(tc.trustBundlezFile)
+			trustBundleEntity := setup_service.CreateCertzEntity(t, setup_service.EntityTypeTrustBundle, trustCertChain)
+
+			cert, err := tls.LoadX509KeyPair(tc.clientCertzFile, tc.clientKeyzFile)
+			if err != nil {
+				t.Fatalf("Failed to load  client cert: %v", err)
+			}
+
+			cacert := x509.NewCertPool()
+			cacertBytes, err := os.ReadFile(tc.trustBundlezFile)
+			if err != nil {
+				t.Fatalf("Failed to read ca bundle :%v", err)
+			}
+			if ok := cacert.AppendCertsFromPEM(cacertBytes); !ok {
+				t.Fatalf("Failed to parse %v", tc.trustBundlezFile)
+			}
+
+			certzClient := gnsiC.Certz()
+			success := setup_service.CertzRotate(t, dut, cacert, cert, certzClient, testProfile, san, &serverCertEntity, &trustBundleEntity)
+			if !success {
+				t.Fatalf("%s:Certz/Rotate failed.", tc.desc)
+			}
+
+			t.Logf("%s:successfully completed second certz/Rotate!", tc.desc)
+			// Verification check of the new connection with the new rotated certificates.
+			t.Run("Verification of fresh new connection after successful rotate ", func(t *testing.T) {
+				result := setup_service.PostValidationCheck(t, dut, cacert, san, serverAddr, username, password, cert)
+				if !result {
+					t.Fatalf("%s postTestcase validation failed.", tc.desc)
+				}
+
+				t.Logf("PASS: %s successfully completed!", tc.desc)
+			})
+		})
+	}
+}
---
+++ feature/security/gnsi/certz/tests/internal/setup_service/setup_service.go
@@ -0,0 +1,511 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package setup_service
+
+import (
+	context "context"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"io"
+	"os"
+	"testing"
+	"time"
+
+	gnmipb "github.com/openconfig/gnmi/proto/gnmi"
+	spb "github.com/openconfig/gnoi/system"
+	authzpb "github.com/openconfig/gnsi/authz"
+	certzpb "github.com/openconfig/gnsi/certz"
+	gribipb "github.com/openconfig/gribi/v1/proto/service"
+	"github.com/openconfig/ondatra"
+	"github.com/openconfig/ondatra/knebind/creds"
+
+	p4rtpb "github.com/p4lang/p4runtime/go/p4/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	entityVersion = "one"
+)
+
+type rpcCredentials struct {
+	*creds.UserPass
+}
+
+func (r *rpcCredentials) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string{
+		"username": r.UserPass.Username,
+		"password": r.UserPass.Password,
+	}, nil
+}
+
+func (r *rpcCredentials) RequireTransportSecurity() bool {
+	return true
+}
+
+type EntityType int8
+
+const (
+	EntityTypeCertificateChain EntityType = 0
+	EntityTypeTrustBundle      EntityType = 1
+	EntityTypeCRL              EntityType = 2
+	EntityTypeAuthPolicy       EntityType = 3
+)
+
+type CertificateChainRequest struct {
+	RequestType     EntityType
+	ServerCertFile  string
+	ServerKeyFile   string
+	TrustBundleFile string
+}
+
+// function to create certificate entity of type certificate chain/trust bundle/CRL/Authpolicy.
+func CreateCertzEntity(t *testing.T, typeOfEntity EntityType, entityContent any) certzpb.Entity {
+
+	createdOnTime := time.Now()
+	varClock := uint64(createdOnTime.Unix())
+
+	switch typeOfEntity {
+	case EntityTypeCertificateChain:
+
+		return certzpb.Entity{
+			Version:   entityVersion,
+			CreatedOn: varClock,
+			Entity:    &certzpb.Entity_CertificateChain{CertificateChain: entityContent.(*certzpb.CertificateChain)}}
+
+	case EntityTypeTrustBundle:
+
+		return certzpb.Entity{
+			Version:   entityVersion,
+			CreatedOn: varClock,
+			Entity:    &certzpb.Entity_TrustBundle{TrustBundle: entityContent.(*certzpb.CertificateChain)}}
+
+	case EntityTypeCRL:
+
+		return certzpb.Entity{
+			Version:   entityVersion,
+			CreatedOn: varClock,
+			Entity:    &certzpb.Entity_CertificateRevocationListBundle{CertificateRevocationListBundle: entityContent.(*certzpb.CertificateRevocationListBundle)}}
+
+	case EntityTypeAuthPolicy:
+
+		return certzpb.Entity{
+			Version:   entityVersion,
+			CreatedOn: varClock,
+			Entity:    &certzpb.Entity_AuthenticationPolicy{AuthenticationPolicy: entityContent.(*certzpb.AuthenticationPolicy)}}
+
+	default:
+		t.Fatalf("Invalid entity type")
+	}
+	return certzpb.Entity{}
+}
+
+// function to get the certificate chain of type certificate chain/trust bundle.
+func CreateCertzChain(t *testing.T, certData CertificateChainRequest) certzpb.CertificateChain {
+
+	switch certData.RequestType {
+	case EntityTypeCertificateChain:
+		if len(certData.ServerCertFile) == 0 {
+			t.Fatalf("Missing server certificate file for creating certificate chain object.")
+		}
+		serverCertContent, err := os.ReadFile(certData.ServerCertFile)
+		if err != nil {
+			t.Fatalf("Error reading Server Certificate file at: %v with error: %v", certData.ServerCertFile, err)
+
+		}
+		if len(certData.ServerKeyFile) != 0 {
+			serverKeyContent, err := os.ReadFile(certData.ServerKeyFile)
+			if err != nil {
+				t.Fatalf("Error reading Server Key file at: %v with error: %v", certData.ServerKeyFile, err)
+			}
+			return certzpb.CertificateChain{Certificate: &certzpb.Certificate{
+				Type:        certzpb.CertificateType_CERTIFICATE_TYPE_X509,
+				Encoding:    certzpb.CertificateEncoding_CERTIFICATE_ENCODING_PEM,
+				Certificate: serverCertContent,
+				PrivateKey:  serverKeyContent}, Parent: nil}
+		}
+		return certzpb.CertificateChain{Certificate: &certzpb.Certificate{
+			Type:        certzpb.CertificateType_CERTIFICATE_TYPE_X509,
+			Encoding:    certzpb.CertificateEncoding_CERTIFICATE_ENCODING_PEM,
+			Certificate: serverCertContent,
+			PrivateKey:  nil}, Parent: nil}
+
+	case EntityTypeTrustBundle:
+		if len(certData.TrustBundleFile) == 0 {
+			t.Fatalf("Missing trust bundle file for creating certificate chain object.")
+		}
+		trustBundleContent, err := os.ReadFile(certData.TrustBundleFile)
+		if err != nil {
+			t.Fatalf("Error reading trust bundle file at: %v with error: %v", certData.TrustBundleFile, err)
+		}
+		return certzpb.CertificateChain{Certificate: &certzpb.Certificate{
+			Type:        certzpb.CertificateType_CERTIFICATE_TYPE_X509,
+			Encoding:    certzpb.CertificateEncoding_CERTIFICATE_ENCODING_PEM,
+			Certificate: trustBundleContent,
+		}, Parent: nil}
+
+	default:
+		t.Fatalf("Invalid request type received.")
+	}
+	return certzpb.CertificateChain{}
+}
+
+// function to create the certificate chain from trust bundle.
+func CreateCertChainFromTrustBundle(fileName string) *certzpb.CertificateChain {
+	pemData, err := os.ReadFile(fileName)
+	if err != nil {
+		return &certzpb.CertificateChain{}
+	}
+	var trust [][]byte
+	for {
+		var block *pem.Block
+		block, pemData = pem.Decode(pemData)
+		if block == nil {
+			break
+		}
+		if block.Type != "CERTIFICATE" {
+			continue
+		}
+		p := pem.EncodeToMemory(block)
+		if p == nil {
+			return &certzpb.CertificateChain{}
+		}
+		trust = append(trust, p)
+	}
+	if len(trust) > 0 {
+		var prevCert *certzpb.CertificateChain
+		var bundleToReturn *certzpb.CertificateChain
+		for i := len(trust) - 1; i >= 0; i-- {
+			if i == len(trust)-1 {
+				bundleToReturn = &certzpb.CertificateChain{Certificate: &certzpb.Certificate{
+					Type:        certzpb.CertificateType_CERTIFICATE_TYPE_X509,
+					Encoding:    certzpb.CertificateEncoding_CERTIFICATE_ENCODING_PEM,
+					Certificate: trust[i],
+				}, Parent: nil}
+				prevCert = bundleToReturn
+			} else {
+				prevCert = bundleToReturn
+				bundleToReturn = &certzpb.CertificateChain{Certificate: &certzpb.Certificate{
+					Type:        certzpb.CertificateType_CERTIFICATE_TYPE_X509,
+					Encoding:    certzpb.CertificateEncoding_CERTIFICATE_ENCODING_PEM,
+					Certificate: trust[i],
+				}, Parent: prevCert}
+			}
+		}
+		return bundleToReturn
+	} else {
+		return &certzpb.CertificateChain{}
+	}
+
+}
+
+// function to request the certz rotation and validate the certificates.
+func CertzRotate(t *testing.T, dut *ondatra.DUTDevice, caCert *x509.CertPool, cert tls.Certificate, certzClient certzpb.CertzClient, profileId string, serverName string, entities ...*certzpb.Entity) bool {
+
+	if len(entities) == 0 {
+		t.Logf("At least one entity required for Rotate request.")
+		return false
+	}
+	uploadRequest := &certzpb.UploadRequest{Entities: entities}
+	rotateRequest := &certzpb.RotateCertificateRequest_Certificates{Certificates: uploadRequest}
+	rotateCertRequest := &certzpb.RotateCertificateRequest{
+		ForceOverwrite: false,
+		SslProfileId:   profileId,
+		RotateRequest:  rotateRequest}
+	rotateRequestClient, err := certzClient.Rotate(context.Background())
+	defer rotateRequestClient.CloseSend()
+	if err != nil {
+		t.Fatalf("Error creating rotate request client: %v", err)
+	}
+	err = rotateRequestClient.Send(rotateCertRequest)
+	if err != nil {
+		t.Fatalf("Error sending rotate request: %v", err)
+	}
+	rotateResponse := &certzpb.RotateCertificateResponse{}
+	for i := 0; i < 6; i++ {
+		rotateResponse, err = rotateRequestClient.Recv()
+		if err == nil {
+			break
+		}
+		t.Logf("Did not receive response ~ %vs after sending rotate request. Sleeping 10s to retry...", i*10)
+		time.Sleep(10 * time.Second)
+	}
+	if err != nil {
+		t.Logf("Error fetching rotate certificate response: %v", err)
+		return false
+	}
+	t.Logf("Received Rotate certificate response: %v", rotateResponse)
+
+	finalizeRequest := &certzpb.RotateCertificateRequest_FinalizeRotation{FinalizeRotation: &certzpb.FinalizeRequest{}}
+	rotateCertRequest = &certzpb.RotateCertificateRequest{
+		ForceOverwrite: false,
+		SslProfileId:   profileId,
+		RotateRequest:  finalizeRequest}
+
+	err = rotateRequestClient.Send(rotateCertRequest)
+	if err != nil {
+		t.Fatalf("Error sending rotate finalize request: %v", err)
+	}
+	err = rotateRequestClient.CloseSend()
+	if err != nil {
+		t.Fatalf("Error sending rotate close send request: %v", err)
+	}
+	return true
+}
+
+// function to validate the gNSI service RPC after successful rotation.
+func TestGnsi(t *testing.T, dut *ondatra.DUTDevice, caCert *x509.CertPool, san, serverAddr, username, password string, cert tls.Certificate) bool {
+
+	credOpts := []grpc.DialOption{grpc.WithBlock(), grpc.WithTransportCredentials(credentials.NewTLS(
+		&tls.Config{
+			Certificates: []tls.Certificate{cert},
+			RootCAs:      caCert,
+			ServerName:   san,
+		}))}
+	creds := &rpcCredentials{&creds.UserPass{Username: username, Password: password}}
+	credOpts = append(credOpts, grpc.WithPerRPCCredentials(creds))
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	target := fmt.Sprintf("%s:%d", serverAddr, 9339)
+	conn, err := grpc.DialContext(ctx, target, credOpts...)
+	if err != nil {
+		t.Fatalf("gRPCdial failed to: %q", target)
+	}
+	defer conn.Close()
+
+	authzClient := authzpb.NewAuthzClient(conn)
+	rsp, err := authzClient.Get(ctx, &authzpb.GetRequest{})
+	if err != nil {
+		statusError, _ := status.FromError(err)
+		if statusError.Code() == codes.FailedPrecondition {
+			t.Logf("Expected error FAILED_PRECONDITION seen for authz Get Request.")
+		} else {
+			t.Errorf("Unexpected error during authz Get Request.")
+		}
+	}
+	t.Logf("gNSI authz get response is %s", rsp)
+	conn.Close()
+	return true
+}
+
+// function to validate the gNOI service RPC after successful rotation.
+func TestGnoi(t *testing.T, caCert *x509.CertPool, san, serverAddr, username, password string, cert tls.Certificate) bool {
+
+	credOpts := []grpc.DialOption{grpc.WithBlock(), grpc.WithTransportCredentials(credentials.NewTLS(
+		&tls.Config{
+			Certificates: []tls.Certificate{cert},
+			RootCAs:      caCert,
+			ServerName:   san,
+		}))}
+	creds := &rpcCredentials{&creds.UserPass{Username: username, Password: password}}
+	credOpts = append(credOpts, grpc.WithPerRPCCredentials(creds))
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	target := fmt.Sprintf("%s:%d", serverAddr, 9339)
+
+	conn, err := grpc.DialContext(ctx, target, credOpts...)
+	if err != nil {
+		t.Fatalf("gRPCdial failed to: %q", target)
+	}
+	defer conn.Close()
+
+	sysClient := spb.NewSystemClient(conn)
+	_, err = sysClient.Ping(context.Background(), &spb.PingRequest{})
+	if err != nil {
+		t.Logf("Unable to connect gnoiClient %v", err)
+	}
+	conn.Close()
+	return true
+}
+
+// function to validate the gNMI service RPC after successful rotation.
+func TestGnmi(t *testing.T, dut *ondatra.DUTDevice, caCert *x509.CertPool, san, serverAddr, username, password string, cert tls.Certificate) bool {
+
+	credOpts := []grpc.DialOption{grpc.WithBlock(), grpc.WithTransportCredentials(credentials.NewTLS(
+		&tls.Config{
+			Certificates: []tls.Certificate{cert},
+			RootCAs:      caCert,
+			ServerName:   san,
+		}))}
+	creds := &rpcCredentials{&creds.UserPass{Username: username, Password: password}}
+	credOpts = append(credOpts, grpc.WithPerRPCCredentials(creds))
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	target := fmt.Sprintf("%s:%d", serverAddr, 9339)
+	conn, err := grpc.DialContext(ctx, target, credOpts...)
+	if err != nil {
+		t.Fatalf("gRpcDial failed to: %q", target)
+		return false
+	}
+	defer conn.Close()
+
+	gnmiClient := gnmipb.NewGNMIClient(conn)
+	stream, err := gnmiClient.Subscribe(ctx)
+	defer stream.CloseSend()
+
+	if err != nil {
+		t.Fatalf("Subscribe failed: %s", err)
+		return false
+	}
+
+	sub := &gnmipb.SubscribeRequest{
+		Request: &gnmipb.SubscribeRequest_Subscribe{
+			Subscribe: &gnmipb.SubscriptionList{
+				Subscription: []*gnmipb.Subscription{
+					{
+						Path: &gnmipb.Path{
+							Elem: []*gnmipb.PathElem{{Name: "system"}, {Name: "state"}, {Name: "software-version"}},
+						},
+					},
+				},
+			},
+		},
+	}
+	t.Logf("Sending subscribe request: %s", sub)
+	err = stream.Send(sub)
+	if err != nil {
+		t.Fatalf("Failed to subscribe: %s", err)
+		return false
+	}
+	response, err := stream.Recv()
+	if err != nil {
+		if err != io.EOF {
+			t.Fatalf("Error received from the server: %s", err)
+			return false
+		}
+	}
+	t.Logf("gNMI response: %s", response)
+	conn.Close()
+	return true
+}
+
+// function to validate the gRIBI service RPC after successful rotation.
+func TestGribi(t *testing.T, caCert *x509.CertPool, san, serverAddr, username, password string, cert tls.Certificate) bool {
+
+	credOpts := []grpc.DialOption{grpc.WithBlock(), grpc.WithTransportCredentials(credentials.NewTLS(
+		&tls.Config{
+			Certificates: []tls.Certificate{cert},
+			RootCAs:      caCert,
+			ServerName:   san,
+		}))}
+	creds := &rpcCredentials{&creds.UserPass{Username: username, Password: password}}
+	credOpts = append(credOpts, grpc.WithPerRPCCredentials(creds))
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	target := fmt.Sprintf("%s:%d", serverAddr, 9340)
+	conn, err := grpc.DialContext(ctx, target, credOpts...)
+	if err != nil {
+		t.Fatalf("gRpcDial failed to: %q", target)
+	}
+	defer conn.Close()
+
+	gRibiClient := gribipb.NewGRIBIClient(conn)
+	_, err = gRibiClient.Get(context.Background(), &gribipb.GetRequest{})
+	if err != nil {
+		t.Fatalf("Unable to connect GribiClient %v", err)
+	}
+	conn.Close()
+	return true
+}
+
+// function to validate the P4rt service RPC after successful rotation.
+func TestP4rt(t *testing.T, caCert *x509.CertPool, san, serverAddr, username, password string, cert tls.Certificate) bool {
+
+	credOpts := []grpc.DialOption{grpc.WithBlock(), grpc.WithTransportCredentials(credentials.NewTLS(
+		&tls.Config{
+			Certificates: []tls.Certificate{cert},
+			RootCAs:      caCert,
+			ServerName:   san,
+		}))}
+	creds := &rpcCredentials{&creds.UserPass{Username: username, Password: password}}
+	credOpts = append(credOpts, grpc.WithPerRPCCredentials(creds))
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	target := fmt.Sprintf("%s:%d", serverAddr, 9559)
+	conn, err := grpc.DialContext(ctx, target, credOpts...)
+	if err != nil {
+		t.Fatalf("gRpcDial failed to: %q", target)
+	}
+	defer conn.Close()
+
+	p4RtClient := p4rtpb.NewP4RuntimeClient(conn)
+	_, err = p4RtClient.Capabilities(context.Background(), &p4rtpb.CapabilitiesRequest{})
+	if err != nil {
+		t.Fatalf("Unable to connect P4rtClient %v", err)
+	}
+	conn.Close()
+	return true
+}
+
+// function to do a validation of gNMI/gNOI/gRIBI/p4RT services before certz rotation.
+func PreInitCheck(t *testing.T, dut *ondatra.DUTDevice, ctx context.Context) bool {
+
+	gnmiC, err := dut.RawAPIs().BindingDUT().DialGNMI(ctx)
+	if err != nil {
+		t.Fatalf("Could not create gNMI Connection %v", err)
+	}
+	t.Logf("Precheck:gNMI connection is successful %v", gnmiC)
+	gribiC, err := dut.RawAPIs().BindingDUT().DialGRIBI(ctx)
+	if err != nil {
+		t.Fatalf("Could not create gRIBI Connection %v", err)
+	}
+	t.Logf("Precheck:gRIBI connection is successful %v", gribiC)
+	gnoiC, err := dut.RawAPIs().BindingDUT().DialGNOI(ctx)
+	if err != nil {
+		t.Fatalf("Could not create gNOI Connection %v", err)
+	}
+	t.Logf("Precheck:gNOI connection is successful %v", gnoiC)
+	p4rtC, err := dut.RawAPIs().BindingDUT().DialP4RT(ctx)
+	if err != nil {
+		t.Fatalf("Could not create p4RT Connection %v", err)
+	}
+	t.Logf("Precheck:p4RT connection is successful %v", p4rtC)
+	return true
+}
+
+// function to do a validation of all services after certz rotation.
+func PostValidationCheck(t *testing.T, dut *ondatra.DUTDevice, caCert *x509.CertPool, san, serverAddr, username, password string, cert tls.Certificate) bool {
+
+	if !TestGnsi(t, dut, caCert, san, serverAddr, username, password, cert) {
+		t.Fatalf("Could not create new gNSI Connection.")
+	}
+	t.Logf("New gNSI connection successfully completed")
+	if !TestGnmi(t, dut, caCert, san, serverAddr, username, password, cert) {
+		t.Fatalf("Could not create new gNMI Connection.")
+	}
+	t.Logf("New gNMI connection successfully completed")
+	if !TestGnoi(t, caCert, san, serverAddr, username, password, cert) {
+		t.Fatalf("Could not create new gNOI Connection")
+	}
+	t.Logf("New gNOI connection successfully completed")
+	if !TestGribi(t, caCert, san, serverAddr, username, password, cert) {
+		t.Fatalf("Could not create new gRIBI Connection")
+	}
+	t.Logf("New gRIBI connection successfully completed")
+	if !TestP4rt(t, caCert, san, serverAddr, username, password, cert) {
+		t.Fatalf("Could not create new P4rt Connection")
+	}
+	t.Logf("New P4rt connection successfully completed")
+	return true
+}
