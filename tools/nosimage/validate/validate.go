b3968eb21ff1fd546acf40e4083a29f777c3950a
wenovus <wenbli@google.com>
2024-01-09 18:17:15 -0800

    gofmt

--- tools/nosimage/validate/validate.go
+++ tools/nosimage/validate/validate.go
@@ -116,7 +116,7 @@ func main() {
 	var hasErr bool
 	paths, invalidPaths, err := ocpaths.ValidatePaths(profile.GetOcpaths().GetOcpaths(), publicPath)
 	if err != nil {
-    fmt.Printf("profile contains %d invalid OCPaths:\n%v", len(invalidPaths), err)
+		fmt.Printf("profile contains %d invalid OCPaths:\n%v", len(invalidPaths), err)
 		fmt.Println(err)
 		hasErr = true
 	} else {
