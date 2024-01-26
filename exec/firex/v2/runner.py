b3fca1f22600bb57701e297e6f9028e06c0846e4
Karim Jahed <kjahed@cisco.com>
2024-01-25 21:51:08 -0500

    use new generator

--- exec/firex/v2/runner.py
+++ exec/firex/v2/runner.py
@@ -717,11 +717,9 @@ def GenerateOndatraTestbedFiles(self, ws, testbed_logs_dir, internal_fp_repo_dir
     pyats_testbed = kwargs.get('testbed', reserved_testbed.get('pyats_testbed', None))
             
     if reserved_testbed.get('sim', False):
-        vxr_testbed = kwargs['testbed_path']
-        check_output(f'/auto/firex/sw/pyvxr_binding/pyvxr_binding.sh staticbind service {vxr_testbed}', 
-            file=ondatra_binding_path)
-        check_output(f'/auto/firex/sw/pyvxr_binding/pyvxr_binding.sh statictestbed service {vxr_testbed}', 
-            file=ondatra_testbed_path)
+        sim_out_dir = os.path.join(testbed_logs_dir, 'testbed_logs', 'bringup_success')
+        pyvxr_generator = _resolve_path_if_needed(internal_fp_repo_dir, os.path.join('exec', 'utils', 'pyvxr', 'generate_bindings.py'))
+        check_output(f'{PYTHON_BIN} {pyvxr_generator} {sim_out_dir} {ondatra_testbed_path} {ondatra_binding_path}')
 
         mgmt_ips = _sim_get_mgmt_ips(testbed_logs_dir)
         if not type(reserved_testbed['baseconf']) is dict:
@@ -945,7 +943,7 @@ def InstallGoDelve(self, ws, repo):
     logger.print(
         check_output(f'{GO_BIN} install github.com/go-delve/delve/cmd/dlv@latest', env=env, cwd=repo)
     )
-
+        
 # noinspection PyPep8Naming
 @app.task(bind=True)
 def ReleaseIxiaPorts(self, ws, binding_file):
