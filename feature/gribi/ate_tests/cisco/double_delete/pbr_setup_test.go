1a48dd9daed931143736e81657a4b084d8e6944d
arvbaska1 <arvbaska@cisco.com>
2024-02-09 19:43:00 -0500

    Add ConfigureDefaultNetworkInstance for pbr

--- feature/gribi/ate_tests/cisco/double_delete/pbr_setup_test.go
+++ feature/gribi/ate_tests/cisco/double_delete/pbr_setup_test.go
@@ -21,13 +21,14 @@ import (
 	"github.com/openconfig/ondatra"
 	"github.com/openconfig/ondatra/gnmi"
 	"github.com/openconfig/ondatra/gnmi/oc"
+	"github.com/openconfig/featureprofiles/internal/fptest"
 	"github.com/openconfig/ygot/ygot"
 )
 
 // configbasePBR, creates class map, policy and configures under source interface
 func configbasePBR(t *testing.T, dut *ondatra.DUTDevice, networkInstance, iptype string, index uint32, protocol oc.E_PacketMatchTypes_IP_PROTOCOL, dscpset []uint8, pbrName string, intfName string) {
 	t.Helper()
-
+	fptest.ConfigureDefaultNetworkInstance(t, dut)
 	r := oc.NetworkInstance_PolicyForwarding_Policy_Rule{}
 	r.SequenceId = ygot.Uint32(index)
 	r.Action = &oc.NetworkInstance_PolicyForwarding_Policy_Rule_Action{NetworkInstance: ygot.String(networkInstance)}
