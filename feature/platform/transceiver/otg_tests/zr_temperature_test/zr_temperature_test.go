3ead807291a7095b32fd81a7bba983d37a917c1f
Sunil Prajapati <sunilpr@google.com>
2024-03-05 17:44:38 +0000

    fix static check

--- feature/platform/transceiver/otg_tests/zr_temperature_test/zr_temperature_test.go
+++ feature/platform/transceiver/otg_tests/zr_temperature_test/zr_temperature_test.go
@@ -23,7 +23,6 @@ import (
         "github.com/openconfig/ondatra"
         "github.com/openconfig/ondatra/gnmi"
         "github.com/openconfig/ondatra/gnmi/oc"
-        "github.com/openconfig/ygnmi/ygnmi"
         "github.com/openconfig/ygot/ygot"
 	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/featureprofiles/internal/samplestream"
