ced72bb5b080e941918052d1867df883c7940451
Sunil Prajapati <sunilpr@google.com>
2024-03-05 17:50:19 +0000

    fix fmt

--- feature/platform/transceiver/otg_tests/zr_temperature_test/zr_temperature_test.go
+++ feature/platform/transceiver/otg_tests/zr_temperature_test/zr_temperature_test.go
@@ -16,16 +16,16 @@ package zr_temperature_test
 
 import (
 	"fmt"
+	"github.com/openconfig/featureprofiles/internal/deviations"
+	"github.com/openconfig/featureprofiles/internal/fptest"
+	"github.com/openconfig/featureprofiles/internal/samplestream"
+	"github.com/openconfig/ondatra"
+	"github.com/openconfig/ondatra/gnmi"
+	"github.com/openconfig/ondatra/gnmi/oc"
+	"github.com/openconfig/ygot/ygot"
 	"reflect"
 	"testing"
 	"time"
-        "github.com/openconfig/featureprofiles/internal/fptest"
-        "github.com/openconfig/ondatra"
-        "github.com/openconfig/ondatra/gnmi"
-        "github.com/openconfig/ondatra/gnmi/oc"
-        "github.com/openconfig/ygot/ygot"
-	"github.com/openconfig/featureprofiles/internal/deviations"
-	"github.com/openconfig/featureprofiles/internal/samplestream"
 )
 
 const (
