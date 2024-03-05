ef9ebdbadb3a764d64199b3de380e0bc3e39f5d4
Sunil Prajapati <sunilpr@google.com>
2024-03-05 05:45:26 +0000

    added changes to use samplestream lib

--- feature/platform/transceiver/otg_tests/zr_temperature_test/zr_temperature_test.go
+++ feature/platform/transceiver/otg_tests/zr_temperature_test/zr_temperature_test.go
@@ -15,21 +15,26 @@
 package zr_temperature_test
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 	"time"
-
-	"github.com/openconfig/featureprofiles/internal/fptest"
-	gpb "github.com/openconfig/gnmi/proto/gnmi"
-	"github.com/openconfig/ondatra"
-	"github.com/openconfig/ondatra/gnmi"
-	"github.com/openconfig/ondatra/gnmi/oc"
-	"github.com/openconfig/ygnmi/ygnmi"
-	"github.com/openconfig/ygot/ygot"
+        "github.com/openconfig/featureprofiles/internal/fptest"
+        "github.com/openconfig/ondatra"
+        "github.com/openconfig/ondatra/gnmi"
+        "github.com/openconfig/ondatra/gnmi/oc"
+        "github.com/openconfig/ygnmi/ygnmi"
+        "github.com/openconfig/ygot/ygot"
+	"github.com/golang/ygot/ygot/ygot"
+	"github.com/openconfig/featureprofiles/internal/deviations/deviations"
+	"github.com/openconfig/featureprofiles/internal/samplestream/samplestream"
 )
 
 const (
-	sensorType = oc.PlatformTypes_OPENCONFIG_HARDWARE_COMPONENT_SENSOR
+	sensorType        = oc.PlatformTypes_OPENCONFIG_HARDWARE_COMPONENT_SENSOR
+	dp16QAM           = 1
+	targetOutputPower = -10
+	frequency         = 193100000
 )
 
 func TestMain(m *testing.M) {
@@ -38,22 +43,50 @@ func TestMain(m *testing.M) {
 
 // Topology:
 //
-//	dut1:port1 <--> port1:dut2
-func gnmiOpts(t *testing.T, dut *ondatra.DUTDevice, mode gpb.SubscriptionMode, interval time.Duration) *gnmi.Opts {
-	return dut.GNMIOpts().WithYGNMIOpts(ygnmi.WithSubscriptionMode(mode), ygnmi.WithSampleInterval(interval))
+//	dut:port1 <--> port2:dut
+
+func interfaceConfig(t *testing.T, dut1 *ondatra.DUTDevice, dp *ondatra.Port) {
+	d := &oc.Root{}
+	i := d.GetOrCreateInterface(dp.Name())
+	i.Enabled = ygot.Bool(true)
+	i.Type = oc.IETFInterfaces_InterfaceType_ethernetCsmacd
+	gnmi.Replace(t, dut1, gnmi.OC().Interface(dp.Name()).Config(), i)
+	// transceiverName := gnmi.Get(t, dut1, gnmi.OC().Interface(dp.Name()).Transceiver().State())
+	// OCcomponent := transceiverName + "-Optical0"
+	OCcomponent := opticalChannelFromPort(t, dut1, dp)
+	gnmi.Replace(t, dut1, gnmi.OC().Component(OCcomponent).OpticalChannel().Config(), &oc.Component_OpticalChannel{
+		TargetOutputPower: ygot.Float64(targetOutputPower),
+		Frequency:         ygot.Uint64(frequency),
+	})
+}
+func verifyTemperatureSensorValue(t *testing.T, dut1 *ondatra.DUTDevice, pStream *samplestream.SampleStream[float64], sensorName string) float64 {
+	temperatureSample := pStream.Next(t)
+	if temperatureSample == nil {
+		t.Fatalf("Temperature telemetry %s is not present", sensorName)
+	}
+	temperatureVal, ok := temperatureSample.Val()
+	if !ok {
+		t.Fatalf("Temperature %q telemetry is not present", temperatureSample)
+	}
+	// Check temperature return value of correct type
+	if reflect.TypeOf(temperatureVal).Kind() != reflect.Float64 {
+		t.Fatalf("Return value is not type float64")
+	} else if temperatureVal <= 0 && temperatureVal >= 300 {
+		t.Fatalf("The variable temperature instent is not between 0 and 300")
+	}
+	t.Logf("Temperature sample value %s: %v", sensorName, temperatureVal)
+	return temperatureVal
 }
 
 func TestZRTemperatureState(t *testing.T) {
-	dut1 := ondatra.DUT(t, "dut1")
+	dut1 := ondatra.DUT(t, "dut")
 	dp1 := dut1.Port(t, "port1")
+	dp2 := dut1.Port(t, "port2")
 	t.Logf("dut1: %v", dut1)
 	t.Logf("dut1 dp1 name: %v", dp1.Name())
-	d := &oc.Root{}
-	i := d.GetOrCreateInterface(dp1.Name())
-	i.Enabled = ygot.Bool(true)
-	i.Type = oc.IETFInterfaces_InterfaceType_ethernetCsmacd
 	intUpdateTime := 2 * time.Minute
-	gnmi.Replace(t, dut1, gnmi.OC().Interface(dp1.Name()).Config(), i)
+	interfaceConfig(t, dut1, dp1)
+	interfaceConfig(t, dut1, dp2)
 	gnmi.Await(t, dut1, gnmi.OC().Interface(dp1.Name()).OperStatus().State(), intUpdateTime, oc.Interface_OperStatus_UP)
 	transceiverName := gnmi.Get(t, dut1, gnmi.OC().Interface(dp1.Name()).Transceiver().State())
 	// Check if TRANSCEIVER is of type 400ZR
@@ -74,82 +107,48 @@ func TestZRTemperatureState(t *testing.T) {
 			}
 		}
 	}
-	temperatureInstantSample := gnmi.Collect(t, gnmiOpts(t, dut1, gpb.SubscriptionMode_SAMPLE, time.Second*10), component1.Temperature().Instant().State(), time.Second*10)
-	temperatureInstantSamples := temperatureInstantSample.Await(t)
-	if len(temperatureInstantSamples) == 0 {
-		t.Fatalf("Get Temeprature Instant sample list for interface %q: got 0, want > 0", dp1.Name())
-	}
-	// Taking first sample
-	temperatureInstant, _ := temperatureInstantSamples[0].Val()
-	// Check temperature return value of correct type
-	if reflect.TypeOf(temperatureInstant).Kind() != reflect.Float64 {
-		t.Fatalf("Return value is not type float64")
-	} else if temperatureInstant <= 0 && temperatureInstant >= 300 {
-		t.Fatalf("The variable temperature instent is not between 0 and 300")
-	}
+	p1StreamInstant := samplestream.New(t, dut1, component1.Temperature().Instant().State(), 10*time.Second)
+	p1StreamAvg := samplestream.New(t, dut1, component1.Temperature().Avg().State(), 10*time.Second)
+	p1StreamMin := samplestream.New(t, dut1, component1.Temperature().Min().State(), 10*time.Second)
+	p1StreamMax := samplestream.New(t, dut1, component1.Temperature().Max().State(), 10*time.Second)
+	temperatureInstant := verifyTemperatureSensorValue(t, dut1, p1StreamInstant, "Instant")
 	t.Logf("Port1 dut1 %s Instant Temperature: %v", dp1.Name(), temperatureInstant)
-	temperatureMinSample := gnmi.Collect(t, gnmiOpts(t, dut1, gpb.SubscriptionMode_SAMPLE, time.Second*10), component1.Temperature().Min().State(), time.Second*10)
-	temperatureMinSamples := temperatureMinSample.Await(t)
-	if len(temperatureMinSamples) == 0 {
-		t.Fatalf("Get Temeprature Min sample list for interface %q: got 0, want > 0", dp1.Name())
-	}
-	// Taking first sample
-	temperatureMin, _ := temperatureMinSamples[0].Val()
-	t.Logf("Port1 dut1 %s Min Temperature: %v", dp1.Name(), temperatureMin)
-	if reflect.TypeOf(temperatureMin).Kind() != reflect.Float64 {
-		t.Fatalf("Return value is not type float64")
-	} else if temperatureMin <= 0 && temperatureMin >= 300 {
-		t.Fatalf("The variable temperature min is not between 0 and 300")
-	}
-	temperatureMaxSample := gnmi.Collect(t, gnmiOpts(t, dut1, gpb.SubscriptionMode_SAMPLE, time.Second*10), component1.Temperature().Max().State(), time.Second*10)
-	temperatureMaxSamples := temperatureMaxSample.Await(t)
-	if len(temperatureMaxSamples) == 0 {
-		t.Fatalf("Get Temeprature Max sample list for interface %q: got 0, want > 0", dp1.Name())
-	}
-	// Taking first sample
-	temperatureMax, _ := temperatureMaxSamples[0].Val()
+	temperatureMax := verifyTemperatureSensorValue(t, dut1, p1StreamMax, "Max")
 	t.Logf("Port1 dut1 %s Max Temperature: %v", dp1.Name(), temperatureMax)
-	if reflect.TypeOf(temperatureMax).Kind() != reflect.Float64 {
-		t.Fatalf("Return value is not type float64")
-	} else if temperatureMax <= 0 && temperatureMax >= 300 {
-		t.Fatalf("The variable temperature min is not between 0 and 300")
-	}
-	temperatureAvgSample := gnmi.Collect(t, gnmiOpts(t, dut1, gpb.SubscriptionMode_SAMPLE, time.Second*10), component1.Temperature().Avg().State(), time.Second*10)
-	temperatureAvgSamples := temperatureAvgSample.Await(t)
-	if len(temperatureAvgSamples) == 0 {
-		t.Fatalf("Get Temeprature Avg sample list for interface %q: got 0, want > 0", dp1.Name())
-	}
-	// Taking first sample
-	temperatureAvg, _ := temperatureAvgSamples[0].Val()
-	if reflect.TypeOf(temperatureAvg).Kind() != reflect.Float64 {
-		t.Fatalf("Return value is not type float64")
-	} else if temperatureAvg <= 0 && temperatureAvg >= 300 {
-		t.Fatalf("The variable temperature avg is not between 0 and 300")
-	}
+	temperatureMin := verifyTemperatureSensorValue(t, dut1, p1StreamMin, "Min")
+	t.Logf("Port1 dut1 %s Min Temperature: %v", dp1.Name(), temperatureMin)
+	temperatureAvg := verifyTemperatureSensorValue(t, dut1, p1StreamAvg, "Avg")
 	t.Logf("Port1 dut1 %s Avg Temperature: %v", dp1.Name(), temperatureAvg)
-	// temperature average should be between min and max
 	if temperatureAvg >= temperatureMin && temperatureAvg <= temperatureMax {
 		t.Logf("The average is between the maximum and minimum values")
 	} else {
 		t.Fatalf("The average is not between the maximum and minimum values")
 	}
+	p1StreamMin.Close()
+	p1StreamMax.Close()
+	p1StreamAvg.Close()
+	p1StreamInstant.Close()
 }
 func TestZRTemperatureStateInterfaceFlap(t *testing.T) {
-	dut1 := ondatra.DUT(t, "dut1")
+	dut1 := ondatra.DUT(t, "dut")
 	dp1 := dut1.Port(t, "port1")
+	dp2 := dut1.Port(t, "port2")
 	t.Logf("dut1: %v", dut1)
 	t.Logf("dut1 dp1 name: %v", dp1.Name())
-	d := &oc.Root{}
-	i := d.GetOrCreateInterface(dp1.Name())
-	i.Enabled = ygot.Bool(false)
-	i.Type = oc.IETFInterfaces_InterfaceType_ethernetCsmacd
-	// Disable interface
-	gnmi.Replace(t, dut1, gnmi.OC().Interface(dp1.Name()).Config(), i)
+	interfaceConfig(t, dut1, dp1)
+	interfaceConfig(t, dut1, dp2)
+	intUpdateTime := 2 * time.Minute
+	gnmi.Await(t, dut1, gnmi.OC().Interface(dp1.Name()).OperStatus().State(), intUpdateTime, oc.Interface_OperStatus_UP)
 	transceiverName := gnmi.Get(t, dut1, gnmi.OC().Interface(dp1.Name()).Transceiver().State())
 	// Check if TRANSCEIVER is of type 400ZR
 	if dp1.PMD() != ondatra.PMD400GBASEZR {
 		t.Fatalf("%s Transceiver is not 400ZR its of type: %v", transceiverName, dp1.PMD())
 	}
+	// Disable interface
+	d := &oc.Root{}
+	i := d.GetOrCreateInterface(dp1.Name())
+	i.Enabled = ygot.Bool(false)
+	gnmi.Replace(t, dut1, gnmi.OC().Interface(dp1.Name()).Config(), i)
 	component1 := gnmi.OC().Component(transceiverName)
 	subcomponents := gnmi.LookupAll[*oc.Component_Subcomponent](t, dut1, component1.SubcomponentAny().State())
 	for _, s := range subcomponents {
@@ -164,108 +163,64 @@ func TestZRTemperatureStateInterfaceFlap(t *testing.T) {
 			}
 		}
 	}
-	// During interface disable temperature sensor should not return invalid type
-	temperatureInstantSample := gnmi.Collect(t, gnmiOpts(t, dut1, gpb.SubscriptionMode_SAMPLE, time.Second*10), component1.Temperature().Instant().State(), time.Second*10)
-	temperatureInstantSamples := temperatureInstantSample.Await(t)
-	if len(temperatureInstantSamples) == 0 {
-		t.Fatalf("Get Temeprature Instant sample list for interface %q: got 0, want > 0", dp1.Name())
-	}
-	// Taking first sample
-	temperatureInstant, _ := temperatureInstantSamples[0].Val()
-	// Check temperature return value of correct type
-	t.Logf("Port1 dut1 %s Instant Temperature: %v", dp1.Name(), temperatureInstant)
-	temperatureMinSample := gnmi.Collect(t, gnmiOpts(t, dut1, gpb.SubscriptionMode_SAMPLE, time.Second*10), component1.Temperature().Min().State(), time.Second*10)
-	temperatureMinSamples := temperatureMinSample.Await(t)
-	if len(temperatureMinSamples) == 0 {
-		t.Fatalf("Get Temeprature Min sample list for interface %q: got 0, want > 0", dp1.Name())
-	}
-	// Taking first sample
-	temperatureMin, _ := temperatureMinSamples[0].Val()
-	t.Logf("Port1 dut1 %s Min Temperature: %v", dp1.Name(), temperatureMin)
-	temperatureMaxSample := gnmi.Collect(t, gnmiOpts(t, dut1, gpb.SubscriptionMode_SAMPLE, time.Second*10), component1.Temperature().Max().State(), time.Second*10)
-	temperatureMaxSamples := temperatureMaxSample.Await(t)
-	if len(temperatureMaxSamples) == 0 {
-		t.Fatalf("Get Temeprature Max sample list for interface %q: got 0, want > 0", dp1.Name())
-	}
-	// Taking first sample
-	temperatureMax, _ := temperatureMaxSamples[0].Val()
-	t.Logf("Port1 dut1 %s Max Temperature: %v", dp1.Name(), temperatureMax)
-	temperatureAvgSample := gnmi.Collect(t, gnmiOpts(t, dut1, gpb.SubscriptionMode_SAMPLE, time.Second*10), component1.Temperature().Avg().State(), time.Second*10)
-	temperatureAvgSamples := temperatureAvgSample.Await(t)
-	if len(temperatureAvgSamples) == 0 {
-		t.Fatalf("Get Temeprature Avg sample list for interface %q: got 0, want > 0", dp1.Name())
-	}
-	// Taking first sample
-	temperatureAvg, _ := temperatureAvgSamples[0].Val()
-	t.Logf("Port1 dut1 %s Avg Temperature: %v", dp1.Name(), temperatureAvg)
-	if reflect.TypeOf(temperatureInstant).Kind() != reflect.Float64 {
-		t.Fatalf("Return value is not type float64")
-	}
-	if reflect.TypeOf(temperatureMin).Kind() != reflect.Float64 {
-		t.Fatalf("Return value is not type float64")
-	}
-	if reflect.TypeOf(temperatureMax).Kind() != reflect.Float64 {
-		t.Fatalf("Return value is not type float64")
-	}
-	if reflect.TypeOf(temperatureAvg).Kind() != reflect.Float64 {
-		t.Fatalf("Return value is not type float64")
-	}
+	p1StreamInstant := samplestream.New(t, dut1, component1.Temperature().Instant().State(), 10*time.Second)
+	p1StreamAvg := samplestream.New(t, dut1, component1.Temperature().Avg().State(), 10*time.Second)
+	p1StreamMin := samplestream.New(t, dut1, component1.Temperature().Min().State(), 10*time.Second)
+	p1StreamMax := samplestream.New(t, dut1, component1.Temperature().Max().State(), 10*time.Second)
 	// Wait 120 sec cooling off period
-	intUpdateTime := 2 * time.Minute
-	temperatureInstantSample = gnmi.Collect(t, gnmiOpts(t, dut1, gpb.SubscriptionMode_SAMPLE, time.Second*10), component1.Temperature().Instant().State(), time.Second*10)
-	temperatureInstantSamples = temperatureInstantSample.Await(t)
-	// Taking first sample
-	temperatureInstant, _ = temperatureInstantSamples[0].Val()
-	// Check temperature return value of correct type
+	gnmi.Await(t, dut1, gnmi.OC().Interface(dp1.Name()).OperStatus().State(), intUpdateTime, oc.Interface_OperStatus_DOWN)
+	temperatureInstant := verifyTemperatureSensorValue(t, dut1, p1StreamInstant, "Instant")
 	t.Logf("Port1 dut1 %s Instant Temperature: %v", dp1.Name(), temperatureInstant)
-	temperatureMinSample = gnmi.Collect(t, gnmiOpts(t, dut1, gpb.SubscriptionMode_SAMPLE, time.Second*10), component1.Temperature().Min().State(), time.Second*10)
-	temperatureMinSamples = temperatureMinSample.Await(t)
-	// Taking first sample
-	temperatureMin, _ = temperatureMinSamples[0].Val()
-	t.Logf("Port1 dut1 %s Min Temperature: %v", dp1.Name(), temperatureMin)
-	temperatureMaxSample = gnmi.Collect(t, gnmiOpts(t, dut1, gpb.SubscriptionMode_SAMPLE, time.Second*10), component1.Temperature().Max().State(), time.Second*10)
-	temperatureMaxSamples = temperatureMaxSample.Await(t)
-	// Taking first sample
-	temperatureMax, _ = temperatureMaxSamples[0].Val()
+	temperatureMax := verifyTemperatureSensorValue(t, dut1, p1StreamMax, "Max")
 	t.Logf("Port1 dut1 %s Max Temperature: %v", dp1.Name(), temperatureMax)
-	temperatureAvgSample = gnmi.Collect(t, gnmiOpts(t, dut1, gpb.SubscriptionMode_SAMPLE, time.Second*10), component1.Temperature().Avg().State(), time.Second*10)
-	temperatureAvgSamples = temperatureAvgSample.Await(t)
-	// Taking first sample
-	temperatureAvg, _ = temperatureAvgSamples[0].Val()
-	// temperature average should be between min and max
-	if temperatureAvg >= temperatureMin && temperatureAvg <= temperatureMax {
-		t.Logf("The average is between the maximum and minimum values")
-	} else {
-		t.Fatalf("The average is not between the maximum and minimum values")
-	}
+	temperatureMin := verifyTemperatureSensorValue(t, dut1, p1StreamMin, "Min")
+	t.Logf("Port1 dut1 %s Min Temperature: %v", dp1.Name(), temperatureMin)
+	temperatureAvg := verifyTemperatureSensorValue(t, dut1, p1StreamAvg, "Avg")
+	t.Logf("Port1 dut1 %s Avg Temperature: %v", dp1.Name(), temperatureAvg)
+	i = d.GetOrCreateInterface(dp1.Name())
 	i.Enabled = ygot.Bool(true)
 	i.Type = oc.IETFInterfaces_InterfaceType_ethernetCsmacd
 	// Enable interface
 	gnmi.Replace(t, dut1, gnmi.OC().Interface(dp1.Name()).Config(), i)
 	gnmi.Await(t, dut1, gnmi.OC().Interface(dp1.Name()).OperStatus().State(), intUpdateTime, oc.Interface_OperStatus_UP)
-	temperatureInstantSample = gnmi.Collect(t, gnmiOpts(t, dut1, gpb.SubscriptionMode_SAMPLE, time.Second*10), component1.Temperature().Instant().State(), time.Second*10)
-	temperatureInstantSamples = temperatureInstantSample.Await(t)
-	// Taking first sample
-	temperatureInstant, _ = temperatureInstantSamples[0].Val()
-	// Check temperature return value of correct type
+	temperatureInstant = verifyTemperatureSensorValue(t, dut1, p1StreamInstant, "Instant")
 	t.Logf("Port1 dut1 %s Instant Temperature: %v", dp1.Name(), temperatureInstant)
-	temperatureMinSample = gnmi.Collect(t, gnmiOpts(t, dut1, gpb.SubscriptionMode_SAMPLE, time.Second*10), component1.Temperature().Min().State(), time.Second*10)
-	temperatureMinSamples = temperatureMinSample.Await(t)
-	// Taking first sample
-	temperatureMin, _ = temperatureMinSamples[0].Val()
-	t.Logf("Port1 dut1 %s Min Temperature: %v", dp1.Name(), temperatureMin)
-	temperatureMaxSample = gnmi.Collect(t, gnmiOpts(t, dut1, gpb.SubscriptionMode_SAMPLE, time.Second*10), component1.Temperature().Max().State(), time.Second*10)
-	temperatureMaxSamples = temperatureMaxSample.Await(t)
-	// Taking first sample
-	temperatureMax, _ = temperatureMaxSamples[0].Val()
+	temperatureMax = verifyTemperatureSensorValue(t, dut1, p1StreamMax, "Max")
 	t.Logf("Port1 dut1 %s Max Temperature: %v", dp1.Name(), temperatureMax)
-	temperatureAvgSample = gnmi.Collect(t, gnmiOpts(t, dut1, gpb.SubscriptionMode_SAMPLE, time.Second*10), component1.Temperature().Avg().State(), time.Second*10)
-	temperatureAvgSamples = temperatureAvgSample.Await(t)
-	// Taking first sample
-	temperatureAvg, _ = temperatureAvgSamples[0].Val()
+	temperatureMin = verifyTemperatureSensorValue(t, dut1, p1StreamMin, "Min")
+	t.Logf("Port1 dut1 %s Min Temperature: %v", dp1.Name(), temperatureMin)
+	temperatureAvg = verifyTemperatureSensorValue(t, dut1, p1StreamAvg, "Avg")
+	t.Logf("Port1 dut1 %s Avg Temperature: %v", dp1.Name(), temperatureAvg)
 	if temperatureAvg >= temperatureMin && temperatureAvg <= temperatureMax {
 		t.Logf("The average is between the maximum and minimum values")
 	} else {
 		t.Fatalf("The average is not between the maximum and minimum values")
+	}
+}
+
+func opticalChannelFromPort(t *testing.T, dut *ondatra.DUTDevice, p *ondatra.Port) string {
+	t.Helper()
+	if deviations.MissingPortToOpticalChannelMapping(dut) {
+		switch dut.Vendor() {
+		case ondatra.ARISTA:
+			transceiverName := gnmi.Get(t, dut, gnmi.OC().Interface(p.Name()).Transceiver().State())
+			return fmt.Sprintf("%s-Optical0", transceiverName)
+		default:
+			t.Fatal("Manual Optical channel name required when deviation missing_port_to_optical_channel_component_mapping applied.")
+		}
+	}
+	compName := gnmi.Get(t, dut, gnmi.OC().Interface(p.Name()).HardwarePort().State())
+	for {
+		comp, ok := gnmi.Lookup(t, dut, gnmi.OC().Component(compName).State()).Val()
+		if !ok {
+			t.Fatalf("Recursive optical channel lookup failed for port: %s, component %s not found.", p.Name(), compName)
+		}
+		if comp.GetType() == oc.PlatformTypes_OPENCONFIG_HARDWARE_COMPONENT_OPTICAL_CHANNEL {
+			return compName
+		}
+		if comp.GetParent() == "" {
+			t.Fatalf("Recursive optical channel lookup failed for port: %s, parent of component %s not found.", p.Name(), compName)
+		}
+		compName = comp.GetParent()
 	}
 }
