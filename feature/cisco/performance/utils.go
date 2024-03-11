e6275b6f526caebfaffc4fef47af6e7945da7bca
Liam Walsh <liawalsh@cisco.com>
2024-03-11 16:00:43 -0400

    remove unused old cpu collector

--- feature/cisco/performance/utils.go
+++ feature/cisco/performance/utils.go
@@ -27,51 +27,6 @@ func BenchMark(ygnmiCli *ygnmi.Client) {
 	}
 }
 
-func CpuVerify(ygnmiCli *ygnmi.Client, isRunning *bool) {
-	// oc leaves for memory do not work!! and cpu information require extra analysis, commenting this code for now
-	go func() {
-		for *isRunning {
-			ctx, cancel := context.WithTimeout(context.Background(), 65*time.Second)
-			//defer cancel()
-			data, err := ygnmi.CollectAll(ctx, ygnmiCli, gnmi.OC().System().CpuAny().State()).Await()
-			if cancel != nil {
-				fmt.Printf("Error %v /n", err)
-			}
-			if err != nil {
-				fmt.Printf("Error %v /n", err)
-			}
-			for _, memUse := range data {
-				usedMem, _ := memUse.Val()
-				fmt.Printf("Cpu info at %v : %v\n", memUse.Timestamp, PrettyPrint(usedMem))
-			}
-		}
-	}()
-}
-
-// func MemoryVerify(ygnmiCli *ygnmi.Client, isRunning *bool) {
-// 	// oc leaves for memory do not work!! and cpu information require extra analysis, commenting this code for now
-// 	go func() {
-// 		for *isRunning {
-// 			ctx, cancel := context.WithTimeout(context.Background(), 65*time.Second)
-// 			//defer cancel()
-// 			data, err := ygnmi.Collect(ctx, ygnmiCli, gnmi.OC().System().State()).Await()
-// 			PrettyPrint(data)
-// 			if cancel != nil {
-// 				fmt.Printf("Error %v /n", err)
-// 			}
-// 			if err != nil {
-// 				fmt.Printf("Error %v /n", err)
-// 			}
-// 			for _, memUse := range data {
-// 				usedMem, _ := memUse.Val()
-// 				fmt.Printf("Cpu info at %v : %v\n", memUse.Timestamp, PrettyPrint(usedMem))
-// 			}
-// 		}
-// 	}()
-// }
-
-// gnmi.OC().System().Memory().State()
-
 func ControlPlaneVerification(ygnmiCli *ygnmi.Client) {
 	// TODO1:Check for Crash
 	// TODO2: Check for Traces
