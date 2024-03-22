11c4bf86731d7395f2034663598d6b4914a7bf38
Yiwen Hu <yiwenhu@google.com>
2024-03-22 01:05:50 +0000

    Fix bugs

--- feature/platform/transceiver/tests/zr_inventory_test/zr_inventory_test.go
+++ feature/platform/transceiver/tests/zr_inventory_test/zr_inventory_test.go
@@ -30,14 +30,14 @@ func TestMain(m *testing.M) {
 	fptest.RunTests(m)
 }
 
-func interfaceConfig(t *testing.T, dut1 *ondatra.DUTDevice, dp *ondatra.Port, frequency uint64, targetOutputPower float64) {
+func configInterface(t *testing.T, dut1 *ondatra.DUTDevice, dp *ondatra.Port, frequency uint64, targetOutputPower float64) {
 	d := &oc.Root{}
 	i := d.GetOrCreateInterface(dp.Name())
 	i.Enabled = ygot.Bool(true)
 	i.Type = oc.IETFInterfaces_InterfaceType_ethernetCsmacd
 	gnmi.Replace(t, dut1, gnmi.OC().Interface(dp.Name()).Config(), i)
-	OCcomponent := opticalChannelComponentFromPort(t, dut1, dp)
-	gnmi.Replace(t, dut1, gnmi.OC().Component(OCcomponent).OpticalChannel().Config(), &oc.Component_OpticalChannel{
+	c := opticalChannelComponentFromPort(t, dut1, dp)
+	gnmi.Replace(t, dut1, gnmi.OC().Component(c).OpticalChannel().Config(), &oc.Component_OpticalChannel{
 		TargetOutputPower: ygot.Float64(targetOutputPower),
 		Frequency:         ygot.Uint64(frequency),
 	})
@@ -45,7 +45,7 @@ func interfaceConfig(t *testing.T, dut1 *ondatra.DUTDevice, dp *ondatra.Port, fr
 
 func verifyAllInventoryValues(t *testing.T, dut1 *ondatra.DUTDevice, pStreamsStr []*samplestream.SampleStream[string], pStreamsUnion []*samplestream.SampleStream[oc.Component_Type_Union]) {
 	for _, stream := range pStreamsStr {
-		inventoryStr := stream.Next(t)
+		inventoryStr := stream.Next()
 		if inventoryStr == nil {
 			t.Fatalf("Inventory telemetry %s was not streamed in the most recent subscription interval", inventoryStr)
 		}
@@ -56,7 +56,7 @@ func verifyAllInventoryValues(t *testing.T, dut1 *ondatra.DUTDevice, pStreamsStr
 	}
 
 	for _, stream := range pStreamsUnion {
-		inventoryUnion := stream.Next(t)
+		inventoryUnion := stream.Next()
 		if inventoryUnion == nil {
 			t.Fatalf("Inventory telemetry %v was not streamed in the most recent subscription interval", inventoryUnion)
 		}
@@ -83,21 +83,23 @@ func TestInventory(t *testing.T) {
 	}
 	component1 := gnmi.OC().Component(tr1)
 
-	interfaceConfig(t, dut1, dp1, frequency, targetOutputPower)
-	interfaceConfig(t, dut1, dp2, frequency, targetOutputPower)
+	configInterface(t, dut1, dp1, frequency, targetOutputPower)
+	configInterface(t, dut1, dp2, frequency, targetOutputPower)
 	// Wait for channels to be up.
 	gnmi.Await(t, dut1, gnmi.OC().Interface(dp1.Name()).OperStatus().State(), timeout, oc.Interface_OperStatus_UP)
 	gnmi.Await(t, dut1, gnmi.OC().Interface(dp2.Name()).OperStatus().State(), timeout, oc.Interface_OperStatus_UP)
 
 	var p1StreamsStr []*samplestream.SampleStream[string]
 	var p1StreamsUnion []*samplestream.SampleStream[oc.Component_Type_Union]
-	p1StreamsStr = append(p1StreamsStr, samplestream.New(t, dut1, component1.SerialNo().State(), samplingInterval))
-	p1StreamsStr = append(p1StreamsStr, samplestream.New(t, dut1, component1.PartNo().State(), samplingInterval))
-	p1StreamsStr = append(p1StreamsStr, samplestream.New(t, dut1, component1.Description().State(), samplingInterval))
-	p1StreamsStr = append(p1StreamsStr, samplestream.New(t, dut1, component1.MfgName().State(), samplingInterval))
-	p1StreamsStr = append(p1StreamsStr, samplestream.New(t, dut1, component1.MfgDate().State(), samplingInterval))
-	p1StreamsStr = append(p1StreamsStr, samplestream.New(t, dut1, component1.HardwareVersion().State(), samplingInterval))
-	p1StreamsStr = append(p1StreamsStr, samplestream.New(t, dut1, component1.FirmwareVersion().State(), samplingInterval))
+	p1StreamsStr = append(p1StreamsStr,
+		samplestream.New(t, dut1, component1.SerialNo().State(), samplingInterval),
+		samplestream.New(t, dut1, component1.PartNo().State(), samplingInterval),
+		samplestream.New(t, dut1, component1.Description().State(), samplingInterval),
+		samplestream.New(t, dut1, component1.MfgName().State(), samplingInterval),
+		samplestream.New(t, dut1, component1.MfgDate().State(), samplingInterval),
+		samplestream.New(t, dut1, component1.HardwareVersion().State(), samplingInterval),
+		samplestream.New(t, dut1, component1.FirmwareVersion().State(), samplingInterval),
+	)
 	p1StreamsUnion = append(p1StreamsUnion, samplestream.New(t, dut1, component1.Type().State(), samplingInterval))
 
 	verifyAllInventoryValues(t, dut1, p1StreamsStr, p1StreamsUnion)
