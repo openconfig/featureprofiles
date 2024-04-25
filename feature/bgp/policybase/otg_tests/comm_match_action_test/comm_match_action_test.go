275d6d3e41db5280e9bef3167a15bd198babd939
Arul Kumar Sekar <128646204+arulkumarsekar@users.noreply.github.com>
2024-04-24 19:01:31 -0700

    Update feature/bgp/policybase/otg_tests/comm_match_action_test/comm_match_action_test.go
    Co-authored-by: Darren Loher <dloher@google.com>

--- feature/bgp/policybase/otg_tests/comm_match_action_test/comm_match_action_test.go
+++ feature/bgp/policybase/otg_tests/comm_match_action_test/comm_match_action_test.go
@@ -19,7 +19,7 @@ import (
 	"time"
 
 	"github.com/open-traffic-generator/snappi/gosnappi"
-	"github.com/openconfig/featureprofiles/internal/cfgplugins/cfgplugins"
+	"github.com/openconfig/featureprofiles/internal/cfgplugins"
 	"github.com/openconfig/featureprofiles/internal/deviations/deviations"
 	"github.com/openconfig/featureprofiles/internal/fptest/fptest"
 	"github.com/openconfig/featureprofiles/internal/otgutils/otgutils"
