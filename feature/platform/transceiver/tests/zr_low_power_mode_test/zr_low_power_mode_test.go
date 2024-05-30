aed7fd4a75b9d48a966155cfa502524d9a51dcdb
Prinika Sankaran Nair <prinikasn@google.com>
2024-05-30 07:43:37 -0700

    update

--- feature/platform/transceiver/tests/zr_low_power_mode_test/zr_low_power_mode_test.go
+++ feature/platform/transceiver/tests/zr_low_power_mode_test/zr_low_power_mode_test.go
@@ -26,6 +26,8 @@ import (
 	"github.com/openconfig/featureprofiles/internal/samplestream"
 	"github.com/openconfig/ondatra"
 	"github.com/openconfig/ondatra/gnmi"
+	"github.com/openconfig/ondatra/gnmi/oc"
+	"github.com/openconfig/ygot/ygot"
 )
 
 const (
