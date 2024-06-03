78cf8667ac2f56ece5323c63da0e18fd3612ce9b
Ram <rmachat@google.com>
2024-06-03 13:31:40 +0530

    added deviation for rt-5.9 (#3049)
    * fixed conflicts
    
    * fix fmt for ipv6_slaac_link_local_test.go

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -1014,6 +1014,11 @@ func FlattenPolicyWithMultipleStatements(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetFlattenPolicyWithMultipleStatements()
 }
 
+// SlaacPrefixLength128 for Slaac generated IPv6 link local address
+func SlaacPrefixLength128(dut *ondatra.DUTDevice) bool {
+	return lookupDUTDeviations(dut).GetSlaacPrefixLength128()
+}
+
 // DefaultRoutePolicyUnsupported returns true if default route policy is not supported
 func DefaultRoutePolicyUnsupported(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetDefaultRoutePolicyUnsupported()
