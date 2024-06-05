abb7920d68684515841d8c2f38ef5992ad1f1d67
SydneyCaulfeild <47483016+SydneyCaulfeild@users.noreply.github.com>
2024-06-05 09:50:54 -0700

    skip fan tray telemetry check when 0 are expected

--- feature/platform/tests/telemetry_inventory_test/telemetry_inventory_test.go
+++ feature/platform/tests/telemetry_inventory_test/telemetry_inventory_test.go
@@ -312,6 +312,8 @@ func TestHardwareCards(t *testing.T) {
 				t.Skip("Skip Linecard Telemetry check for fixed form factor devices.")
 			} else if tc.desc == "Supervisor" && *args.NumControllerCards <= 0 {
 				t.Skip("Skip Supervisor Telemetry check for fixed form factor devices.")
+			} else if tc.desc == "Fan Tray" && *args.NumFanTrays == 0 {
+				t.Skip("Skip Fan Tray Telemetry check for fixed form factor devices.")
 			}
 			cards := components[tc.desc]
 			t.Logf("%s components count: %d", tc.desc, len(cards))
