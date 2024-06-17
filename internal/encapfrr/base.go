7293deafc8fef2bdad56dad3217810c8af7ca72b
Pramod Maurya <pramodmaurya@google.com>
2024-06-17 16:32:28 +0000

    package name change

--- internal/encapfrr/base.go
+++ internal/encapfrr/base.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package base contains utility functions for encap frr using repair VRF.
-package base
+// Package encapfrr contains utility functions for encap frr using repair VRF.
+package encapfrr
 
 import (
 	"context"
