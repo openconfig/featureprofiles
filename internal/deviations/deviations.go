b42b6e0580c065c5f050dc3fc41254d4029e7cb2
ampattan <amit.pattanaik@nokia.com>
2024-06-21 14:39:11 +0530

    -removal of explicit_port_speed deviation
    "This code is a Contribution to the OpenConfig Feature Profiles project ("Work") made under the Google Software Grant and Corporate Contributor License Agreement ("CLA") and governed by the Apache License 2.0. No other rights or licenses in or to any of Nokia's intellectual property are granted for any other purpose. This code is provided on an "as is" basis without any warranties of any kind."

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -364,12 +364,6 @@ func NetworkInstanceTableDeletionRequired(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetNetworkInstanceTableDeletionRequired()
 }
 
-// ExplicitPortSpeed returns if device requires port-speed to be set because its default value may not be usable.
-// Fully compliant devices selects the highest speed available based on negotiation.
-func ExplicitPortSpeed(dut *ondatra.DUTDevice) bool {
-	return lookupDUTDeviations(dut).GetExplicitPortSpeed()
-}
-
 // ExplicitInterfaceInDefaultVRF returns if device requires explicit attachment of an interface or subinterface to the default network instance.
 // OpenConfig expects an unattached interface or subinterface to be implicitly part of the default network instance.
 // Fully-compliant devices should pass with and without this deviation.
