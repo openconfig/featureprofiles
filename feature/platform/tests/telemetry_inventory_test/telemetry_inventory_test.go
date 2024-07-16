9c81c8e61f4b12890ae933a6ed570da10fa1bbd9
SydneyCaulfeild <47483016+SydneyCaulfeild@users.noreply.github.com>
2024-07-16 11:53:12 -0700

    Fix brackets in telemetry_inventory_test.go.

--- feature/platform/tests/telemetry_inventory_test/telemetry_inventory_test.go
+++ feature/platform/tests/telemetry_inventory_test/telemetry_inventory_test.go
@@ -358,7 +358,7 @@ func TestHardwareCards(t *testing.T) {
 				t.Skip("Skip Supervisor Telemetry check for fixed form factor devices.")
 			} else if tc.desc == "Fan Tray" && *args.NumFanTrays == 0 {
 				t.Skip("Skip Fan Tray Telemetry check for fixed form factor devices.")
-			} else if tc.desc == "Fan" && (*args.NumFans == 0) {
+			} else if tc.desc == "Fan" && *args.NumFans == 0 {
 				t.Skip("Skip Fan Telemetry check for fixed form factor devices.")
 			}
 			cards := components[tc.desc]
