5f113b5b66bdaa5692a787e2c6d499fb6ff4349f
bkreddy143 <krishna.bovilla@nokia.com>
2024-07-16 20:13:49 -0700

    corrected deviaiton usage,isis await time increase
    1. Added additional check to verify no adjancies presence when state
       information missing after session timedout
    2. Corrected use of deviaiton as per PR
    3. Modified Await timeout to 90 seconds to accomidated 3 hello packets
       drop time
    
    "This code is a Contribution to the OpenConfig Feature Profiles project ("Work") made under the Google Software Grant and Corporate Contributor License Agreement ("CLA") and governed by the Apache License 2.0. No other rights or licenses in or to any of Nokia's intellectual property are granted for any other purpose. This code is provided on an "as is" basis without any warranties of any kind."

--- feature/interface/aggregate/otg_tests/aggregate_all_not_viable_test/aggregate_all_not_forwarding_viable_test.go
+++ feature/interface/aggregate/otg_tests/aggregate_all_not_viable_test/aggregate_all_not_forwarding_viable_test.go
@@ -215,7 +215,9 @@ func TestAggregateAllNotForwardingViable(t *testing.T) {
 		// Ensure ISIS Adjacency is Down on LAG_2
 
 		if ok := awaitAdjacency(t, dut, aggIDs[1], oc.Isis_IsisInterfaceAdjState_DOWN); !ok {
-			t.Fatal("ISIS Adjacency is Established on LAG_2")
+			if presence := gnmi.LookupAll(t, dut, ocpath.Root().NetworkInstance(deviations.DefaultNetworkInstance(dut)).Protocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_ISIS, isisInstance).Isis().Interface(aggIDs[1]).LevelAny().AdjacencyAny().AdjacencyState().State()); len(presence) > 0 {
+				t.Fatalf("ISIS Adjacency is Established on LAG_2 ")
+			}
 		}
 		startTraffic(t, dut, ate, top)
 		if err := confirmNonViableForwardingTraffic(t, dut, ate, atePortList[1:agg2.ateLagCount+1], dutPortList[1:agg2.ateLagCount+1]); err != nil {
@@ -292,7 +294,9 @@ func TestAggregateAllNotForwardingViable(t *testing.T) {
 		configForwardingViable(t, dut, dutPortList[1:2], false)
 		// Ensure ISIS Adjacency is Down on LAG_2
 		if ok := awaitAdjacency(t, dut, aggIDs[1], oc.Isis_IsisInterfaceAdjState_DOWN); !ok {
-			t.Fatal("ISIS Adjacency is Established on LAG_2")
+			if presence := gnmi.LookupAll(t, dut, ocpath.Root().NetworkInstance(deviations.DefaultNetworkInstance(dut)).Protocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_ISIS, isisInstance).Isis().Interface(aggIDs[1]).LevelAny().AdjacencyAny().AdjacencyState().State()); len(presence) > 0 {
+				t.Fatalf("ISIS Adjacency is Established on LAG_2")
+			}
 		}
 		startTraffic(t, dut, ate, top)
 		if err := confirmNonViableForwardingTraffic(t, dut, ate, atePortList[1:(agg2.ateLagCount+1)], dutPortList[1:(agg2.ateLagCount+1)]); err != nil {
@@ -394,7 +398,7 @@ func configureDUT(t *testing.T, dut *ondatra.DUTDevice) []string {
 	configureRoutingPolicy(t, dut)
 	configureDUTISIS(t, dut, aggIDs)
 
-	if deviations.MaxEcmpPaths(dut) {
+	if !deviations.MaxEcmpPaths(dut) {
 		isisPath := gnmi.OC().NetworkInstance(deviations.DefaultNetworkInstance(dut)).Protocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_ISIS, isisInstance).Isis()
 		gnmi.Update(t, dut, isisPath.Global().MaxEcmpPaths().Config(), 2)
 	}
@@ -807,7 +811,7 @@ func incrementMAC(mac string, i int) (string, error) {
 
 func createFlows(t *testing.T, dut *ondatra.DUTDevice, top gosnappi.Config, aggIDs []string) []gosnappi.Flow {
 	for _, aggID := range aggIDs {
-		dutAggMac = append(dutAggMac, gnmi.Get(t, dut, gnmi.OC().Lacp().Interface(aggID).SystemIdMac().State()))
+		dutAggMac = append(dutAggMac, gnmi.Get(t, dut, gnmi.OC().Interface(aggID).Ethernet().MacAddress().State()))
 	}
 	f1V4 := configureFlows(t, top, pfx1AdvV4, pfx2AdvV4, "pfx1ToPfx2_3", agg1, []*aggPortData{agg2, agg3}, dutAggMac[0], ipRange[1])
 	f2V4 := configureFlows(t, top, pfx1AdvV4, pfx4AdvV4, "pfx1ToPfx4", agg1, []*aggPortData{agg2, agg3}, dutAggMac[0], ipRange[0])
@@ -958,7 +962,7 @@ func awaitAdjacency(t *testing.T, dut *ondatra.DUTDevice, intfName string, state
 	isisPath := ocpath.Root().NetworkInstance(deviations.DefaultNetworkInstance(dut)).Protocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_ISIS, isisInstance).Isis()
 	intf := isisPath.Interface(intfName)
 	query := intf.LevelAny().AdjacencyAny().AdjacencyState().State()
-	_, ok := gnmi.WatchAll(t, dut, query, time.Minute, func(val *ygnmi.Value[oc.E_Isis_IsisInterfaceAdjState]) bool {
+	_, ok := gnmi.WatchAll(t, dut, query, 90*time.Second, func(val *ygnmi.Value[oc.E_Isis_IsisInterfaceAdjState]) bool {
 		v, ok := val.Val()
 		return v == state && ok
 	}).Await(t)
