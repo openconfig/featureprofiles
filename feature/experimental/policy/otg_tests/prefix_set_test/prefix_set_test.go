780f41893f504d4f50dc452b36f5a67bde80a6b7
Pramod Maurya <pramodmaurya@google.com>
2024-07-18 16:25:18 +0000

    ci check fix

--- feature/experimental/policy/otg_tests/prefix_set_test/prefix_set_test.go
+++ feature/experimental/policy/otg_tests/prefix_set_test/prefix_set_test.go
@@ -18,9 +18,9 @@ import (
 	"testing"
 
 	"github.com/openconfig/featureprofiles/internal/fptest"
+	"github.com/openconfig/ondatra"
 	"github.com/openconfig/ondatra/gnmi"
 	"github.com/openconfig/ondatra/gnmi/oc"
-	"github.com/openconfig/ondatra"
 )
 
 const (
