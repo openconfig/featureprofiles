d5d2fa0a8e8d07bbc692728c858c839e75b19b51
Liam Walsh <liawalsh@cisco.com>
2024-08-08 15:28:07 -0400

    use fixed for jenkins

--- feature/platform/integrated_circuit/otg_tests/copp_counters_test/copp_counters_test.go
+++ feature/platform/integrated_circuit/otg_tests/copp_counters_test/copp_counters_test.go
@@ -55,7 +55,7 @@ func TestMain(m *testing.M) {
 }
 
 func getSubsriptionSlice() []*gnmipb.Subscription {
-	filesList := []string{"./OC_Paths_Trap_d.txt", "./OC_Paths_LPTS_d.txt"}
+	filesList := []string{"./OC_Paths_Trap.txt", "./OC_Paths_LPTS.txt"}
 	// filesList := []string{"./OC_Paths_LPTS_alt.txt"}
 
 	var lines []string
