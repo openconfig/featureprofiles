d1940786cea33b14c0f1fc63c931274fe7e9d298
bkreddy143 <krishna.bovilla@nokia.com>
2024-08-19 16:23:11 -0700

    fixed the failure that was introduced by PR-3376
    PR- 3376 changes changed the signal type to KILL instead of TERM  that
    resulted to non graceful application restart , fixed by adding that as an parameter to the  KillProces() function.
    
    "This code is a Contribution to the OpenConfig Feature Profiles project ("Work") made under the Google Software Grant and Corporate Contributor License Agreement ("CLA") and governed by the Apache License 2.0. No other rights or licenses in or to any of Nokia’s intellectual property are granted for any other purpose. This code is provided on an "as is" basis without any warranties of any kind."

--- feature/experimental/gribi/otg_tests/dut_daemon_failure/dut_daemon_failure_test.go
+++ feature/experimental/gribi/otg_tests/dut_daemon_failure/dut_daemon_failure_test.go
@@ -310,7 +310,7 @@ func TestDUTDaemonFailure(t *testing.T) {
 	})
 
 	t.Run("KillGRIBIDaemon", func(t *testing.T) {
-		gnoi.KillProcess(t, dut, gnoi.GRIBI, true)
+		gnoi.KillProcess(t, dut, gnoi.GRIBI, gnoi.SIG_TERM, true, true)
 
 		t.Logf("Time check: %s", time.Since(start))
 
--- internal/gnoi/gnoi.go
+++ internal/gnoi/gnoi.go
@@ -71,8 +71,17 @@ const (
 	ROUTING Daemon = "ROUTING"
 )
 
+// signal type of termination request
+const (
+	SIG_TERM       = spb.KillProcessRequest_SIGNAL_TERM
+	SIG_KILL       = spb.KillProcessRequest_SIGNAL_KILL
+	SIG_HUP        = spb.KillProcessRequest_SIGNAL_HUP
+	SIG_ABORT      = spb.KillProcessRequest_SIGNAL_ABRT
+	SIG_UNSPECIFED = spb.KillProcessRequest_SIGNAL_UNSPECIFIED
+)
+
 // KillProcess terminates the daemon on the DUT.
-func KillProcess(t *testing.T, dut *ondatra.DUTDevice, daemon Daemon, waitForRestart bool) {
+func KillProcess(t *testing.T, dut *ondatra.DUTDevice, daemon Daemon, signal spb.KillProcessRequest_Signal, restart bool, waitForRestart bool) {
 	t.Helper()
 
 	daemonName, err := FetchProcessName(dut, daemon)
@@ -86,10 +95,10 @@ func KillProcess(t *testing.T, dut *ondatra.DUTDevice, daemon Daemon, waitForRes
 
 	gnoiClient := dut.RawAPIs().GNOI(t)
 	killProcessRequest := &spb.KillProcessRequest{
-		Signal:  spb.KillProcessRequest_SIGNAL_KILL,
+		Signal:  signal,
 		Name:    daemonName,
 		Pid:     uint32(pid),
-		Restart: true,
+		Restart: restart,
 	}
 	gnoiClient.System().KillProcess(context.Background(), killProcessRequest)
 
