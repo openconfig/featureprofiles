535ec7ebcd25a63b429d53a08731cb457b279f23
Nisha Sadhasivam <nsadhasivam@google.com>
2024-09-09 10:54:22 +0000

    static analysis

--- internal/core/core.go
+++ internal/core/core.go
@@ -88,8 +88,7 @@ func newChecker(dut binding.DUT) (*checker, error) {
 	if _, ok := vendorCoreFileNamePattern[dutVendor]; !ok {
 		return nil, fmt.Errorf("add support for vendor %v in var vendorCoreFileNamePattern", dutVendor)
 	}
-	gClients, err := dut.DialGNOI(context.Background())
-        gClients.WaitForStateChange( context, connectivity.Ready )
+	gClients, err := dut.DialGNOI(context.Background(), grpc.WithBlock())
 	if err != nil {
 		return nil, err
 	}
