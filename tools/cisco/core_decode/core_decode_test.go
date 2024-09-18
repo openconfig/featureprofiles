451c826d64d6b468e75383ff107d91898c8079ad
Krishnakumar Natarajan <krinata2@cisco.com>
2024-09-18 14:51:30 +0530

    gen core file in peer too

--- tools/cisco/core_decode/core_decode_test.go
+++ tools/cisco/core_decode/core_decode_test.go
@@ -14,6 +14,7 @@ func TestMain(m *testing.M) {
 
 func TestCoreFileDecode(t *testing.T) {
 	dut := ondatra.DUT(t, "dut")
+	peer := ondatra.DUT(t, "peer")
 
 	processList := []string{
 		"bundlemgr_checker", "ifmgr", "netio", "pkt_trace_agent",
@@ -22,6 +23,7 @@ func TestCoreFileDecode(t *testing.T) {
 	for _, process := range processList {
 		t.Run("create core file", func(t *testing.T) {
 			dut.CLI().RunResult(t, fmt.Sprintf("dumpcore running %s location 0/RP0/CPU0\n", process))
+			peer.CLI().RunResult(t, fmt.Sprintf("dumpcore running %s location 0/RP0/CPU0\n", process))
 		})
 	}
 }
