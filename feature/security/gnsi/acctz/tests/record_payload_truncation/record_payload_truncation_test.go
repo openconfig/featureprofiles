e0fbbdb5995e8f613f79001e201e2b5a93398035
Dipesh Chauhan (Nokia) <dipesh.chauhan@nokia.com>
2024-09-20 17:52:13 -0400

    Fix comments
    "This code is a Contribution to the OpenConfig Feature Profiles project ("Work") made under the Google Software Grant and Corporate Contributor License Agreement ("CLA") and governed by the Apache License 2.0. No other rights or licenses in or to any of Nokia's intellectual property are granted for any other purpose. This code is provided on an "as is" basis without any warranties of any kind."

--- feature/security/gnsi/acctz/tests/record_payload_truncation/record_payload_truncation_test.go
+++ feature/security/gnsi/acctz/tests/record_payload_truncation/record_payload_truncation_test.go
@@ -1,3 +1,17 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package record_payload_truncation_test
 
 import (
@@ -24,19 +38,15 @@ type recordRequestResult struct {
 }
 
 func sendOversizedPayload(t *testing.T, dut *ondatra.DUTDevice) {
-	// perhaps other vendors will need a different payload/size/etc., for now we'll just send a
-	// giant set of network instances + static routes that should hopefully work for everyone!
-
+	// Perhaps other vendors will need a different payload/size/etc., for now we'll just send a
+	// giant set of network instances + static routes which should hopefully work for everyone.
 	ocRoot := &oc.Root{}
 
 	for i := 0; i < 50; i++ {
 		ni := ocRoot.GetOrCreateNetworkInstance(fmt.Sprintf("acctz-test-ni-%d", i))
-
-		ni.SetDescription("this is a pointlessly long description in order to make enbiggen the payload")
+		ni.SetDescription("This is a pointlessly long description in order to make the payload bigger.")
 		ni.SetType(oc.NetworkInstanceTypes_NETWORK_INSTANCE_TYPE_L3VRF)
-
 		staticProtocol := ni.GetOrCreateProtocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_STATIC, "static")
-
 		for j := 0; j < 254; j++ {
 			staticProtocol.GetOrCreateStatic(fmt.Sprintf("10.%d.0.0/24", j))
 		}
@@ -47,23 +57,20 @@ func sendOversizedPayload(t *testing.T, dut *ondatra.DUTDevice) {
 
 func TestAccountzRecordPayloadTruncation(t *testing.T) {
 	dut := ondatra.DUT(t, "dut")
-
 	startTime := time.Now()
-
 	sendOversizedPayload(t, dut)
-
 	acctzClient := dut.RawAPIs().GNSI(t).Acctz()
 
 	acctzSubClient, err := acctzClient.RecordSubscribe(context.Background())
 	if err != nil {
-		t.Fatalf("failed getting accountz record subscribe client, error: %s", err)
+		t.Fatalf("Failed getting accountz record subscribe client, error: %s", err)
 	}
 
 	err = acctzSubClient.Send(&acctz.RecordRequest{
 		Timestamp: timestamppb.New(startTime),
 	})
 	if err != nil {
-		t.Fatalf("failed sending record request, error: %s", err)
+		t.Fatalf("Failed sending record request, error: %s", err)
 	}
 
 	for {
@@ -71,9 +78,7 @@ func TestAccountzRecordPayloadTruncation(t *testing.T) {
 
 		go func(r chan recordRequestResult) {
 			var response *acctz.RecordResponse
-
 			response, err = acctzSubClient.Recv()
-
 			r <- recordRequestResult{
 				record: response,
 				err:    err,
@@ -81,7 +86,6 @@ func TestAccountzRecordPayloadTruncation(t *testing.T) {
 		}(r)
 
 		var done bool
-
 		var resp recordRequestResult
 
 		select {
@@ -92,17 +96,17 @@ func TestAccountzRecordPayloadTruncation(t *testing.T) {
 		}
 
 		if done {
-			t.Fatal("done receiving records and did not find our record...")
+			t.Fatal("Done receiving records and did not find our record...")
 		}
 
 		if resp.err != nil {
-			t.Fatalf("failed receiving record response, error: %s", resp.err)
+			t.Fatalf("Failed receiving record response, error: %s", resp.err)
 		}
 
 		grpcServiceRecord := resp.record.GetGrpcService()
 
 		if grpcServiceRecord.GetServiceType() != acctz.GrpcService_GRPC_SERVICE_TYPE_GNMI {
-			// not our gnmi set, nothing to see here
+			// Not our gnmi set, nothing to see here.
 			continue
 		}
 
@@ -111,8 +115,7 @@ func TestAccountzRecordPayloadTruncation(t *testing.T) {
 		}
 
 		if grpcServiceRecord.GetPayloadIstruncated() {
-			t.Log("found truncated payload of gnmi.Set after start timestamp, success!")
-
+			t.Log("Found truncated payload of gnmi.Set after start timestamp, success!")
 			break
 		}
 	}
