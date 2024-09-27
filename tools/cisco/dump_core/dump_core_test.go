d739b257fa012a6411034f8a3c7f28b4bcc60365
Krishnakumar Natarajan <krinata2@cisco.com>
2024-09-27 19:47:57 +0530

    process dut generically

--- tools/cisco/dump_core/dump_core_test.go
+++ tools/cisco/dump_core/dump_core_test.go
@@ -1,48 +1,83 @@
 package basetest
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/openconfig/featureprofiles/internal/fptest"
+	bindpb "github.com/openconfig/featureprofiles/topologies/proto/binding"
 	"github.com/openconfig/ondatra"
+	"google.golang.org/protobuf/encoding/prototext"
 )
 
-type targetInfo struct {
-	dut string
-}
+var (
+	processList = flag.String("processlist", "", "Comma-separated list of process")
+)
 
-type Targets struct {
-	targetInfo map[string]targetInfo
-}
-
-// NewTargets initializes a new Targets instance and sets up SSH information for each target
-func NewTargets(t *testing.T) *Targets {
-	t.Helper()
-	nt := Targets{
-		targetInfo: make(map[string]targetInfo),
+var (
+	// default process to dump core
+	processes = []string{
+		"bundlemgr_checker", "ifmgr", "netio", "pkt_trace_agent",
 	}
-	return &nt
-}
+)
 
 func TestMain(m *testing.M) {
 	fptest.RunTests(m)
 }
 
 func TestCoreFileDecode(t *testing.T) {
-	processList := []string{
-		"bundlemgr_checker", "ifmgr", "netio", "pkt_trace_agent",
+	if *processList != "" {
+		processes = strings.Split(*processList, ",")
 	}
-	targets := NewTargets(t)
-	for dutID := range targets.targetInfo {
-		dut := ondatra.DUT(t, dutID)
-		t.Logf("Start dumping core for device : %s", dutID)
-		for _, process := range processList {
-			t.Run("create core file", func(t *testing.T) {
-				t.Logf("Dumping core for device : %s , process name : %s", dutID, process)
+
+	b, err := processBindingFile(t)
+	if err != nil {
+		t.Fatalf("Failed to process binding file: %v", err)
+	}
+
+	for _, device := range b.Duts {
+		dutName := device.Name
+		dut := ondatra.DUT(t, dutName)
+		t.Run(fmt.Sprintf("dump core files for device %s", dutName), func(t *testing.T) {
+			t.Logf("Start dumping core for device: %s", dutName)
+			for _, process := range processes {
+				process := process // Capture the current value of process
+				t.Logf("Dumping core for device: %s, process name: %s", dutName, process)
+				t.Logf("%s>dumpcore running %s location 0/RP0/CPU0\n", dutName, process)
 				dut.CLI().RunResult(t, fmt.Sprintf("dumpcore running %s location 0/RP0/CPU0\n", process))
-			})
-			t.Logf("Finished dumping core for device : %s", dutID)
-		}
+			}
+			t.Logf("Finished dumping core for device: %s", dutName)
+		})
 	}
+}
+
+func processBindingFile(t *testing.T) (*bindpb.Binding, error) {
+	t.Helper()
+	t.Log("Starting processing binding file")
+
+	bf := flag.Lookup("binding")
+	if bf == nil {
+		return nil, fmt.Errorf("binding file flag not set correctly")
+	}
+
+	bindingFile := bf.Value.String()
+	if bindingFile == "" {
+		return nil, fmt.Errorf("binding file path is empty")
+	}
+
+	in, err := os.ReadFile(bindingFile)
+	if err != nil {
+		return nil, fmt.Errorf("error reading binding file: %v", err)
+	}
+
+	b := &bindpb.Binding{}
+	if err := prototext.Unmarshal(in, b); err != nil {
+		return nil, fmt.Errorf("error processing binding file: %v", err)
+	}
+
+	t.Log("Processing binding file successful")
+	return b, nil
 }
