0850645f9aa5baee50e54e97da5598c0440dbcfb
hattikals <hattikals@juniper.net>
2024-10-16 02:35:12 -0700

    resolved conflicts

--- feature/system/tests/system_base_test/g_protocol_test.go
+++ feature/system/tests/system_base_test/g_protocol_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/ondatra"
 	"github.com/openconfig/ondatra/binding/introspect"
 	"google.golang.org/grpc"
@@ -58,8 +59,14 @@ func TestGNMIClient(t *testing.T) {
 	dut := ondatra.DUT(t, "dut")
 	conn := dialConn(t, dut, introspect.GNMI, 9339)
 	c := gpb.NewGNMIClient(conn)
-	if _, err := c.Get(context.Background(), &gpb.GetRequest{Encoding: gpb.Encoding_JSON_IETF, Path: []*gpb.Path{{Elem: []*gpb.PathElem{}}}}); err != nil {
-		t.Fatalf("gnmi.Get failed: %v", err)
+	if deviations.GnmiGetRequiresConfigType(dut) {
+		if _, err := c.Get(context.Background(), &gpb.GetRequest{Encoding: gpb.Encoding_JSON_IETF, Path: []*gpb.Path{{Elem: []*gpb.PathElem{}}}, Type: gpb.GetRequest_CONFIG}); err != nil {
+			t.Fatalf("gnmi.Get failed: %v", err)
+		}
+	} else {
+		if _, err := c.Get(context.Background(), &gpb.GetRequest{Encoding: gpb.Encoding_JSON_IETF, Path: []*gpb.Path{{Elem: []*gpb.PathElem{}}}}); err != nil {
+			t.Fatalf("gnmi.Get failed: %v", err)
+		}
 	}
 }
 
