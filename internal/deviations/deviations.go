481af565221c15277d5688fca96f92eb1f964b4c
hattikals <hattikals@juniper.net>
2024-10-17 06:32:08 -0700

    Corrected the formatting errors

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -1175,6 +1175,8 @@ func EthChannelAssignmentCiscoNumbering(dut *ondatra.DUTDevice) bool {
 // ChassisGetRPCUnsupported returns true if a Healthz Get RPC against the Chassis component is unsupported
 func ChassisGetRPCUnsupported(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetChassisGetRpcUnsupported()
+}
+
 // OperationalModeUnsupported returns true if operational-mode leaf is unsupported
 func OperationalModeUnsupported(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetOperationalModeUnsupported()
