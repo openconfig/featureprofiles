e965ab01f8cc542d2e870052664d082efcdc2f15
Chris Morrow <chris+github@as701.net>
2024-11-13 00:02:22 -0800

    Rename Packages in features/security. (#3581)
    The package name config for each golang test package
    could have been more specified.

--- feature/security/gnsi/acctz/tests/record_subscribe_full/record_subscribe_full_test.go
+++ feature/security/gnsi/acctz/tests/record_subscribe_full/record_subscribe_full_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package recordsubscribefull
+package recordsubscribefull_test
 
 import (
 	"context"
--- feature/security/gnsi/acctz/tests/record_subscribe_non_grpc/record_subscribe_non_grpc_test.go
+++ feature/security/gnsi/acctz/tests/record_subscribe_non_grpc/record_subscribe_non_grpc_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package recordsubscribenongrpc
+package recordsubscribenongrpc_test
 
 import (
 	"context"
