b1788e19fa6ffc284d9b447eb2d7d3cdfb5def85
Nisha Sadhasivam <nsadhasivam@google.com>
2024-11-14 11:43:09 +0000

    updated import

--- internal/security/acctz/acctz.go
+++ internal/security/acctz/acctz.go
@@ -27,7 +27,7 @@ import (
 	"testing"
 	"time"
 
-	system_pb "github.com/openconfig/gnoi/system"
+	systempb "github.com/openconfig/gnoi/system"
 	acctzpb "github.com/openconfig/gnsi/acctz"
 	authzpb "github.com/openconfig/gnsi/authz"
 	cpb "github.com/openconfig/gnsi/credentialz"
@@ -43,7 +43,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/metadata"
-	gnmi_pb "github.com/openconfig/gnmi/proto/gnmi"
+	gnmipb "github.com/openconfig/gnmi/proto/gnmi"
 	"google.golang.org/protobuf/types/known/anypb"
 )
 
@@ -113,7 +113,7 @@ func setupUserPassword(t *testing.T, dut *ondatra.DUTDevice, username, password 
 	time.Sleep(time.Second)
 }
 
-func nokiaFailCliRole(t *testing.T) *gnmi_pb.SetRequest {
+func nokiaFailCliRole(t *testing.T) *gnmipb.SetRequest {
 	failRoleData, err := json.Marshal([]any{
 		map[string]any{
 			"services": []string{"cli"},
@@ -126,22 +126,22 @@ func nokiaFailCliRole(t *testing.T) *gnmi_pb.SetRequest {
 		t.Fatalf("Error with json marshal: %v", err)
 	}
 
-	return &gnmi_pb.SetRequest{
-		Prefix: &gnmi_pb.Path{
+	return &gnmipb.SetRequest{
+		Prefix: &gnmipb.Path{
 			Origin: "native",
 		},
-		Replace: []*gnmi_pb.Update{
+		Replace: []*gnmipb.Update{
 			{
-				Path: &gnmi_pb.Path{
-					Elem: []*gnmi_pb.PathElem{
+				Path: &gnmipb.Path{
+					Elem: []*gnmipb.PathElem{
 						{Name: "system"},
 						{Name: "aaa"},
 						{Name: "authorization"},
 						{Name: "role", Key: map[string]string{"rolename": failRoleName}},
 					},
 				},
-				Val: &gnmi_pb.TypedValue{
-					Value: &gnmi_pb.TypedValue_JsonIetfVal{
+				Val: &gnmipb.TypedValue{
+					Value: &gnmipb.TypedValue_JsonIetfVal{
 						JsonIetfVal: failRoleData,
 					},
 				},
@@ -157,7 +157,7 @@ func SetupUsers(t *testing.T, dut *ondatra.DUTDevice, configureFailCliRole bool)
 	successUser.SetRole(oc.AaaTypes_SYSTEM_DEFINED_ROLES_SYSTEM_ROLE_ADMIN)
 	failUser := auth.GetOrCreateUser(failUsername)
 	if configureFailCliRole {
-		var SetRequest *gnmi_pb.SetRequest
+		var SetRequest *gnmipb.SetRequest
 
 		// Create failure cli role in native.
 		switch dut.Vendor() {
@@ -325,13 +325,13 @@ func SendGnmiRPCs(t *testing.T, dut *ondatra.DUTDevice) []*acctzpb.RecordRespons
 
 	var records []*acctzpb.RecordResponse
 	grpcConn := dialGrpc(t, target)
-	gnmiClient := gnmi_pb.NewGNMIClient(grpcConn)
+	gnmiClient := gnmipb.NewGNMIClient(grpcConn)
 	ctx := context.Background()
 	ctx = metadata.AppendToOutgoingContext(ctx, "username", failUsername)
 	ctx = metadata.AppendToOutgoingContext(ctx, "password", failPassword)
 
 	// Send an unsuccessful gNMI capabilities request (bad creds in context).
-	_, err := gnmiClient.Capabilities(ctx, &gnmi_pb.CapabilityRequest{})
+	_, err := gnmiClient.Capabilities(ctx, &gnmipb.CapabilityRequest{})
 	if err != nil {
 		t.Logf("Got expected error fetching capabilities with bad creds, error: %s", err)
 	} else {
@@ -364,7 +364,7 @@ func SendGnmiRPCs(t *testing.T, dut *ondatra.DUTDevice) []*acctzpb.RecordRespons
 	ctx = context.Background()
 	ctx = metadata.AppendToOutgoingContext(ctx, "username", successUsername)
 	ctx = metadata.AppendToOutgoingContext(ctx, "password", successPassword)
-	req := &gnmi_pb.CapabilityRequest{}
+	req := &gnmipb.CapabilityRequest{}
 	payload, err := anypb.New(req)
 	if err != nil {
 		t.Fatal("Failed creating anypb payload.")
@@ -422,14 +422,14 @@ func SendGnoiRPCs(t *testing.T, dut *ondatra.DUTDevice) []*acctzpb.RecordRespons
 
 	var records []*acctzpb.RecordResponse
 	grpcConn := dialGrpc(t, target)
-	gnoiSystemClient := system_pb.NewSystemClient(grpcConn)
+	gnoiSystemClient := systempb.NewSystemClient(grpcConn)
 	ctx := context.Background()
 	ctx = metadata.AppendToOutgoingContext(ctx, "username", failUsername)
 	ctx = metadata.AppendToOutgoingContext(ctx, "password", failPassword)
 
 	// Send an unsuccessful gNOI system time request (bad creds in context), we don't
 	// care about receiving on it, just want to make the request.
-	gnoiSystemPingClient, err := gnoiSystemClient.Ping(ctx, &system_pb.PingRequest{
+	gnoiSystemPingClient, err := gnoiSystemClient.Ping(ctx, &systempb.PingRequest{
 		Destination: "127.0.0.1",
 		Count:       1,
 	})
@@ -468,7 +468,7 @@ func SendGnoiRPCs(t *testing.T, dut *ondatra.DUTDevice) []*acctzpb.RecordRespons
 	ctx = context.Background()
 	ctx = metadata.AppendToOutgoingContext(ctx, "username", successUsername)
 	ctx = metadata.AppendToOutgoingContext(ctx, "password", successPassword)
-	req := &system_pb.PingRequest{
+	req := &systempb.PingRequest{
 		Destination: "127.0.0.1",
 		Count:       1,
 	}
