2ff22d3ac034ea9c5b43972216f252e63e6317d4
trathod <tushar.rathod@nokia.com>
2024-11-20 18:05:18 -0500

    Adding 3 sec delay for consistent routes exchange with KNE

--- feature/bgp/policybase/otg_tests/community_test/community_test.go
+++ feature/bgp/policybase/otg_tests/community_test/community_test.go
@@ -280,6 +280,8 @@ func TestCommunitySet(t *testing.T) {
 
 			// Verify BGP session after its reset with OTG push config & start
 			cfgplugins.VerifyDUTBGPEstablished(t, bs.DUT)
+			// Adding 3 seconds delay for consistency with KNE routes exchange
+			time.Sleep(3 * time.Second)
 
 			t.Logf("Starting traffic for IPv4 and v6")
 			bs.ATE.OTG().StartTraffic(t)
