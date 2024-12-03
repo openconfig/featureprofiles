73afad2ec7a79e65d3f3223934c65dcde8adbe03
karremil <karthikeya.remilla@gmail.com>
2024-12-03 21:42:29 +0530

    improve error logging

--- feature/cisco/gnoi/healthz/healthz_test.go
+++ feature/cisco/gnoi/healthz/healthz_test.go
@@ -36,7 +36,7 @@ func TestInvalidGetRpc(t *testing.T) {
 		getResp, err := gnoiClient.Healthz().Get(context.Background(), req)
 		t.Logf("Get response: %v", getResp)
 		if err == nil {
-			t.Errorf("Expected non-zero error after Invalid Get")
+			t.Fatal("Expected non-zero error after Invalid Get")
 		}
 	}
 }
@@ -58,7 +58,7 @@ func TestInvalidListRpc(t *testing.T) {
 		listResp, err := gnoiClient.Healthz().List(context.Background(), listReq)
 		t.Logf("List response: %v", listResp)
 		if err == nil {
-			t.Errorf("Expected non-zero error after Invalid List")
+			t.Fatal("Expected non-zero error after Invalid List")
 		}
 	}
 }
@@ -113,11 +113,11 @@ func TestInvalidCheckRpc(t *testing.T) {
 		getResp, err := gnoiClient.Healthz().Get(context.Background(), getReq)
 		t.Logf("Get response: %v", getResp)
 		if err != nil {
-			t.Errorf("Error on Get(%q): %v", activeRp, err)
+			t.Fatalf("Error on Get(%q): %v", activeRp, err)
 		}
 		eventId := getResp.GetComponent().GetId()
 		if eventId == "" {
-			t.Errorf("Get returned an empty event_id")
+			t.Fatal("Get returned an empty event_id")
 		}
 		componentNames := []map[string]string{{"name": "0/RP3/CPU0-CHASSIS"}}
 		for _, componentName := range componentNames {
@@ -134,7 +134,7 @@ func TestInvalidCheckRpc(t *testing.T) {
 			checkResp, err := gnoiClient.Healthz().Check(context.Background(), checkReq)
 			t.Logf("Check response: %v", checkResp)
 			if err == nil {
-				t.Errorf("Error on Check(%q): %v", componentName, err)
+				t.Fatalf("Error on Check(%q): %v", componentName, err)
 			}
 		}
 	})
@@ -155,7 +155,7 @@ func TestInvalidCheckRpc(t *testing.T) {
 			checkResp, err := gnoiClient.Healthz().Check(context.Background(), checkReq)
 			t.Logf("Check response: %v", checkResp)
 			if err == nil {
-				t.Errorf("Error on Check(%q): %v", componentName, err)
+				t.Fatalf("Error on Check(%q): %v", componentName, err)
 			}
 		}
 	})
