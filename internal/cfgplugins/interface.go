4514b6509a5539bb733286d8dafc28b32561a167
Jian Chen <jchenjian@google.com>
2024-12-17 19:43:01 +0000

    bug fix for channel definition in the interface.go file

--- internal/cfgplugins/interface.go
+++ internal/cfgplugins/interface.go
@@ -173,7 +173,7 @@ func ConfigETHChannel(t *testing.T, dut *ondatra.DUTDevice, interfaceName, trans
 			Assignment:         assignment,
 		}
 	} else {
-		channel := &oc.TerminalDevice_Channel{
+		channel = &oc.TerminalDevice_Channel{
 			Description:        ygot.String("ETH Logical Channel"),
 			Index:              ygot.Uint32(ethIndex),
 			LogicalChannelType: oc.TransportTypes_LOGICAL_ELEMENT_PROTOCOL_TYPE_PROT_ETHERNET,
