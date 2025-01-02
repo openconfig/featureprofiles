84509cec2eeb2a40e030e93d9c9f924bd81f1425
vishnureddybadveli <112267356+vishnureddybadveli@users.noreply.github.com>
2025-01-02 07:51:23 -0800

    Update ingress_police_default_test.go

--- feature/qos/otg_tests/ingress_police_default/ingress_police_default_test.go
+++ feature/qos/otg_tests/ingress_police_default/ingress_police_default_test.go
@@ -198,7 +198,7 @@ func TestInterfaceIngressPolicer(t *testing.T) {
 				count, ok := gnmi.Watch(t, dut, gnmi.OC().Qos().Interface(dp2.Name()).Output().Queue(data.queue).TransmitPkts().State(), timeout, isPresent).Await(t)
 				if !ok {
 					t.Logf("TransmitPkts count for queue %q on interface %q not available within %v", dp2.Name(),
-					       data.queue, timeout)
+					        data.queue, timeout)
 				}
 				dutQosPktsBeforeTraffic[data.queue], _ = count.Val()
 
@@ -223,7 +223,7 @@ func TestInterfaceIngressPolicer(t *testing.T) {
 				ateOutPkts[data.queue] += ateTxPkts
 				ateInPkts[data.queue] += ateRxPkts
 				t.Logf("ateInPkts: %v, txPkts %v, Queue: %v", ateInPkts[data.queue], dutQosPktsAfterTraffic[data.queue],
-				       data.queue)
+				        data.queue)
 				if ateTxPkts == 0 {
 					t.Fatalf("TxPkts == 0, want >0.")
 				}
