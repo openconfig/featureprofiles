b3df970214f4dc032464346feded3234c9bb5325
cprabha <cprabha@juniper.net>
2025-01-15 17:54:45 -0800

    updating prefx length

--- feature/bgp/policybase/otg_tests/nested_policies/nested_policies_test.go
+++ feature/bgp/policybase/otg_tests/nested_policies/nested_policies_test.go
@@ -37,16 +37,16 @@ import (
 const (
 	ipv4PrefixLen     = 30
 	ipv6PrefixLen     = 126
-	v41Route          = "203.0.113.0"
+	v41Route          = "203.0.113.1"
 	v41TrafficStart   = "203.0.113.1"
-	v42Route          = "198.51.100.0"
+	v42Route          = "198.51.100.1"
 	v42TrafficStart   = "198.51.100.1"
-	v4RoutePrefix     = uint32(24)
-	v61Route          = "2001:db8:128:128::"
+	v4RoutePrefix     = uint32(32)
+	v61Route          = "2001:db8:128:128::1"
 	v61TrafficStart   = "2001:db8:128:128::1"
-	v62Route          = "2001:db8:128:129::"
+	v62Route          = "2001:db8:128:129::1"
 	v62TrafficStart   = "2001:db8:128:129::1"
-	v6RoutePrefix     = uint32(64)
+	v6RoutePrefix     = uint32(128)
 	dutAS             = uint32(65656)
 	ateAS1            = uint32(65657)
 	ateAS2            = uint32(65658)
@@ -226,9 +226,13 @@ func TestBGPNestedPolicies(t *testing.T) {
 			tc.validate(t, dut, ate)
 			if tc.ipv4 {
 				createFlow(t, td, tc.flowConfig)
+				td.verifyDUTBGPEstablished(t)
+				td.verifyOTGBGPEstablished(t)
 				checkTraffic(t, td, v4Flow)
 			} else {
 				createFlowV6(t, td, tc.flowConfig)
+				td.verifyDUTBGPEstablished(t)
+				td.verifyOTGBGPEstablished(t)
 				checkTraffic(t, td, v6Flow)
 			}
 		})
@@ -910,3 +914,4 @@ func configureOTG(t *testing.T, ate *ondatra.ATEDevice, top gosnappi.Config) []g
 	d2 := atePort2.AddToOTG(top, p2, &dutPort2)
 	return []gosnappi.Device{d1, d2}
 }
+
