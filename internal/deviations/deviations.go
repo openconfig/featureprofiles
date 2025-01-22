6d707bbaedeef0a9cd39f601f09927e85acb2f9a
vishnureddybadveli <112267356+vishnureddybadveli@users.noreply.github.com>
2025-01-22 10:50:48 -0800

    Removed redundant deviations after merge to latest

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -1302,8 +1302,3 @@ func QosSchedulerIngressPolicer(dut *ondatra.DUTDevice) bool {
 func GribiEncapHeaderUnsupported(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetGribiEncapHeaderUnsupported()
 }
-
-// QosSchedulerIngressPolicer returns true if qos ingress policing is unsupported
-func QosSchedulerIngressPolicer(dut *ondatra.DUTDevice) bool {
-	return lookupDUTDeviations(dut).GetQosSchedulerIngressPolicerUnsupported()
-}
