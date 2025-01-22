12e8e70a7c4fba405511c3115b4dc275bd2eec5e
vishnureddybadveli <112267356+vishnureddybadveli@users.noreply.github.com>
2025-01-22 11:11:59 -0800

    Merge issue

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -1293,6 +1293,11 @@ func DefaultBgpInstanceName(dut *ondatra.DUTDevice) string {
 	return "DEFAULT"
 }
 
+// ChannelRateClassParametersUnsupported returns true if channel rate class parameters are unsupported
+func ChannelRateClassParametersUnsupported(dut *ondatra.DUTDevice) bool {
+        return lookupDUTDeviations(dut).GetChannelAssignmentRateClassParametersUnsupported()
+}
+
 // QosSchedulerIngressPolicer returns true if qos ingress policing is unsupported
 func QosSchedulerIngressPolicer(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetQosSchedulerIngressPolicerUnsupported()
