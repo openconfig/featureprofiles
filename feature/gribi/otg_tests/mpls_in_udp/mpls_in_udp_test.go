6dbbf172945ddeede6cbe92e61ebe7c0295b41cf
vishnureddybadveli <112267356+vishnureddybadveli@users.noreply.github.com>
2025-01-22 13:25:20 -0800

    Update mpls_in_udp_test.go removed a new line.

--- feature/gribi/otg_tests/mpls_in_udp/mpls_in_udp_test.go
+++ feature/gribi/otg_tests/mpls_in_udp/mpls_in_udp_test.go
@@ -20,7 +20,6 @@ import (
 	"testing"
 	"time"
 	
-	
 	"github.com/open-traffic-generator/snappi/gosnappi"
 	"github.com/openconfig/featureprofiles/internal/attrs"
 	"github.com/openconfig/featureprofiles/internal/deviations"
