c122ffa746a0001fc68758003a734db0666b6393
AmrNJ <155722765+AmrNJ@users.noreply.github.com>
2025-01-30 16:46:16 +0530

    Update banner_test.go

--- feature/system/tests/system_base_test/banner_test.go
+++ feature/system/tests/system_base_test/banner_test.go
@@ -103,8 +103,7 @@ func TestLoginBanner(t *testing.T) {
                 {"Empty String", ""},
                 {"Single Character", "x"},
                 {"Short String", "Warning Text"},
-                {"Long String", "WARNING : Unauthorized access to this system is forbidden and will be prosecuted by law. By accessing this system, you agree that your actions may be monitored if unauthorized usage is s
-uspected."},
+                {"Long String", "WARNING : Unauthorized access to this system is forbidden and will be prosecuted by law. By accessing this system, you agree that your actions may be monitored if unauthorized usage is suspected."},
         }
 
         dut := ondatra.DUT(t, "dut")
