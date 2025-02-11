32543f9dceb165a3731006d828838301c636e740
sosdog <71899601+sosdog@users.noreply.github.com>
2025-02-11 03:50:37 -0800

    _ to -

--- feature/bgp/addpath/otg_tests/route_propagation_test/route_propagation_test.go
+++ feature/bgp/addpath/otg_tests/route_propagation_test/route_propagation_test.go
@@ -21,7 +21,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/open_traffic_generator/gosnappi/gosnappi"
+	"github.com/open-traffic-generator/gosnappi/gosnappi"
 	"github.com/openconfig/featureprofiles/internal/attrs/attrs"
 	"github.com/openconfig/featureprofiles/internal/cfgplugins/cfgplugins"
 	"github.com/openconfig/featureprofiles/internal/deviations/deviations"
