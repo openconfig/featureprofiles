9b7987afa24f19dfb99027af7f57b32c00610cd6
sosdog <71899601+sosdog@users.noreply.github.com>
2025-02-11 04:58:31 -0800

    Update route_propagation_test.go
    missed internal <> external translation

--- feature/bgp/addpath/otg_tests/route_propagation_test/route_propagation_test.go
+++ feature/bgp/addpath/otg_tests/route_propagation_test/route_propagation_test.go
@@ -23,7 +23,7 @@ import (
 
 	"github.com/open-traffic-generator/snappi/gosnappi"
 	"github.com/openconfig/featureprofiles/internal/attrs"
-	"github.com/openconfig/featureprofiles/internal/cfgplugins/cfgplugins"
+	"github.com/openconfig/featureprofiles/internal/cfgplugins"
 	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/featureprofiles/internal/fptest"
 	"github.com/openconfig/ondatra"
