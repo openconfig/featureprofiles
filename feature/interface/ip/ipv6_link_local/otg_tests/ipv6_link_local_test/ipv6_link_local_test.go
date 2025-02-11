fd15ad3ca628bd237e64a829d42aa4727e7642f9
root <hattikals@juniper.net>
2025-02-10 22:11:05 -0800

    Fixed the formatting issue

--- feature/interface/ip/ipv6_link_local/otg_tests/ipv6_link_local_test/ipv6_link_local_test.go
+++ feature/interface/ip/ipv6_link_local/otg_tests/ipv6_link_local_test/ipv6_link_local_test.go
@@ -255,7 +255,7 @@ func verifyLinkLocalTraffic(t *testing.T, dut *ondatra.DUTDevice, ate *ondatra.A
 	p1 := dut.Port(t, "port1")
 	beforeInPkts := gnmi.Get(t, dut, gnmi.OC().Interface(p1.Name()).Counters().InPkts().State())
 	ate.OTG().StartTraffic(t)
-        time.Sleep(15 * time.Second)	
+	time.Sleep(15 * time.Second)
 	_, ok := gnmi.Watch(t, dut, gnmi.OC().Interface(p1.Name()).Counters().InPkts().State(), time.Second*30, func(v *ygnmi.Value[uint64]) bool {
 		gotPkts, present := v.Val()
 		return present && (gotPkts-beforeInPkts) >= 100
@@ -287,7 +287,7 @@ func verifyGlobalUnicastTraffic(t *testing.T, dut *ondatra.DUTDevice, ate *ondat
 	ate.OTG().StartTraffic(t)
 	time.Sleep(15 * time.Second)
 	ate.OTG().StopTraffic(t)
-        time.Sleep(15 * time.Second)
+	time.Sleep(15 * time.Second)
 	otgutils.LogFlowMetrics(t, ate.OTG(), top)
 
 	flowMetrics := gnmi.Get(t, ate.OTG(), gnmi.OTG().Flow(flowName).Counters().State())
