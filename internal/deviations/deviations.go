cb0f7f70f7315f242c8f7cada0d65979b2944e03
Mohana <mohanas@juniper.net>
2025-02-11 20:19:10 -0800

    TE-9.2: Added deviation static_lsp_unsupported (#3347)
    * Added deviation static_lsp_unsupported
    
    * Updated the native config for the deviation
    
    * Resolving merge conflict
    
    * Resolving conflicts
    
    ---------
    
    Co-authored-by: KandukuriSudheer <schandrak@juniper.net>
    Co-authored-by: cprabha <cprabha@juniper.net>

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -1306,3 +1306,8 @@ func P4RTCapabilitiesUnsupported(dut *ondatra.DUTDevice) bool {
 func GNMIGetOnRootUnsupported(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetGnmiGetOnRootUnsupported()
 }
+
+// StaticLspConfigUnsupported returns true if static lsp config is not supported
+func StaticLspConfigUnsupported(dut *ondatra.DUTDevice) bool {
+	return lookupDUTDeviations(dut).GetStaticLspUnsupported()
+}
