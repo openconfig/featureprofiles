521ab10cd1704124727da7556dd62c84c42ff978
vishnureddybadveli <112267356+vishnureddybadveli@users.noreply.github.com>
2025-02-21 17:39:31 -0800

    Update mpls_gre_udp_decap_test.go

--- feature/policy_forwarding/otg_tests/mpls_gre_udp_decap_test/mpls_gre_udp_decap_test.go
+++ feature/policy_forwarding/otg_tests/mpls_gre_udp_decap_test/mpls_gre_udp_decap_test.go
@@ -208,11 +208,10 @@ func configureDecapIPGroups(t *testing.T, dut *ondatra.DUTDevice) {
 	switch dut.Vendor() {
 	case ondatra.ARISTA:
 		var defaultPolicyCLI string
-		defaultPolicyCLI = fmt.Sprintf("ip decap-group pf-decap-range")
-		helpers.GnmiCLIConfig(t, dut, defaultPolicyCLI)
+		helpers.GnmiCLIConfig(t, dut, "ip decap-group pf-decap-range")
 		// Arista does not support IPv6 yet revisit when functionality is available
 		// defaultPolicyCLI = fmt.Sprintf("tunnel decap-ip %s", outerIPv6Src)
-		helpers.GnmiCLIConfig(t, dut, defaultPolicyCLI)
+		// helpers.GnmiCLIConfig(t, dut, defaultPolicyCLI)
 		helpers.GnmiCLIConfig(t, dut, fmt.Sprintf("tunnel type gre \n %s \n %s \n", defaultPolicyCLI, "tunnel overlay mpls qos map mpls-traffic-class to traffic-class"))
 	default:
 		t.Fatalf("Unsupported vendor %s for native command support for deviation 'GribiEncapHeaderUnsupported'", dut.Vendor())
