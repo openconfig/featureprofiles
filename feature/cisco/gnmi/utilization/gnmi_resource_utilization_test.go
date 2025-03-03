78d4902e9ebf79e1e3b762bf6c34f5d7604f0b5c
Liam Walsh <liawalsh@cisco.com>
2025-03-03 16:31:26 -0500

    ensure timeout format is correct

--- feature/cisco/gnmi/utilization/gnmi_resource_utilization_test.go
+++ feature/cisco/gnmi/utilization/gnmi_resource_utilization_test.go
@@ -68,7 +68,7 @@ func stressTestComponent(t testing.TB, dut *ondatra.DUTDevice, location string, 
 
 func stressCPU(t testing.TB, dut *ondatra.DUTDevice, location string) {
 	t.Helper()
-	cmd := "run /var/xr/scratch/stress --cpu 300 --timeout 40"
+	cmd := "run /var/xr/scratch/stress --cpu 300 --timeout 40s"
 	if location != "" {
 		cmd = fmt.Sprintf("attach location %s \n ", location) + cmd
 	}
@@ -78,7 +78,7 @@ func stressCPU(t testing.TB, dut *ondatra.DUTDevice, location string) {
 func stressMem(t testing.TB, dut *ondatra.DUTDevice, location string) {
 	t.Helper()
 	// spawn 100 workers spinning on malloc()/free()
-	cmd := "run /var/xr/scratch/stress --vm 200 --timeout 40"
+	cmd := "run /var/xr/scratch/stress --vm 200 --timeout 40s"
 	if location != "" {
 		cmd = fmt.Sprintf("attach location %s \n ", location) + cmd
 	}
