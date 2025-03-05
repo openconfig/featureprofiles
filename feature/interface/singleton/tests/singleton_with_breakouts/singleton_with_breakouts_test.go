7f651cb24e48e6b721cc44eee33201ebae51a2e1
Priya Ranjan Nanda <151918045+priyaranjannanda@users.noreply.github.com>
2025-03-05 15:11:48 +0530

    Update singleton_with_breakouts_test.go to have package name singleton_with_breakouts_test

--- feature/interface/singleton/tests/singleton_with_breakouts/singleton_with_breakouts_test.go
+++ feature/interface/singleton/tests/singleton_with_breakouts/singleton_with_breakouts_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package singleton_with_breakouts
+package singleton_with_breakouts_test
 
 import (
 	"context"
