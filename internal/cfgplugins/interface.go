e80b1d2b01557c978f4ddbf9d4b453fbe8b6d79e
snaragund <sham_r.naragund@nokia.com>
2025-03-17 11:59:36 +0530

    OTN to Eth logical channel configuration in ConfigOTNChannel func as per https://partnerissuetracker.corp.google.com/issues/384673249
    "This code is a Contribution to the OpenConfig Feature Profiles project ("Work") made under the Google Software Grant and Corporate Contributor License Agreement ("CLA") and governed by the Apache License 2.0. No other rights or licenses in or to any of Nokia's intellectual property are granted for any other purpose. This code is provided on an "as is" basis without any warranties of any kind."

--- internal/cfgplugins/interface.go
+++ internal/cfgplugins/interface.go
@@ -135,6 +135,13 @@ func ConfigOTNChannel(t *testing.T, dut *ondatra.DUTDevice, och string, otnIndex
 			},
 		})
 	} else {
+		t.Logf(" otnIndex:%v, ethIndex: %v", otnIndex, ethIndex)
+		gnmi.Replace(t, dut, gnmi.OC().TerminalDevice().Channel(ethIndex).Config(), &oc.TerminalDevice_Channel{
+			Description:        ygot.String("ETH Logical Channel"),
+			Index:              ygot.Uint32(ethIndex),
+			LogicalChannelType: oc.TransportTypes_LOGICAL_ELEMENT_PROTOCOL_TYPE_PROT_ETHERNET,
+			TribProtocol:       oc.TransportTypes_TRIBUTARY_PROTOCOL_TYPE_PROT_400GE,
+		})
 		gnmi.Replace(t, dut, gnmi.OC().TerminalDevice().Channel(otnIndex).Config(), &oc.TerminalDevice_Channel{
 			Description:        ygot.String("OTN Logical Channel"),
 			Index:              ygot.Uint32(otnIndex),
