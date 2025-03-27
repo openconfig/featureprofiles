e94809f94b6cc841436ceae5110ed103d0600eff
Yini <yini@arista.com>
2025-03-27 07:26:31 +0000

    RT-2.14: Add deviation isis_mpls_unsupported
    RT-2.14: Add deviation isis_mpls_unsupported

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -1342,3 +1342,8 @@ func SkipTransceiverDescription(dut *ondatra.DUTDevice) bool {
 func ContainerzOCUnsupported(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetContainerzOcUnsupported()
 }
+
+// IsisMplsUnsupported returns true if there's no OC support for MPLS under ISIS
+func IsisMplsUnsupported(dut *ondatra.DUTDevice) bool {
+	return lookupDUTDeviations(dut).GetIsisMplsUnsupported()
+}
