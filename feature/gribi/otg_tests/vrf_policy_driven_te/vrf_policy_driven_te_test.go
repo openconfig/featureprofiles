e09dd61b9846abbfb186f6353e5acfc69f1fed1d
Krishnakumar Natarajan <kk200488@gmail.com>
2025-04-03 20:45:39 +0530

    te17.1 fix

--- feature/gribi/otg_tests/vrf_policy_driven_te/vrf_policy_driven_te_test.go
+++ feature/gribi/otg_tests/vrf_policy_driven_te/vrf_policy_driven_te_test.go
@@ -112,7 +112,7 @@ const (
 	defaultRoute            = "0.0.0.0/0"
 	wantLoss                = true
 	routeDelete             = true
-	correspondingTTL        = 64
+	correspondingTTL        = 63
 	correspondingHopLimit   = 64
 	flow6in4                = "flow6in4"
 	flow4in4                = "flow4in4"
@@ -1627,7 +1627,7 @@ func validateTrafficTTL(t *testing.T, packetSource *gopacket.PacketSource) {
 			packetCheckCount++
 			ipPacket, _ := ipLayer.(*layers.IPv4)
 			if ipPacket.TTL != correspondingTTL {
-				t.Errorf("IP TTL value is altered to: %d", ipPacket.TTL)
+				t.Errorf("Wrong IP TTL value received, got: %d, want: %d", ipPacket.TTL, correspondingTTL)
 			}
 			innerPacket := gopacket.NewPacket(ipPacket.Payload, ipPacket.NextLayerType(), gopacket.Default)
 			ipInnerLayer := innerPacket.Layer(layers.LayerTypeIPv4)
