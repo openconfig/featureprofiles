b0c2a6d6eef0b0515aacd34beba85eb78e7b78fe
Swetha Haridasula <haridasula@google.com>
2025-04-30 06:54:38 +0000

    fix error

--- feature/system/control_plane_traffic/otg_tests/ingress_acl/control_plane_traffic_ingress_acl_test.go
+++ feature/system/control_plane_traffic/otg_tests/ingress_acl/control_plane_traffic_ingress_acl_test.go
@@ -15,19 +15,19 @@
 package control_plane_ingress_acl_test
 
 import (
+	"sort"
 	"testing"
 	"time"
-	"sort"
-	
-	"github.com/openconfig/ygot/ygot"
+
 	"github.com/open-traffic-generator/snappi/gosnappi"
 	"github.com/openconfig/featureprofiles/internal/attrs"
 	"github.com/openconfig/featureprofiles/internal/fptest"
 	"github.com/openconfig/featureprofiles/internal/otgutils"
+	"github.com/openconfig/ondatra"
 	"github.com/openconfig/ondatra/gnmi"
 	"github.com/openconfig/ondatra/gnmi/oc"
 	"github.com/openconfig/ondatra/netutil"
-	"github.com/openconfig/ondatra"
+	"github.com/openconfig/ygot/ygot"
 )
 
 // Constants for test parameters
