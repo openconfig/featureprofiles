2c3d3dd13840d887c3b7053ba75403e690c45300
Swetha-haridasula <haridasula@google.com>
2025-04-30 12:33:19 +0530

    Update flow_control_test.go

--- feature/interface/singleton/otg_tests/flow_control/flow_control_test.go
+++ feature/interface/singleton/otg_tests/flow_control/flow_control_test.go
@@ -23,7 +23,7 @@ import (
 	"github.com/openconfig/featureprofiles/internal/fptest"
 	"github.com/openconfig/ondatra/gnmi"
 	"github.com/openconfig/ondatra/gnmi/oc"
-	"github.com/openconfig/ondatra/ondatra"
+	"github.com/openconfig/ondatra"
 )
 
 const (
