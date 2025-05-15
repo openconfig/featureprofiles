d46396b90bf9199b4fb87451c910bc7724f6322d
AmrNJ <155722765+AmrNJ@users.noreply.github.com>
2025-05-15 17:29:23 +0530

    Update zr_supply_voltage_test.go

--- feature/platform/transceiver/supply_voltage/tests/zr_supply_voltage_test/zr_supply_voltage_test.go
+++ feature/platform/transceiver/supply_voltage/tests/zr_supply_voltage_test/zr_supply_voltage_test.go
@@ -36,7 +36,9 @@ const (
 )
 
 var (
-	operationalModeFlag = flag.Int("operational_mode", 1, "vendor-specific operational-mode for the channel")
+	operationalModeFlagCisco   = flag.Int("operational_mode", 5003, "vendor-specific operational-mode for the channel")
+	operationalModeFlagArista  = flag.Int("operational_mode", 1, "vendor-specific operational-mode for the channel")
+	operationalModeFlagDefault = flag.Int("operational_mode", 1, "default operational-mode for the channel")
 	operationalMode     uint16
 )
 
@@ -64,7 +66,14 @@ func verifyVoltageValue(t *testing.T, pStream *samplestream.SampleStream[float64
 func TestZrSupplyVoltage(t *testing.T) {
 	dut := ondatra.DUT(t, "dut")
 
-	operationalMode = uint16(*operationalModeFlag)
+	switch dut.Vendor() {
+	case ondatra.CISCO:
+		operationalMode = uint16(*operationalModeFlagCisco)
+	case ondatra.ARISTA:
+		operationalMode = uint16(*operationalModeFlagArista)
+	default:
+		operationalMode = uint16(*operationalModeFlagDefault)
+	}
 	cfgplugins.Initialize(operationalMode)
 	cfgplugins.InterfaceConfig(t, dut, dut.Port(t, "port1"))
 	cfgplugins.InterfaceConfig(t, dut, dut.Port(t, "port2"))
