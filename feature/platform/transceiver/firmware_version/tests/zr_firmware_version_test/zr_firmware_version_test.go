e7191b9f57446015b49910b11e4db95298c0cefa
AmrNJ <155722765+AmrNJ@users.noreply.github.com>
2025-05-15 17:23:41 +0530

    Update zr_firmware_version_test.go

--- feature/platform/transceiver/firmware_version/tests/zr_firmware_version_test/zr_firmware_version_test.go
+++ feature/platform/transceiver/firmware_version/tests/zr_firmware_version_test/zr_firmware_version_test.go
@@ -40,7 +40,9 @@ func TestMain(m *testing.M) {
 }
 
 var (
-	operationalModeFlag = flag.Int("operational_mode", 1, "vendor-specific operational-mode for the channel")
+	operationalModeFlagCisco   = flag.Int("operational_mode", 5003, "vendor-specific operational-mode for the channel")
+	operationalModeFlagArista  = flag.Int("operational_mode", 1, "vendor-specific operational-mode for the channel")
+	operationalModeFlagDefault = flag.Int("operational_mode", 1, "default operational-mode for the channel")
 	operationalMode     uint16
 )
 
@@ -79,7 +81,14 @@ func TestZRFirmwareVersionState(t *testing.T) {
 	t.Logf("dut1: %v", dut1)
 	t.Logf("dut1 dp1 name: %v", dp1.Name())
 
-	operationalMode = uint16(*operationalModeFlag)
+	switch dut.Vendor() {
+	case ondatra.CISCO:
+		operationalMode = uint16(*operationalModeFlagCisco)
+	case ondatra.ARISTA:
+		operationalMode = uint16(*operationalModeFlagArista)
+	default:
+		operationalMode = uint16(*operationalModeFlagDefault)
+	}
 	cfgplugins.Initialize(operationalMode)
 	cfgplugins.InterfaceConfig(t, dut1, dp1)
 	cfgplugins.InterfaceConfig(t, dut1, dp2)
