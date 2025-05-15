9fe79d8fda91f1af1ca94c40b9354f132ab6fd0b
AmrNJ <155722765+AmrNJ@users.noreply.github.com>
2025-05-15 14:08:58 +0000

    gofmt

--- feature/platform/transceiver/logical_channels/tests/zr_logical_channels_test/zr_logical_channels_test.go
+++ feature/platform/transceiver/logical_channels/tests/zr_logical_channels_test/zr_logical_channels_test.go
@@ -31,7 +31,7 @@ var (
 	operationalModeFlagCisco   = flag.Int("operational_mode", 5003, "vendor-specific operational-mode for the channel")
 	operationalModeFlagArista  = flag.Int("operational_mode", 1, "vendor-specific operational-mode for the channel")
 	operationalModeFlagDefault = flag.Int("operational_mode", 1, "default operational-mode for the channel")
-	operationalMode     uint16
+	operationalMode            uint16
 )
 
 type testcase struct {
