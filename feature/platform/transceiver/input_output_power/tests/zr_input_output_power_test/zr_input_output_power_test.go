0b09a443dd6acb7c13babfb3928bfe9594ad996a
AmrNJ <155722765+AmrNJ@users.noreply.github.com>
2025-05-16 23:01:24 +0530

    Update zr_input_output_power_test.go

--- feature/platform/transceiver/input_output_power/tests/zr_input_output_power_test/zr_input_output_power_test.go
+++ feature/platform/transceiver/input_output_power/tests/zr_input_output_power_test/zr_input_output_power_test.go
@@ -27,10 +27,7 @@ const (
 var (
 	frequencies                = []uint64{191400000, 196100000}
 	targetOpticalPowers        = []float64{-9, -13}
-	operationalModeFlagCisco   = flag.Int("operational_mode", 5003, "vendor-specific operational-mode for the channel")
-	operationalModeFlagArista  = flag.Int("operational_mode", 1, "vendor-specific operational-mode for the channel")
-	operationalModeFlagDefault = flag.Int("operational_mode", 1, "default operational-mode for the channel")
-	operationalMode            uint16
+	operationalModeFlag = flag.Int("operational_mode", 0, "vendor-specific operational-mode for the channel")
 )
 
 func TestMain(m *testing.M) {
@@ -40,16 +37,7 @@ func TestMain(m *testing.M) {
 func TestOpticalPower(t *testing.T) {
 	dut := ondatra.DUT(t, "dut")
 	fptest.ConfigureDefaultNetworkInstance(t, dut)
-
-	switch dut.Vendor() {
-	case ondatra.CISCO:
-		operationalMode = uint16(*operationalModeFlagCisco)
-	case ondatra.ARISTA:
-		operationalMode = uint16(*operationalModeFlagArista)
-	default:
-		operationalMode = uint16(*operationalModeFlagDefault)
-	}
-	cfgplugins.Initialize(operationalMode)
+	cfgplugins.Initialize(operationalModeFlag, dut)
 	cfgplugins.InterfaceConfig(t, dut, dut.Port(t, "port1"))
 	cfgplugins.InterfaceConfig(t, dut, dut.Port(t, "port2"))
 
