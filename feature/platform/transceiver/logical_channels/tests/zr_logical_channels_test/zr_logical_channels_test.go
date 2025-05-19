4affbbec1f6d6833ec1ce13f6b3f4e233583f5ac
AmrNJ <155722765+AmrNJ@users.noreply.github.com>
2025-05-19 15:56:07 +0530

    Update zr_logical_channels_test.go

--- feature/platform/transceiver/logical_channels/tests/zr_logical_channels_test/zr_logical_channels_test.go
+++ feature/platform/transceiver/logical_channels/tests/zr_logical_channels_test/zr_logical_channels_test.go
@@ -28,7 +28,9 @@ const (
 )
 
 var (
-	operationalModeFlag = flag.Int("operational_mode", 0, "vendor-specific operational-mode for the channel")
+	operationalModeFlag  = flag.Int("operational_mode", 0, "Vendor-specific operational-mode for the channel.")
+	operationalModeValue uint16
+	operationalMode      uint16
 )
 
 type testcase struct {
@@ -48,7 +50,9 @@ func Test400ZRLogicalChannels(t *testing.T) {
 	p2 := dut.Port(t, "port2")
 
 	fptest.ConfigureDefaultNetworkInstance(t, dut)
-	cfgplugins.Initialize(operationalModeFlag, dut)
+	operationalModeValue = uint16(*operationalModeFlag)
+	cfgplugins.Initialize(t, dut, operationalModeValue)
+	operationalMode = cfgplugins.GetOpMode()
 
 	cfgplugins.InterfaceConfig(t, dut, p1)
 	cfgplugins.InterfaceConfig(t, dut, p2)
