51185f812c95f762f3b993da2a4faa5212372064
AmrNJ <155722765+AmrNJ@users.noreply.github.com>
2025-05-20 10:15:29 +0530

    Update zr_inventory_test.go

--- feature/platform/transceiver/inventory/tests/zr_inventory_test/zr_inventory_test.go
+++ feature/platform/transceiver/inventory/tests/zr_inventory_test/zr_inventory_test.go
@@ -21,8 +21,7 @@ const (
 )
 
 var (
-	operationalModeFlag  = flag.Int("operational_mode", 0, "Vendor-specific operational-mode for the channel.")
-	operationalModeValue uint16
+	operationalModeFlag  = flag.Int("operational_mode", 0, "vendor-specific operational-mode for the channel.")
 	operationalMode      uint16
 )
 
@@ -64,9 +63,8 @@ func TestInventory(t *testing.T) {
 	dp1 := dut.Port(t, "port1")
 	dp2 := dut.Port(t, "port2")
 	fptest.ConfigureDefaultNetworkInstance(t, dut)
-	operationalModeValue = uint16(*operationalModeFlag)
-	cfgplugins.Initialize(t, dut, operationalModeValue)
-	operationalMode = cfgplugins.GetOpMode()
+	operationalMode = uint16(*operationalModeFlag)
+	cfgplugins.Initialize(t, dut, operationalMode)
 	cfgplugins.InterfaceConfig(t, dut, dp1)
 	cfgplugins.InterfaceConfig(t, dut, dp2)
 
