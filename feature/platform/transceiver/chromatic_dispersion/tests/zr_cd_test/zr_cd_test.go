911f5f6a2974840d52b6acf5984d67d82e59a46f
AmrNJ <155722765+AmrNJ@users.noreply.github.com>
2025-05-20 10:12:54 +0530

    Update zr_cd_test.go

--- feature/platform/transceiver/chromatic_dispersion/tests/zr_cd_test/zr_cd_test.go
+++ feature/platform/transceiver/chromatic_dispersion/tests/zr_cd_test/zr_cd_test.go
@@ -32,7 +32,7 @@ const (
 var (
 	frequencies          = []uint64{191400000, 196100000} // 400ZR OIF wavelength range
 	targetOutputPowers   = []float64{-13, -9}             // 400ZR OIF Tx power range
-	operationalModeFlag  = flag.Int("operational_mode", 0, "Vendor-specific operational-mode for the channel.")
+	operationalModeFlag  = flag.Int("operational_mode", 0, "vendor-specific operational-mode for the channel.")
 	operationalMode      uint16
 )
 
