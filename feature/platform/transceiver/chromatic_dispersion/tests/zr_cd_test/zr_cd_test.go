367d4e455c4a7c1bfffee76213c1e261257fea89
AmrNJ <155722765+AmrNJ@users.noreply.github.com>
2025-05-20 10:11:26 +0530

    Update zr_cd_test.go

--- feature/platform/transceiver/chromatic_dispersion/tests/zr_cd_test/zr_cd_test.go
+++ feature/platform/transceiver/chromatic_dispersion/tests/zr_cd_test/zr_cd_test.go
@@ -33,7 +33,6 @@ var (
 	frequencies          = []uint64{191400000, 196100000} // 400ZR OIF wavelength range
 	targetOutputPowers   = []float64{-13, -9}             // 400ZR OIF Tx power range
 	operationalModeFlag  = flag.Int("operational_mode", 0, "Vendor-specific operational-mode for the channel.")
-	operationalModeValue uint16
 	operationalMode      uint16
 )
 
@@ -100,9 +99,8 @@ func TestCDValue(t *testing.T) {
 
 	dp1 := dut.Port(t, "port1")
 	dp2 := dut.Port(t, "port2")
-	operationalModeValue = uint16(*operationalModeFlag)
-	cfgplugins.Initialize(t, dut, operationalModeValue)
-	operationalMode = cfgplugins.GetOpMode()
+	operationalMode = uint16(*operationalModeFlag)
+	cfgplugins.Initialize(t, dut, operationalMode)
 	cfgplugins.InterfaceConfig(t, dut, dp1)
 	cfgplugins.InterfaceConfig(t, dut, dp2)
 
