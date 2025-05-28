dbfbda1c73520bd96ab655e3c3c2055ab62d6ee8
arvbaska1 <123760606+arvbaska1@users.noreply.github.com>
2025-05-27 23:56:25 -0400

    zrp_inventory_test | set config container leaf for transceiver enable (#4188)
    * set config container leaf for transciver enable
    
    * remove
    
    * change transciever power on to use existing var

--- feature/platform/transceiver/inventory/tests/zrp_inventory_test/zrp_inventory_test.go
+++ feature/platform/transceiver/inventory/tests/zrp_inventory_test/zrp_inventory_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/openconfig/ondatra"
 	"github.com/openconfig/ondatra/gnmi"
 	"github.com/openconfig/ondatra/gnmi/oc"
+	"github.com/openconfig/ygot/ygot"
 )
 
 const (
@@ -145,7 +146,7 @@ func TestInventoryTransceiverOnOff(t *testing.T) {
 	dp1 := dut.Port(t, "port1")
 	dp2 := dut.Port(t, "port2")
 	tr1 := gnmi.Get(t, dut, gnmi.OC().Interface(dp1.Name()).Transceiver().State())
-	// tr2 := gnmi.Get(t, dut, gnmi.OC().Interface(dp2.Name()).Transceiver().State())
+	tr2 := gnmi.Get(t, dut, gnmi.OC().Interface(dp2.Name()).Transceiver().State())
 	och1 := components.OpticalChannelComponentFromPort(t, dut, dp1)
 	och2 := components.OpticalChannelComponentFromPort(t, dut, dp2)
 	fptest.ConfigureDefaultNetworkInstance(t, dut)
@@ -186,6 +187,10 @@ func TestInventoryTransceiverOnOff(t *testing.T) {
 		// for transceiver disable, the input needs to be the transceiver name instead of the interface name
 		tr := gnmi.Get(t, dut, gnmi.OC().Interface(p.Name()).Transceiver().State())
 		gnmi.Update(t, dut, gnmi.OC().Component(p.Name()).Name().Config(), p.Name())
+		setConfigLeaf := gnmi.OC().Component(tr)
+		gnmi.Update(t, dut, setConfigLeaf.Config(), &oc.Component{
+			Name: ygot.String(tr),
+		})
 		gnmi.Update(t, dut, gnmi.OC().Component(tr).Transceiver().Enabled().Config(), false)
 	}
 	t.Logf("Interfaces are down: %v, %v", dp1.Name(), dp2.Name())
@@ -193,11 +198,10 @@ func TestInventoryTransceiverOnOff(t *testing.T) {
 
 	time.Sleep(3 * waitInterval)
 	//  power on interface transceiver.
-	for _, p := range dut.Ports() {
-		tr := gnmi.Get(t, dut, gnmi.OC().Interface(p.Name()).Transceiver().State())
-		gnmi.Update(t, dut, gnmi.OC().Component(p.Name()).Name().Config(), p.Name())
-		gnmi.Update(t, dut, gnmi.OC().Component(tr).Transceiver().Enabled().Config(), true)
-	}
+	gnmi.Update(t, dut, gnmi.OC().Component(dp1.Name()).Name().Config(), dp1.Name())
+	gnmi.Update(t, dut, gnmi.OC().Component(tr1).Transceiver().Enabled().Config(), true)
+	gnmi.Update(t, dut, gnmi.OC().Component(dp2.Name()).Name().Config(), dp2.Name())
+	gnmi.Update(t, dut, gnmi.OC().Component(tr2).Transceiver().Enabled().Config(), true)
 	// Wait for channels to be up.
 	gnmi.Await(t, dut, gnmi.OC().Interface(dp1.Name()).OperStatus().State(), timeout, oc.Interface_OperStatus_UP)
 	gnmi.Await(t, dut, gnmi.OC().Interface(dp2.Name()).OperStatus().State(), timeout, oc.Interface_OperStatus_UP)
