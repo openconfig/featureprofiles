db23bf10b2722f6ee4b37f206661adce4c16fc08
mnp-juniper <mnp@juniper.net>
2025-06-11 09:25:14 -0700

    fixed formatting

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -1456,6 +1456,8 @@ func BGPSetMedActionUnsupported(dut *ondatra.DUTDevice) bool {
 // UseOldOCPathStaticLspNh returns true if the old OC path for static lsp next-hop is used
 func UseOldOCPathStaticLspNh(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetUseOldOcPathStaticLspNh()
+}
+
 // Config Leaf Create required before GNMI get
 func ConfigLeafCreateRequired(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetConfigLeafCreateRequired()
