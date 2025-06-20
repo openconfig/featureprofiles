122f6e08e3c14e7d2c438a9b61d2c701e62a2f91
arvbaska <arvbaska@cisco.com>
2025-06-20 19:55:42 -0400

    add readme doc

---
+++ feature/cisco/forwarding/loadbalancing/README.md
@@ -0,0 +1,51 @@
+# Loadbalancing tests using Google DVT topology
+
+## Testbed type
+DVT SIM topology Testbed ID: DVT-SIM-TOPO
+
+## Summary
+
+This test suite is to test Loadbalancing tests using Google DVT topology,
+with different hashing parameters 
+    * algorithm-adjust
+    * RTF profiles/swizzle
+    * extended-entropy
+    * ECMP/SPA seed
+
+## Procedure
+
+*   TODO
+
+
+## Feature Info and documents 
+```
+FEAT: FEAT-37197 Google: Avoid Polarization via Dupe+Masking of fields
+SFS/Scoping Doc: https://cisco-my.sharepoint.com/:x:/p/arvbaska/ETLBkY66uNpCgSkmxKQj7YIBC8F636MazdoROFayce1ixg
+WIKI/TOI: https://wiki.cisco.com/display/SFFWD/Spitfire+CEF+Load+balancing%3A+Per+NPU+Hash+Rotate+feature
+MISC: 
+```
+
+## OpenConfig Path and RPC Coverage
+
+Not applicable
+
+```yaml
+paths:
+  ## Config Paths ##
+
+  ## State Paths ##
+
+
+rpcs:
+  gnmi:
+    gNMI.Subscribe:
+```
+
+## Control Protocol Coverage
+
+BGP
+IS-IS
+
+## Minimum DUT Platform Requirement
+
+8000 Q200
--- feature/cisco/forwarding/loadbalancing/hashing_test.go
+++ feature/cisco/forwarding/loadbalancing/hashing_test.go
@@ -57,7 +57,12 @@ const (
 
 var (
 	trafficTolerance = 0.02
-	dutPort1         = attrs.Attributes{
+	rSiteV4Prefix    = "10.240.118.50"
+	eSiteV4Prefix    = "10.240.118.35"
+	rSiteV6Prefix    = "2001:0db8::10:240:118:50"
+	eSiteV6Prefix    = "2001:0db8::10:240:118:35"
+
+	dutPort1 = attrs.Attributes{
 		Name:    "port1",
 		Desc:    "dutPort1",
 		MAC:     "00:aa:00:bb:00:cc",
@@ -127,9 +132,9 @@ var (
 		DstMacAddress:     dutPort1.MAC,
 		OuterProtocolType: "IPv4",
 		OuterSrcStart:     "192.1.1.1",
-		OuterDstStart:     "10.240.118.35",
+		OuterDstStart:     eSiteV4Prefix,
 		OuterSrcStep:      "0.0.0.1",
-		OuterSrcFlowCount: 1,
+		OuterSrcFlowCount: 50000,
 		OuterDstFlowCount: 1,
 		OuterDstStep:      "0.0.0.1",
 		OuterDSCP:         10,
@@ -142,7 +147,7 @@ var (
 		L4SrcPortStart:    1000,
 		L4DstPortStart:    2000,
 		L4FlowStep:        1,
-		L4FlowCount:       10,
+		L4FlowCount:       50000,
 		TrafficPPS:        100,
 		PacketSize:        128,
 	}
@@ -151,9 +156,9 @@ var (
 		DstMacAddress:     dutPort2.MAC,
 		OuterProtocolType: "IPv4",
 		OuterSrcStart:     "192.0.2.6",
-		OuterDstStart:     "10.240.118.50",
+		OuterDstStart:     rSiteV4Prefix,
 		OuterSrcStep:      "0.0.0.1",
-		OuterSrcFlowCount: 1,
+		OuterSrcFlowCount: 50000,
 		OuterDstFlowCount: 1,
 		OuterDstStep:      "0.0.0.1",
 		OuterDSCP:         10,
@@ -166,17 +171,34 @@ var (
 		L4SrcPortStart:    1000,
 		L4DstPortStart:    2000,
 		L4FlowStep:        1,
-		L4FlowCount:       10,
+		L4FlowCount:       50000,
 		TrafficPPS:        100,
 		PacketSize:        128,
 	}
 )
 
 func TestWANLinksLoadBalancing(t *testing.T) {
-	dut1R := ondatra.DUT(&testing.T{}, "dut1")
-	// dut1E := ondatra.DUT(&testing.T{}, "dut5")
-	dut1E := ondatra.DUT(t, "B4E1")
-	dutList2 := []*ondatra.DUTDevice{dut1R, dut1E}
+	// DUT var for R, E, V and Jupiter nodes
+	dut1R := ondatra.DUT(t, "1R")
+	dut2R := ondatra.DUT(t, "2R")
+	dut3R := ondatra.DUT(t, "3R")
+	dut4R := ondatra.DUT(t, "4R")
+	dut1E := ondatra.DUT(t, "1E")
+	dut2E := ondatra.DUT(t, "2E")
+	dut1V := ondatra.DUT(t, "1V")
+	dut2V := ondatra.DUT(t, "2V")
+	dutJupiterE := ondatra.DUT(t, "Jupiter_R")
+	dutJupiterR := ondatra.DUT(t, "Jupiter_E")
+	siteRDUTList := []*ondatra.DUTDevice{dut1R, dut2R, dut3R, dut4R}
+	siteEDUTList := []*ondatra.DUTDevice{dut1E, dut2E}
+	siteVDUTList := []*ondatra.DUTDevice{dut1V, dut2V}
+	jupiterDUTList := []*ondatra.DUTDevice{dutJupiterE, dutJupiterR}
+	//Just to use variable and compile
+	t.Log("siteRDUTList", siteRDUTList)
+	t.Log("siteEDUTList", siteEDUTList)
+	t.Log("siteVDUTList", siteVDUTList)
+	t.Log("jupiterDUTList", jupiterDUTList)
+	dvtCiscoDUTList := []*ondatra.DUTDevice{dut1R, dut2R, dut3R, dut4R, dut1E, dut2E}
 	tgenParam := helper.TgenConfigParam{
 		DutIntfAttr:      []attrs.Attributes{dutPort1, dutPort2},
 		TgenIntfAttr:     []attrs.Attributes{atePort1, atePort2},
@@ -219,59 +241,6 @@ func TestWANLinksLoadBalancing(t *testing.T) {
 	t.Log("InputIF", InputIF, OutputIFWeight)
 
 	//Verify Traffic stats on InputIF and match with OutputIF
-
-	// pfxNH, NHG := helper.FIB.GetPrefixAFTNH(t, dut1E, "10.240.118.35/32", deviations.DefaultNetworkInstance(dut1E))
-	// t.Log("pfxNH", pfxNH)
-	// t.Log("NHIP", NHIP)
-	// helper.Loadbalancing.GetPrefixOutGoingInterfaces(t, dut1E, "10.240.118.0/24", deviations.DefaultNetworkInstance(dut1E))
-	// util.ClearInterfaceCountersAll(t, dut1E)
-	// util.CheckDUTTrafficViaInterfaceTelemetry(t, dut1E, []string{"Bundle-Ether1", "Bundle-Ether2", "Bundle-Ether3", "Bundle-Ether4", "Bundle-Ether5"}, []string{"Bundle-Ether14", "Bundle-Ether15"}, []float64{0.5, 0.5}, 2)
-	// t.Logf("Get list of LCs")
-	// lcList = util.GetLCList(t, dut)
-	// cases := []testCase{
-	// 	{
-	// 		name:    "Test perNP CLI config for 1 NP",
-	// 		desc:    "Configure per-NP hash value on single NP0 for all LCs",
-	// 		npval:   npList[:1],
-	// 		hashval: rand.Intn(34) + 1, // valid range (1-35]
-	// 		lcloc:   lcList,
-	// 	},
-	// 	{
-	// 		name:    "Test perNP CLI config for All NPs",
-	// 		desc:    "Configure per-NP hash value on All the NPs for all LCs",
-	// 		npval:   npList,
-	// 		hashval: rand.Intn(34) + 1,
-	// 		lcloc:   lcList,
-	// 	},
-	// }
-	// for _, tc := range cases {
-	// 	tc := tc
-	// 	t.Run(tc.name, func(t *testing.T) {
-	// 		for _, lc := range tc.lcloc {
-	// 			for _, np := range tc.npval {
-	// 				setPerNPHashConfig(t, dut, tc.hashval, np, lc, true)
-	// 				defer setPerNPHashConfig(t, dut, tc.hashval, np, lc, false)
-	// 				if got, want := getPerLCPerNPHashVal(t, dut, np, lc), verifyPerNPHashCLIVal(tc.hashval); got != want {
-	// 					t.Errorf("per-NP hash rotate value for LC %v NP%v is not per calculation got %v, want %v", lc, tc.npval, got, want)
-	// 				}
-	// 			}
-	// 		}
-	// 	})
-	// }
-
-	// var setExtendedEntropy *extendedEntropyCLIOptions
-	// var setAlgorithmAdjust *algorithmAdjustCLIOptions
-
-	// //set extended entropy CLI options
-	// setExtendedEntropy.perChasiss = "cef platform load-balancing extended-entropy auto-global"
-	// setExtendedEntropy.perNPU = "cef platform load-balancing extended-entropy auto-instance"
-	// setExtendedEntropy.specificVal = fmt.Sprintf("cef platform load-balancing extended-entropy profile-index %s", rand.Intn(215))
-
-	// //set algorithm adjust CLI options
-	// setAlgorithmAdjust.perChasiss = "cef platform load-balancing algorithm adjust auto-global"
-	// setAlgorithmAdjust.perNPU = "cef platform load-balancing algorithm adjust auto-instance"
-	// setAlgorithmAdjust.specificVal = fmt.Sprintf("cef platform load-balancing algorithm adjust %s", rand.Intn(255))
-
 	cases := []testCase{
 		{
 			name: "Default",
@@ -281,32 +250,12 @@ func TestWANLinksLoadBalancing(t *testing.T) {
 			name: "Both auto-global",
 			desc: "auto-global Hash parameters for both Extended Entropy and Algorithm Adjust",
 			extendedEntropyOption: func(t *testing.T, hashParameter, optionType string, dutList []*ondatra.DUTDevice) {
-				configureOptions(t, "extendedEntropyOption", "perChassis", dutList2)
+				configureOptions(t, "extendedEntropyOption", "perChassis", dvtCiscoDUTList)
 			},
 			algorithmAdjustOption: func(t *testing.T, hashParameter, optionType string, dutList []*ondatra.DUTDevice) {
-				configureOptions(t, "algorithmAdjustOption", "perChassis", dutList2)
+				configureOptions(t, "algorithmAdjustOption", "perChassis", dvtCiscoDUTList)
 			},
 		},
-		// {
-		// 	name: "Decap with NO DSCP match & Mixed Prefix Length Decap gRIBI Entries",
-		// 	desc: "match on source and protocol, no match on DSCP; flow VRF_DECAP hit -> DEFAULT",
-		// 	fn:   testMixDecapNoDscpMatch,
-		// },
-		// {
-		// 	name: "Tunneled traffic with NO Decap",
-		// 	desc: "IPinIP tunneled traffic recived on cluster interfaces are sent to TE VRF when no match in DECAP VRF",
-		// 	fn:   testTunnelWithNoDecap,
-		// },
-		// {
-		// 	name: "TE Disabled Default class match",
-		// 	desc: "TE disabled IPinIP/IP cluster traffic arriving on WAN facing ports > Send to Default class",
-		// 	fn:   testTEDisabledTraffic,
-		// },
-		// {
-		// 	name: "Decap and encap",
-		// 	desc: "Decap and then match route in post-Decap Encap VRF >> Traffic forwarded out with Encap header",
-		// 	fn:   testDecapEncap,
-		// },
 	}
 	for _, tt := range cases {
 		t.Run(tt.name, func(t *testing.T) {
