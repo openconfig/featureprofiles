8677fc0ae662304dcfbf5c5dec96895bb0969d89
Rohit Rattan <rohitrattan@google.com>
2025-06-23 05:33:13 +0000

    applying fix to isissessions

--- internal/isissession/isissession.go
+++ internal/isissession/isissession.go
@@ -196,13 +196,25 @@ func New(t testing.TB) (*TestSession, error) {
 	if err != nil {
 		return nil, fmt.Errorf("unable to connect to gNMI on %v: %w", s.DUT, err)
 	}
+	s.DUTConf = &oc.Root{}
 	s.DUTPort1 = s.DUT.Port(t, "port1")
 	s.DUTPort2 = s.DUT.Port(t, "port2")
-	s.DUTConf = &oc.Root{}
 	// configure dut ports
-	DUTISISAttrs.ConfigOCInterface(s.DUTConf.GetOrCreateInterface(s.DUTPort1.Name()), s.DUT)
-	DUTTrafficAttrs.ConfigOCInterface(s.DUTConf.GetOrCreateInterface(s.DUTPort2.Name()), s.DUT)
+	dutIntf1 := s.DUTConf.GetOrCreateInterface(s.DUTPort1.Name())
+	dutIntf2 := s.DUTConf.GetOrCreateInterface(s.DUTPort2.Name())
 
+	for port, dutIntf := range map[*ondatra.Port]*oc.Interface{
+		s.DUTPort1: dutIntf1,
+		s.DUTPort2: dutIntf2,
+	} {
+		if deviations.FrBreakoutFix(s.DUT) && port.PMD() == ondatra.PMD100GBASEFR {
+			dutIntf.GetOrCreateEthernet().AutoNegotiate = ygot.Bool(false)
+			dutIntf.GetOrCreateEthernet().DuplexMode = oc.Ethernet_DuplexMode_FULL
+			dutIntf.GetOrCreateEthernet().PortSpeed = oc.IfEthernet_ETHERNET_SPEED_SPEED_100GB
+		}
+		DUTISISAttrs.ConfigOCInterface(dutIntf1, s.DUT)
+		DUTTrafficAttrs.ConfigOCInterface(dutIntf2, s.DUT)
+	}
 	// If there is no ate, any operation that requires the ATE will call
 	// t.Fatal() instead. This is helpful for debugging the parts of the test
 	// that don't use an ATE.
