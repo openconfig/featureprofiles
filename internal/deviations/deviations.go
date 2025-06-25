a9b6cc1449bafc7ce75ca31246fa4a91009e8f41
Fernando Molina Rodriguez <fmolinar@cisco.com>
2025-06-25 00:05:09 -0700

    PLT-1.1 (#4280)
    * PLT-1.1 adding deviations breakout_group_index_0 and num_physical_channels_unsupported
    
    * changing to vendor switch from deviation
    
    ---------
    
    Co-authored-by: Nikki Kumar <nikkikumar@google.com>

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -1463,6 +1463,11 @@ func BGPSetMedActionUnsupported(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetBgpSetMedActionUnsupported()
 }
 
+// NumPhysyicalChannelsUnsupported returns true if there's no OC support for num-physical-channels
+func NumPhysyicalChannelsUnsupported(dut *ondatra.DUTDevice) bool {
+	return lookupDUTDeviations(dut).GetNumPhysicalChannelsUnsupported()
+}
+
 // UseOldOCPathStaticLspNh returns true if the old OC path for static lsp next-hop is used
 func UseOldOCPathStaticLspNh(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetUseOldOcPathStaticLspNh()
