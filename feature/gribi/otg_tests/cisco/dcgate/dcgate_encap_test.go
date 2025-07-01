eae7fa7a8517cf9615b195f094dbe91856a9b9f0
Arvindan Baskaran (arvbaska) <arvbaska@cisco.com>
2025-07-01 15:39:07 -0400

    Update dcgate_encap_test.go

--- feature/gribi/otg_tests/cisco/dcgate/dcgate_encap_test.go
+++ feature/gribi/otg_tests/cisco/dcgate/dcgate_encap_test.go
@@ -130,6 +130,7 @@ func TestBasicEncap(t *testing.T) {
 	for _, tc := range test {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Logf("Name: %s", tc.name)
+			t.Logf("ARVBASKA")
 			if tc.skip {
 				t.SkipNow()
 			}
