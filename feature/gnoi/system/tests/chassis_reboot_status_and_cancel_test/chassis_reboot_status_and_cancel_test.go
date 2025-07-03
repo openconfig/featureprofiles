c5b128adca8a9c9f8f062226e440ef49067accd7
Swetha-haridasula <haridasula@google.com>
2025-07-03 16:22:02 +0530

    Update chassis_reboot_status_and_cancel_test.go

--- feature/gnoi/system/tests/chassis_reboot_status_and_cancel_test/chassis_reboot_status_and_cancel_test.go
+++ feature/gnoi/system/tests/chassis_reboot_status_and_cancel_test/chassis_reboot_status_and_cancel_test.go
@@ -16,20 +16,71 @@ package chassis_reboot_status_and_cancel_test
 
 import (
 	"context"
+	"fmt"
+	"regexp"
 	"testing"
+	"time"
 
+	"github.com/openconfig/ygot/ygot"
+	"github.com/openconfig/featureprofiles/internal/attrs"
 	"github.com/openconfig/featureprofiles/internal/components"
 	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/featureprofiles/internal/fptest"
+	fpb "github.com/openconfig/gnoi/file"
 	spb "github.com/openconfig/gnoi/system"
 	tpb "github.com/openconfig/gnoi/types"
+	"github.com/openconfig/gnoigo"
+	"github.com/openconfig/ondatra/gnmi"
 	"github.com/openconfig/ondatra"
+	"github.com/openconfig/ondatra/netutil"
 	"github.com/openconfig/ondatra/gnmi/oc"
 )
 
 const (
 	oneMinuteInNanoSecond = 6e10
 	rebootDelay           = 120
+	controlcardType       = oc.PlatformTypes_OPENCONFIG_HARDWARE_COMPONENT_CONTROLLER_CARD
+	setRequestTimeout     = 30 * time.Second
+	IPv4PrefixLen         = 30
+	IPv6PrefixLen         = 126
+	localASN              = 65535
+	peerASN               = 65536
+	bgpPeerGrpName        = "BGP-PEER-GROUP"
+	globalRouterID        = "192.168.1.1"
+	sleepTimeBtwAttempts  = 1 * time.Second
+	maxResponseTime       = 30 * time.Second
+	getRequestTimeout     = 30 * time.Second
+)
+
+// configParams holds the parameters for the OpenConfig configuration
+type configParams struct {
+	NumLAGInterfaces            int
+	NumEthernetInterfacesPerLAG int
+	NumBGPNeighbors             int
+}
+
+type activeStandByControllerCards struct {
+	activeRP  string
+	standbyRP string
+}
+
+type setRequest func(t *testing.T, dut *ondatra.DUTDevice) error
+
+var (
+	numPorts           int
+	params             configParams
+	vendorCoreFilePath = map[ondatra.Vendor]string{
+		ondatra.JUNIPER: "/var/core/",
+		ondatra.CISCO:   "/misc/disk1/",
+		ondatra.NOKIA:   "/var/core/",
+		ondatra.ARISTA:  "/var/core/",
+	}
+	vendorCoreFileNamePattern = map[ondatra.Vendor]*regexp.Regexp{
+		ondatra.JUNIPER: regexp.MustCompile(".*.tar.gz"),
+		ondatra.CISCO:   regexp.MustCompile("/misc/disk1/.*core.*"),
+		ondatra.NOKIA:   regexp.MustCompile("/var/core/coredump-.*"),
+		ondatra.ARISTA:  regexp.MustCompile("/var/core/core.*"),
+	}
 )
 
 func TestMain(m *testing.M) {
@@ -61,6 +112,7 @@ func TestMain(m *testing.M) {
 //
 
 func TestRebootStatus(t *testing.T) {
+	t.Skipf("b/408031530")
 	dut := ondatra.DUT(t, "dut")
 	gnoiClient := dut.RawAPIs().GNOI(t)
 
@@ -135,6 +187,7 @@ func TestRebootStatus(t *testing.T) {
 }
 
 func TestCancelReboot(t *testing.T) {
+	t.Skipf("b/408031530")
 	dut := ondatra.DUT(t, "dut")
 	gnoiClient := dut.RawAPIs().GNOI(t)
 
@@ -189,73 +242,84 @@ func TestCancelReboot(t *testing.T) {
 	}
 }
 
+func getSubCompPath(t *testing.T, dut *ondatra.DUTDevice) *tpb.Path {
+	t.Helper()
+	controllerCards := components.FindComponentsByType(t, dut, oc.PlatformTypes_OPENCONFIG_HARDWARE_COMPONENT_CONTROLLER_CARD)
+	if len(controllerCards) == 0 {
+		t.Fatal("No controller card components found in DUT.")
+	}
+	activeRP := controllerCards[0]
+	if len(controllerCards) == 2 {
+		_, activeRP = components.FindStandbyControllerCard(t, dut, controllerCards)
+	}
+	useNameOnly := deviations.GNOISubcomponentPath(dut)
+	return components.GetSubcomponentPath(activeRP, useNameOnly)
+}
 func TestRebootPlusConfigPush(t *testing.T) {
 	dut := ondatra.DUT(t, "dut")
+	timestamp := uint64(time.Now().UTC().Unix())
 	gnoiClient := dut.RawAPIs().GNOI(t)
 	LargeConfigPush(t)
-	cases := []struct {
+	tc := struct {
 		desc          string
 		rebootRequest *spb.RebootRequest
 		rebootActive  bool
 		cancelReboot  bool
 	}{
-		{
-			desc: "reboot requested without delay",
-			rebootRequest: &spb.RebootRequest{
-				Method:  spb.RebootMethod_COLD,
-				Delay:   0,
-				Message: "Reboot chassis without delay",
-				Force:   true,
-			},
-			rebootActive: true,
+		desc: "reboot requested without delay",
+		rebootRequest: &spb.RebootRequest{
+			Method:  spb.RebootMethod_COLD,
+			Delay:   0,
+			Message: "Reboot chassis without delay",
+			Force:   true,
 		},
+		rebootActive: true,
 	}
 
 	statusReq := &spb.RebootStatusRequest{Subcomponents: []*tpb.Path{}}
 	if !deviations.GNOIStatusWithEmptySubcomponent(dut) {
 		statusReq.Subcomponents = append(statusReq.Subcomponents, getSubCompPath(t, dut))
 	}
-	for _, tc := range cases {
-		t.Run(tc.desc, func(t *testing.T) {
-			if tc.rebootRequest != nil {
-				t.Logf("Send reboot request: %v", tc.rebootRequest)
-				rebootResponse, err := gnoiClient.System().Reboot(context.Background(), tc.rebootRequest)
-				defer gnoiClient.System().CancelReboot(context.Background(), &spb.CancelRebootRequest{})
-				t.Logf("Got reboot response: %v, err: %v", rebootResponse, err)
-				if err != nil {
-					t.Fatalf("Failed to request reboot with unexpected err: %v", err)
-				}
-			}
-			resp, err := gnoiClient.System().RebootStatus(context.Background(), statusReq)
-			t.Logf("DUT rebootStatus: %v, err: %v", resp, err)
+	t.Run(tc.desc, func(t *testing.T) {
+		if tc.rebootRequest != nil {
+			t.Logf("Send reboot request: %v", tc.rebootRequest)
+			rebootResponse, err := gnoiClient.System().Reboot(context.Background(), tc.rebootRequest)
+			defer gnoiClient.System().CancelReboot(context.Background(), &spb.CancelRebootRequest{})
+			t.Logf("Got reboot response: %v, err: %v", rebootResponse, err)
 			if err != nil {
-				t.Fatalf("Failed to get reboot status with unexpected err: %v", err)
+				t.Fatalf("Failed to request reboot with unexpected err: %v", err)
 			}
-			if resp.GetActive() != tc.rebootActive {
-				t.Errorf("resp.GetActive(): got %v, want %v", resp.GetActive(), tc.rebootActive)
-			}
-
-			if tc.rebootRequest != nil {
-				if resp.GetReason() != tc.rebootRequest.GetMessage() {
-					t.Errorf("resp.GetReason(): got %v, want %v", resp.GetReason(), tc.rebootRequest.GetMessage())
-				}
-				if resp.GetWait() > tc.rebootRequest.GetDelay() {
-					t.Errorf("resp.GetWait(): got %v, want <= %v", resp.GetWait(), tc.rebootRequest.GetDelay())
-				}
-				if resp.GetWhen() == 0 {
-					t.Errorf("resp.GetWhen(): got %v, want > 0", resp.GetWhen())
-				}
-			}
-		})
-
-		t.Logf("Cancel reboot request after the test")
-
-		rebootCancel, err := gnoiClient.System().CancelReboot(context.Background(), &spb.CancelRebootRequest{})
-		if err != nil {
-			t.Fatalf("Failed to cancel reboot with unexpected err: %v", err)
 		}
-		t.Logf("DUT CancelReboot response: %v, err: %v", rebootCancel, err)
+		resp, err := gnoiClient.System().RebootStatus(context.Background(), statusReq)
+		t.Logf("DUT rebootStatus: %v, err: %v", resp, err)
+		if err != nil {
+			t.Fatalf("Failed to get reboot status with unexpected err: %v", err)
+		}
+		if resp.GetActive() != tc.rebootActive {
+			t.Errorf("resp.GetActive(): got %v, want %v", resp.GetActive(), tc.rebootActive)
+		}
+
+		if tc.rebootRequest != nil {
+			if resp.GetReason() != tc.rebootRequest.GetMessage() {
+				t.Errorf("resp.GetReason(): got %v, want %v", resp.GetReason(), tc.rebootRequest.GetMessage())
+			}
+			if resp.GetWait() > tc.rebootRequest.GetDelay() {
+				t.Errorf("resp.GetWait(): got %v, want <= %v", resp.GetWait(), tc.rebootRequest.GetDelay())
+			}
+			if resp.GetWhen() == 0 {
+				t.Errorf("resp.GetWhen(): got %v, want > 0", resp.GetWhen())
+			}
+		}
+	})
+
+	t.Logf("Cancel reboot request after the test")
+
+	rebootCancel, err := gnoiClient.System().CancelReboot(context.Background(), &spb.CancelRebootRequest{})
+	if err != nil {
+		t.Fatalf("Failed to cancel reboot with unexpected err: %v", err)
 	}
+	t.Logf("DUT CancelReboot response: %v, err: %v", rebootCancel, err)
+	coreFileCheck(t, dut, gnoiClient, timestamp, true)
 }
 
 func LargeConfigPush(t *testing.T) {
@@ -267,11 +331,10 @@ func LargeConfigPush(t *testing.T) {
 	// Config the hostname to prevent the test failure when oc base config is not loaded
 	gnmi.Replace(t, dut, gnmi.OC().System().Hostname().Config(), "ondatraHost")
 	// Configuring the network instance as some devices only populate OC after configuration.
-	fptest.ConfigureDefaultNetworkInstance(t, dut)	
+	fptest.ConfigureDefaultNetworkInstance(t, dut)
 	ctx := context.Background()
-	t.Run("testLargeConfigSetRequest", func(t *testing.T) {
-		_ = sendSetRequest(ctx, t, dut, setConfig)
-	})
+	_ = sendSetRequest(ctx, t, dut, setConfig)
+
 }
 
 func setConfig(t *testing.T, dut *ondatra.DUTDevice) error {
@@ -306,7 +369,7 @@ func setConfig(t *testing.T, dut *ondatra.DUTDevice) error {
 
 	networkInterface := device.GetOrCreateNetworkInstance(deviations.DefaultNetworkInstance(dut))
 
-	isisProto := networkInterface.GetOrCreateProtocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_ISIS, "ISIS")
+	isisProto := networkInterface.GetOrCreateProtocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_ISIS, "DEFAULT")
 	isisProto.Enabled = ygot.Bool(true)
 	isis := isisProto.GetOrCreateIsis()
 	for _, agg := range aggIDs {
@@ -390,3 +453,57 @@ func sendSetRequest(ctx context.Context, t *testing.T, dut *ondatra.DUTDevice, s
 	}
 }
 
+// coreFileCheck function is used to check if cores are found on the DUT.
+func coreFileCheck(t *testing.T, dut *ondatra.DUTDevice, gnoiClient gnoigo.Clients, sysConfigTime uint64, retry bool) {
+	t.Helper()
+	t.Log("Checking for core files on DUT")
+
+	dutVendor := dut.Vendor()
+	// vendorCoreFilePath and vendorCoreProcName should be provided to fetch core file on dut.
+	if _, ok := vendorCoreFilePath[dutVendor]; !ok {
+		t.Fatalf("Please add support for vendor %v in var vendorCoreFilePath ", dutVendor)
+	}
+	if _, ok := vendorCoreFileNamePattern[dutVendor]; !ok {
+		t.Fatalf("Please add support for vendor %v in var vendorCoreFileNamePattern.", dutVendor)
+	}
+
+	in := &fpb.StatRequest{
+		Path: vendorCoreFilePath[dutVendor],
+	}
+	validResponse, err := gnoiClient.File().Stat(context.Background(), in)
+	if err != nil {
+		if retry {
+			t.Logf("Retry GNOI request to check %v for core files on DUT", vendorCoreFilePath[dutVendor])
+			validResponse, err = gnoiClient.File().Stat(context.Background(), in)
+		}
+		if err != nil {
+			t.Fatalf("Unable to stat path %v for core files on DUT, %v", vendorCoreFilePath[dutVendor], err)
+		}
+	}
+
+	// Check cores creation time is greater than test start time.
+	for _, fileStatsInfo := range validResponse.GetStats() {
+		if fileStatsInfo.GetLastModified() > sysConfigTime {
+			coreFileName := fileStatsInfo.GetPath()
+			r := vendorCoreFileNamePattern[dutVendor]
+			if r.MatchString(coreFileName) {
+				t.Errorf("INFO: Found core %v on DUT.", coreFileName)
+				t.Logf("INFO: Core file %v creation time is %v and it is greater than test start time %v", coreFileName, fileStatsInfo.GetLastModified(), sysConfigTime)
+			}
+		}
+		in = &fpb.StatRequest{
+			Path: fileStatsInfo.GetPath(),
+		}
+		validResponse, err := gnoiClient.File().Stat(context.Background(), in)
+		if err != nil {
+			t.Fatalf("Unable to stat path %v for core files on DUT, %v", vendorCoreFilePath[dutVendor], err)
+		}
+		for _, fileStatsInfo := range validResponse.GetStats() {
+			coreFileName := fileStatsInfo.GetPath()
+			r := vendorCoreFileNamePattern[dutVendor]
+			if r.MatchString(coreFileName) {
+				t.Logf("INFO: Found core %v on DUT.", coreFileName)
+			}
+		}
+	}
+}
