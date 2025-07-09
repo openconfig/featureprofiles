ab96125ac67e3407e13303128efdebf4819d7eb3
Avnish Singh <singhavnish@google.com>
2025-07-09 13:43:45 +0000

    Revert files used for testing

--- some_script.sh
+++
@@ -1,7 +0,0 @@
-readonly BASE="$(git merge-base origin/main "${HEAD}")"
-for path in $(git diff --name-only "${BASE}" "${HEAD}" | grep -E '^\W*feature' | sort -u); do
-  readme_file="${dir}/README.md"
-  if [[ "$file" == *README.md ]]; then
-    echo "$file"
-  fi
-done
