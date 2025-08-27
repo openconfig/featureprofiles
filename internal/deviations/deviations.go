e013ee9afd38d193dd93ebffb78f732f13a0439a
anilrao <anilrao@arista.com>
2025-08-27 07:18:05 -0700

    fix format of deviations.go with gofmt

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -1610,5 +1610,5 @@ func NetworkInstanceImportExportPolicyOCUnsupported(dut *ondatra.DUTDevice) bool
 
 // SkipOrigin returns true if the device does not support the 'origin' field in gNMI/gNOI RPC paths.
 func SkipOrigin(dut *ondatra.DUTDevice) bool {
-    return lookupDUTDeviations(dut).GetSkipOrigin()
+	return lookupDUTDeviations(dut).GetSkipOrigin()
 }
