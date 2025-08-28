051502c9d236eac4bc050f5760f1cc72ae00672d
asaggarw <ashna.aggarwal@keysight.com>
2025-08-28 09:49:29 +0000

    fixed ci/cd issues

--- feature/mpls/sr/otg_tests/srte_egress_node_forwarding/egress_node_forwading_test.go
+++ feature/mpls/sr/otg_tests/srte_egress_node_forwarding/egress_node_forwading_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package egressnodeforwardingtest
+package egressnodeforwarding_test
 
 import (
 	"fmt"
