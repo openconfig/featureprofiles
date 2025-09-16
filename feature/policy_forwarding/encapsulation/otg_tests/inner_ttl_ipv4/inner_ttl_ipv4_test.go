0cff4089210cbfd67ec928ab22302882dd620f6a
Mohan Reddy <40664949+keysightgems@users.noreply.github.com>
2025-09-16 16:17:34 +0530

    Update inner_ttl_ipv4_test.go

--- feature/policy_forwarding/encapsulation/otg_tests/inner_ttl_ipv4/inner_ttl_ipv4_test.go
+++ feature/policy_forwarding/encapsulation/otg_tests/inner_ttl_ipv4/inner_ttl_ipv4_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package ingressinnerttltest
+package inner_ttl_ipv4_test
 
 import (
 	"bytes"
