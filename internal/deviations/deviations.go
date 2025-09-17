7caf51e6bf4c9ed57fad70947edc24a4f77a807a
yini101 <86263319+yini101@users.noreply.github.com>
2025-09-16 23:07:40 -0700

    RT-7.5: Add deviation to do community REPLACE instead of REMOVE then ADD (#4301)
    Add `use_bgp_set_community_option_type_replace` deviation to change the community from REMOVE all communities and then ADD LBW community to REPLACE with LBW community.

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -1627,3 +1627,9 @@ func DecapsulateGueOCUnsupported(dut *ondatra.DUTDevice) bool {
 func LinePortUnsupported(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetLinePortUnsupported()
 }
+
+// UseBgpSetCommunityOptionTypeReplace returns true if BGP community set REPLACE
+// option is required
+func UseBgpSetCommunityOptionTypeReplace(dut *ondatra.DUTDevice) bool {
+	return lookupDUTDeviations(dut).GetUseBgpSetCommunityOptionTypeReplace()
+}
