a0ec65a680b91773ad3e865755a844ad26543788
geokamel-eg <geokamel@google.com>
2025-09-19 21:59:02 +0100

    Update isis_scale_test.go

--- feature/isis/otg_tests/isis_scale_test/isis_scale_test.go
+++ feature/isis/otg_tests/isis_scale_test/isis_scale_test.go
@@ -241,7 +241,7 @@ func configureDUT(t *testing.T, dut *ondatra.DUTDevice, dutData *dutData) {
 		gnmi.Await(t, dut, gnmi.OC().Interface(l.LagName).AdminStatus().State(), 30*time.Second, oc.Interface_AdminStatus_UP)
 	}
 	dutData.isisData.ISISInterfaceNames = createISISInterfaceNames(t, dut, dutData)
-	rootPath := cfgplugins.NewISIS(t, dut, dutData.isisData)
+	rootPath := cfgplugins.NewISISComplex(t, dut, dutData.isisData)
 	gnmi.Update(t, dut, gnmi.OC().Config(), rootPath)
 }
 
