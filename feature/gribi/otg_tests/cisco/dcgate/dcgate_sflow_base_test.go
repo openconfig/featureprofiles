def5a662b45c3d7d51dfadca6f6100c4093bc849
Prem Singh <sprem@cisco.com>
2025-09-26 15:21:18 +0530

    dumb mistake :P

--- feature/gribi/otg_tests/cisco/dcgate/dcgate_sflow_base_test.go
+++ feature/gribi/otg_tests/cisco/dcgate/dcgate_sflow_base_test.go
@@ -64,7 +64,7 @@ func configureLoopbackAndSFlow(t *testing.T, dut *ondatra.DUTDevice) {
 }
 
 func validateSflowCapture(t *testing.T, args *testArgs, ports []string, sfc *s.SflowAttr) {
-	if sfc == nil && !*capture_sflow {
+	if sfc == nil || !*capture_sflow {
 		return
 	}
 
