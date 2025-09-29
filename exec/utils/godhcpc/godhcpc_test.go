1e5db85f3d0c5394302397dee920ae5bc8956d37
Karim Jahed <kjahed@cisco.com>
2025-09-29 17:21:54 -0400

    vxr

--- exec/utils/godhcpc/godhcpc_test.go
+++ exec/utils/godhcpc/godhcpc_test.go
@@ -2,6 +2,7 @@ package godhcpc_test
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -48,13 +49,21 @@ func TestAddDHCPEntry(t *testing.T) {
 
 	mgmtIpAddress := ""
 	mgmtPrefixLength := uint8(0)
-	intfs := gnmi.GetAll(t, dut, gnmi.OC().InterfaceAny().State())
-	for _, intf := range intfs {
-		if intf.GetManagement() {
-			for _, subIntf := range intf.GetOrCreateSubinterfaceMap() {
-				for _, addr := range subIntf.GetOrCreateIpv4().GetOrCreateAddressMap() {
-					mgmtIpAddress = addr.GetIp()
-					mgmtPrefixLength = addr.GetPrefixLength()
+	gw := *gateway
+
+	if util.IsPlatformVXR(context.Background(), t, dut) {
+		mgmtIpAddress = "1.0.0.5"
+		mgmtPrefixLength = 24
+		gw = "1.0.0.2"
+	} else {
+		intfs := gnmi.GetAll(t, dut, gnmi.OC().InterfaceAny().State())
+		for _, intf := range intfs {
+			if intf.GetManagement() {
+				for _, subIntf := range intf.GetOrCreateSubinterfaceMap() {
+					for _, addr := range subIntf.GetOrCreateIpv4().GetOrCreateAddressMap() {
+						mgmtIpAddress = addr.GetIp()
+						mgmtPrefixLength = addr.GetPrefixLength()
+					}
 				}
 			}
 		}
@@ -65,7 +74,6 @@ func TestAddDHCPEntry(t *testing.T) {
 	}
 	t.Logf("Management IP address: %s", mgmtIpAddress)
 
-	gw := *gateway
 	if gw != "" {
 		t.Logf("Using configured gateway: %s", gw)
 	} else {
