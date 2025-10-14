7679244eb95aa13857460c82f02e680125da571f
Avnish Singh <singhavnish@google.com>
2025-10-14 09:23:34 +0000

    Fix formatting issues

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -1680,7 +1680,7 @@ func MatchAsPathSetUnsupported(dut *ondatra.DUTDevice) bool {
 func SameAfiSafiAndPeergroupPoliciesUnsupported(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetSameAfiSafiAndPeergroupPoliciesUnsupported()
 }
-	
+
 // SyslogOCUnsupported returns true if the device does not support syslog OC configuration for below OC paths.
 // '/system/logging/remote-servers/remote-server/config/network-instance'
 func SyslogOCUnsupported(dut *ondatra.DUTDevice) bool {
