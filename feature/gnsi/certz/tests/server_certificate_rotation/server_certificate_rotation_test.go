4ea07cd66c37447a7d8127b665a2cb3d90cfd577
priyacj <priyacj@juniper.net>
2025-10-14 09:04:11 -0700

    fixed package in go script

--- feature/gnsi/certz/tests/server_certificate_rotation/server_certificate_rotation_test.go
+++ feature/gnsi/certz/tests/server_certificate_rotation/server_certificate_rotation_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package servercertificaterotationtest
+package server_certificate_rotation_test 
 
 import (
 	"context"
