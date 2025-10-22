5c78ef80870ffe8da7c844e8a3ff86aac61a7f51
geokamel-eg <geokamel@google.com>
2025-10-22 20:43:00 +0100

    Update isis_scale_test.go

--- feature/isis/otg_tests/isis_scale_test/isis_scale_test.go
+++ feature/isis/otg_tests/isis_scale_test/isis_scale_test.go
@@ -19,7 +19,7 @@ import (
 	"github.com/openconfig/ondatra/gnmi"
 	"github.com/openconfig/ondatra/gnmi/oc"
 	"github.com/openconfig/ondatra/netutil"
-	"github.com/openconfig/ygnmi/ygnmi/ygnmi"
+	"github.com/openconfig/ygnmi/ygnmi"
 	"github.com/open-traffic-generator/snappi/gosnappi"
 )
 
