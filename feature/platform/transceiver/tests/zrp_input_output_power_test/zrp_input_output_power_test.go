6b1be3894bf5ac0e7d2289bd723d728335be67b1
snaragund <sham_r.naragund@nokia.com>
2025-10-23 21:56:45 +0530

    -Spacing fixed.
    "This code is a Contribution to the OpenConfig Feature Profiles project ("Work") made under the Google Software Grant and Corporate Contributor License Agreement ("CLA") and governed by the Apache License 2.0. No other rights or licenses in or to any of Nokia's intellectual property are granted for any other purpose. This code is provided on an "as is" basis without any warranties of any kind."

--- feature/platform/transceiver/tests/zrp_input_output_power_test/zrp_input_output_power_test.go
+++ feature/platform/transceiver/tests/zrp_input_output_power_test/zrp_input_output_power_test.go
@@ -38,13 +38,13 @@ func TestMain(m *testing.M) {
 
 func TestOpticalPower(t *testing.T) {
 	dut := ondatra.DUT(t, "dut")
-	
+
 	fptest.ConfigureDefaultNetworkInstance(t, dut)
 
-    operationalMode = uint16(*operationalModeFlag)
-    operationalMode = cfgplugins.InterfaceInitialize(t, dut, operationalMode)
-    cfgplugins.InterfaceConfig(t, dut, dut.Port(t, "port1"))
-    cfgplugins.InterfaceConfig(t, dut, dut.Port(t, "port2"))
+	operationalMode = uint16(*operationalModeFlag)
+	operationalMode = cfgplugins.InterfaceInitialize(t, dut, operationalMode)
+	cfgplugins.InterfaceConfig(t, dut, dut.Port(t, "port1"))
+	cfgplugins.InterfaceConfig(t, dut, dut.Port(t, "port2"))
 
 	var (
 		trs  = make(map[string]string)
@@ -70,8 +70,7 @@ func TestOpticalPower(t *testing.T) {
 				cfgplugins.ConfigOpticalChannel(t, dut, ochs[p.Name()], frequency, targetOpticalPower, operationalMode)
 			}
 
-            t.Logf(" Frequency: %v, targetOpticalPower: %v", frequency, targetOptical
-Power)
+			t.Logf(" Frequency: %v, targetOpticalPower: %v", frequency, targetOpticalPower)
 			// Create sample streams for each port.
 			ochStreams := make(map[string]*samplestream.SampleStream[*oc.Component_OpticalChannel])
 			trStreams := make(map[string]*samplestream.SampleStream[*oc.Component_Transceiver_Channel])
