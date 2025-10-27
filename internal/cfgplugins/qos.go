e8dc1c8f26fe906e888d61f8dbbdd80117566ac6
desaimg1 <desai.mg@keysight.com>
2025-10-27 05:45:14 +0000

    dp2.2

--- internal/cfgplugins/qos.go
+++ internal/cfgplugins/qos.go
@@ -499,3 +499,45 @@ func QosClassificationOCConfig(t *testing.T) {
 	// stmt := policy.GetOrCreateStatement("class-default")
 	// stmt.GetOrCreateActions().SetForwardingGroup = ygot.String("forwarding-group-tc3")
 }
+
+func configureOneRateTwoColorSchedulerFromOC(batch *gnmi.SetBatch, params *SchedulerParams) {
+	qos := &oc.Qos{}
+	sp := qos.GetOrCreateSchedulerPolicy(params.SchedulerName)
+	sp.Name = ygot.String(params.SchedulerName)
+	sched := sp.GetOrCreateScheduler(params.SequenceNumber)
+	sched.Sequence = ygot.Uint32(1)
+	sched.Type = oc.QosTypes_QOS_SCHEDULER_TYPE_ONE_RATE_TWO_COLOR
+	input := sched.GetOrCreateInput(params.PolicerName)
+	input.InputType = oc.Input_InputType_QUEUE
+	input.Queue = ygot.String(params.QueueName)
+	trtc := sched.GetOrCreateOneRateTwoColor()
+	trtc.Cir = ygot.Uint64(params.CirValue)
+	trtc.Bc = ygot.Uint32(params.BurstSize)
+	trtc.GetOrCreateExceedAction().Drop = ygot.Bool(false)
+	qosPath := gnmi.OC().Qos().Config()
+	gnmi.BatchUpdate(batch, qosPath, qos)
+}
+
+func configureOneRateTwoColorSchedulerFromCLI(t *testing.T, dut *ondatra.DUTDevice, params *SchedulerParams) {
+	switch dut.Vendor() {
+	case ondatra.ARISTA:
+		cliConfig := fmt.Sprintf(`
+        policy-map type quality-of-service %s
+        class %s
+        set traffic-class %d 
+        police cir %d bps bc %d bytes
+        !
+        `, params.SchedulerName, params.ClassName, params.QueueID, params.CirValue, params.BurstSize)
+		helpers.GnmiCLIConfig(t, dut, cliConfig)
+	default:
+		t.Errorf("Unsupported CLI command for dut %v %s", dut.Vendor(), dut.Name())
+	}
+}
+
+func NewOneRateTwoColorScheduler(t *testing.T, dut *ondatra.DUTDevice, batch *gnmi.SetBatch, params *SchedulerParams) {
+	if deviations.QosTwoRateThreeColorPolicerOCUnsupported(dut) {
+		configureOneRateTwoColorSchedulerFromCLI(t, dut, params)
+	} else {
+		configureOneRateTwoColorSchedulerFromOC(batch, params)
+	}
+}
