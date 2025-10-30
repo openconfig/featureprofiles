1abd3941d7561cd6ed27adea8a44f87e52b23d11
AmrNJ <155722765+AmrNJ@users.noreply.github.com>
2025-10-30 18:11:24 +0000

    gofmt

--- feature/interface/lacp/tests/lacp_interval_test/lacp_interval_test.go
+++ feature/interface/lacp/tests/lacp_interval_test/lacp_interval_test.go
@@ -46,187 +46,186 @@ func TestMain(m *testing.M) {
 // consistent with IPv4.
 
 const (
-        plen4          = 30
-        plen6          = 126
-        opUp           = oc.Interface_OperStatus_UP
-        adminUp        = oc.Interface_AdminStatus_UP
-        ethernetCsmacd = oc.IETFInterfaces_InterfaceType_ethernetCsmacd
-        ieee8023adLag  = oc.IETFInterfaces_InterfaceType_ieee8023adLag
-        lagTypeLACP    = oc.IfAggregate_AggregationType_LACP
+	plen4          = 30
+	plen6          = 126
+	opUp           = oc.Interface_OperStatus_UP
+	adminUp        = oc.Interface_AdminStatus_UP
+	ethernetCsmacd = oc.IETFInterfaces_InterfaceType_ethernetCsmacd
+	ieee8023adLag  = oc.IETFInterfaces_InterfaceType_ieee8023adLag
+	lagTypeLACP    = oc.IfAggregate_AggregationType_LACP
 )
 
 var (
-        dut1Src = attrs.Attributes{
-                Desc:    "dutdut",
-                IPv4:    "192.0.2.1",
-                IPv6:    "2001:db8::1",
-                IPv4Len: plen4,
-                IPv6Len: plen6,
-        }
+	dut1Src = attrs.Attributes{
+		Desc:    "dutdut",
+		IPv4:    "192.0.2.1",
+		IPv6:    "2001:db8::1",
+		IPv4Len: plen4,
+		IPv6Len: plen6,
+	}
 
-        dut2Src = attrs.Attributes{
-                Name:    "atesrc",
-                MAC:     "02:11:01:00:00:01",
-                IPv4:    "192.0.2.2",
-                IPv6:    "2001:db8::2",
-                IPv4Len: plen4,
-                IPv6Len: plen6,
-        }
+	dut2Src = attrs.Attributes{
+		Name:    "atesrc",
+		MAC:     "02:11:01:00:00:01",
+		IPv4:    "192.0.2.2",
+		IPv6:    "2001:db8::2",
+		IPv4Len: plen4,
+		IPv6Len: plen6,
+	}
 )
 
 type testCase struct {
-        lagType      oc.E_IfAggregate_AggregationType
-        dut1         *ondatra.DUTDevice
-        dut2         *ondatra.DUTDevice
-        lacpInterval oc.E_Lacp_LacpPeriodType
+	lagType      oc.E_IfAggregate_AggregationType
+	dut1         *ondatra.DUTDevice
+	dut2         *ondatra.DUTDevice
+	lacpInterval oc.E_Lacp_LacpPeriodType
 
-        // dutPorts is the set of ports the DUT -- the first (i.e., dutPorts[0])
-        // is not configured in the aggregate interface.
-        dut1Ports []*ondatra.Port
-        // atePorts is the set of ports on the ATE -- the first, as with the DUT
-        // is not configured in the aggregate interface.
-        // is not configured in the aggregate interface.
-        dut2Ports []*ondatra.Port
-        aggID     string
+	// dutPorts is the set of ports the DUT -- the first (i.e., dutPorts[0])
+	// is not configured in the aggregate interface.
+	dut1Ports []*ondatra.Port
+	// atePorts is the set of ports on the ATE -- the first, as with the DUT
+	// is not configured in the aggregate interface.
+	// is not configured in the aggregate interface.
+	dut2Ports []*ondatra.Port
+	aggID     string
 }
 
 // sortPorts sorts the ports by the testbed port ID.
 func sortPorts(ports []*ondatra.Port) []*ondatra.Port {
-        sort.SliceStable(ports, func(i, j int) bool {
-                return ports[i].ID() < ports[j].ID()
-        })
-        return ports
+	sort.SliceStable(ports, func(i, j int) bool {
+		return ports[i].ID() < ports[j].ID()
+	})
+	return ports
 }
 
 func (tc *testCase) verifyAggID(t *testing.T, dp *ondatra.Port, dut *ondatra.DUTDevice) {
-        dip := gnmi.OC().Interface(dp.Name())
-        di := gnmi.Get(t, dut, dip.State())
-        if lagID := di.GetEthernet().GetAggregateId(); lagID != tc.aggID {
-                t.Errorf("%s LagID got %v, want %v", dp, lagID, tc.aggID)
-        }
+	dip := gnmi.OC().Interface(dp.Name())
+	di := gnmi.Get(t, dut, dip.State())
+	if lagID := di.GetEthernet().GetAggregateId(); lagID != tc.aggID {
+		t.Errorf("%s LagID got %v, want %v", dp, lagID, tc.aggID)
+	}
 }
 
 func (tc *testCase) configureDUT(t *testing.T) {
-        t.Logf("dut1 ports = %v, dut2 ports = %v", tc.dut1Ports, tc.dut2Ports)
-        if len(tc.dut1Ports) < 2 || len(tc.dut2Ports) < 2 {
-                t.Fatalf("Testbed requires at least 2 ports, got %d and %d", len(tc.dut1Ports), len(tc.dut2Ports))
-        }
+	t.Logf("dut1 ports = %v, dut2 ports = %v", tc.dut1Ports, tc.dut2Ports)
+	if len(tc.dut1Ports) < 2 || len(tc.dut2Ports) < 2 {
+		t.Fatalf("Testbed requires at least 2 ports, got %d and %d", len(tc.dut1Ports), len(tc.dut2Ports))
+	}
 
-        lacpMode := oc.Lacp_LacpActivityType_ACTIVE
-        lacpData := &cfgplugins.LACPParams{
-                Activity: &lacpMode,
-                Period:   &tc.lacpInterval,
-        }
+	lacpMode := oc.Lacp_LacpActivityType_ACTIVE
+	lacpData := &cfgplugins.LACPParams{
+		Activity: &lacpMode,
+		Period:   &tc.lacpInterval,
+	}
 
-        dut1AggData := &cfgplugins.DUTAggData{
-                LagName:      tc.aggID,
-                AggType:      tc.lagType,
-                LacpParams:   lacpData,
-                Attributes:   dut1Src,
-                OndatraPorts: tc.dut1Ports,
-        }
+	dut1AggData := &cfgplugins.DUTAggData{
+		LagName:      tc.aggID,
+		AggType:      tc.lagType,
+		LacpParams:   lacpData,
+		Attributes:   dut1Src,
+		OndatraPorts: tc.dut1Ports,
+	}
 
-        dut1Batch := &gnmi.SetBatch{}
-        cfgplugins.NewAggregateInterface(t, tc.dut1, dut1Batch, dut1AggData)
-        dut1Batch.Set(t, tc.dut1)
-        t.Logf("Configured DUT1 aggregate interface %s", tc.aggID)
+	dut1Batch := &gnmi.SetBatch{}
+	cfgplugins.NewAggregateInterface(t, tc.dut1, dut1Batch, dut1AggData)
+	dut1Batch.Set(t, tc.dut1)
+	t.Logf("Configured DUT1 aggregate interface %s", tc.aggID)
 
-        dut2AggData := &cfgplugins.DUTAggData{
-                LagName:      tc.aggID,
-                AggType:      tc.lagType,
-                LacpParams:   lacpData,
-                Attributes:   dut2Src,
-                OndatraPorts: tc.dut2Ports,
-        }
+	dut2AggData := &cfgplugins.DUTAggData{
+		LagName:      tc.aggID,
+		AggType:      tc.lagType,
+		LacpParams:   lacpData,
+		Attributes:   dut2Src,
+		OndatraPorts: tc.dut2Ports,
+	}
 
-        dut2Batch := &gnmi.SetBatch{}
-        cfgplugins.NewAggregateInterface(t, tc.dut2, dut2Batch, dut2AggData)
-        dut2Batch.Set(t, tc.dut2)
-        t.Logf("Configured DUT2 aggregate interface %s", tc.aggID)
+	dut2Batch := &gnmi.SetBatch{}
+	cfgplugins.NewAggregateInterface(t, tc.dut2, dut2Batch, dut2AggData)
+	dut2Batch.Set(t, tc.dut2)
+	t.Logf("Configured DUT2 aggregate interface %s", tc.aggID)
 
-        // TODO - to remove this sleep later
-        time.Sleep(10 * time.Second)
+	// TODO - to remove this sleep later
+	time.Sleep(10 * time.Second)
 
-        if deviations.ExplicitInterfaceInDefaultVRF(tc.dut1) {
-                duts := []*ondatra.DUTDevice{tc.dut1, tc.dut2}
-                for _, dut := range duts {
-                        fptest.AssignToNetworkInstance(t, dut, tc.aggID,
-                                deviations.DefaultNetworkInstance(dut), 0)
-                }
-        }
+	if deviations.ExplicitInterfaceInDefaultVRF(tc.dut1) {
+		duts := []*ondatra.DUTDevice{tc.dut1, tc.dut2}
+		for _, dut := range duts {
+			fptest.AssignToNetworkInstance(t, dut, tc.aggID,
+				deviations.DefaultNetworkInstance(dut), 0)
+		}
+	}
 
-        if deviations.ExplicitPortSpeed(tc.dut1) {
-                for _, port := range tc.dut1Ports {
-                        fptest.SetPortSpeed(t, port)
-                }
-        }
-        if deviations.ExplicitPortSpeed(tc.dut2) {
-                for _, port := range tc.dut2Ports {
-                        fptest.SetPortSpeed(t, port)
-                }
-        }
+	if deviations.ExplicitPortSpeed(tc.dut1) {
+		for _, port := range tc.dut1Ports {
+			fptest.SetPortSpeed(t, port)
+		}
+	}
+	if deviations.ExplicitPortSpeed(tc.dut2) {
+		for _, port := range tc.dut2Ports {
+			fptest.SetPortSpeed(t, port)
+		}
+	}
 }
 
 func (tc *testCase) verifyDUT(t *testing.T) {
-        // Wait for LAG negotiation and verify LAG type for the aggregate interface.
-        gnmi.Await(t, tc.dut1, gnmi.OC().Interface(tc.aggID).Type().State(), time.Minute, ieee8023adLag)
+	// Wait for LAG negotiation and verify LAG type for the aggregate interface.
+	gnmi.Await(t, tc.dut1, gnmi.OC().Interface(tc.aggID).Type().State(), time.Minute, ieee8023adLag)
 
-        for _, port := range tc.dut1Ports {
-                tc.verifyInterfaceDUT(t, port, tc.dut1)
-                tc.verifyAggID(t, port, tc.dut1)
-        }
-        for _, port := range tc.dut2Ports {
-                tc.verifyInterfaceDUT(t, port, tc.dut2)
-                tc.verifyAggID(t, port, tc.dut2)
-        }
-        lacpIntervals := gnmi.OC().Lacp().Interface(tc.aggID).Interval().State()
-        lacpIntervalDUT1 := gnmi.Get(t, tc.dut1, lacpIntervals)
-        lacpIntervalDUT2 := gnmi.Get(t, tc.dut2, lacpIntervals)
+	for _, port := range tc.dut1Ports {
+		tc.verifyInterfaceDUT(t, port, tc.dut1)
+		tc.verifyAggID(t, port, tc.dut1)
+	}
+	for _, port := range tc.dut2Ports {
+		tc.verifyInterfaceDUT(t, port, tc.dut2)
+		tc.verifyAggID(t, port, tc.dut2)
+	}
+	lacpIntervals := gnmi.OC().Lacp().Interface(tc.aggID).Interval().State()
+	lacpIntervalDUT1 := gnmi.Get(t, tc.dut1, lacpIntervals)
+	lacpIntervalDUT2 := gnmi.Get(t, tc.dut2, lacpIntervals)
 
-        if lacpIntervalDUT1 != lacpIntervalDUT2 {
-                t.Errorf("LACP Interval is not same on both the DUTs, DUT1: %v, DUT2: %v", lacpIntervalDUT1, lacpIntervalDUT2)
-        }
-        if lacpIntervalDUT1 != tc.lacpInterval {
-                t.Errorf("LACP Interval is not same as configured, got: %v, want: %v", lacpIntervalDUT1, tc.lacpInterval)
-        }
-        t.Logf("LACP Interval is same as configured, got: %v, want: %v", lacpIntervalDUT1, tc.lacpInterval)
+	if lacpIntervalDUT1 != lacpIntervalDUT2 {
+		t.Errorf("LACP Interval is not same on both the DUTs, DUT1: %v, DUT2: %v", lacpIntervalDUT1, lacpIntervalDUT2)
+	}
+	if lacpIntervalDUT1 != tc.lacpInterval {
+		t.Errorf("LACP Interval is not same as configured, got: %v, want: %v", lacpIntervalDUT1, tc.lacpInterval)
+	}
+	t.Logf("LACP Interval is same as configured, got: %v, want: %v", lacpIntervalDUT1, tc.lacpInterval)
 }
 
 func (tc *testCase) verifyInterfaceDUT(t *testing.T, dp *ondatra.Port, dut *ondatra.DUTDevice) {
-        dip := gnmi.OC().Interface(dp.Name())
-        di := gnmi.Get(t, dut, dip.State())
-        fptest.LogQuery(t, dp.String()+" before Await", dip.State(), di)
+	dip := gnmi.OC().Interface(dp.Name())
+	di := gnmi.Get(t, dut, dip.State())
+	fptest.LogQuery(t, dp.String()+" before Await", dip.State(), di)
 
-        if got := di.GetAdminStatus(); got != adminUp {
-                t.Errorf("%s admin-status got %v, want %v", dp, got, adminUp)
-        }
+	if got := di.GetAdminStatus(); got != adminUp {
+		t.Errorf("%s admin-status got %v, want %v", dp, got, adminUp)
+	}
 
-        // LAG members may fall behind, so wait for them to be up.
-        gnmi.Await(t, dut, dip.OperStatus().State(), time.Minute, opUp)
+	// LAG members may fall behind, so wait for them to be up.
+	gnmi.Await(t, dut, dip.OperStatus().State(), time.Minute, opUp)
 }
 
 func TestLacpTimers(t *testing.T) {
-        dut1 := ondatra.DUT(t, "dut1")
-        dut2 := ondatra.DUT(t, "dut2")
-        aggID := netutil.NextAggregateInterface(t, dut1)
+	dut1 := ondatra.DUT(t, "dut1")
+	dut2 := ondatra.DUT(t, "dut2")
+	aggID := netutil.NextAggregateInterface(t, dut1)
 
-        lacpIntervals := []oc.E_Lacp_LacpPeriodType{oc.Lacp_LacpPeriodType_FAST, oc.Lacp_LacpPeriodType_SLOW}
+	lacpIntervals := []oc.E_Lacp_LacpPeriodType{oc.Lacp_LacpPeriodType_FAST, oc.Lacp_LacpPeriodType_SLOW}
 
-        for _, lacpInterval := range lacpIntervals {
+	for _, lacpInterval := range lacpIntervals {
 
-                tc := &testCase{
-                        dut1:         dut1,
-                        dut2:         dut2,
-                        lagType:      lagTypeLACP,
-                        lacpInterval: lacpInterval,
-                        dut1Ports:    sortPorts(dut1.Ports()),
-                        dut2Ports:    sortPorts(dut2.Ports()),
-                        aggID:        aggID,
-                }
-                t.Run(fmt.Sprintf("lacpInterval=%s", lacpInterval), func(t *testing.T) {
-                        tc.configureDUT(t)
-                        tc.verifyDUT(t)
-                })
-        }
+		tc := &testCase{
+			dut1:         dut1,
+			dut2:         dut2,
+			lagType:      lagTypeLACP,
+			lacpInterval: lacpInterval,
+			dut1Ports:    sortPorts(dut1.Ports()),
+			dut2Ports:    sortPorts(dut2.Ports()),
+			aggID:        aggID,
+		}
+		t.Run(fmt.Sprintf("lacpInterval=%s", lacpInterval), func(t *testing.T) {
+			tc.configureDUT(t)
+			tc.verifyDUT(t)
+		})
+	}
 }
-
