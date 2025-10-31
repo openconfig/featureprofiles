52c17cb86efed7d156bd948b70dadae412e2b959
AmrNJ <155722765+AmrNJ@users.noreply.github.com>
2025-10-31 15:01:30 +0530

    Update interface.go

--- internal/cfgplugins/interface.go
+++ internal/cfgplugins/interface.go
@@ -890,8 +890,6 @@ func NewAggregateInterface(t *testing.T, dut *ondatra.DUTDevice, b *gnmi.SetBatc
 	}
 	agg.GetOrCreateAggregation().LagType = l.AggType
 
-	gnmi.BatchReplace(b, gnmi.OC().Interface(aggID).Config(), agg)
-
 	// Set LACP mode to ACTIVE for the LAG interface
 	if l.LacpParams != nil {
 		if l.LacpParams.Activity == nil || l.LacpParams.Period == nil {
@@ -901,9 +899,10 @@ func NewAggregateInterface(t *testing.T, dut *ondatra.DUTDevice, b *gnmi.SetBatc
 		lacp.LacpMode = *l.LacpParams.Activity
 		lacp.Interval = *l.LacpParams.Period
 		lacpPath := gnmi.OC().Lacp().Interface(aggID)
-		gnmi.BatchUpdate(b, lacpPath.Config(), lacp)
+		gnmi.BatchReplace(b, lacpPath.Config(), lacp)
 	}
-
+	
+	gnmi.BatchReplace(b, gnmi.OC().Interface(aggID).Config(), agg)
 	gnmi.BatchDelete(b, gnmi.OC().Interface(aggID).Aggregation().MinLinks().Config())
 
 	l.PopulateOndatraPorts(t, dut)
