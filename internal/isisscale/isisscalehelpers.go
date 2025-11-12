af120481424ba2e64fe9e95c1f9c43bba4d46527
geokamel-eg <geokamel@google.com>
2025-11-12 15:34:03 +0000

    minor change

--- internal/isisscale/isisscalehelpers.go
+++ internal/isisscale/isisscalehelpers.go
@@ -22,7 +22,7 @@ import (
 	"github.com/openconfig/ondatra/gnmi/oc"
 	"github.com/openconfig/ondatra/netutil"
 	"github.com/openconfig/ondatra"
-	"github.com/openconfig/openconfig/ygnmi"
+	"github.com/openconfig/openconfig/ygnmi/ygnmi"
 )
 
 const (
