f29a94f3aa70f9785057701f0d1bc5244877f2d1
karremil <karthikeya.remilla@gmail.com>
2025-11-12 13:27:49 +0530

    go fmt

--- feature/isis/otg_tests/static_route_isis_redistribution/static_route_isis_redistribution_test.go
+++ feature/isis/otg_tests/static_route_isis_redistribution/static_route_isis_redistribution_test.go
@@ -588,17 +588,17 @@ func TestStaticToISISRedistribution(t *testing.T) {
 	})
 
 	cases := []struct {
-		desc               string
-		policyStmtType     oc.E_RoutingPolicy_PolicyResultType
-		metricPropogation  bool
-		protoAf            oc.E_Types_ADDRESS_FAMILY
-		RplName            string
-		RplStatement       string
-		verifyTrafficStats bool
-		verifyRouteFunc    func(t *testing.T, ts *isissession.TestSession)
-		trafficFlows       []string
-		TagSetCondition    bool
-		PrefixSetCondition bool
+		desc                            string
+		policyStmtType                  oc.E_RoutingPolicy_PolicyResultType
+		metricPropogation               bool
+		protoAf                         oc.E_Types_ADDRESS_FAMILY
+		RplName                         string
+		RplStatement                    string
+		verifyTrafficStats              bool
+		verifyRouteFunc                 func(t *testing.T, ts *isissession.TestSession)
+		trafficFlows                    []string
+		TagSetCondition                 bool
+		PrefixSetCondition              bool
 		IsisSetMetricStyleTypeCondition bool
 	}{{
 		desc:              "RT-2.12.1: Redistribute IPv4 static route to IS-IS with metric propagation disabled",
@@ -641,15 +641,15 @@ func TestStaticToISISRedistribution(t *testing.T) {
 		policyStmtType:    oc.RoutingPolicy_PolicyResultType_REJECT_ROUTE,
 		verifyRouteFunc:   verifyNonMatchingPrefix,
 	}, {
-		desc:               "RT-2.12.6: Redistribute IPv4 static route to IS-IS matching a prefix using a route-policy",
-		protoAf:            oc.Types_ADDRESS_FAMILY_IPV4,
-		RplName:            v4RoutePolicy,
-		metricPropogation:  false,
-		policyStmtType:     oc.RoutingPolicy_PolicyResultType_ACCEPT_ROUTE,
-		verifyTrafficStats: true,
-		trafficFlows:       []string{v4Flow},
-		PrefixSetCondition: true,
-		verifyRouteFunc:    verifyMatchingPrefixWithMetricPropagationWithRoutePolicy,
+		desc:                            "RT-2.12.6: Redistribute IPv4 static route to IS-IS matching a prefix using a route-policy",
+		protoAf:                         oc.Types_ADDRESS_FAMILY_IPV4,
+		RplName:                         v4RoutePolicy,
+		metricPropogation:               false,
+		policyStmtType:                  oc.RoutingPolicy_PolicyResultType_ACCEPT_ROUTE,
+		verifyTrafficStats:              true,
+		trafficFlows:                    []string{v4Flow},
+		PrefixSetCondition:              true,
+		verifyRouteFunc:                 verifyMatchingPrefixWithMetricPropagationWithRoutePolicy,
 		IsisSetMetricStyleTypeCondition: true,
 	}, {
 		desc:               "RT-2.12.7: Redistribute IPv4 static route to IS-IS matching a tag",
@@ -662,15 +662,15 @@ func TestStaticToISISRedistribution(t *testing.T) {
 		TagSetCondition:    true,
 		verifyRouteFunc:    verifyMatchingPrefixWithTag,
 	}, {
-		desc:               "RT-2.12.8: Redistribute IPv6 static route to IS-IS matching a prefix using a route-policy",
-		protoAf:            oc.Types_ADDRESS_FAMILY_IPV6,
-		RplName:            v6RoutePolicy,
-		metricPropogation:  true,
-		policyStmtType:     oc.RoutingPolicy_PolicyResultType_ACCEPT_ROUTE,
-		verifyTrafficStats: true,
-		trafficFlows:       []string{v6Flow},
-		PrefixSetCondition: true,
-		verifyRouteFunc:    verifyMatchingV6PrefixWithMetricPropagationWithRoutePolicy,
+		desc:                            "RT-2.12.8: Redistribute IPv6 static route to IS-IS matching a prefix using a route-policy",
+		protoAf:                         oc.Types_ADDRESS_FAMILY_IPV6,
+		RplName:                         v6RoutePolicy,
+		metricPropogation:               true,
+		policyStmtType:                  oc.RoutingPolicy_PolicyResultType_ACCEPT_ROUTE,
+		verifyTrafficStats:              true,
+		trafficFlows:                    []string{v6Flow},
+		PrefixSetCondition:              true,
+		verifyRouteFunc:                 verifyMatchingV6PrefixWithMetricPropagationWithRoutePolicy,
 		IsisSetMetricStyleTypeCondition: true,
 	}, {
 		desc:               "RT-2.12.9: Redistribute IPv6 static route to IS-IS matching a prefix using a tag",
