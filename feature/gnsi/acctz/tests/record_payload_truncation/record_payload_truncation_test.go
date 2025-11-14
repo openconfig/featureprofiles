7e420859dc26319e1beff016a91e7c6c364c6bff
Krishnakumar Natarajan <38819711+nkrishk@users.noreply.github.com>
2025-11-14 20:38:10 +0530

    Update record_payload_truncation_test.go

--- feature/gnsi/acctz/tests/record_payload_truncation/record_payload_truncation_test.go
+++ feature/gnsi/acctz/tests/record_payload_truncation/record_payload_truncation_test.go
@@ -74,7 +74,7 @@ func TestAccountzRecordPayloadTruncation(t *testing.T) {
 
 	acctzSubClient, err := acctzClient.RecordSubscribe(context.Background(), &acctzpb.RecordRequest{
 		Timestamp: timestamppb.New(startTime),
-	}, grpc.MaxCallRecvMsgSize(30000000))
+	}, grpc.MaxCallRecvMsgSize(45000000))
 	if err != nil {
 		t.Fatalf("Failed getting accountz record subscribe client, error: %s", err)
 	}
