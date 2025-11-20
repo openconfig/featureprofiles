2f10a77cbd1b415f766e0048cf7eb1e1f96f8a43
geokamel-eg <geokamel@google.com>
2025-11-20 15:19:20 +0000

    change of libraries from google to github

--- feature/isis/otg_tests/isis_scale_multi_adjacency_test/isis_scale_multi_adjacency_test.go
+++ feature/isis/otg_tests/isis_scale_multi_adjacency_test/isis_scale_multi_adjacency_test.go
@@ -8,12 +8,12 @@ import (
 	"testing"
 	"time"
 
-	"google3/third_party/openconfig/featureprofiles/internal/cfgplugins/cfgplugins"
-	"google3/third_party/openconfig/featureprofiles/internal/deviations/deviations"
-	"google3/third_party/openconfig/featureprofiles/internal/fptest/fptest"
-	"google3/third_party/openconfig/featureprofiles/internal/isisscale/isisscalehelpers"
-	otgconfighelpers "google3/third_party/openconfig/featureprofiles/internal/otg_helpers/otg_config_helpers/otgconfighelpers"
-	"google3/third_party/openconfig/ondatra/gnmi/oc/oc"
+	"github.com/openconfig/featureprofiles/internal/cfgplugins"
+	"github.com/openconfig/featureprofiles/internal/deviations"
+	"github.com/openconfig/featureprofiles/internal/fptest"
+	"github.com/openconfig/featureprofiles/internal/isisscale/isisscalehelpers"
+	otgconfighelpers "github.com/openconfig/featureprofiles/internal/otg_helpers/otg_config_helpers"
+	"github.com/openconfig/ondatra/gnmi/oc"
 )
 
 type descriptor struct {
