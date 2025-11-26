553b3517397bf20fdc8b3526e2a499fdf24a4a5a
ssundari <ssundari@cisco.com>
2025-11-25 17:29:59 -0800

    P200 egressp4rt script fix

--- feature/gribi/ate_tests/cisco/egressp4rt/egressp4rt_test.go
+++ feature/gribi/ate_tests/cisco/egressp4rt/egressp4rt_test.go
@@ -991,11 +991,11 @@ func testWithPoPUnoptimized(ctx context.Context, t *testing.T, args *testArgs, i
 		t.Logf("Source port %v", sport)
 		var inputPorts, inputPort1 []string
 		portinps := []string{p[0].Name(), p[2].Name()}
-		switch args.memberCount {
-		case 16:
+		switch {
+		case args.memberCount >= 16:
 			inputPorts = []string{p[1].Name(), p[3].Name(), p[5].Name(), p[7].Name(), p[4].Name(), p[8].Name(), p[9].Name(), p[10].Name(), p[11].Name(), p[12].Name(), p[13].Name(), p[14].Name(), p[7].Name()}
 			inputPort1 = []string{p[1].Name(), p[3].Name(), p[5].Name(), p[7].Name()}
-		case 8:
+		case args.memberCount == 8:
 			inputPorts = []string{p[1].Name(), p[3].Name(), p[5].Name(), p[7].Name()}
 			inputPort1 = inputPorts
 		}
