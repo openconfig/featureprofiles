7f08b0ffd2328f4bf3edb7413d310b1005598647
asaggarw <ashna.aggarwal@keysight.com>
2025-12-12 19:27:11 +0000

    update branch

--- proto/metadata_go_proto/metadata.pb.go
+++ proto/metadata_go_proto/metadata.pb.go
@@ -21,12 +21,13 @@
 package metadata_go_proto
 
 import (
-	proto "github.com/openconfig/ondatra/proto"
-	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
-	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
 	sync "sync"
 	unsafe "unsafe"
+
+	proto "github.com/openconfig/ondatra/proto"
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 )
 
 const (
