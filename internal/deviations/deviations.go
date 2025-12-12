5e2a4ca5e94910d1b5727851cb15383e9be4ff70
Ravi Verman <raviv@arista.com>
2025-12-12 00:52:35 -0800

    RT-2.15 Add deviation for aggregate SID counter out packets telemetry
    Arista: b/449592554
    Devices that do not support
    /network-instances/network-instance/mpls/signaling-protocols
    /segment-routing/aggregate-sid-counters/aggregate-sid-counter/state/out-pkts.

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -1802,3 +1802,9 @@ func ConfigACLValueAnyOcUnsupported(dut *ondatra.DUTDevice) bool {
 func ConfigAclOcUnsupported(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetConfigAclOcUnsupported()
 }
+
+// AggregateSIDCounterOutPktsUnsupported returns true if device does not support
+// /network-instances/network-instance/mpls/signaling-protocols/segment-routing/aggregate-sid-counters/aggregate-sid-counter/state/out-pkts
+func AggregateSIDCounterOutPktsUnsupported(dut *ondatra.DUTDevice) bool {
+	return lookupDUTDeviations(dut).GetAggregateSidCounterOutPktsUnsupported()
+}
