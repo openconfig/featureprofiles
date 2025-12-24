faf682753b1bc83bb8354ca8ac1be0175d903922
AmrNJ <155722765+AmrNJ@users.noreply.github.com>
2025-12-24 17:46:43 +0530

    Update traceroute_packetin_test.go

--- feature/p4rt/otg_tests/traceroute_packetin_test/traceroute_packetin_test.go
+++ feature/p4rt/otg_tests/traceroute_packetin_test/traceroute_packetin_test.go
@@ -29,6 +29,7 @@ import (
 	"github.com/openconfig/featureprofiles/internal/attrs"
 	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/featureprofiles/internal/fptest"
+	"github.com/openconfig/featureprofiles/internal/otgutils"
 	"github.com/openconfig/featureprofiles/internal/p4rtutils"
 	"github.com/openconfig/ondatra"
 	"github.com/openconfig/ondatra/gnmi"
