c55f1c68f37947673e1213784ded80db6344fe43
Ram Machat <rmachat@google.com>
2026-01-12 08:46:32 +0000

    Fixed the package name

--- feature/bgp/policybase/otg_tests/multipath_bgp_ecmp_protocol_nexthop/multipath_bgp_ecmp_protocol_nexthop_test.go
+++ feature/bgp/policybase/otg_tests/multipath_bgp_ecmp_protocol_nexthop/multipath_bgp_ecmp_protocol_nexthop_test.go
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package multipath_bgp_ecmp_protocol_nexthop
+package multipath_bgp_ecmp_protocol_nexthop_test
 
 import (
 	"fmt"
