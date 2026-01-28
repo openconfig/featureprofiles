4824f88a9d9da63b7d932bd57c91bfa0a397e4f0
Adithya Chittem <chitadi@google.com>
2026-01-28 09:56:25 +0000

    added the fnttest checker

---
+++ tools/gap-analyzer/read_fnttests.go
@@ -0,0 +1,30 @@
+// tools/run_fnt_workflow/read_fnttests.go
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+var (
+	failScenario = flag.Bool("fail_scenario", false, "If true, intentionally fail the script.")
+)
+
+func main() {
+	flag.Parse()
+
+	fmt.Println(strings.Repeat("=", 50))
+	fmt.Println("Running FNT Gap Analysis Test Script...")
+
+	if *failScenario {
+		fmt.Println("--- Intentionally Failing Scenario ---")
+		log.Fatalf("Intentionally failing for testing: --fail_scenario=true was set.")
+	} else {
+		fmt.Println("--- Successful Scenario ---")
+		fmt.Println("Successful run of workflow")
+	}
+	fmt.Println(strings.Repeat("=", 50))
+}
