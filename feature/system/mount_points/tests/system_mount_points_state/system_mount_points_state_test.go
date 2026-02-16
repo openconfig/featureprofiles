f5fb0c3a430b31f1bfc2000750b67ba1488a78fc
Rishabh Agarwal <rishabh2001agarwal@gmail.com>
2026-02-16 11:20:14 +0000

    Fix functional-translator registrar import path

--- feature/system/mount_points/tests/system_mount_points_state/system_mount_points_state_test.go
+++ feature/system/mount_points/tests/system_mount_points_state/system_mount_points_state_test.go
@@ -19,7 +19,7 @@ import (
 
 	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/featureprofiles/internal/fptest"
-	"github.com/openconfig/featureprofiles/internal/functional_translators/registrar"
+	"github.com/openconfig/functional-translators/registrar"
 	"github.com/openconfig/ondatra"
 	"github.com/openconfig/ondatra/gnmi"
 	"github.com/openconfig/ondatra/gnmi/oc"
