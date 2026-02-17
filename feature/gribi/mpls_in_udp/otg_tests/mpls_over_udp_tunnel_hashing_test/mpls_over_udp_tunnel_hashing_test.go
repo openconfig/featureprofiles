3593ae1492bac6de4db53cdcd99cfd3af0d067b6
nikunj <nikunj@arista.com>
2026-02-16 23:00:40 -0800

    Refactor DisableHardwareNexthopProxy deviation into cfgplugins

--- feature/gribi/mpls_in_udp/otg_tests/mpls_over_udp_tunnel_hashing_test/mpls_over_udp_tunnel_hashing_test.go
+++ feature/gribi/mpls_in_udp/otg_tests/mpls_over_udp_tunnel_hashing_test/mpls_over_udp_tunnel_hashing_test.go
@@ -34,7 +34,6 @@ import (
 	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/featureprofiles/internal/fptest"
 	"github.com/openconfig/featureprofiles/internal/gribi"
-	"github.com/openconfig/featureprofiles/internal/helpers"
 	"github.com/openconfig/featureprofiles/internal/otgutils"
 	"github.com/openconfig/gribigo/client"
 	"github.com/openconfig/gribigo/constants"
@@ -165,17 +164,8 @@ func TestMPLSOverUDPTunnelHashing(t *testing.T) {
 	otgutils.WaitForARP(t, ate.OTG(), topo, "IPv4")
 	otgutils.WaitForARP(t, ate.OTG(), topo, "IPv6")
 
-	// Disable hardware nexthop proxying for Arista devices to ensure FIB-ACK works correctly.
-	// See: https://partnerissuetracker.corp.google.com/issues/422275961
-	if deviations.DisableHardwareNexthopProxy(dut) {
-		switch dut.Vendor() {
-		case ondatra.ARISTA:
-			const aristaDisableNHGProxyCLI = "ip hardware fib next-hop proxy disabled"
-			helpers.GnmiCLIConfig(t, dut, aristaDisableNHGProxyCLI)
-		default:
-			t.Errorf("Deviation DisableHardwareNexthopProxy is not handled for the dut: %v", dut.Vendor())
-		}
-	}
+	// Disable hardware nexthop proxying for devices that require it to ensure FIB-ACK works correctly.
+	cfgplugins.DisableHardwareNexthopProxy(t, dut)
 
 	// Configure gRIBI client
 	c := gribi.Client{
