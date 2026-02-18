9a784d3274ec852a8d99e833033cf582478c0142
Ram Machat <rmachat@google.com>
2026-02-18 05:20:39 +0000

    Fixed addpath_scale_test regarding validation and traffic push

--- feature/bgp/addpath/otg_tests/addpath_scale/addpath_scale_test.go
+++ feature/bgp/addpath/otg_tests/addpath_scale/addpath_scale_test.go
@@ -233,7 +233,7 @@ func TestAddPathScale(t *testing.T) {
 	t.Run("configureBGP", func(t *testing.T) {
 		dutConf := bgpWithNbr(dutAS, nbrList, dut, "ALLOW", "ALLOW")
 		gnmi.Replace(t, dut, dutConfPath.Config(), dutConf)
-		fptest.LogQuery(t, "DUT BGP Config", dutConfPath.Config(), gnmi.Get(t, dut, dutConfPath.Config()))
+		// fptest.LogQuery(t, "DUT BGP Config", dutConfPath.Config(), gnmi.Get(t, dut, dutConfPath.Config()))
 
 		atePort2.configureBGPOnATE(t, top, 1, ateAS2)
 		atePort3.configureBGPOnATE(t, top, 2, ateAS3)
@@ -295,7 +295,7 @@ func TestAddPathScaleWithRoutePolicy(t *testing.T) {
 	t.Run("configureBGP", func(t *testing.T) {
 		dutConf := bgpWithNbr(dutAS, nbrList, dut, "community-match", "ALLOW")
 		gnmi.Replace(t, dut, dutConfPath.Config(), dutConf)
-		fptest.LogQuery(t, "DUT BGP Config", dutConfPath.Config(), gnmi.Get(t, dut, dutConfPath.Config()))
+		// fptest.LogQuery(t, "DUT BGP Config", dutConfPath.Config(), gnmi.Get(t, dut, dutConfPath.Config()))
 
 		atePort2.configureBGPOnATE(t, top, 1, ateAS2)
 		atePort3.configureBGPOnATE(t, top, 2, ateAS3)
@@ -305,7 +305,7 @@ func TestAddPathScaleWithRoutePolicy(t *testing.T) {
 		ate.OTG().StartProtocols(t)
 	})
 
-	createTrafficFlow(t, top, ate)
+	createTrafficFlow(t, top, ate, true)
 	ate.OTG().StartTraffic(t)
 	time.Sleep(30 * time.Second)
 	ate.OTG().StopTraffic(t)
@@ -427,9 +427,9 @@ func validateAddPathDisabled(t *testing.T, dut *ondatra.DUTDevice, ate *ondatra.
 }
 
 func validateAddPathReceive(t *testing.T, dut *ondatra.DUTDevice, ate *ondatra.ATEDevice, top gosnappi.Config) {
-	validatePrefixes(t, dut, "200.0.0.2", "1000::200:0:0:2")
+	validatePrefixes(t, dut, "50.1.1.2", "1000:1::50:1:1:2")
 
-	createTrafficFlow(t, top, ate)
+	createTrafficFlow(t, top, ate, false)
 	ate.OTG().StartTraffic(t)
 	time.Sleep(30 * time.Second)
 	ate.OTG().StopTraffic(t)
@@ -442,7 +442,7 @@ func validateAddPathReceive(t *testing.T, dut *ondatra.DUTDevice, ate *ondatra.A
 }
 
 func validateAddPathReceiveSend(t *testing.T, dut *ondatra.DUTDevice, ate *ondatra.ATEDevice, top gosnappi.Config) {
-	validatePrefixes(t, dut, "200.0.0.2", "1000::200:0:0:2")
+	validatePrefixes(t, dut, "50.1.1.2", "1000:1::50:1:1:2")
 }
 
 func configAddPathDisabled(t *testing.T, dut *ondatra.DUTDevice) {
@@ -509,8 +509,8 @@ func validatePrefixes(t *testing.T, dut *ondatra.DUTDevice, neighborIPv4, neighb
 		t.Errorf("Received IPv4 Prefixes - got: %v, want: %v", gotReceived, ipv4Prefixes)
 	}
 
-	if gotInstalled, ok := gnmi.Await(t, dut, ipv4Pfx.Received().State(), 15*time.Minute, ipv4Prefixes).Val(); !ok {
-		t.Errorf("Received IPv4 Prefixes - got: %v, want: %v", gotInstalled, ipv4Prefixes)
+	if gotInstalled, ok := gnmi.Await(t, dut, ipv4Pfx.Installed().State(), 15*time.Minute, ipv4Prefixes).Val(); !ok {
+		t.Errorf("Installed IPv4 Prefixes - got: %v, want: %v", gotInstalled, ipv4Prefixes)
 	}
 
 	ipv6Pfx := bgpPath.Neighbor(neighborIPv6).AfiSafi(oc.BgpTypes_AFI_SAFI_TYPE_IPV6_UNICAST).Prefixes()
@@ -518,8 +518,8 @@ func validatePrefixes(t *testing.T, dut *ondatra.DUTDevice, neighborIPv4, neighb
 		t.Errorf("Received IPv6 Prefixes - got: %v, want: %v", gotReceived, ipv6Prefixes)
 	}
 
-	if gotInstalled, ok := gnmi.Await(t, dut, ipv6Pfx.Received().State(), 15*time.Minute, ipv6Prefixes).Val(); !ok {
-		t.Errorf("Received IPv6 Prefixes - got: %v, want: %v", gotInstalled, ipv6Prefixes)
+	if gotInstalled, ok := gnmi.Await(t, dut, ipv6Pfx.Installed().State(), 15*time.Minute, ipv6Prefixes).Val(); !ok {
+		t.Errorf("Installed IPv6 Prefixes - got: %v, want: %v", gotInstalled, ipv6Prefixes)
 	}
 }
 
@@ -561,7 +561,7 @@ func (a *attributes) configInterfaceDUT(t *testing.T, d *ondatra.DUTDevice) {
 	a.configSubinterfaceDUT(t, i, d)
 	intfPath := gnmi.OC().Interface(p.Name())
 	gnmi.Update(t, d, intfPath.Config(), i)
-	fptest.LogQuery(t, "DUT", intfPath.Config(), gnmi.Get(t, d, intfPath.Config()))
+	// fptest.LogQuery(t, "DUT", intfPath.Config(), gnmi.Get(t, d, intfPath.Config()))
 }
 
 func (a *attributes) configSubinterfaceDUT(t *testing.T, intf *oc.Interface, dut *ondatra.DUTDevice) {
@@ -994,7 +994,7 @@ func bgpWithNbr(as uint32, nbrs []*bgpNeighbor, dut *ondatra.DUTDevice, impPolic
 	return niProto
 }
 
-func createTrafficFlow(t *testing.T, top gosnappi.Config, ate *ondatra.ATEDevice) {
+func createTrafficFlow(t *testing.T, top gosnappi.Config, ate *ondatra.ATEDevice, withCommunities bool) {
 	t.Helper()
 
 	prefixesV4 := createPrefixesV4(t)
@@ -1004,11 +1004,20 @@ func createTrafficFlow(t *testing.T, top gosnappi.Config, ate *ondatra.ATEDevice
 	}
 
 	top.Flows().Clear()
-	for i := uint32(1); i <= atePort2.numSubIntf; i++ {
+	numFlows := atePort2.numSubIntf
+	if numFlows > 10 {
+		// b/485393718 Limit the number of flows to 10 to avoid OTG port limitations. This section of code can be removed if we have more advanced card from Ixia
+		numFlows = 10
+	}
+	for i := uint32(1); i <= numFlows; i++ {
 		rxName := fmt.Sprintf(`v4-bgpNet-%sdst%d.Dev`, atePort2.Name, i)
 		rxNames := []string{}
-		for _, pfxLen := range []int{22, 24, 30} {
-			rxNames = append(rxNames, fmt.Sprintf(`%s-%d`, rxName, pfxLen))
+		if withCommunities {
+			for _, pfxLen := range []int{22, 24, 30} {
+				rxNames = append(rxNames, fmt.Sprintf(`%s-%d`, rxName, pfxLen))
+			}
+		} else {
+			rxNames = append(rxNames, rxName)
 		}
 		flow := top.Flows().Add().SetName(fmt.Sprintf("flow%d", i))
 		flow.Metrics().SetEnable(true)
@@ -1017,7 +1026,6 @@ func createTrafficFlow(t *testing.T, top gosnappi.Config, ate *ondatra.ATEDevice
 		flow.Rate().SetPps(100)
 		e1 := flow.Packet().Add().Ethernet()
 		e1.Src().SetValue(atePort1.MAC)
-		e1.Dst().SetValue(atePort2.MAC)
 		v4 := flow.Packet().Add().Ipv4()
 		v4.Src().SetValue(atePort1.IPv4)
 		v4.Dst().SetValues(dstIps)
