708d55b1e312adcda3075b997528bccd78778e94
AmrNJ <155722765+AmrNJ@users.noreply.github.com>
2026-02-18 10:24:18 +0530

    gNMI-1.27 and RT-7.1 - Minor fixes (#5093)
    * Update gnmi_sample_mode_test.go
    
    * Update bgp_default_policies_test.go

--- feature/bgp/policybase/otg_tests/default_policies_test/bgp_default_policies_test.go
+++ feature/bgp/policybase/otg_tests/default_policies_test/bgp_default_policies_test.go
@@ -179,10 +179,7 @@ func configureDUT(t *testing.T, dut *ondatra.DUTDevice) {
 func verifyPortsUp(t *testing.T, dev *ondatra.Device) {
 	t.Helper()
 	for _, p := range dev.Ports() {
-		status := gnmi.Get(t, dev, gnmi.OC().Interface(p.Name()).OperStatus().State())
-		if want := oc.Interface_OperStatus_UP; status != want {
-			t.Errorf("%s Status: got %v, want %v", p, status, want)
-		}
+		gnmi.Await(t, dev, gnmi.OC().Interface(p.Name()).OperStatus().State(), time.Minute, oc.Interface_OperStatus_UP)
 	}
 }
 
