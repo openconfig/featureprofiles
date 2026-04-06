3da062590a7019c5a0e34533952f279ff0055d75
Avnish Singh <singhavnish@google.com>
2026-04-06 15:56:54 +0530

    Update afts_base_test.go

--- feature/afts/otg_tests/afts_base/afts_base_test.go
+++ feature/afts/otg_tests/afts_base/afts_base_test.go
@@ -28,7 +28,6 @@ import (
 	"github.com/openconfig/featureprofiles/internal/cfgplugins"
 	"github.com/openconfig/featureprofiles/internal/deviations"
 	"github.com/openconfig/featureprofiles/internal/fptest"
-	"github.com/openconfig/featureprofiles/internal/helpers"
 	"github.com/openconfig/featureprofiles/internal/isissession"
 	"github.com/openconfig/featureprofiles/internal/telemetry/aftcache"
 	"github.com/openconfig/ondatra"
