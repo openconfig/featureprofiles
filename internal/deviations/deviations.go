00d90daf4ab5182aa2cbbb45f8477d0ed24346b4
shpaulch <shpaulch@cisco.com>
2026-04-08 02:19:59 -0700

    RT-1.4 BGP Graceful restart deviation (#4621)
    * RT-6.1 fix
    
    * Revert "RT-6.1 fix"
    
    This reverts commit dae9169d9abc8ba563bdb6272b7588e935ffe6d5.
    
    * RT-1.4 BGP Graceful restart deviation
    
    * resolving proto conflicts + correcting docstring errors
    
    * Add reference link for BGP Graceful Restart peer group support
    
    * fix conflicts
    
    * fix error1
    
    * fix conflict
    
    ---------
    
    Co-authored-by: karremil <karthikeya.remilla@gmail.com>

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -1731,6 +1731,7 @@ func ISISAdjacencyStreamUnsupported(dut *ondatra.DUTDevice) bool {
 }
 
 // LocalhostForContainerz returns true if the device uses an IPv6 address instead of localhost.
+// TODO enhancement: this should be renamed to LocalhostForContainerzUnsupported for clarity
 func LocalhostForContainerz(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetLocalhostForContainerz()
 }
@@ -2047,4 +2048,10 @@ func AcctzRecordsAuthzStatusDenyUnsupported(dut *ondatra.DUTDevice) bool {
 // Cisco: https://partnerissuetracker.corp.google.com/issues/429156503
 func FpgaFt(dut *ondatra.DUTDevice) string {
 	return lookupDUTDeviations(dut).GetFpgaFt()
+}
+
+// BgpGracefulRestartPeerGroupUnsupported returns true for devices that do not support BGP Graceful restart for Peer Group
+// Cisco: https://partnerissuetracker.corp.google.com/issues/468284935
+func BgpGracefulRestartPeerGroupUnsupported(dut *ondatra.DUTDevice) bool {
+	return lookupDUTDeviations(dut).GetBgpGracefulRestartPeerGroupUnsupported()
 }
