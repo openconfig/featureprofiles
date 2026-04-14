a33967f7275d4d68fc715758f423b0b749bb2fb1
lvaish05 <lvaish@juniper.net>
2026-04-14 00:10:45 -0700

    changes done to record_payload_truncation_test.go

--- feature/gnsi/acctz/tests/record_payload_truncation/record_payload_truncation_test.go
+++ feature/gnsi/acctz/tests/record_payload_truncation/record_payload_truncation_test.go
@@ -50,6 +50,13 @@ func sendOversizedPayload(t *testing.T, dut *ondatra.DUTDevice) {
 		ni.SetType(oc.NetworkInstanceTypes_NETWORK_INSTANCE_TYPE_L3VRF)
 		staticProtocol := ni.GetOrCreateProtocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_STATIC, deviations.StaticProtocolName(dut))
 		nhAddress := fmt.Sprintf("192.%d.2.1", i)
+                nstatRoutes := 0
+		switch dut.Vendor() {
+		case ondatra.JUNIPER:
+			nstatRoutes = 1
+		default:
+			nstatRoutes = 254
+		}
 		for j := 0; j < 254; j++ {
 			sr1 := staticProtocol.GetOrCreateStatic(fmt.Sprintf("10.%d.0.0/24", j))
 			nh1 := sr1.GetOrCreateNextHop("0")
