78524b82385c3c263b073875316929e015c954a5
Roland Phung <roland@arista.com>
2026-05-01 16:28:05 -0700

    [Health-1.1] Devaition for secondary controller card expectations
    EOS does not support streaming telemetry information about the secondary
    controller card. This means we would bypass the controller card checks
    when the redundant role is secondary. Deviations
    secondary_controller_card_cpu_utilization_unsupported and
    secondary_controller_card_memory_utilization_unsupported are created to
    manage this behavior.
    
    EOS also doesn't have linecard CPUs and memory so enabling the
    linecard_cpu_utilization_unsupported and
    linecard_memory_utilization_unsupported are required.

--- feature/system/health/tests/system_generic_health_check/system_generic_health_check_test.go
+++ feature/system/health/tests/system_generic_health_check/system_generic_health_check_test.go
@@ -267,7 +267,20 @@ func TestControllerCardsNoHighCPUSpike(t *testing.T) {
 			})
 
 		}
+
+		if deviations.SecondaryControllerCardCpuUtilizationUnsupported(dut) {
+			var activeControllerCards []string
+			for _, controllerCard := range controllerCards {
+				if gnmi.Get(t, dut, gnmi.OC().Component(controllerCard).RedundantRole().State()) == oc.Platform_ComponentRedundantRole_SECONDARY {
+					t.Logf("INFO: Ignoring controller card component %s due to deviation secondary_controller_card_cpu_utilization_unsupported.", controllerCard)
+				} else {
+					activeControllerCards = append(activeControllerCards, controllerCard)
+				}
+			}
+			controllerCards = activeControllerCards
+		}
 	}
+
 	if len(controllerCards) > 0 {
 		t.Errorf("ERROR: Didn't find cpu card for controllerCards %s", controllerCards)
 	}
@@ -402,12 +415,22 @@ func TestComponentsNoHighMemoryUtilization(t *testing.T) {
 			timestamp := time.Now().Round(time.Second)
 			componentState := gnmi.Get(t, dut, query)
 			componentType := componentState.GetType()
+			componentPath := gnmi.OC().Component(component)
+
 			if componentType == lineCardType && deviations.LinecardMemoryUtilizationUnsupported(dut) {
 				t.Skipf("INFO: Skipping test for linecard component %s due to deviation linecard_memory_utilization_unsupported", component)
 			}
-			if componentType == lineCardType && !gnmi.Get(t, dut, gnmi.OC().Component(component).Removable().State()) {
+
+			if componentType == lineCardType && !gnmi.Get(t, dut, componentPath.Removable().State()) {
 				t.Skipf("Skip the test on non-removable linecard.")
 			}
+
+			if deviations.SecondaryControllerCardMemoryUtilizationUnsupported(dut) {
+				if componentType == controllerCardType && gnmi.Get(t, dut, componentPath.RedundantRole().State()) == oc.Platform_ComponentRedundantRole_SECONDARY {
+					t.Skipf("INFO: Skipping test for secondary controller card component %s due to deviation secondary_controller_card_memory_utilization_unsupported", component)
+				}
+			}
+
 			memoryState := componentState.GetMemory()
 			if memoryState == nil {
 				t.Errorf("ERROR: %s - Device: %s - %s: %-40s - Type: %-20s - Memory data not available",
