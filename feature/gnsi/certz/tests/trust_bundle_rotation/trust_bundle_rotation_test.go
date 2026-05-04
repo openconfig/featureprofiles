9c83dd1ac7a6ddfd2b5514501960abd77f6aa17f
Chris Morrow <morrowc@google.com>
2026-05-04 04:32:26 +0000

    Fix slices import, remove it

--- feature/gnsi/certz/tests/trust_bundle_rotation/trust_bundle_rotation_test.go
+++ feature/gnsi/certz/tests/trust_bundle_rotation/trust_bundle_rotation_test.go
@@ -22,7 +22,6 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"slices"
 	"testing"
 	"time"
 
@@ -45,11 +44,11 @@ type DUTCredentialer interface {
 }
 
 var (
-	serverAddr          string
-	creds               DUTCredentialer
-	testProfile         string = "rotationprofile"
-	logTime             string = time.Now().String()
-	expectedResult      bool   = true
+	serverAddr     string
+	creds          DUTCredentialer
+	testProfile    string = "rotationprofile"
+	logTime        string = time.Now().String()
+	expectedResult bool   = true
 )
 
 func TestMain(m *testing.M) {
