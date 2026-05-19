d4a9202c4a7ed3dbd9aefb6f10be43df28202976
prathap chandran varadharajan <31721079+prathapcv@users.noreply.github.com>
2026-05-19 10:11:26 +0530

    Add JUNIPER deviation to skip TestRetrieveLogs (#5407)
    * Add JUNIPER deviation to skip TestRetrieveLogs
    
    * Add issue tracker URL to ContainerzRetrieveLogsUnsupported deviation

--- internal/deviations/deviations.go
+++ internal/deviations/deviations.go
@@ -1338,6 +1338,12 @@ func ContainerzOCUnsupported(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetContainerzOcUnsupported()
 }
 
+// ContainerzRetrieveLogsUnsupported returns true if Containerz log retrieval is unsupported.
+// https://partnerissuetracker.corp.google.com/issues/510547636
+func ContainerzRetrieveLogsUnsupported(dut *ondatra.DUTDevice) bool {
+	return lookupDUTDeviations(dut).GetContainerzRetrieveLogsUnsupported()
+}
+
 // NextHopGroupOCUnsupported returns true if devices do not support next-hop-group config
 func NextHopGroupOCUnsupported(dut *ondatra.DUTDevice) bool {
 	return lookupDUTDeviations(dut).GetNextHopGroupConfigUnsupported()
