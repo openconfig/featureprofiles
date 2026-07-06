3b14b038c93b4d27dedfa55b06e9125a8727bacd
cprabha <cprabha@juniper.net>
2026-07-06 16:29:34 -0700

    addressing review comments

--- feature/bgp/bmp/otg_tests/bmp_pre_policy_test/bmp_pre_policy_test.go
+++ feature/bgp/bmp/otg_tests/bmp_pre_policy_test/bmp_pre_policy_test.go
@@ -631,8 +631,6 @@ func TestBMPBaseSession(t *testing.T) {
 	dut := ondatra.DUT(t, "dut")
 	ate := ondatra.ATE(t, "ate")
 
-	fptest.ConfigureDefaultNetworkInstance(t, dut)
-
 	t.Log("Start DUT Configuration")
 	configureDUT(t, dut)
 	t.Log("Start ATE Configuration")
