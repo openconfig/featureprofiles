6fd3e3c8cab4ac63c80c4789d91f1b30a74579fc
Liam Walsh <liawalsh@cisco.com>
2026-08-10 14:42:13 -0400

    add peer-group AFI send-community handling to RT-7.8
    Ported-from: bf98d2562
    Traceability-ID: F23627-014

--- feature/bgp/policybase/otg_tests/comm_match_action_test/bgp_comm_match_action_test.go
+++ feature/bgp/policybase/otg_tests/comm_match_action_test/bgp_comm_match_action_test.go
@@ -223,13 +223,11 @@ func bgpCreateNbr(localAs, peerAs uint32, dut *ondatra.DUTDevice) *oc.NetworkIns
 
 		if deviations.SkipBgpSendCommunityType(dut) {
 			pg.SetSendCommunity(oc.E_Bgp_CommunityType(oc.Bgp_CommunityType_STANDARD))
+		} else if deviations.SkipBgpPeerGroupSendCommunityType(dut) || deviations.BgpPeerGroupSendCommunityRequiresAfiSafi(dut) {
+			as4.SetSendCommunityType([]oc.E_Bgp_CommunityType{oc.Bgp_CommunityType_STANDARD})
+			as6.SetSendCommunityType([]oc.E_Bgp_CommunityType{oc.Bgp_CommunityType_STANDARD})
 		} else {
-			if deviations.SkipBgpPeerGroupSendCommunityType(dut) {
-				as4.SetSendCommunityType([]oc.E_Bgp_CommunityType{oc.Bgp_CommunityType_STANDARD})
-				as6.SetSendCommunityType([]oc.E_Bgp_CommunityType{oc.Bgp_CommunityType_STANDARD})
-			} else {
-				pg.SetSendCommunityType([]oc.E_Bgp_CommunityType{oc.Bgp_CommunityType_STANDARD})
-			}
+			pg.SetSendCommunityType([]oc.E_Bgp_CommunityType{oc.Bgp_CommunityType_STANDARD})
 		}
 
 		bgpNbr := bgp.GetOrCreateNeighbor(nbr.nbrAddr)
