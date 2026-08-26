0889e34d145ed83180752ca189608299c30a1697
AmrNJ <155722765+AmrNJ@users.noreply.github.com>
2026-08-26 11:08:59 +0530

    Update mpls_gre_ipv4_decap_scale_test.go

--- feature/policy_forwarding/otg_tests/mpls_gre_ipv4_decap_scale_test/mpls_gre_ipv4_decap_scale_test.go
+++ feature/policy_forwarding/otg_tests/mpls_gre_ipv4_decap_scale_test/mpls_gre_ipv4_decap_scale_test.go
@@ -171,8 +171,8 @@ var (
 	decapValidationIPv4 = &packetvalidationhelpers.PacketValidation{
 		PortName:    "port1",
 		CaptureName: "ipv4_decap",
+		Validations: validationsIPv4,
 		IPv4Layer:   &packetvalidationhelpers.IPv4Layer{DstIP: innerDstIPv4, Tos: 10, TTL: 64, Protocol: packetvalidationhelpers.TCP},
-		IPv4Layer:   &packetvalidationhelpers.IPv4Layer{DstIP: innerDstIPv4, Tos: 10, TTL: 64, SkipProtocolCheck: true},
 	}
 	decapValidationIPv6 = &packetvalidationhelpers.PacketValidation{
 		PortName:    "port2",
