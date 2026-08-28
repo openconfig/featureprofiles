465dc5e43324678379a28ade509a69c45d35aae3
priyacj <143042911+priyacj@users.noreply.github.com>
2026-08-28 11:48:25 -0700

    Fix package name in trustbundlerotation_test.go

--- feature/gnsi/certz/tests/trust_bundle_rotation/trustbundlerotation_test.go
+++ feature/gnsi/certz/tests/trust_bundle_rotation/trustbundlerotation_test.go
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package trustbundlerotatation_test
+//package trustbundlerotatation_test
+package trust_bundle_rotation_test
 
 import (
 	"context"
