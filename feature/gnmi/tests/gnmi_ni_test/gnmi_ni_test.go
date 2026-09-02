63bd8ff2905d944c16f03dbd3ad6ac9ce3f15f96
ampattan <amit.pattanaik@nokia.com>
2026-09-02 14:28:36 +0530

    review comment addressed
    "This code is a Contribution to the OpenConfig Feature Profiles project ("Work") made under the Google Software Grant and Corporate Contributor License Agreement ("CLA") and governed by the Apache License 2.0. No other rights or licenses in or to any of Nokia's intellectual property are granted for any other purpose. This code is provided on an "as is" basis without any warranties of any kind."

--- feature/gnmi/tests/gnmi_ni_test/gnmi_ni_test.go
+++ feature/gnmi/tests/gnmi_ni_test/gnmi_ni_test.go
@@ -80,7 +80,7 @@ func ConfigureDUT(batch *gnmi.SetBatch, t *testing.T, dut *ondatra.DUTDevice) {
 	// Configure default network instance.
 	cfgplugins.NewNetworkInstance(t, dut, batch, &dutPort1NetworkInstanceIParams)
 	// Configure gNMI server on default network instance.
-	cfgplugins.CreateGNMIServer(t, dut, batch, &dutPort1NetworkInstanceIParams, gNMIPort, grpcTransportSecurity(dut))
+	cfgplugins.CreateGNMIServer(t, dut, batch, &dutPort1NetworkInstanceIParams, gNMIPort, deviations.RequireTransportSecurity(dut))
 
 }
 
@@ -97,14 +97,7 @@ func ConfigureAdditionalNetworkInstance(batch *gnmi.SetBatch, t *testing.T, dut 
 	// Configure non-default network instance.
 	cfgplugins.NewNetworkInstance(t, dut, batch, &dutPort2NetworkInstanceIParams)
 	// Configure non-default gNMI server.
-	cfgplugins.CreateGNMIServer(t, dut, batch, &dutPort2NetworkInstanceIParams, gNMIPort, grpcTransportSecurity(dut))
-}
-
-func grpcTransportSecurity(dut *ondatra.DUTDevice) bool {
-	if deviations.RequireTransportSecurity(dut) {
-		return false
-	}
-	return transportSecurity
+	cfgplugins.CreateGNMIServer(t, dut, batch, &dutPort2NetworkInstanceIParams, gNMIPort, deviations.RequireTransportSecurity(dut))
 }
 
 func ValidateNetworkInstance(t *testing.T, dut *ondatra.DUTDevice) {
