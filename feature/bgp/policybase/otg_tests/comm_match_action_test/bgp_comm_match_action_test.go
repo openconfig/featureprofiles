9cbff12a9a058fbb0e0abfcb17a1a1aa55f92d76
Liam Walsh <liawalsh@cisco.com>
2026-09-11 09:50:53 -0400

    preserve RT-7.8 peer-group community exception
    Traceability-ID: BGP-UPREV-DEFAULT-RT-7-8

--- feature/bgp/policybase/otg_tests/comm_match_action_test/bgp_comm_match_action_test.go
+++ feature/bgp/policybase/otg_tests/comm_match_action_test/bgp_comm_match_action_test.go
@@ -223,6 +223,9 @@ func bgpCreateNbr(localAs, peerAs uint32, dut *ondatra.DUTDevice) *oc.NetworkIns
 
 		if deviations.SkipBgpSendCommunityType(dut) {
 			pg.SetSendCommunity(oc.E_Bgp_CommunityType(oc.Bgp_CommunityType_STANDARD))
+		} else if deviations.SkipBgpPeerGroupSendCommunityType(dut) {
+			as4.SetSendCommunityType([]oc.E_Bgp_CommunityType{oc.Bgp_CommunityType_STANDARD})
+			as6.SetSendCommunityType([]oc.E_Bgp_CommunityType{oc.Bgp_CommunityType_STANDARD})
 		} else {
 			as4.SetSendCommunityType([]oc.E_Bgp_CommunityType{oc.Bgp_CommunityType_STANDARD})
 			as6.SetSendCommunityType([]oc.E_Bgp_CommunityType{oc.Bgp_CommunityType_STANDARD})
