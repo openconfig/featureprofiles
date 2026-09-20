449f18a234e431d5a7d9efe072c558faf8b61b34
Krishnakumar Natarajan <kk200488@gmail.com>
2026-09-20 08:52:12 +0530

    fix(acctz): use compliant next-hop addresses

--- feature/gnsi/acctz/tests/record_payload_truncation/record_payload_truncation_test.go
+++ feature/gnsi/acctz/tests/record_payload_truncation/record_payload_truncation_test.go
@@ -53,7 +53,7 @@ func sendOversizedPayload(t *testing.T, dut *ondatra.DUTDevice) {
 		ni.SetDescription("This is a pointlessly long description in order to make the payload bigger.")
 		ni.SetType(oc.NetworkInstanceTypes_NETWORK_INSTANCE_TYPE_L3VRF)
 		staticProtocol := ni.GetOrCreateProtocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_STATIC, deviations.StaticProtocolName(dut))
-		nhAddress := fmt.Sprintf("192.%d.2.1", i)
+		nhAddress := fmt.Sprintf("100.64.%d.1", i)
 		nstatRoutes := 0
 		switch dut.Vendor() {
 		case ondatra.JUNIPER:
